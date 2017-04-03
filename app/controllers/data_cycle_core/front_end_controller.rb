832e1fa2d03496a2e614143b1ed9325c2ef37392
patrick0918 <patrick.rainer.08@gmail.com>
2017-04-03 15:45:42 +0200

    changed menu

--- app/controllers/data_cycle_core/front_end_controller.rb
+++ app/controllers/data_cycle_core/front_end_controller.rb
@@ -2,7 +2,7 @@ module DataCycleCore
   class FrontEndController < ApplicationController
     before_action :authenticate_user!   # from devise (authenticate)
     #load_and_authorize_resource         # from cancancan (authorize)
-
+    
     def index
 
     end
---
+++ config/initializers/material_icons.rb
@@ -0,0 +1,4 @@
+# Initialize material icons setup
+MaterialIcons.setup do |config|
+  config.unicode = true
+end
