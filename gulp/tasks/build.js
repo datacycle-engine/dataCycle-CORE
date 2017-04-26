33ddf5f86289ddcf20723736f08ee72fc2e352b6
Patrick Rainer <rainer@pixelpoint.at>
2017-04-26 16:42:20 +0200

    updated deployment

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -4,6 +4,7 @@ var gulpSequence = require('gulp-sequence')
 gulp.task('build', function(cb) {
   var tasks = ['clean', ['sass', 'browserify']];
   if(process.env.RAILS_ENV === 'production') tasks.push('rev');
+  if(process.env.RAILS_ENV === 'staging') tasks.push('rev');
   tasks.push(cb);
   gulpSequence.apply(this, tasks);
 });
