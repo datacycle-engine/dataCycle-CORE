3adc5c3ad24ffc08c5e9d116b63e7033b07db9ec
Martin Oehzelt <oehzelt@pixelpoint.at>
2017-05-12 10:11:16 +0200

    first CreativeWork.set_data_type

--- app/models/data_cycle_core/creative_work.rb
+++ app/models/data_cycle_core/creative_work.rb
@@ -21,26 +21,36 @@ module DataCycleCore
 
     # get data as specified in the data template
     # data hash with keys named as in schema.org
-    def get_data_schema
-      data_type = metadata["validation"]
+    def get_data_type_schema
+      data_type = metadata['validation']
       data_hash = {}
-      data_type["properties"].each do |key,value|
-        data_hash[key] = storage_cases(key,data_type["properties"][key])
+      data_type['properties'].each do |key,value|
+        data_hash[key] = storage_cases_get(key,data_type['properties'][key])
       end
       data_hash
     end
 
     # get data as specified in the data template
     # data hash with key names as specified in the template
-    def get_data
-      data_type = metadata["validation"]
-      data_hash = collect_template_data(data_type["properties"])
+    def get_data_type
+      data_type = metadata['validation']
+      data_hash = collect_template_data(data_type['properties'])
+    end
+
+    def set_data_type(data_hash)
+      template_hash = metadata['validation']
+      unless validate?(data_hash)
+        return validate(data_hash)
+      end
+      ActiveRecord::Base.transaction do
+        set_template_data(template_hash['properties'], data_hash)
+      end
     end
 
     # validates given data-hash (key names as specified in the template)
     # and returns true/false
     def validate?(data = collect_data, strict = false)
-      template_hash = metadata["validation"]
+      template_hash = metadata['validation']
       DataCycleCore::MasterData::ValidateData.new.valid?(data, template_hash, strict)
     end
 
@@ -70,20 +80,43 @@ module DataCycleCore
         pluck(:classification_alias_id)
     end
 
+    def set_relation_ids(storage_location, ids)
+      ids.each do |location_id|
+        DataCycleCore::ClassificationCreativeWork.
+          find_or_create_by(
+            creative_work_id: self.id,
+            classification_alias_id: location_id
+          )
+      end
+    end
+
     def collect_template_data(properties)
       data_hash = {}
       properties.each do |key,value|
-        key_label = properties[key]["label"]
-        if properties[key]["type"] == "object"
-          data_hash[key_label] = walk_data_tree(properties[key]["properties"], self.method(properties[key]["storage_location"]).call[key])
+        key_label = properties[key]['label']
+        if properties[key]['type'] == 'object'
+          data_hash[key_label] = walk_data_tree(properties[key]['properties'], self.method(properties[key]['storage_location']).call[key])
           next
         end
-        data_hash[key_label] = storage_cases(key, properties[key])
+        data_hash[key_label] = storage_cases_get(key, properties[key])
       end
       data_hash
     end
 
-    def storage_cases(key, properties)
+    def set_template_data(properties, data_hash)
+      puts "set_template_data"
+      properties.each do |key,value|
+        key_label = properties[key]['label']
+        if properties[key]['type'] == 'object'
+          build_hash = set_data_tree(properties[key]['properties'], data_hash[key_label])
+          self.method(properties[key]['storage_location']).call[key] = build_hash
+          next
+        end
+        storage_cases_set(key, data_hash[key_label], properties[key]) unless data_hash[key_label].blank?
+      end
+    end
+
+    def storage_cases_get(key, properties)
       case properties["storage_location"]
       when "column"
         self.method(key).call
@@ -98,14 +131,55 @@ module DataCycleCore
       end
     end
 
+    def storage_cases_set(key, value, properties)
+      puts " key ----> #{key} | value: #{value} | #{properties}"
+      case properties["storage_location"]
+      when "column"
+        self.method("#{key}=").call(value)
+      when "content"
+        if self.content.blank?
+          self.content = { key => value }
+        else
+          self.content[key] = value
+        end
+      when "metadata"
+        if self.metadata.blank?
+          self.metadata = { key => value }
+        else
+          self.metadata[key] = value
+        end
+      when "properties"
+        if self.properties.blank?
+          self.properties = { key => value }
+        else
+          self.properties[key] = value
+        end
+      when "classification_creative_works"
+        set_relation_ids(properties["storage_location"], value)
+      end
+    end
+
     def walk_data_tree(data_definitions, data)
       data_hash = {}
       data_definitions.each do |key,value|
-        key_label = data_definitions[key]["label"]
-        unless data_definitions[key]["type"] == "object"
+        key_label = data_definitions[key]['label']
+        unless data_definitions[key]['type'] == 'object'
           data_hash[key_label] = data[key]
         else
-          data_hash[key_label] = walk_data_tree(data_definitions[key]["properties"],data[key])
+          data_hash[key_label] = walk_data_tree(data_definitions[key]['properties'],data[key])
+        end
+      end
+      data_hash
+    end
+
+    def set_data_tree(data_definitions, data)
+      data_hash = {}
+      data_definitions.each do |key,value|
+        key_label = data_definitions[key]['label']
+        unless data_definitions[key]['type'] == 'object'
+          data_hash[key] = data[key_label]
+        else
+          data_hash[key] = set_data_tree(data_definitions[key]['properties'],data[key_label])
         end
       end
       data_hash
