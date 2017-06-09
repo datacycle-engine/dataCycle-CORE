ce9a34163a16c2a201f6108209f7a7afce0c8c89
Patrick Rainer <rainer@pixelpoint.at>
2017-06-09 09:02:23 +0200

    [refactored]: added recherche logik create action

--- app/controllers/data_cycle_core/creative_works_controller.rb
+++ app/controllers/data_cycle_core/creative_works_controller.rb
@@ -30,51 +30,26 @@ module DataCycleCore
 
     def create
 
-      @creativeWork = DataCycleCore::CreativeWork.new(creative_work_params)    # Not the final implementation!
+      @creativeWork = create_creative_work(params[:template])
+
+      if @creativeWork.nil?
+        render 'new'
+        return
+      end
 
       if params[:template] != "Thema"
         if params['parent'].nil? || params['parent'].blank?
           #create new thema
-          raise @creativeWork.inspect
+          thema = create_creative_work("Thema")
+          @creativeWork.isPartOf = thema.id unless thema.nil?
         else
           #set as parent
           @creativeWork.isPartOf = params['parent']
         end
-        #create Recherche
       end
-
-
-      #todo: make this generic
-      template = DataCycleCore::CreativeWork.where(template: true, headline: params[:template], description: "CreativeWork").first
-      validation = template.metadata['validation']
-
-      @creativeWork.metadata = { 'validation' => validation }
-      @creativeWork.save
-
-      datahash = {'headline' => creative_work_params[:headline], 'creator' => current_user[:id]}
-
-      #add data_pool
-      unless validation['properties']['data_pool'].nil?
-        data_pool_classification = DataCycleCore::Classification.joins(classification_aliases: [classification_trees: [:classification_tree_label]])
-            .where("classification_tree_labels.name = ?", validation['properties']['data_pool']['type_name'])
-            .where("classification_aliases.name = ?", validation['properties']['data_pool']['default_value']).first
-
-        datahash['data_pool'] = [data_pool_classification.id] unless data_pool_classification.nil?
-      end
-
-      #add data_type
-      unless validation['properties']['data_type'].nil?
-        data_type_classification = DataCycleCore::Classification.joins(classification_aliases: [classification_trees: [:classification_tree_label]])
-            .where("classification_tree_labels.name = ?", validation['properties']['data_type']['type_name'])
-            .where("classification_aliases.name = ?", validation['properties']['data_type']['default_value']).first
-
-        datahash['data_type'] = [data_type_classification.id] unless data_type_classification.nil?
-      end
-
-      @creativeWork.set_data_hash(datahash)
 
       #validate ?
-      if @creativeWork.save
+      if @creativeWork.nil? || @creativeWork.save
         flash[:success] = "Successfully added new creativeWork!"
         redirect_to @creativeWork
       else
@@ -145,6 +120,46 @@ module DataCycleCore
       def creative_work_params
         params.require(:creative_work).permit(:headline, :datahash => [:headline,:text,:description,:state => [],:topics => [],:markets => [],:tags => [], :validityPeriod => [:validFrom, :validUntil], :image => [], :video => []])
         # params.require(:creative_work).permit!
+      end
+
+      def create_creative_work(template)
+
+        creative_work = DataCycleCore::CreativeWork.new(creative_work_params)
+
+        template = DataCycleCore::CreativeWork.where(template: true, headline: template, description: "CreativeWork").first
+        validation = template.metadata['validation']
+
+        creative_work.metadata = { 'validation' => validation }
+        creative_work.save
+
+        datahash = {'headline' => creative_work_params[:headline], 'creator' => current_user[:id]}
+
+        unless validation['properties']['data_pool'].nil?
+          data_pool_classification = DataCycleCore::Classification.joins(classification_aliases: [classification_trees: [:classification_tree_label]])
+              .where("classification_tree_labels.name = ?", validation['properties']['data_pool']['type_name'])
+              .where("classification_aliases.name = ?", validation['properties']['data_pool']['default_value']).first
+
+          datahash['data_pool'] = [data_pool_classification.id] unless data_pool_classification.nil?
+        end
+
+        #add data_type
+        unless validation['properties']['data_type'].nil?
+          data_type_classification = DataCycleCore::Classification.joins(classification_aliases: [classification_trees: [:classification_tree_label]])
+              .where("classification_tree_labels.name = ?", validation['properties']['data_type']['type_name'])
+              .where("classification_aliases.name = ?", validation['properties']['data_type']['default_value']).first
+
+          datahash['data_type'] = [data_type_classification.id] unless data_type_classification.nil?
+        end
+
+        creative_work.set_data_hash(datahash)
+
+        #validate ?
+        if creative_work.save
+          return creative_work
+        else
+          return nil
+        end
+
       end
 
   end
