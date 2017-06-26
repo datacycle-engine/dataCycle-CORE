fdb461a4dcffef8453c5e1fc7f51cd8c065cb395
Manuel Mitterer <mitterer@pixelpoint.at>
2017-06-26 10:52:27 +0200

    changed function of reset button

--- gulp/assets/javascripts/modules/initializers/filter_init.js
+++ gulp/assets/javascripts/modules/initializers/filter_init.js
@@ -44,10 +44,20 @@ module.exports.initialize = function () {
     });
   }
 
+  function clearForm() {
+    $(':input').not(':button, :submit, :reset, :hidden, :checkbox, :radio').val('');
+    $(':checkbox, :radio').prop('checked', false);
+  }
+
   function setup() {
     // Reset selected Tags
-    $(document).on('click', '#reset-filter', function () {
-      $('#primary_nav_wrap .your-choice.tags label').remove();
+    $(document).on('click', '#reset-filter', function (e) {
+      e.preventDefault();
+      var form = $(this).closest('#search-form');
+      var search = form.find('#search').val();
+      clearForm();
+      form.find('#search').val(search);
+      form.submit();
     });
 
     // remove your-choice tags on click
