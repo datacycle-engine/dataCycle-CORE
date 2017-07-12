d7921ddf99aeb3458224ff41c2e19334b1b57405
Patrick Rainer <rainer@pixelpoint.at>
2017-07-12 12:57:15 +0200

    Introduced DataHashService

--- app/controllers/data_cycle_core/creative_works_controller.rb
+++ app/controllers/data_cycle_core/creative_works_controller.rb
@@ -95,8 +95,8 @@ module DataCycleCore
       @creativeWork = DataCycleCore::CreativeWork.find(params[:id])
       set_breadcrumb_for @creativeWork
       add_breadcrumb "", "Edit", creative_work_path(@creativeWork)
-
-      datahash = flatten_datahash_value(creative_work_params[:datahash])
+      #raise DataCycleCore::DataHashService.instance_methods.inspect
+      datahash = DataCycleCore::DataHashService.flatten_datahash_value(creative_work_params[:datahash])
 
       # add creator id
       datahash[:creator] = current_user[:id]
@@ -126,8 +126,8 @@ module DataCycleCore
     def validate_single_data
       @creativeWork = DataCycleCore::CreativeWork.find(params[:id])
 
-      datahash = flatten_datahash_value(creative_work_params[:datahash])
-      #datahash = creative_work_params[:datahash]
+      #datahash = flatten_datahash_value(creative_work_params[:datahash])
+      datahash = DataCycleCore::DataHashService.flatten_datahash_value(creative_work_params[:datahash])
       valid = @creativeWork.validate(datahash)
 
       render :json => valid.to_json
@@ -254,68 +254,6 @@ module DataCycleCore
         
         params.require(:creative_work).permit(:headline, :datahash => datahash)
         # params.require(:creative_work).permit!
-      end
-
-      #todo make this more fancy
-      def flatten_datahash_value(datahash)
-
-        if datahash.key?(:quotation) && !datahash[:quotation].empty?
-          datahash[:quotation] = datahash[:quotation].values
-        end
-
-        if datahash.key?(:website) && !datahash[:website].empty?
-          datahash[:website] = datahash[:website].values
-        end
-
-        if datahash.key?(:mobileApplication) && !datahash[:mobileApplication].empty?
-          datahash[:mobileApplication] = datahash[:mobileApplication].values
-        end
-
-        if datahash.key?(:timelineItem) && !datahash[:timelineItem].empty?
-          datahash[:timelineItem] = datahash[:timelineItem].values
-        end
-
-        if datahash.key?(:suggestedAnswer) && !datahash[:suggestedAnswer].empty?
-          datahash[:suggestedAnswer] = datahash[:suggestedAnswer].values
-        end
-
-        if datahash.key?(:totalTime) && !datahash[:totalTime].blank?
-          datahash[:totalTime] = datahash[:totalTime].to_i
-        end
-
-        if datahash.key?(:recipeComponent) && !datahash[:recipeComponent].empty?
-          #datahash[:recipeComponent] = datahash[:recipeComponent].values
-          temp_recipeComponent= []
-
-          datahash[:recipeComponent].values.each do |component|
-            temp = component
-            if temp.key?(:totalTime) && !temp[:totalTime].blank?
-              temp[:totalTime] = temp[:totalTime].to_i
-            end
-            temp_recipeComponent.push(temp)
-          end
-          datahash[:recipeComponent] = temp_recipeComponent
-        end
-
-        if datahash.key?(:question) && !datahash[:question].empty?
-
-          temp_question = []
-
-          datahash[:question].values.each do |question|
-            temp = question
-            if temp.key?(:suggestedAnswer) && !temp[:suggestedAnswer].empty?
-              temp[:suggestedAnswer] = temp[:suggestedAnswer].values
-            end
-            if temp.key?(:acceptedAnswer) && !temp[:acceptedAnswer].empty?
-              temp[:acceptedAnswer] = temp[:acceptedAnswer].values
-            end
-            temp_question.push(temp)
-          end
-          datahash[:question] = temp_question
-        end
-
-        return datahash
-
       end
 
       def is_number? string
---
+++ app/services/data_cycle_core/data_hash_service.rb
@@ -0,0 +1,68 @@
+module DataCycleCore
+  class DataHashService
+
+    #todo make this more fancy
+    def self.flatten_datahash_value(datahash)
+
+      if datahash.key?(:quotation) && !datahash[:quotation].empty?
+        datahash[:quotation] = datahash[:quotation].values
+      end
+
+      if datahash.key?(:website) && !datahash[:website].empty?
+        datahash[:website] = datahash[:website].values
+      end
+
+      if datahash.key?(:mobileApplication) && !datahash[:mobileApplication].empty?
+        datahash[:mobileApplication] = datahash[:mobileApplication].values
+      end
+
+      if datahash.key?(:timelineItem) && !datahash[:timelineItem].empty?
+        datahash[:timelineItem] = datahash[:timelineItem].values
+      end
+
+      if datahash.key?(:suggestedAnswer) && !datahash[:suggestedAnswer].empty?
+        datahash[:suggestedAnswer] = datahash[:suggestedAnswer].values
+      end
+
+      if datahash.key?(:totalTime) && !datahash[:totalTime].blank?
+        datahash[:totalTime] = datahash[:totalTime].to_i
+      end
+
+      if datahash.key?(:recipeComponent) && !datahash[:recipeComponent].empty?
+        #datahash[:recipeComponent] = datahash[:recipeComponent].values
+        temp_recipeComponent= []
+
+        datahash[:recipeComponent].values.each do |component|
+          temp = component
+          if temp.key?(:totalTime) && !temp[:totalTime].blank?
+            temp[:totalTime] = temp[:totalTime].to_i
+          end
+          temp_recipeComponent.push(temp)
+        end
+        datahash[:recipeComponent] = temp_recipeComponent
+      end
+
+      if datahash.key?(:question) && !datahash[:question].empty?
+
+        temp_question = []
+
+        datahash[:question].values.each do |question|
+          temp = question
+          if temp.key?(:suggestedAnswer) && !temp[:suggestedAnswer].empty?
+            temp[:suggestedAnswer] = temp[:suggestedAnswer].values
+          end
+          if temp.key?(:acceptedAnswer) && !temp[:acceptedAnswer].empty?
+            temp[:acceptedAnswer] = temp[:acceptedAnswer].values
+          end
+          temp_question.push(temp)
+        end
+        datahash[:question] = temp_question
+      end
+
+      return datahash
+
+    end
+
+  end
+
+end
