64b9d1b81d5ef47d1a0b46871baf86866790d2c2
Patrick Rainer <rainer@pixelpoint.at>
2017-07-18 15:35:01 +0200

    fixed embedded objects

--- app/helpers/data_cycle_core/creative_works_helper.rb
+++ app/helpers/data_cycle_core/creative_works_helper.rb
@@ -52,18 +52,18 @@ module DataCycleCore
 
     end
 
-    def data_cycle_hidden_field(key, value = nil, parents=[])
-      object_key = get_object_key(key, parents)
+    def data_cycle_hidden_field(key, value = nil, parent_object_keys=[])
+      object_key = get_object_key(key, parent_object_keys)
       hidden_field_tag(object_key, value)
     end
 
-    def data_cycle_field(key, prop, value = nil, options = {}, parents=[])
+    def data_cycle_field(key, prop, value = nil, options = {}, parent_object_keys=[])
       unless prop['editor'] || prop['type'] == 'object'
         # return "No properties for editor set"
         return
       end
 
-      object_key = get_object_key(key, parents)
+      object_key = get_object_key(key, parent_object_keys)
 
       if prop['type'] == 'object'
         object_key = key
@@ -76,42 +76,42 @@ module DataCycleCore
       end
 
       if respond_to?('render_'+ data_type +'_field')
-          send('render_'+ data_type +'_field', object_key, prop, normalize_value(value), options, parents)
+          send('render_'+ data_type +'_field', object_key, prop, normalize_value(value), options, parent_object_keys)
       else
          "Unknown data_type: #{prop['type']}"
       end
 
     end
 
-    def render_classificationTreeLabel_field(key, prop, value=nil, options={}, parents=[])
+    def render_classificationTreeLabel_field(key, prop, value=nil, options={}, parent_object_keys=[])
       if !prop['editor'].nil? && !prop['editor']['type'].nil?
-        render partial: "#{@@partials_path}#{prop['editor']['type']}", locals: {key: key, prop: prop, value: value, options: options, parents: parents}
+        render partial: "#{@@partials_path}#{prop['editor']['type']}", locals: {key: key, prop: prop, value: value, options: options, parent_object_keys: parent_object_keys}
       else
         # 'editor not set'
       end
     end
 
-    def render_embeddedLinkArray_field(key, prop, value=nil, options={}, parents=[])
+    def render_embeddedLinkArray_field(key, prop, value=nil, options={}, parent_object_keys=[])
       if !prop.blank? && !prop['type_name'].blank?
-        render partial: "#{@@partials_path}#{prop['type']}", locals: {key: key, prop: prop, value: value, options: options, parents: parents}
+        render partial: "#{@@partials_path}#{prop['type']}", locals: {key: key, prop: prop, value: value, options: options, parent_object_keys: parent_object_keys}
       end
     end
 
-    def render_objectBrowser_field(key, prop, value=nil, options={}, parents=[])
+    def render_objectBrowser_field(key, prop, value=nil, options={}, parent_object_keys=[])
       if !prop.blank? && !prop['editor']['type'].nil?
-        render partial: "#{@@partials_path}#{prop['editor']['type']}", locals: {key: key, prop: prop, value: value, options: options, parents: parents}
+        render partial: "#{@@partials_path}#{prop['editor']['type']}", locals: {key: key, prop: prop, value: value, options: options, parent_object_keys: parent_object_keys}
       end
     end
 
-    def render_embeddedObject_field(key, prop, value=nil, options={}, parents=[])
+    def render_embeddedObject_field(key, prop, value=nil, options={}, parent_object_keys=[])
       if !prop.blank? && !prop['editor']['type'].nil?
         internal_template = get_internal_template(prop['storage_location'], prop['name'], prop['description'])
         internal_objects = get_internal_data(prop['storage_location'], value)
-        render partial: "#{@@partials_path}#{prop['editor']['type']}", locals: {key: key, prop: prop, value: value, options: options, internal_objects: internal_objects, internal_template: internal_template, parents: parents}
+        render partial: "#{@@partials_path}#{prop['editor']['type']}", locals: {key: key, prop: prop, value: value, options: options, internal_objects: internal_objects, internal_template: internal_template, parent_object_keys: parent_object_keys}
       end
     end
 
-    def render_object_field(key, prop, value=nil, options={}, parents=[])
+    def render_object_field(key, prop, value=nil, options={}, parent_object_keys=[])
       #raise prop.inspect
       if !prop['properties'].nil?
         output = []
@@ -128,9 +128,9 @@ module DataCycleCore
 
           case prop['editor']['type']
             when 'embeddedObject'
-              render_embeddedObject_field(key, prop, value, options, parents)
+              render_embeddedObject_field(key, prop, value, options, parent_object_keys)
             when 'objectBrowser'
-              key = get_object_key(key, parents)
+              key = get_object_key(key, parent_object_keys)
               render_objectBrowser_field(key, prop, value, options)
           end
 
@@ -140,56 +140,56 @@ module DataCycleCore
 
     end
 
-    def render_string_field(key, prop, value=nil, options={}, parents=[])
+    def render_string_field(key, prop, value=nil, options={}, parent_object_keys=[])
 
       if !prop['editor'].nil? && !prop['editor']['type'].nil?
         case prop['editor']['type']
           when 'input'
-            render_input_text_field(key, prop, value, prop['label'], options, parents: parents)
+            render_input_text_field(key, prop, value, prop['label'], options, parent_object_keys)
           when 'date'
-            render_date_input_text_field(key, prop, value, prop['label'], options, parents: parents)
+            render_date_input_text_field(key, prop, value, prop['label'], options, parent_object_keys)
           when 'datetime'
-            render_datetime_input_text_field(key, prop, value, prop['label'], options, parents: parents)
+            render_datetime_input_text_field(key, prop, value, prop['label'], options, parent_object_keys)
           when 'quillEditor'
-            render_fe_editor(key, prop, value, prop['label'], options, parents: parents)
+            render_fe_editor(key, prop, value, prop['label'], options, parent_object_keys)
         end
       else
-        render_input_text_field(key, prop, value, prop['label'], options, parents: parents)
+        render_input_text_field(key, prop, value, prop['label'], options, parent_object_keys)
       end
 
     end
 
-    def render_number_field(key, prop, value=nil, options={}, parents=[])
+    def render_number_field(key, prop, value=nil, options={}, parent_object_keys=[])
 
       if !prop['editor'].nil? && !prop['editor']['type'].nil?
         case prop['editor']['type']
           when 'duration'
-            render_input_duration_field(key, prop, value, prop['label'], options, parents: parents)
+            render_input_duration_field(key, prop, value, prop['label'], options, parent_object_keys)
         end
       else
-        render_input_text_field(key, prop, value, prop['label'], options, parents: parents)
+        render_input_text_field(key, prop, value, prop['label'], options, parent_object_keys)
       end
 
     end
 
-    def render_fe_editor(key, prop, value=nil, label=nil, options={}, parents=[])
-      render partial: "#{@@partials_path}feEditor", locals: {key: key, value: value, label: label, prop: prop, options: options, parents: parents}
+    def render_fe_editor(key, prop, value=nil, label=nil, options={}, parent_object_keys=[])
+      render partial: "#{@@partials_path}feEditor", locals: {key: key, value: value, label: label, prop: prop, options: options, parent_object_keys: parent_object_keys}
     end
 
-    def render_input_text_field(key, prop, value=nil, label=nil, options={}, parents=[])
-      render partial: "#{@@partials_path}input", locals: {key: key, value: value, label: label, prop: prop, options: options, parents: parents}
+    def render_input_text_field(key, prop, value=nil, label=nil, options={}, parent_object_keys=[])
+      render partial: "#{@@partials_path}input", locals: {key: key, value: value, label: label, prop: prop, options: options, parent_object_keys: parent_object_keys}
     end
 
-    def render_date_input_text_field(key, prop, value=nil, label=nil, options={}, parents=[])
-      render partial: "#{@@partials_path}dateInput", locals: {key: key, value: value, label: label, prop: prop, options: options, parents: parents}
+    def render_date_input_text_field(key, prop, value=nil, label=nil, options={}, parent_object_keys=[])
+      render partial: "#{@@partials_path}dateInput", locals: {key: key, value: value, label: label, prop: prop, options: options, parent_object_keys: parent_object_keys}
     end
 
-    def render_datetime_input_text_field(key, prop, value=nil, label=nil, options={}, parents=[])
-      render partial: "#{@@partials_path}dateTimeInput", locals: {key: key, value: value, label: label, prop: prop, options: options, parents: parents}
+    def render_datetime_input_text_field(key, prop, value=nil, label=nil, options={}, parent_object_keys=[])
+      render partial: "#{@@partials_path}dateTimeInput", locals: {key: key, value: value, label: label, prop: prop, options: options, parent_object_keys: parent_object_keys}
     end
 
-    def render_input_duration_field(key, prop, value=nil, label=nil, options={}, parents=[])
-      render partial: "#{@@partials_path}duration", locals: {key: key, value: value, label: label, prop: prop, options: options, parents: parents}
+    def render_input_duration_field(key, prop, value=nil, label=nil, options={}, parent_object_keys=[])
+      render partial: "#{@@partials_path}duration", locals: {key: key, value: value, label: label, prop: prop, options: options, parent_object_keys: parent_object_keys}
     end
 
     # Show action
@@ -207,12 +207,12 @@ module DataCycleCore
 
     private
 
-      def get_object_key(key, parents = [])
-        parent_keys = ''
-        if !parents.empty?
-          parent_keys = parents.map{ |parent| "[#{parent}]" }.join('')
+      def get_object_key(key, parent_object_keys = [])
+        parent_object_keys_string = ''
+        if !parent_object_keys.empty?
+          parent_object_keys_string = parent_object_keys.map{ |parent| "[#{parent}]" }.join('')
         end
-        object_key = "#{@@key_prefix}#{parent_keys}[#{key}]"
+        object_key = "#{@@key_prefix}#{parent_object_keys_string}[#{key}]"
       end
 
       def get_internal_data(storage_location, value)
