a2d1028331eba3278dc7c238f851c3fc29839e1e
Manuel Mitterer <mitterer@pixelpoint.at>
2017-07-26 10:55:17 +0200

    fixes for watchlists

--- app/models/data_cycle_core/watch_list.rb
+++ app/models/data_cycle_core/watch_list.rb
@@ -1,5 +1,6 @@
 module DataCycleCore
   class WatchList < ApplicationRecord
+    validates :headline, presence: true
     # associations
     has_many :watch_list_data_hashes, dependent: :destroy
     
