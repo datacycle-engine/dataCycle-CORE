0d9eec82cc9080cbeee1b5a1633b0e61d44d940a
Manuel Mitterer <mitterer@pixelpoint.at>
2017-08-07 11:45:10 +0200

    fix for place_translations

--- db/migrate/20170806152208_add_translations_for_descriptions_to_places.rb
+++ db/migrate/20170806152208_add_translations_for_descriptions_to_places.rb
@@ -1,9 +1,11 @@
 class AddTranslationsForDescriptionsToPlaces < ActiveRecord::Migration[5.0]
   def up
-    DataCycleCore::Place.add_translation_fields! description: :text    
+    DataCycleCore::Place.add_translation_fields! description: :text   
+    remove_column :places, :description
   end
 
   def down
     remove_column :place_translations, :description
+    add_column :places, :description, :text
   end
 end
