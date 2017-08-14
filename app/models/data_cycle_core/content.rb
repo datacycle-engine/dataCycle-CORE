3403450ffc7e70421fcc4189b50af9ea027a03fa
Johannes Zlattinger <johannes@zlattinger.net>
2017-08-14 12:57:30 +0200

    minor changes

--- app/models/data_cycle_core/content.rb
+++ app/models/data_cycle_core/content.rb
@@ -1,5 +1,9 @@
 module DataCycleCore
   class Content < DataHash
+    NESTED_STORAGE_LOCATIONS = ['metadata', 'content', 'properties']
+
+    PLAIN_PROPERTY_TYPES = ['string', 'text', 'number', 'geographic']
+
     self.abstract_class = true
 
     def property_definitions
@@ -7,20 +11,39 @@ module DataCycleCore
     end
 
     def method_missing(name, *args, &block)
-      property_definition = property_definitions[name.to_s.gsub(/=$/, '')]
+      property_definition = property_definitions.try(:[], name.to_s.gsub(/=$/, ''))
 
-      if name.to_s.ends_with?('=') && property_definition && ['metadata', 'content', 'properties'].include?(property_definition['storage_location'])
+      if property_definition && NESTED_STORAGE_LOCATIONS.include?(property_definition['storage_location']) && name.to_s.ends_with?('=')
         raise ArgumentError.new("wrong number of arguments (given #{args.size}, expected 0)") unless args.size == 1
 
-        send(property_definition['storage_location'] + '=', 
-          (send(property_definition['storage_location']) || {}).merge({name.to_s.gsub(/=$/, '') => args.first})
-        )        
-      elsif property_definition && ['metadata', 'content', 'properties'].include?(property_definition['storage_location'])
+        set_property_value(name.to_s.gsub(/=$/, ''), property_definition, args.first)
+      elsif property_definition && NESTED_STORAGE_LOCATIONS.include?(property_definition['storage_location'])
         raise ArgumentError.new("wrong number of arguments (given #{args.size}, expected 0)") unless args.blank?
 
-        send(property_definition['storage_location'])[name.to_s]
+        get_property_value(name.to_s.gsub(/=$/, ''), property_definition)
       else
         super
+      end
+    end
+
+
+    private
+
+    def get_property_value(property_name, property_definition)
+      if PLAIN_PROPERTY_TYPES.include?(property_definition['storage_type'])
+        send(property_definition['storage_location'])[property_name.to_s]      
+      else
+        raise NotImplementedError
+      end
+    end
+
+    def set_property_value(property_name, property_definition, value)
+      if PLAIN_PROPERTY_TYPES.include?(property_definition['storage_type'])
+        send(property_definition['storage_location'] + '=', 
+            (send(property_definition['storage_location']) || {}).merge({property_name => value})  
+          )
+      else
+        raise NotImplementedError
       end
     end
   end
