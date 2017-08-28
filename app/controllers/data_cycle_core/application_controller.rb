a347a2fe9c44a76d420cfcd5f98d3a132c857abe
Johannes Zlattinger <johannes@zlattinger.net>
2017-08-28 21:04:06 +0200

    minor changes to be able override abilities on a project basis

--- app/controllers/data_cycle_core/application_controller.rb
+++ app/controllers/data_cycle_core/application_controller.rb
@@ -3,7 +3,7 @@ module DataCycleCore
     protect_from_forgery with: :exception
     
     def current_ability
-      @current_ability ||= Ability.new(current_user, session)
+      @current_ability ||= ::Ability.new(current_user, session)
     end
 
     rescue_from CanCan::AccessDenied do |exception|
