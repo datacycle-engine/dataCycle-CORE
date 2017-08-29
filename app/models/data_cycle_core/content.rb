fe294a302a035137cab61592d7d58a09f35e376a
Martin Oehzelt <oehzelt@pixelpoint.at>
2017-08-29 14:35:26 +0200

    handle included objects with OpenStruct

--- app/models/data_cycle_core/content.rb
+++ app/models/data_cycle_core/content.rb
@@ -14,7 +14,7 @@ module DataCycleCore
       property_definition = property_definitions.try(:[], name.to_s.gsub(/=$/, ''))
 
       if property_definition && name.to_s.ends_with?('=')
-        raise ArgumentError.new("wrong number of arguments (given #{args.size}, expected 0)") unless args.size == 1
+        raise ArgumentError.new("wrong number of arguments (given #{args.size}, expected 1)") unless args.size == 1
 
         set_property_value(name.to_s.gsub(/=$/, ''), property_definition, args.first)
       elsif property_definition
@@ -26,6 +26,11 @@ module DataCycleCore
       end
     end
 
+    def respond_to?(method_name, include_private = false)
+      property_names.map{|item| [item.to_sym, (item.to_s+"=").to_sym]}.flatten.include?(method_name.to_sym) || super
+    end
+
+
     def property_names
       property_definitions.keys
     end
@@ -33,8 +38,8 @@ module DataCycleCore
     def translatable_property_names
       translated_columns = (self.class.to_s + "::Translation").constantize.column_names
 
-      property_definitions.select { |property_name, definition| 
-          ['content', 'properties'].include?(definition['storage_location']) || 
+      property_definitions.select { |property_name, definition|
+          ['content', 'properties'].include?(definition['storage_location']) ||
           (definition['storage_location'] == 'column' && translated_columns.include?(property_name))
         }.keys
     end
@@ -42,26 +47,26 @@ module DataCycleCore
     def untranslatable_property_names
       untranslated_columns = self.class.column_names
 
-      property_definitions.select { |property_name, definition| 
-          ['key', 'metadata'].include?(definition['storage_location']) || 
+      property_definitions.select { |property_name, definition|
+          ['key', 'metadata'].include?(definition['storage_location']) ||
           (definition['storage_location'] == 'column' && untranslated_columns.include?(property_name))
         }.keys
     end
 
     def plain_property_names
-      property_definitions.select { |property_name, definition| 
+      property_definitions.select { |property_name, definition|
         PLAIN_PROPERTY_TYPES.include?(definition['type'])
       }.keys
     end
 
     def linked_property_names
-      property_definitions.select { |property_name, definition| 
+      property_definitions.select { |property_name, definition|
         definition['type'] == 'embeddedLink' || definition['type'] == 'embeddedLinkArray'
       }.keys
     end
 
     def embedded_property_names
-      property_definitions.select { |property_name, definition| 
+      property_definitions.select { |property_name, definition|
         definition['type'] == 'object'
       }.keys
     end
@@ -80,20 +85,30 @@ module DataCycleCore
     private
 
     def get_property_value(property_name, property_definition)
+      # linked data via embeddedLink/embeddedLinkArray
       if linked_property_names.include?(property_name)
         load_linked_data(
-            "DataCycleCore::#{property_definition['type_name'].singularize.camelize}", 
+            "DataCycleCore::#{property_definition['type_name'].singularize.camelize}",
             send(property_definition['storage_location'])[property_name.to_s]
           )
+      # included subobjects
+      elsif embedded_property_names.include?(property_name) &&  NESTED_STORAGE_LOCATIONS.include?(property_definition['storage_location'])
+        load_included_data(
+          property_name,
+          property_definition
+        )
+
       elsif embedded_property_names.include?(property_name) && property_definition['storage_location'] != self.class.table_name
         send(property_definition['storage_location'])
+
       elsif embedded_property_names.include?(property_name) && property_definition['storage_location'] == self.class.table_name
         load_linked_data(
-            self.class.to_s, 
+            self.class.to_s,
             send('metadata')[property_name.to_s + '_hasPart']
           )
+      # plain properties (e.g. string,text, ... )
       elsif PLAIN_PROPERTY_TYPES.include?(property_definition['storage_type'])
-        send(property_definition['storage_location'])[property_name.to_s]      
+        send(property_definition['storage_location'])[property_name.to_s]
       else
         raise NotImplementedError
       end
@@ -103,10 +118,30 @@ module DataCycleCore
       class_name.safe_constantize.find(ids)
     end
 
+    def load_included_data(property_name, property_definition)
+      sub_property_definitions = property_definition.try(:[], 'properties')
+      raise StandardError.new("Template for included data #{property_name} has no Subproperties defined.") if sub_property_definitions.blank?
+      OpenStruct.new(get_subproperty_hash(sub_property_definitions, property_definition['storage_location'], send(property_definition['storage_location']).try(:[], property_name))).freeze
+    end
+
+    def get_subproperty_hash(sub_properties, storage_location, sub_properties_data)
+      sub_properties.map{ |key, item|
+        if item['type'] == 'object'
+          {key => OpenStruct.new(get_subproperty_hash(item['properties'], storage_location, sub_properties_data[key.to_s])).freeze}
+        elsif item['storage_location'] == storage_location
+          {key => sub_properties_data[key.to_s]}
+        elsif item['storage_location'] == 'column'
+          {key => send(key)}
+        else
+          raise StandardError.new()
+        end
+      }.inject(&:merge!)
+    end
+
     def set_property_value(property_name, property_definition, value)
       if PLAIN_PROPERTY_TYPES.include?(property_definition['storage_type'])
-        send(property_definition['storage_location'] + '=', 
-            (send(property_definition['storage_location']) || {}).merge({property_name => value})  
+        send(property_definition['storage_location'] + '=',
+            (send(property_definition['storage_location']) || {}).merge({property_name => value})
           )
       else
         raise NotImplementedError
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -16,7 +16,7 @@ RSpec.shared_examples "for properties" do |storage_location, data_provider|
               type: 'string',
               storage_type: 'string',
               storage_location: storage_location
-            }        
+            }
           }
         }
       }
@@ -32,24 +32,24 @@ RSpec.shared_examples "for properties" do |storage_location, data_provider|
     }
 
     it "provides names of plain properties" do
-      expect(subject.plain_property_names).to eq(['property', 'existing_property'])      
+      expect(subject.plain_property_names).to eq(['property', 'existing_property'])
     end
 
     it "provides existing data" do
       expect(subject.existing_property).to eq(property_value)
-    end    
+    end
 
     it "updates existing data" do
       different_property_value = data_provider.call
       subject.existing_property = different_property_value
 
-      expect(subject.existing_property).to eq(different_property_value)      
+      expect(subject.existing_property).to eq(different_property_value)
     end
 
     it "creates data for new property" do
       subject.property = "some data"
 
-      expect(subject.property).to eq('some data')      
+      expect(subject.property).to eq('some data')
     end
   end
 end
@@ -74,13 +74,13 @@ RSpec.shared_examples "for properties with no content yet" do |storage_location|
     }
 
     it "provides names of plain properties" do
-      expect(subject.plain_property_names).to eq(['property'])      
+      expect(subject.plain_property_names).to eq(['property'])
     end
 
     it "creates data for new property" do
       subject.property = "some data"
 
-      expect(subject.property).to eq('some data')      
+      expect(subject.property).to eq('some data')
     end
   end
 end
@@ -104,13 +104,13 @@ RSpec.describe DataCycleCore::Content, type: :model do
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'key'
-              },              
+              },
               headline: {
                 label: 'headline',
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'column'
-              },              
+              },
               '1' => {
                 label: '1',
                 type: 'string',
@@ -122,28 +122,57 @@ RSpec.describe DataCycleCore::Content, type: :model do
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'metadata'
-              },              
+              },
               '3' => {
                 label: '3',
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'content'
-              },              
+              },
               '4' => {
                 label: '4',
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'properties'
-              }              
+              }
             }
           }
         })
     }
 
     it "provides names of plain properties" do
-      expect(subject.plain_property_names).to eq(['id', 'headline', '1', '2', '3', '4'])      
+      expect(subject.plain_property_names).to eq(['id', 'headline', '1', '2', '3', '4'])
     end
 
+    it "provides methods for all property names as string" do
+      ['id', 'headline', '1', '2', '3', '4'].each do |item|
+        expect(subject).to respond_to item
+        expect(subject).to respond_to "#{item}="
+      end
+    end
+
+    it "provides methods for all property names as symbol" do
+      ['id', 'headline', '1', '2', '3', '4'].each do |item|
+        expect(subject).to respond_to item.to_sym
+        expect(subject).to respond_to "#{item}=".to_sym
+      end
+    end
+
+    it "fails to provide methods for properties that are not specified in the subject" do
+      ['abcd', 'jklm'].each do |item|
+        expect(subject.respond_to? item).to be false
+        expect(subject.respond_to? item.to_sym).to be false
+        expect(subject.respond_to? "#{item}=").to be false
+        expect(subject.respond_to? "#{item}=".to_sym).to be false
+      end
+    end
+
+    it "raises NameError when not specified methods are called" do
+      ['abcd', 'jklm'].each do |item|
+        expect{ subject.method(item).call() }.to raise_error(NameError)
+        expect{ subject.method("#{item}=").call("test") }.to raise_error(NameError)
+      end
+    end
 
     it "provides list of untranslatable properties" do
       expect(subject.untranslatable_property_names).to eq(['id', '1', '2'])
@@ -164,7 +193,7 @@ RSpec.describe DataCycleCore::Content, type: :model do
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'key'
-              },              
+              },
               existing_locations: {
                 label: 'Location',
                 type: 'embeddedLinkArray',
@@ -204,7 +233,7 @@ RSpec.describe DataCycleCore::Content, type: :model do
         .and_return(double('DataCycleCore::Place'))
 
       expect(subject.existing_main_location).not_to be_nil
-    end    
+    end
   end
 
   describe "with embedded properties" do
@@ -217,7 +246,7 @@ RSpec.describe DataCycleCore::Content, type: :model do
                 type: 'string',
                 storage_type: 'string',
                 storage_location: 'key'
-              },              
+              },
               existing_locations: {
                 label: 'Location',
                 type: 'object',
@@ -252,5 +281,179 @@ RSpec.describe DataCycleCore::Content, type: :model do
 
       expect(subject.nested_creative_works.size).to eq(3)
     end
-  end  
+  end
+
+  describe "with included properties" do
+    subject {
+      DataCycleCore::CreativeWork.new(metadata: {
+          validation: {
+            properties: {
+              id: {
+                label: 'id',
+                type: 'string',
+                storage_type: 'string',
+                storage_location: 'key'
+              },
+              description: {
+                label: 'description',
+                type: 'string',
+                storage_type: 'string',
+                storage_location: 'column'
+              },
+              included_object: {
+                label: 'Nested Data',
+                type: 'object',
+                storage_location: 'metadata',
+                properties: {
+                  property1: {
+                    label: 'property_name a',
+                    type: 'string',
+                    storage_type: 'string',
+                    storage_location: 'metadata'
+                  },
+                  property2: {
+                    label: 'property_name b',
+                    type: 'string',
+                    storage_type: 'string',
+                    storage_location: 'metadata'
+                  }
+                }
+              }
+            }
+          },
+          "included_object" => {
+            "property1" => "data property1",
+            "property2" => "data property2"
+          }
+        },
+        description: "dies ist ein Test"
+      )
+    }
+
+    it "provides names of included property" do
+      expect(subject.embedded_property_names).to eq(['included_object'])
+    end
+
+    it "provides plain_property_names" do
+      expect(subject.plain_property_names).to eq(['id', 'description'])
+    end
+
+    it "provides translatable_property_names" do
+      expect(subject.translatable_property_names).to eq(['description'])
+    end
+
+    it "returns value for translatable_property" do
+      expect(subject.description).to eq("dies ist ein Test")
+    end
+
+    it "returns an hash for included property" do
+      expect(subject.included_object.to_h.deep_stringify_keys).to eq({"property1" => "data property1", "property2" => "data property2"})
+    end
+
+    it "returns values for included sub_properties" do
+      expect(subject.included_object.property1).to eq("data property1")
+      expect(subject.included_object.property2).to eq("data property2")
+    end
+
+  end
+
+  describe "with included properties" do
+    subject {
+      DataCycleCore::CreativeWork.new(metadata: {
+          validation: {
+            properties: {
+              id: {
+                label: 'id',
+                type: 'string',
+                storage_type: 'string',
+                storage_location: 'key'
+              },
+              description: {
+                label: 'description',
+                type: 'string',
+                storage_type: 'string',
+                storage_location: 'column'
+              },
+              included_object: {
+                label: 'Nested Data',
+                type: 'object',
+                storage_location: 'metadata',
+                properties: {
+                  property1: {
+                    label: 'property_name a',
+                    type: 'string',
+                    storage_type: 'string',
+                    storage_location: 'metadata'
+                  },
+                  property2: {
+                    label: 'property_name b',
+                    type: 'string',
+                    storage_type: 'string',
+                    storage_location: 'metadata'
+                  },
+                  deep_included_object: {
+                    label: 'Nested Data',
+                    type: 'object',
+                    storage_location: 'metadata',
+                    properties: {
+                      property_deep1: {
+                        label: 'deep_property_name a',
+                        type: 'string',
+                        storage_type: 'string',
+                        storage_location: 'metadata'
+                      },
+                      property_deep2: {
+                        label: 'deep_property_name b',
+                        type: 'string',
+                        storage_type: 'string',
+                        storage_location: 'metadata'
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          "included_object" => {
+            "property1" => "data property1",
+            "property2" => "data property2",
+            "deep_included_object" => {
+              "property_deep1" => "data property_deep1",
+              "property_deep2" => "data property_deep2"
+            }
+          }
+        },
+        description: "dies ist ein Test"
+      )
+    }
+
+    it "returns values for included sub_properties" do
+      expect(subject.included_object.property1).to eq("data property1")
+      expect(subject.included_object.property2).to eq("data property2")
+    end
+
+    it "returns deep_included_object" do
+      expect(subject.included_object.deep_included_object.to_h.deep_stringify_keys).to eq({"property_deep1" => "data property_deep1", "property_deep2" => "data property_deep2"})
+    end
+
+    it "returns attributes for deep_included_object" do
+      expect(subject.included_object.deep_included_object.property_deep1).to eq("data property_deep1")
+      expect(subject.included_object.deep_included_object.property_deep2).to eq("data property_deep2")
+    end
+
+    it "returns an hash for included property" do
+      expect(subject.included_object.to_h.deep_stringify_keys).to eq({
+        "property1" => "data property1",
+        "property2" => "data property2",
+        "deep_included_object" => {
+          "property_deep1" => "data property_deep1",
+          "property_deep2" => "data property_deep2"
+        }
+      })
+    end
+
+  end
+
+
+
 end
