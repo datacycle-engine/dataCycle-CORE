355e430d597c6f924c9a4ee85b525a683b8d3594
Johannes Zlattinger <johannes@zlattinger.net>
2017-09-11 18:58:19 +0200

    fixed new classifications api (due to a problem with deleted ancestors)

--- app/models/data_cycle_core/classification_alias.rb
+++ app/models/data_cycle_core/classification_alias.rb
@@ -8,8 +8,10 @@ module DataCycleCore
     acts_as_paranoid
 
     has_one :classification_tree, dependent: :destroy
-    # has_one :classification_tree, -> (classification_alias) { classification_alias.deleted? ? with_deleted : self }, dependent: :destroy
     has_one :parent_classification_alias, through: :classification_tree
+
+    has_one :classification_tree_with_deleted, -> { with_deleted }, class_name: 'ClassificationTree', foreign_key: 'classification_alias_id'
+    has_one :parent_classification_alias_with_deleted, through: :classification_tree_with_deleted, source: :parent_classification_alias
 
     has_many :sub_classification_trees, class_name: 'ClassificationTree', foreign_key: 'parent_classification_alias_id', dependent: :destroy
     has_many :sub_classification_alias, through: :sub_classification_trees
@@ -19,10 +21,10 @@ module DataCycleCore
 
     def ancestors
       Rails.cache.fetch("#{cache_key}/ancestors", expires_in: 10.minutes) do
-        if parent_classification_alias
-          [parent_classification_alias] + parent_classification_alias.ancestors
+        if parent_classification_alias_with_deleted
+          [parent_classification_alias_with_deleted] + parent_classification_alias_with_deleted.ancestors
         else
-          [classification_tree.classification_tree_label]
+          [classification_tree_with_deleted.classification_tree_label_with_deleted]
         end
       end
     end
