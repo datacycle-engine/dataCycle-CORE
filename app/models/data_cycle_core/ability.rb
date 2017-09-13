e7abfd42d1d94d76a3a88a6c2f816d7d42a06901
Johannes Zlattinger <johannes@zlattinger.net>
2017-09-13 08:54:48 +0200

    fixed a problem when unsing object browser with objects stored as embedded link array

--- app/models/data_cycle_core/ability.rb
+++ app/models/data_cycle_core/ability.rb
@@ -3,7 +3,6 @@ module DataCycleCore
     include CanCan::Ability
 
     def initialize(user, session = {})
-
       if user
         can :read, :all
         cannot :read, DataCycleCore::WatchList
@@ -21,9 +20,8 @@ module DataCycleCore
 
         if user.role == "admin" || user.admin?
           can :manage, :dash_board
-
         elsif user.role == "user"
-
+          ###
         elsif user.role == "guest"
           DataCycleCore::DataLink.session_edit_links(session[:can_edit_ids]).each do |link|
             can [:update, :validate_single_data], link.item_type.constantize, {id: link.item_id}
