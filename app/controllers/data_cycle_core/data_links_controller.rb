986e0b54b80c62037ebd34b29f27a8917d60373c
Manuel Mitterer <mitterer@pixelpoint.at>
2017-09-25 16:22:43 +0200

    added comment to data_links

--- app/controllers/data_cycle_core/data_links_controller.rb
+++ app/controllers/data_cycle_core/data_links_controller.rb
@@ -34,7 +34,7 @@ module DataCycleCore
       receiver = send_link_params[:receiver]
 
       if receiver =~ Devise.email_regexp
-        DataLinkMailer.mail_link(@data_link.creator, receiver, url_for(@data_link), params[:type]).deliver_later
+        DataLinkMailer.mail_link(@data_link.creator, receiver, url_for(@data_link), params[:type], send_link_params[:comment]).deliver_later
         redirect_back(fallback_location: root_path, notice: (I18n.t :sent, scope: [:controllers, :success]))
       else
         redirect_back(fallback_location: root_path, alert: (I18n.t :invalid_mail, scope: [:controllers, :success]))
@@ -54,7 +54,7 @@ module DataCycleCore
     end
 
     def send_link_params
-      params.require(:data_link).permit(:receiver)
+      params.require(:data_link).permit(:receiver, :comment, :type)
     end
 
     def guest_user
--- app/mailers/data_cycle_core/data_link_mailer.rb
+++ app/mailers/data_cycle_core/data_link_mailer.rb
@@ -1,9 +1,10 @@
 module DataCycleCore
   class DataLinkMailer < ApplicationMailer
-    def mail_link(user, receiver, url, action_text)
+    def mail_link(user, receiver, url, action_text, comment)
       @user = user
-      @url  = url
-      @action  = action_text
+      @url = url
+      @action = action_text
+      @comment = comment
       mail(to: receiver, cc: user.email, subject: 'Generierter Link zu einem Inhalt')
     end
   end
