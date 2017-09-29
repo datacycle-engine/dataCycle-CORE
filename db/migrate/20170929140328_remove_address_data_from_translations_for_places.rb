beaa0b9b0c4922edbae15451e462f2bdbb651e5c
Johannes Zlattinger <johannes@zlattinger.net>
2017-09-29 19:49:48 +0200

    updated content types to apply ruby naming conventions to place histories

--- db/migrate/20170929140328_remove_address_data_from_translations_for_places.rb
+++ db/migrate/20170929140328_remove_address_data_from_translations_for_places.rb
@@ -17,5 +17,23 @@ class RemoveAddressDataFromTranslationsForPlaces < ActiveRecord::Migration[5.0]
     add_column :places, :fax_number, :string
     add_column :places, :telephone, :string
     add_column :places, :email, :string
+
+    remove_column :place_history_translations, :addressLocality, :string
+    remove_column :place_history_translations, :streetAddress, :string
+    remove_column :place_history_translations, :postalCode, :string
+    remove_column :place_history_translations, :addressCountry, :string
+    remove_column :place_history_translations, :faxNumber, :string
+    remove_column :place_history_translations, :telephone, :string
+    remove_column :place_history_translations, :email, :string
+
+    rename_column :place_history_translations, :hoursAvailable, :hours_available
+
+    add_column :place_histories, :address_locality, :string
+    add_column :place_histories, :street_address, :string
+    add_column :place_histories, :postal_code, :string
+    add_column :place_histories, :address_country, :string
+    add_column :place_histories, :fax_number, :string
+    add_column :place_histories, :telephone, :string
+    add_column :place_histories, :email, :string
   end
 end
