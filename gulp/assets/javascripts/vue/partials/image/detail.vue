bf5fabacc54659ba375976e9c55ca93f4465c979
Patrick Rainer <rainer@pixelpoint.at>
2017-10-04 11:28:07 +0200

    refactor rails convention: Bild

--- gulp/assets/javascripts/vue/partials/image/detail.vue
+++ gulp/assets/javascripts/vue/partials/image/detail.vue
@@ -6,20 +6,20 @@
       <dd>{{ item.headline }}</dd>
     </dl>
     <dl v-if="item.metadata != undefined">
-      <dt class="block-flex" v-if="item.metadata.thumbnailUrl != undefined"></dt>
-      <dd class="block-flex" v-if="item.metadata.thumbnailUrl != undefined">
-        <img :src=" item.metadata.thumbnailUrl ">
+      <dt class="block-flex" v-if="item.metadata.thumbnail_url != undefined"></dt>
+      <dd class="block-flex" v-if="item.metadata.thumbnail_url != undefined">
+        <img :src=" item.metadata.thumbnail_url ">
       </dd>
       <dt class="block-flex" v-if="item.metadata.url != undefined">Datei Url: </dt>
       <dd class="block-flex" v-if="item.metadata.url != undefined">
         <a :href="item.metadata.url " target="_blank ">{{ item.metadata.url }}</a>
       </dd>
-      <dt v-if="item.metadata.fileFormat != undefined">Format: </dt>
-      <dd v-if="item.metadata.fileFormat != undefined">{{ item.metadata.fileFormat }}</dd>
+      <dt v-if="item.metadata.file_format != undefined">Format: </dt>
+      <dd v-if="item.metadata.file_format != undefined">{{ item.metadata.file_format }}</dd>
       <dt v-if="item.metadata.width != undefined && item.metadata.height != undefined">Abmessungen: </dt>
       <dd v-if="item.metadata.width != undefined && item.metadata.height != undefined">{{ item.metadata.width }} x {{ item.metadata.height }}</dd>
-      <dt v-if="item.metadata.contentSize != undefined">Dateigröße: </dt>
-      <dd v-if="item.metadata.contentSize != undefined">{{ formatSize(item.metadata.contentSize) }}</dd>
+      <dt v-if="item.metadata.content_size != undefined">Dateigröße: </dt>
+      <dd v-if="item.metadata.content_size != undefined">{{ formatSize(item.metadata.content_size) }}</dd>
       <dt class="block-flex" v-if="item.metadata.license != undefined">Lizenz: </dt>
       <dd class="block-flex" v-if="item.metadata.license != undefined">{{ item.metadata.license }}</dd>
       <dt class="block-flex" v-if="item.metadata.restrictions != undefined && item.metadata.restrictions != '' && item.metadata.restrictions != 'Keine Einschränkung;'">Einschränkungen: </dt>
