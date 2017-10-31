509404a3932fa6b059f2931bdb9d42b24597cecd
Manuel Mitterer <mitterer@pixelpoint.at>
2017-10-31 16:00:44 +0100

    changed webhook logger to rails logger

--- app/models/data_cycle_core/webhook/base.rb
+++ app/models/data_cycle_core/webhook/base.rb
@@ -3,8 +3,8 @@ module DataCycleCore
     class Base
 
       def self.log(webhook, message)
-        logger = Logger.new('webhook')
-        logger.info("#{webhook}: #{message}")
+        # logger = Logger.new('webhook')
+        Rails.logger.info("#{webhook}: #{message}")
       end
 
       def self.get_webhooks_for(action)
