9bf43dfe1fa66a59fab4d6255f3b1a93362c20e1
Manuel Mitterer <mitterer@pixelpoint.at>
2017-11-08 14:08:44 +0100

    changes to abilities

--- app/models/data_cycle_core/ability.rb
+++ app/models/data_cycle_core/ability.rb
@@ -14,8 +14,9 @@ module DataCycleCore
 
         if user.has_rank?(0)
           DataCycleCore::DataLink.session_edit_links(session[:can_edit_ids]).each do |link|
-            can [:update, :validate_single_data], link.item_type.constantize, {id: link.item_id}
+            can [:update, :validate_single_data, :import], link.item_type.constantize, {id: link.item_id}
           end
+          can :create_in_objectbrowser, [DataCycleCore::Person, DataCycleCore::CreativeWork, DataCycleCore::Place]
         end
 
         if user.has_rank?(1)
