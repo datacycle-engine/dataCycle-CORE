5a5fb2c0dea579ec967e89523a5bc17ad26f5dd9
Manuel Mitterer <mitterer@pixelpoint.at>
2017-11-24 10:44:57 +0100

    small fixes + includes

--- gulp/assets/javascripts/modules/initializers/datalist_init.js
+++ gulp/assets/javascripts/modules/initializers/datalist_init.js
@@ -4,11 +4,6 @@ module.exports.initialize = function () {
   $('.ajax-datalist').each(function () {
     var list = $(this).attr('list');
     $("#" + list).html('');
-    $.get('/' + list + '/search', function (data) {
-      for (var i = 0; i < data.length; i++) {
-        $("#" + list).append('<option data-familyname="' + data[i].family_name + '" data-givenname="' + data[i].given_name + '" value="' + data[i].email + '">');
-      }
-    });
 
     $(this).on('input', function (ev) {
       $.get('/' + list + '/search', {
