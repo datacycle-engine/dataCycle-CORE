2ae1f705aea7ed9e5bb4d8f7bd026cc2c7300fb5
Patrick Rainer <rainer@pixelpoint.at>
2017-11-24 15:26:26 +0100

    [WIP] contents_api

--- app/controllers/data_cycle_core/api/v1/contents_controller.rb
+++ app/controllers/data_cycle_core/api/v1/contents_controller.rb
@@ -1,5 +1,8 @@
 module DataCycleCore
   class Api::V1::ContentsController < Api::V1::ApiBaseController
+
+    @@default_per = 50
+
     def show
       @content = Object.const_get("DataCycleCore::#{params[:type].classify}")
         .includes({classifications: [], translations: []})
@@ -27,6 +30,54 @@ module DataCycleCore
       # @content.destroy
       # render json: {"success" => @content.destroyed?}
 
+    end
+
+    def search
+      @language = params[:language] unless params[:language].blank?
+      @language ||= 'de'
+
+      search_string = (params[:search] || '').split(' ').join('%')
+      order_string = "boost * (
+      8 * similarity(classification_string,'%#{search_string}%') +
+      4 * similarity(headline, '%#{search_string}%') +
+      2 * ts_rank_cd(words, plainto_tsquery('simple', '#{(params[:search] || '').squish}'),16) +
+      1 * similarity(full_text, '%#{search_string}%'))
+      DESC NULLS LAST,
+      updated_at DESC"
+
+      classification_aliases = DataCycleCore::ClassificationAlias.joins(
+          :classification_tree_label
+      ).where(
+          classification_trees: {
+              classification_tree_label: DataCycleCore::ClassificationTreeLabel.find_by(name: 'Inhaltstypen')
+          },
+          name: ['Artikel']
+      )
+
+      query = DataCycleCore::Filter::Search.new(@language).where(content_data_type: DataCycleCore::CreativeWork).in_validity_period
+      query = query.with_classification_alias_ids(classification_aliases.map(&:id))
+      query = query.fulltext_search(params[:search]) unless params[:search].blank?
+      query = query.order(order_string)
+
+      @per = params[:per] unless params[:per].blank?
+      @per ||= @@default_per
+
+      @total = query.count
+      pages = @total.fdiv(@per.to_i).ceil
+
+      unless params[:page].blank?
+        @page = params[:page]
+        @page = pages if params[:page].to_i > pages
+      end
+      @page ||= 1
+
+      @contents = query.page(@page).per(@per).map(&:content_data)
+    end
+
+    private
+
+    def content_params
+      params.permit(:page, :per, :language, :search, :token)
     end
 
   end
--- app/views/data_cycle_core/api/v1/api_base/_content_header.json.jbuilder
+++ app/views/data_cycle_core/api/v1/api_base/_content_header.json.jbuilder
@@ -13,6 +13,16 @@ case options[:header_type]
     json.set! 'identifier', send("#{content.class.class_name.tableize.singularize}_url", content)
     json.set! 'dateCreated', content.created_at
     json.set! 'dateModified', content.updated_at
+
+    if content.metadata['validity_period'] && (content.metadata['validity_period']['date_published'] || content.metadata['validity_period']['valid_from'])
+      json.set! 'datePublished', content.metadata['validity_period']['date_published'] || content.metadata['validity_period']['valid_from']
+    end
+    if content.metadata['validity_period'] && (content.metadata['validity_period']['expires'] || content.metadata['validity_period']['valid_until'])
+      json.set! 'expires', content.metadata['validity_period']['expires'] || content.metadata['validity_period']['valid_until']
+    end
+
+    json.set! 'url', send("#{content.class.class_name.tableize.singularize}_url", content)
+
     json.set! 'classifications' do
       json.array! content.classifications, partial: 'classification', as: :classification
     end
