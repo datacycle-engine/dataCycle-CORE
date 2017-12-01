8e3628f4059f6a356af54f9d4cbc79af1cbcbd88
Patrick Rainer <rainer@pixelpoint.at>
2017-12-01 13:57:03 +0100

    filter for webhooks

--- app/models/data_cycle_core/webhook/base.rb
+++ app/models/data_cycle_core/webhook/base.rb
@@ -7,8 +7,23 @@ module DataCycleCore
         Rails.logger.info("#{webhook}: #{message}")
       end
 
-      def self.get_webhooks_for(action)
-        DataCycleCore.webhooks.try(:[], action.try(:to_sym)).nil? == false ? DataCycleCore.webhooks.try(:[], action.try(:to_sym)) : []
+      def self.get_webhooks_for(action, data)
+        webhooks = DataCycleCore.webhooks.try(:[], action.try(:to_sym))
+
+        return webhooks.blank? ? [] : webhooks.collect{ |webhook| validate_webhook(webhook,data) }.reject(&:blank?)
+      end
+
+      def self.validate_webhook(webhook, data)
+
+        return webhook if webhook.kind_of?(Class)
+
+        if webhook.kind_of?(Hash)
+          webhook_class = webhook.keys.first
+          filter = webhook[webhook_class].fetch(:filter){ raise KeyError, "Filter must be supplied for webhook" }
+
+          return webhook_class if filter.call(data)
+        end
+
       end
 
     end
