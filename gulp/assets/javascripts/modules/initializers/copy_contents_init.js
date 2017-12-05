18541063fae7cfa3de212b60c101c19cce5aa9b2
Manuel Mitterer <mitterer@pixelpoint.at>
2017-12-05 16:09:14 +0100

    added possibility to copy single images/videos

--- gulp/assets/javascripts/modules/initializers/copy_contents_init.js
+++ gulp/assets/javascripts/modules/initializers/copy_contents_init.js
@@ -5,6 +5,10 @@ module.exports.initialize = function () {
     var label = $(this).data('label');
     var ids = $(this).data('id');
     if ($('.flex-box .edit-content [data-label=' + label + ']').length > 0 && ids.length > 0) {
+      // add buttons to copy single elements
+      // console.log($(this).find('.copy-single'));
+      $(this).find('.copy-single').append('<a class="button-prime small copy-single-button"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>');
+
       if ($(this).find('.buttons').length > 0) $(this).find('.buttons').append('<a class="button-prime small copy"><i class="fa fa-arrow-right" aria-hidden="true"></i></a>');
       else $(this).append('<div class="buttons"><a class="button-prime small copy"><i class="fa fa-arrow-right" aria-hidden="true"></i></a></div>');
     }
@@ -14,9 +18,20 @@ module.exports.initialize = function () {
     ev.preventDefault();
     var id = $(this).parents('[data-editor]').data('id');
     var label = $(this).parents('[data-editor]').data('label');
+    copy_contents(id);
+  });
+
+  $(document).on('click', '.flex-box .copy-single-button', function (ev) {
+    ev.preventDefault();
+    var id = $(this).parents('.copy-single').data('id');
+    var label = $(this).parents('[data-editor]').data('label');
+    copy_contents(id, label);
+  });
+
+  function copy_contents(ids, label) {
     var target_container = $('.flex-box .edit-content [data-label=' + label + ']');
     target_container.find('.media-thumbs > div').trigger('import-data', {
-      ids: id
+      ids: ids
     });
 
     var first_error_offset = target_container.first().offset().top - target_container.offsetParent().offset().top;
@@ -24,7 +39,6 @@ module.exports.initialize = function () {
     $('.flex-box .edit-content').animate({
       scrollTop: first_error_offset - 50
     }, 500);
-
-  });
+  }
 
 };
