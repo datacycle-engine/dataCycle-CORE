521d16b6e13e63e5fc69c243ff10f64991695de4
Manuel Mitterer <mitterer@pixelpoint.at>
2017-12-19 08:47:51 +0100

    fix for redirecting guest users

--- app/controllers/data_cycle_core/application_controller.rb
+++ app/controllers/data_cycle_core/application_controller.rb
@@ -16,8 +16,12 @@ module DataCycleCore
     rescue_from CanCan::AccessDenied do |exception|
       respond_to do |format|
         format.json { head :forbidden, content_type: 'text/html' }
-        format.html { redirect_back fallback_location: root_path, alert: exception.message }
         format.js   { head :forbidden, content_type: 'text/html' }
+        if current_user.is_rank?(0)
+          format.html { redirect_to info_path, alert: exception.message }
+        else
+          format.html { redirect_back fallback_location: root_path, alert: exception.message }
+        end
       end
     end
 
