5beaa09cf0bd39f8abad271727e0f59503848a5f
Manuel Mitterer <mitterer@pixelpoint.at>
2018-01-10 13:14:43 +0100

    updated login test

--- cypress/integration/1_general_spec.js
+++ cypress/integration/1_general_spec.js
@@ -1,12 +1,32 @@
 describe('General', function () {
-  beforeEach(function () {
-    cy.login('admin')
+
+  it('login', function () {
+    cy.fixture('login_users').as('usersJSON').then(() => {
+      cy.visit('/users/sign_in')
+      var authenticity_token = ""
+      cy.get('input[name=authenticity_token]').then(($elem) => {
+        authenticity_token = $elem.val()
+        const user = this.usersJSON['admin']
+
+        cy.get('input#user_email:visible').type(user.email)
+        cy.get('input#user_password:visible').type(user.password + '{enter}')
+
+        cy.get('.flash.callout').should('have.class', 'success').find('.close-button').click()
+
+        cy.location().should((loc) => {
+          expect(loc.pathname).to.eq('/')
+        })
+      })
+    })
   })
 
-  it('test logout', function () {
+  it('logout', function () {
+    cy.login('admin')
+
     cy.get('.show-sidebar').click()
     cy.get('.logout-link').click()
-    cy.url().should('contain', '/users/sign_in')
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq('/users/sign_in')
+    })
   })
-
 })
