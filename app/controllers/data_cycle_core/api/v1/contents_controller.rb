2da1668997a0b6e6d4447e413b10a4975c699621
Johannes Zlattinger <zlattinger@pixelpoint.at>
2018-01-16 12:54:47 +0100

    improved possibilites for extending the contents controller for specific projects

--- app/controllers/data_cycle_core/api/v1/contents_controller.rb
+++ app/controllers/data_cycle_core/api/v1/contents_controller.rb
@@ -1,6 +1,5 @@
 module DataCycleCore
   class Api::V1::ContentsController < Api::V1::ApiBaseController
-
     @@default_per = 50
 
     def show
@@ -33,55 +32,17 @@ module DataCycleCore
     end
 
     def search
-
-      @language = params[:language] unless params[:language].blank?
-      @language ||= 'de'
-
-      order_string = DataCycleCore::Filter::ObjectBrowserQueryBuilder::get_order_by_query_string(params[:search])
-
-      classification_aliases = DataCycleCore::ClassificationAlias.joins(
-          :classification_tree_label
-      ).where(
-          classification_trees: {
-              classification_tree_label: DataCycleCore::ClassificationTreeLabel.find_by(name: 'Inhaltstypen')
-          }
-      )
-
-      unless DataCycleCore.allowed_content_api_classifications.blank?
-        classification_aliases = classification_aliases.where(name: DataCycleCore.allowed_content_api_classifications)
-      end
-
-      query = DataCycleCore::Filter::Search.new(@language).where(content_data_type: DataCycleCore::CreativeWork)
-      query = query.with_classification_alias_ids(classification_aliases.map(&:id))
-      query = query.fulltext_search(params[:search]) unless params[:search].blank?
-
-      if params[:modified_since]
-        query = query.modified_since(params[:modified_since])
-      end
-
-      if params[:created_since]
-        query = query.created_since(params[:created_since])
-      end
-
-      if params[:modified_since].blank? && params[:created_since].blank?
-        query = query.in_validity_period
-      end
-
-      query = query.order(order_string)
-
-      @per = params[:per] unless params[:per].blank?
-      @per ||= @@default_per
+      query = build_search_query
+      query = query.where(content_data_type: content_data_type) if content_data_type
+      query = query.modified_since(params[:modified_since]) unless params[:modified_since].blank?
+      query = query.created_since(params[:created_since]) unless params[:created_since].blank?
+      query = query.in_validity_period if params[:modified_since].blank? && params[:created_since].blank?
+      query = query.fulltext_search(params[:search]) if params[:search]
+      query = apply_ordering(query)
 
       @total = query.count
-      pages = @total.fdiv(@per.to_i).ceil
 
-      unless params[:page].blank?
-        @page = params[:page]
-        @page = pages if params[:page].to_i > pages
-      end
-      @page ||= 1
-
-      @contents = query.page(@page).per(@per).map(&:content_data)
+      @contents = apply_paging(query).map(&:content_data)
     end
 
     def get_deleted
@@ -117,8 +78,32 @@ module DataCycleCore
     private
 
     def content_params
-      params.permit(:page, :per, :language, :search, :token, :modified_since, :created_since, :deleted_since)
+      params.permit(:format, :type, :page, :per, :language, :search, :token, :modified_since, :created_since, :deleted_since)
     end
 
+    def build_search_query
+      query = DataCycleCore::Filter::Search.new(content_params.fetch(:language, 'de'))
+      query
+    end
+
+    def content_data_type
+      if content_params[:type]
+        Object.const_get("DataCycleCore::#{content_params[:type].classify}")
+      end
+    end
+
+    def apply_ordering(query)
+      if content_params[:search].blank?
+        query
+      else
+        query.order(DataCycleCore::Filter::ObjectBrowserQueryBuilder::get_order_by_query_string())
+      end
+    end
+
+    def apply_paging(query)
+      query
+        .page([content_params.fetch(:page, 1).to_i,(query.count / content_params.fetch(:per, @@default_per).to_i).ceil].min)
+        .per(content_params.fetch(:per, @@default_per).to_i)
+    end
   end
 end
