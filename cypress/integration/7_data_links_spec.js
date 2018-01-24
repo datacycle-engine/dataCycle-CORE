a8bd0d39f8840f45022166557fc43924edfad472
Patrick Rainer <rainer@pixelpoint.at>
2018-01-24 15:07:24 +0100

    testing cypress

--- cypress/integration/7_data_links_spec.js
+++ cypress/integration/7_data_links_spec.js
@@ -65,45 +65,45 @@ describe('DataLink', function () {
   //   })
   // })
 
-  // it('lock link', function () {
-  //   cy.get('#search').type(updated_name + '{enter}', {
-  //     force: true
-  //   }).get('.search-results .grid-item:contains(' + updated_name + ')').should('have.length', 1).click()
-  //   cy.get('.detail-header-functions [data-toggle="send-link"]').click()
-  //   cy.get('#send-link').should('be.visible').find('li:contains("' + email + '")').should('have.length', 1).then(function ($elem) {
-  //     const url = '/data_links/' + $elem.prop('id').replace('data-link-', '')
-  //     $elem.find('.invalidate-data-link').click()
-  //     cy.get('.flash.callout').should('have.class', 'success').find('.close-button').click()
-  //
-  //     cy.logout()
-  //     cy.request({
-  //       url: url,
-  //       failOnStatusCode: false
-  //     }).then(function (resp) {
-  //       expect(resp.status).to.eq(500)
-  //     })
-  //
-  //   })
-  // })
-  //
-  // it('unlock link', function () {
-  //   cy.get('#search').type(updated_name + '{enter}', {
-  //     force: true
-  //   }).get('.search-results .grid-item:contains(' + updated_name + ')').should('have.length', 1).click()
-  //   cy.get('.detail-header-functions [data-toggle="send-link"]').click()
-  //   cy.get('#send-link').should('be.visible').find('li:contains("' + email + '")').should('have.length', 1).then(function ($elem) {
-  //     const url = '/data_links/' + $elem.prop('id').replace('data-link-', '')
-  //     const overlay_id = $elem.prop('id').replace('data-link-', 'data-link-overlay-')
-  //     $elem.find('.send-link-button').click()
-  //
-  //     cy.get('#' + overlay_id + ' input[value="write"]').should('be.visible').check()
-  //     cy.get('#' + overlay_id + ' [name="data_link[valid_until]"]').should('have.length', 1).siblings('.flatpickr-input').clear().type(new Date(Date.now() + 24 * 60 * 60 * 1000).toLocaleDateString())
-  //     cy.get('#' + overlay_id + ' [type="submit"]').should('be.visible').click()
-  //     cy.get('.flash.callout').should('have.class', 'success').find('.close-button').click()
-  //
-  //     cy.logout()
-  //     cy.visit(url)
-  //     cy.get('.headline input[type=text]').should('be.visible').should('have.value', updated_name)
-  //   })
-  // })
+  it('lock link', function () {
+    cy.get('#search').type(updated_name + '{enter}', {
+      force: true
+    }).get('.search-results .grid-item:contains(' + updated_name + ')').should('have.length', 1).click()
+    cy.get('.detail-header-functions [data-toggle="send-link"]').click()
+    cy.get('#send-link').should('be.visible').find('li:contains("' + email + '")').should('have.length', 1).then(function ($elem) {
+      const url = '/data_links/' + $elem.prop('id').replace('data-link-', '')
+      $elem.find('.invalidate-data-link').click()
+      cy.get('.flash.callout').should('have.class', 'success').find('.close-button').click()
+
+      cy.logout()
+      cy.request({
+        url: url,
+        failOnStatusCode: false
+      }).then(function (resp) {
+        expect(resp.status).to.eq(500)
+      })
+
+    })
+  })
+
+  it('unlock link', function () {
+    cy.get('#search').type(updated_name + '{enter}', {
+      force: true
+    }).get('.search-results .grid-item:contains(' + updated_name + ')').should('have.length', 1).click()
+    cy.get('.detail-header-functions [data-toggle="send-link"]').click()
+    cy.get('#send-link').should('be.visible').find('li:contains("' + email + '")').should('have.length', 1).then(function ($elem) {
+      const url = '/data_links/' + $elem.prop('id').replace('data-link-', '')
+      const overlay_id = $elem.prop('id').replace('data-link-', 'data-link-overlay-')
+      $elem.find('.send-link-button').click()
+
+      cy.get('#' + overlay_id + ' input[value="write"]').should('be.visible').check()
+      cy.get('#' + overlay_id + ' [name="data_link[valid_until]"]').should('have.length', 1).siblings('.flatpickr-input').clear().type(new Date(Date.now() + 24 * 60 * 60 * 1000).toLocaleDateString())
+      cy.get('#' + overlay_id + ' [type="submit"]').should('be.visible').click()
+      cy.get('.flash.callout').should('have.class', 'success').find('.close-button').click()
+
+      cy.logout()
+      cy.visit(url)
+      cy.get('.headline input[type=text]').should('be.visible').should('have.value', updated_name)
+    })
+  })
 })
