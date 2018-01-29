26a1b7ad07f605a00b01528ffcab919d52d3232e
Patrick Rainer <rainer@pixelpoint.at>
2018-01-29 10:49:53 +0100

    updated dummy config

--- test/dummy/config/environments/test.rb
+++ test/dummy/config/environments/test.rb
@@ -11,10 +11,19 @@ Rails.application.configure do
 
   config.consider_all_requests_local = true
 
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = {
-    'Cache-Control' => 'public, max-age=3600'
-  }
+  # Enable/disable caching. By default caching is disabled.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => 'public, max-age=172800'
+    }
+  else
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
+  end
 
   # Show full error reports.
   config.action_controller.perform_caching = false
@@ -23,8 +32,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
   # Disable request forgery protection in test environment.
-  # config.action_controller.allow_forgery_protection = false
+  config.action_controller.allow_forgery_protection = false
 
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
@@ -36,6 +48,14 @@ Rails.application.configure do
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :stderr
+
+  # Debug mode disables concatenation and preprocessing of assets.
+  # This option may cause significant delays in view rendering with a large
+  # number of complex assets.
+  config.assets.debug = true
+
+  # Suppress logger output for asset requests.
+  config.assets.quiet = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
