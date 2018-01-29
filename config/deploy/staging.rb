ae9d7a84d73694ae85992c720f4c5c4d7b86d1e2
Patrick Rainer <rainer@pixelpoint.at>
2018-01-29 13:46:00 +0100

    added deployment config for staging env

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,6 +5,6 @@
 
 # activate for deployment:
 server '37.61.206.122', user: 'pixelpoint', roles: ['app', 'db', 'web']
-set :branch, 'feature/dummy_app'
+set :branch, 'staging'
 set :deploy_to, '/var/www/data-cycle-core/staging'
 set :rails_env, 'staging'
