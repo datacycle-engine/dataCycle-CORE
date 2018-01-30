197b0059ffacd874d7c5ed078f70216c22dfd73c
Johannes Zlattinger <zlattinger@pixelpoint.at>
2018-01-30 16:13:37 +0100

    implemented possibility to combine classification filters with AND

--- app/controllers/data_cycle_core/api/v1/events_controller.rb
+++ app/controllers/data_cycle_core/api/v1/events_controller.rb
@@ -1,19 +1,23 @@
 module DataCycleCore
   class Api::V1::EventsController < DataCycleCore::Api::V1::ContentsController
     def index
-      query = Event.includes(:translations, :classifications)
-        .with_classification_alias_names(DataCycleCore.allowed_content_api_classifications)
-        .with_translations(params.fetch(:language, 'de'))
+      query = Event.with_classification_alias_names(DataCycleCore.allowed_content_api_classifications)
 
       query = query.where(Event.arel_table[:end_date].gteq(Time.zone.now))
 
-      if params.dig(:filter, :classifications)
-        query = query.with_classification_alias_ids(params.dig(:filter, :classifications).split(',').map(&:strip))
+      if params&.dig(:filter, :classifications)
+        params.dig(:filter, :classifications).map { |classifications|
+          classifications.split(',').map(&:strip).reject(&:blank?)
+        }.reject(&:empty?).each do |classifications|
+          query = query.with_classification_alias_ids(classifications)
+        end
       end
 
-      query = query.sort_by_proximity
+      query = query.with_translations(params.fetch(:language, 'de'))
 
       @total = query.count
+
+      query = query.includes(:translations, :classifications).sort_by_proximity
 
       @contents = apply_paging(query)
 
@@ -25,7 +29,7 @@ module DataCycleCore
     end
 
     def permitted_parameter_keys
-      super + [{ filter: :classifications }]
+      super + [{ filter: { classifications: [] } }]
     end
   end
 end
