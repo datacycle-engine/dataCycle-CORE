08e4271a167c56bf5ceb818c24ab6880fb190248
Johannes Zlattinger <zlattinger@pixelpoint.at>
2018-01-30 08:14:25 +0100

    added possibility to filter events by classifications when using the api

--- app/controllers/data_cycle_core/api/v1/events_controller.rb
+++ app/controllers/data_cycle_core/api/v1/events_controller.rb
@@ -2,10 +2,16 @@ module DataCycleCore
   class Api::V1::EventsController < DataCycleCore::Api::V1::ContentsController
     def index
       query = Event.includes(:translations, :classifications)
-        .with_classification_aliase_names(DataCycleCore.allowed_content_api_classifications)
+        .with_classification_alias_names(DataCycleCore.allowed_content_api_classifications)
         .with_translations(params.fetch(:language, 'de'))
-        .where(Event.arel_table[:end_date].gteq(Time.zone.now))
-        .sort_by_proximity
+
+      query = query.where(Event.arel_table[:end_date].gteq(Time.zone.now))
+
+      if params.dig(:filter, :classifications)
+        query = query.with_classification_alias_ids(params.dig(:filter, :classifications).split(',').map(&:strip))
+      end
+
+      query = query.sort_by_proximity
 
       @total = query.count
 
@@ -16,6 +22,10 @@ module DataCycleCore
 
     def show
       @content = Event.includes(:classifications, :translations).find(params[:id])
+    end
+
+    def permitted_parameter_keys
+      super + [{ filter: :classifications }]
     end
   end
 end
--- app/models/data_cycle_core/content_filters.rb
+++ app/models/data_cycle_core/content_filters.rb
@@ -1,8 +1,13 @@
 module DataCycleCore
   module ContentFilters
-    def with_classification_aliase_names(*names)
-      joins(:classification_aliases)
-        .where(classification_aliases: { id: ClassificationAlias.with_name(names.flatten) })
+    def with_classification_alias_names(*names)
+      where(id: name.constantize.joins(:classification_aliases)
+        .merge(ClassificationAlias.with_name(names.flatten).with_descendants))
+    end
+
+    def with_classification_alias_ids(*ids)
+      where(id: name.constantize.joins(:classification_aliases)
+        .merge(ClassificationAlias.where(id: ids).with_descendants))
     end
   end
 end
