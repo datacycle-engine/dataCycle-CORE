2399c98e86cdaff81ca54a97b0e2215047803513
Patrick Rainer <rainer@pixelpoint.at>
2018-02-01 14:13:09 +0100

    minor change to abilities

--- test/dummy/app/models/ability.rb
+++ test/dummy/app/models/ability.rb
@@ -1,6 +1,6 @@
 class Ability < DataCycleCore::Ability
   def initialize(user, _session = {})
-    super
+    super user
 
     return unless user && user&.has_rank?(0)
     can :create_in_objectbrowser, [DataCycleCore::Person, DataCycleCore::Place]
