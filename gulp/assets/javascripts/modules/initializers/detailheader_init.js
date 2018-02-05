4927e31afafdc03061b913b41d7ba9b0c4d547d7
Manuel Mitterer <mitterer@pixelpoint.at>
2018-02-05 16:30:33 +0100

    added possibility to copy Stored_Filter API link to clipboard

--- gulp/assets/javascripts/modules/initializers/detailheader_init.js
+++ gulp/assets/javascripts/modules/initializers/detailheader_init.js
@@ -1,67 +1,22 @@
 // Reveal Blur
 module.exports.initialize = function () {
 
-  $('.copy-button').on('click', function (ev) {
-    ev.preventDefault();
-    var succeeded = copyToClipboard($(this).siblings('.copy-input')[0]);
-    if (succeeded) {
-      $(this).before('<span class="copied-to-clipboard">In Zwischenablage kopiert.</span>');
-      setTimeout(function () {
-        $('.copied-to-clipboard').fadeOut(function () {
-          $(this).remove();
-        });
-      }, 1000);
-    }
+  $('.copy-to-clipboard').on('click', function (event) {
+    event.preventDefault();
+    var text = this.href;
+    var inp = document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value = text;
+    inp.select();
+    document.execCommand('copy', false);
+    inp.remove();
+
+    $(this).before('<span class="clipboard-notice">In Zwischenablage kopiert.</span>');
+    setTimeout(function () {
+      $(this).siblings('.clipboard-notice').fadeOut('fast', function () {
+        $(this).remove();
+      });
+    }.bind(this), 3000);
   });
 
-
-  function copyToClipboard(elem) {
-    // create hidden text element, if it doesn't already exist
-    var targetId = "_hiddenCopyText_";
-    var isInput = elem.tagName === "INPUT" || elem.tagName === "TEXTAREA";
-    var origSelectionStart, origSelectionEnd;
-    if (isInput) {
-      // can just use the original source element for the selection and copy
-      target = elem;
-      origSelectionStart = elem.selectionStart;
-      origSelectionEnd = elem.selectionEnd;
-    } else {
-      // must use a temporary form element for the selection and copy
-      target = document.getElementById(targetId);
-      if (!target) {
-        var target = document.createElement("textarea");
-        target.style.position = "absolute";
-        target.style.left = "-9999px";
-        target.style.top = "0";
-        target.id = targetId;
-        document.body.appendChild(target);
-      }
-      target.textContent = elem.textContent;
-    }
-    // select the content
-    var currentFocus = document.activeElement;
-    target.focus();
-    target.setSelectionRange(0, target.value.length);
-
-    // copy the selection
-    var succeed;
-    try {
-      succeed = document.execCommand("copy");
-    } catch (e) {
-      succeed = false;
-    }
-    // restore original focus
-    if (currentFocus && typeof currentFocus.focus === "function") {
-      currentFocus.focus();
-    }
-
-    if (isInput) {
-      // restore prior selection
-      elem.setSelectionRange(origSelectionStart, origSelectionEnd);
-    } else {
-      // clear temporary content
-      target.textContent = "";
-    }
-    return succeed;
-  }
 };
