84e3c506b5865a1070d9c619bf911cb11ecdfe54
Patrick Rainer <rainer@pixelpoint.at>
2018-02-14 15:10:59 +0100

    updated dummy app initializer

--- test/dummy/config/initializers/data_cycle_core.rb
+++ test/dummy/config/initializers/data_cycle_core.rb
@@ -2,4 +2,10 @@ DataCycleCore.setup do |config|
   config.access_tokens = [
     'd48a84faseei512hjkl159ggg9a72adf'
   ]
+
+  config.template_path = Rails.root.join('config', 'data_definitions')
+  config.default_template_paths = [
+    # Rails.root.join('vendor', 'gems', 'data-cycle-core', 'config', 'data_definitions', 'basic'),
+    # Rails.root.join('vendor', 'gems', 'data-cycle-core', 'config', 'data_definitions', 'enhanced')
+  ]
 end
