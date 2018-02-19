9ba11f445a100090d534275c1a36856c41087586
Patrick Rainer <rainer@pixelpoint.at>
2018-02-19 12:53:36 +0100

    introduced production + release environment

--- test/dummy/config/environments/release.rb
+++ test/dummy/config/environments/release.rb
@@ -83,5 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'core-production.datacycle.at', protocol: 'http' }
+  config.action_mailer.default_url_options = { host: 'core-release.datacycle.at', protocol: 'http' }
 end
