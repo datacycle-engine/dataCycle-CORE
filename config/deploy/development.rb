446cbab53dd88d40cb2998be4993e7ffb3ca1ee0
Patrick Rainer <rainer@pixelpoint.at>
2018-02-20 14:08:50 +0100

    updated environments + deployment

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -6,5 +6,7 @@
 # activate for deployment:
 server '37.61.206.122', user: 'pixelpoint', roles: ['app', 'db', 'web']
 set :branch, 'develop'
+set :rails_env, 'staging'
 set :deploy_to, '/var/www/data-cycle-core/development'
-# set :rails_env, 'development'
+set :cmd_prefix, 'app:'
+set :application_root_path, 'test/dummy/'
--- config/deploy/pre_release.rb
+++ config/deploy/pre_release.rb
@@ -5,7 +5,8 @@
 
 # activate for deployment:
 server '37.61.206.122', user: 'pixelpoint', roles: ['app', 'db', 'web']
-set :branch, 'staging'
-set :deploy_to, '/var/www/data-cycle-core/staging'
+set :branch, 'pre-release'
+set :rails_env, 'pre_release'
+set :deploy_to, '/var/www/data-cycle-core/pre-release'
 set :cmd_prefix, 'app:'
 set :application_root_path, 'test/dummy/'
