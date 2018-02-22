ecbca7417b23ec19f4cef5038e17f6b9c76d7ad5
Martin Oehzelt <oehzelt@pixelpoint.at>
2018-02-22 09:53:07 +0100

    update dummy app

--- test/dummy/config/initializers/data_cycle_core.rb
+++ test/dummy/config/initializers/data_cycle_core.rb
@@ -2,4 +2,10 @@ DataCycleCore.setup do |config|
   config.access_tokens = [
     'd48a84faseei512hjkl159ggg9a72adf'
   ]
+
+  config.template_path = Rails.root.join('config', 'data_definitions')
+  config.default_template_paths = [
+    Rails.root.join('..', '..', 'config', 'data_definitions', 'basic'),
+    # Rails.root.join('..', '.', 'config', 'data_definitions', 'enhanced')
+  ]
 end
