b3f2ae1c2253aaa5cb8674b312fedbbe2f6101fd
Johannes Zlattinger <johannes@zlattinger.net>
2018-03-07 12:39:46 +0100

    introduced print button for entities

--- app/models/data_cycle_core/ability.rb
+++ app/models/data_cycle_core/ability.rb
@@ -1,5 +1,8 @@
 module DataCycleCore
   class Ability
+
+    CONTENT_MODELS = [DataCycleCore::Person, DataCycleCore::Organization, DataCycleCore::CreativeWork, DataCycleCore::Place]
+
     include CanCan::Ability
 
     def initialize(user, session = {})
@@ -16,6 +19,10 @@ module DataCycleCore
         if user.has_rank?(0)
           DataCycleCore::DataLink.session_edit_links(session[:can_edit_ids]).each do |link|
             can [:update, :validate_single_data, :import], link.item_type.constantize, { id: link.item_id } if link.is_valid?
+          end
+
+          can :print, CONTENT_MODELS do |content|
+            ['entity'].include?(content.schema['content_type'])
           end
         end
 
