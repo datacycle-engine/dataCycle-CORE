4105e53a6ffa9e7a8d3991a38ef499f7c80cfe5d
Patrick Rainer <rainer@pixelpoint.at>
2018-03-15 14:50:04 +0100

    updated classifications + removed Datei + Bild content_type for dummy

--- test/dummy/config/initializers/data_cycle_core.rb
+++ test/dummy/config/initializers/data_cycle_core.rb
@@ -7,11 +7,11 @@ DataCycleCore.setup do |config|
   config.default_template_paths = [
     Rails.root.join('..', '..', 'config', 'data_definitions', 'basic'),
     Rails.root.join('..', '..', 'config', 'data_definitions', 'enhanced'),
-    # Rails.root.join('..', '..', 'config', 'data_definitions', 'container')
+    Rails.root.join('..', '..', 'config', 'data_definitions', 'container')
     # Rails.root.join('..', '..', 'config', 'data_definitions', 'test')
   ]
 
   config.external_sources_path = Rails.root.join('config', 'external_sources')
 
-  # config.excluded_new_item_objects = ['Event', 'Person', 'Örtlichkeit', 'Bild', 'Organization', 'Zeitleiste', 'Linktipps']
+  config.excluded_new_item_objects = ['Bild', 'Datei']
 end
