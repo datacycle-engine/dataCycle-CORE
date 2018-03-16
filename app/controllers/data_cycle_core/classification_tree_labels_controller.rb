c21b83f4bca3b3ff724a9e3cf41e282706a81c05
Manuel Mitterer <mitterer@pixelpoint.at>
2018-03-16 13:34:28 +0100

    [WIP] theme tree view, records not yet uniq

--- app/controllers/data_cycle_core/classification_tree_labels_controller.rb
+++ app/controllers/data_cycle_core/classification_tree_labels_controller.rb
@@ -8,20 +8,37 @@ module DataCycleCore
         format.html do
           @classification_trees = @classification_tree_label.classification_trees.accessible_by(current_ability)
             .where(parent_classification_alias: nil)
-            .order(:created_at).includes(sub_classification_alias: [:sub_classification_trees, :classifications, :external_source])
+            .where.not(classification_aliases: { name: DataCycleCore.excluded_filter_classifications })
+            .order('classification_aliases.name').includes(sub_classification_alias: [:sub_classification_trees, :classifications, :external_source])
+
+          @content_count = @classification_trees.map { |c| [c.id, DataCycleCore::Filter::Search.new(nil, DataCycleCore::Search.distinct).with_classification_alias_ids_without_recursion(c.sub_classification_alias.id).pluck(:content_data_id).count] }.to_h
         end
         format.js do
           if classification_tree_label_params[:classification_tree_id].present?
             @classification_tree = DataCycleCore::ClassificationTree.find(classification_tree_label_params[:classification_tree_id])
-            @classification_trees = @classification_tree.sub_classification_alias.sub_classification_trees.accessible_by(current_ability).order(:created_at)
+            @classification_trees = @classification_tree.sub_classification_alias.sub_classification_trees
+              .includes(sub_classification_alias: [:sub_classification_trees, :classifications, :external_source]).accessible_by(current_ability)
+              .where.not(classification_aliases: { name: DataCycleCore.excluded_filter_classifications })
+              .order('classification_aliases.name')
+
+            @content_count = @classification_trees.map { |c| [c.id, DataCycleCore::Filter::Search.new(nil, DataCycleCore::Search.distinct).with_classification_alias_ids_without_recursion(c.sub_classification_alias.id).pluck(:content_data_id).count] }.to_h
 
             query = DataCycleCore::Filter::Search.new(nil, DataCycleCore::Search)
-            @contents = query.with_classification_alias_ids_without_recursion(@classification_tree.sub_classification_alias.id).includes(content_data: [:display_classification_aliases]).map(&:content_data)
+
+            @contents = query
+              .with_classification_alias_ids_without_recursion(@classification_tree.sub_classification_alias.id)
+              .includes(content_data: [:display_classification_aliases])
+              .order(boost: :desc, data_type: :asc, headline: :asc)
+
+            @contents = @contents.page(params[:page])
+
+            @page = @contents.current_page
+            @total_count = @contents.total_count
+            @total_pages = @contents.total_pages
+            @contents = @contents.map(&:content_data)
           end
         end
       end
-
-      # raise "test"
     end
 
     private
