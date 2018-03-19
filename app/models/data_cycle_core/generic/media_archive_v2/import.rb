401aebd5e2c1084def254081599575e5fec3296d
Patrick Rainer <rainer@pixelpoint.at>
2018-03-19 14:21:11 +0100

    media archive import v2

--- app/models/data_cycle_core/generic/media_archive_v2/import.rb
+++ app/models/data_cycle_core/generic/media_archive_v2/import.rb
@@ -1,14 +1,16 @@
-module DataCycleCore::Generic::MediaArchive::Import
+module DataCycleCore::Generic::MediaArchiveV2::Import
   def import_data(**options)
     @place_template = options[:import][:place_template] || 'Örtlichkeit'
+    @person_template = options[:import][:person_template] || 'Person'
     load_transformations
     import_contents(@source_type, @target_type, method(:load_contents).to_proc, method(:process_content).to_proc, **options)
   end
 
   def load_transformations
-    @image_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_to_bild
-    @video_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_to_video
-    @content_location_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_to_content_location
+    @image_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_v2_to_bild
+    @video_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_v2_to_video
+    @content_location_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_v2_to_content_location
+    @person_transformation = DataCycleCore::Generic::Transformations::Transformations.media_archive_to_person
   end
 
   protected
@@ -23,13 +25,26 @@ module DataCycleCore::Generic::MediaArchive::Import
         DataCycleCore::Place,
         load_template(DataCycleCore::Place, @place_template),
         extract_content_location_data(raw_data['contentLocation'])
-          .merge({ 'external_key' => raw_data['url'] }).with_indifferent_access
+          .merge({ 'external_key' => "#{raw_data['contentType']}-#{@place_template}: #{raw_data['url'].split('/').last}"}).with_indifferent_access
       )
 
-      # keywords = raw_data['keywords'] || []
-      # keywords.each { |item| import_classification({ name: item, external_id: "MedienArchive - keyword - #{item}", tree_name: 'MediaArchive - Tags' }) }
+      director = create_or_update_content(
+        DataCycleCore::Person,
+        load_template(DataCycleCore::Person, @person_template),
+        extract_person_data(raw_data['director'])
+          .merge({ 'external_key' => "Regie: #{raw_data['url'].split('/').last}"}).with_indifferent_access
+      )
+
+      contributor = create_or_update_content(
+        DataCycleCore::Person,
+        load_template(DataCycleCore::Person, @person_template),
+        extract_person_data(raw_data['contributor'])
+          .merge({ 'external_key' => "Kamera: #{raw_data['url'].split('/').last}"}).with_indifferent_access
+      )
 
       raw_data['content_location'] = [{ 'id' => content_location.try(:id) }] unless content_location.blank?
+      raw_data['director'] = [{ 'id' => director.try(:id) }] unless director.blank?
+      raw_data['contributor'] = [{ 'id' => contributor.try(:id) }] unless contributor.blank?
 
       case raw_data['contentType']
       when 'Bild'
@@ -40,7 +55,7 @@ module DataCycleCore::Generic::MediaArchive::Import
         data = nil
         ap "Unkown contentType #{raw_data}"
       end
-
+      # ap data
       unless data.nil?
         content = create_or_update_content(
           @target_type,
@@ -61,5 +76,9 @@ module DataCycleCore::Generic::MediaArchive::Import
 
   def extract_content_location_data(raw_data)
     raw_data.nil? ? {} : @content_location_transformation.call(raw_data)
+  end
+
+  def extract_person_data(raw_data)
+    raw_data.nil? ? {} : @person_transformation.call(raw_data)
   end
 end
