78806ddcd7bec84ddad073e41b7a0761cdbc100a
Manuel Mitterer <mitterer@pixelpoint.at>
2018-04-04 10:37:30 +0200

    fix for dropdown-pane without list-items

--- gulp/assets/javascripts/modules/initializers/dropdown_pane_init.js
+++ gulp/assets/javascripts/modules/initializers/dropdown_pane_init.js
@@ -2,11 +2,12 @@
 module.exports.initialize = function () {
 
   $(document).on('show.zf.dropdown', '.dropdown-pane.bottom', event => {
-    if ($(event.currentTarget).children('.list-items').length) {
-      let linked_item = $('[data-toggle="' + $(event.currentTarget).prop('id') + '"]');
+    let linked_item = $('[data-toggle="' + $(event.currentTarget).prop('id') + '"]');
 
-      if ((linked_item.offset().top + linked_item.outerHeight() - $(document).scrollTop() + $(event.currentTarget).outerHeight() + 20) >= $(window).height() && (linked_item.offset().top - $(document).scrollTop()) > ($(window).height() - (linked_item.offset().top - $(document).scrollTop() + linked_item.outerHeight()))) {
-        $(event.currentTarget).addClass('align-top');
+    if ((linked_item.offset().top + linked_item.outerHeight() - $(document).scrollTop() + $(event.currentTarget).outerHeight() + 20) >= $(window).height() && (linked_item.offset().top - $(document).scrollTop()) > ($(window).height() - (linked_item.offset().top - $(document).scrollTop() + linked_item.outerHeight()))) {
+      $(event.currentTarget).addClass('align-top');
+
+      if ($(event.currentTarget).children('.list-items').length) {
         $(event.currentTarget).children('.list-items').first().css('max-height', '');
 
         if ($(document).scrollTop() < ($('header').outerHeight() + 5) && (linked_item.offset().top - $(document).scrollTop() - $(event.currentTarget).outerHeight()) <= ($('header').outerHeight())) {
@@ -14,8 +15,10 @@ module.exports.initialize = function () {
         } else if ((linked_item.offset().top - $(document).scrollTop() - $(event.currentTarget).outerHeight()) <= 20) {
           $(event.currentTarget).children('.list-items').first().css('max-height', $(event.currentTarget).children('.list-items').first().outerHeight() - 30 + (linked_item.offset().top - $(document).scrollTop() - $(event.currentTarget).outerHeight()));
         }
-      } else {
-        $(event.currentTarget).removeClass('align-top');
+      }
+    } else {
+      $(event.currentTarget).removeClass('align-top');
+      if ($(event.currentTarget).children('.list-items').length) {
         $(event.currentTarget).children('.list-items').first().css('max-height', '');
 
         if (($(window).height() - (linked_item.offset().top + linked_item.outerHeight() - $(document).scrollTop() + $(event.currentTarget).outerHeight())) <= 20) {
