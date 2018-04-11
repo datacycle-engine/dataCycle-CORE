46e79af13cb75e419873c4826f09dae0bcc0295f
Patrick Rainer <rainer@pixelpoint.at>
2018-04-11 16:41:42 +0200

    fixed initializers for rails tests

--- lib/data_cycle_core/engine.rb
+++ lib/data_cycle_core/engine.rb
@@ -190,9 +190,6 @@ module DataCycleCore
     config.action_controller.per_form_csrf_tokens = true
     config.action_controller.forgery_protection_origin_check = true
 
-    # Require `belongs_to` associations by default. Previous versions had false.
-    config.active_record.belongs_to_required_by_default = true
-
     # use active_record as orm (!not mongoid)
     config.app_generators.orm = :active_record
     config.active_record.schema_format = :sql
