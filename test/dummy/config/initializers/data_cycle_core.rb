c6ccce115c3e503fe34efaa5a28cf18d52479b21
Patrick Rainer <rainer@pixelpoint.at>
2018-05-02 13:34:09 +0200

    added gitlab_ci template set

--- test/dummy/config/initializers/data_cycle_core.rb
+++ test/dummy/config/initializers/data_cycle_core.rb
@@ -15,16 +15,23 @@ DataCycleCore.setup do |config|
   ]
 
   config.template_path = Rails.root.join('config', 'data_definitions').freeze
-  config.default_template_paths = [
-    Rails.root.join('..', '..', 'config', 'data_definitions', 'basic'),
-    # Rails.root.join('..', '..', 'config', 'data_definitions', 'enhanced'),
-    # Rails.root.join('..', '..', 'config', 'data_definitions', 'media_archive')
-    # Rails.root.join('..', '..', 'config', 'data_definitions', 'container')
-    # Rails.root.join('..', '..', 'config', 'data_definitions', 'test')
-  ].freeze
 
+  if ENV['RAILS_ENV'] == 'test'
+    config.default_template_paths = [
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'gitlab_ci')
+    ].freeze
+  else
+    config.default_template_paths = [
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'basic'),
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'enhanced'),
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'media_archive')
+      # Rails.root.join('..', '..', 'config', 'data_definitions', 'container')
+      # Rails.root.join('..', '..', 'config', 'data_definitions', 'test')
+      # Rails.root.join('..', '..', 'config', 'data_definitions', 'gitlab_ci')
+    ].freeze
+  end
   config.external_sources_path = Rails.root.join('config', 'external_sources').freeze
-  config.excluded_new_item_objects = ['Event', 'Person', 'Örtlichkeit', 'Bild', 'Organization', 'Zeitleiste', 'Linktipps', 'Datei', 'Tour', 'Video', 'Unterkunft']
+  # config.excluded_new_item_objects = ['Event', 'Person', 'Örtlichkeit', 'Bild', 'Organization', 'Zeitleiste', 'Linktipps', 'Datei', 'Tour', 'Video', 'Unterkunft']
 
   config.features = config.features.merge(
     {
