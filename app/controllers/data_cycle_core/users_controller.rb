f0a0b0860965a2a3cbd26e0934e0ae82fcfc7bfa
Manuel Mitterer <mitterer@pixelpoint.at>
2018-05-04 09:38:54 +0200

    added ordering for users/user_groups index page

--- app/controllers/data_cycle_core/users_controller.rb
+++ app/controllers/data_cycle_core/users_controller.rb
@@ -7,9 +7,9 @@ module DataCycleCore
     def index
       authorize! :crud, DataCycleCore::User
       if current_user.is_rank?(10)
-        @paginateObject = DataCycleCore::User.includes(:role).page(params[:page])
+        @paginateObject = DataCycleCore::User.includes(:role).order(:email).page(params[:page])
       else
-        @paginateObject = DataCycleCore::User.where(locked_at: nil).includes(:role).page(params[:page])
+        @paginateObject = DataCycleCore::User.where(locked_at: nil).includes(:role).order(:email).page(params[:page])
       end
     end
 
