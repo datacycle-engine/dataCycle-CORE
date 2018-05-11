3a4a66b0d09dd2dd067d5565fdd3af12faabd466
Manuel Mitterer <mitterer@pixelpoint.at>
2018-05-11 13:24:26 +0200

    minor js fixes

--- gulp/assets/javascripts/modules/helpers/progress_helper.js
+++ gulp/assets/javascripts/modules/helpers/progress_helper.js
@@ -2,18 +2,18 @@
 module.exports = {
   progress: (event, container) => {
     var percentage = (event.loaded * 100) / event.total;
-    if (container.hasClass('progress-container')) {
+    if (container.find('.progressbar > .progressbar-meter').length) {
       container.find('.progressbar > .progressbar-meter').css('width', percentage + '%');
     } else {
       var text = container.html();
-      container.html('').addClass('progress-container');
+      container.html('');
       container.append('<span class="progresstitle">' + text + '</span><span class="progressbar"><span class="progressbar-meter style="width: ' + percentage + '%;"></span></span>');
     }
 
     if (percentage >= 100) {
       var text = container.find('.progresstitle').html();
       container.find('.progressbar').fadeOut(500, () => {
-        container.html(text).removeClass('progress-container');
+        container.html(text);
       });
     }
   }
