2eb5a97c729fd042bf4c494c4251008be22d205e
Patrick Rainer <rainer@pixelpoint.at>
2018-05-14 10:06:24 +0200

    fixed datetime validations

--- app/models/data_cycle_core/master_data/validators/datetime.rb
+++ app/models/data_cycle_core/master_data/validators/datetime.rb
@@ -2,11 +2,23 @@ module DataCycleCore
   module MasterData
     module Validators
       class Datetime < BasicValidator
+        def datetime_keywords
+          ['min']
+        end
+
         def validate(data, template)
-          @template_key = template['label']
           if data.is_a?(::Time) || data.is_a?(::Date)
             # all good
           elsif data.is_a?(::String)
+            if template.key?('validations')
+              template['validations'].each_key do |key|
+                if datetime_keywords.include?(key)
+                  method(key).call(data, template['validations'][key])
+                else
+                  (@error[:warning][@template_key] ||= []) << I18n.t(:string, scope: [:validation, :warnings], data: data, key: key, template: template, locale: DataCycleCore.ui_language) unless key == 'type'
+                end
+              end
+            end
             datetime(data)
           elsif data.blank?
             (@error[:warning][@template_key] ||= []) << I18n.t(:no_data, scope: [:validation, :warning], data: template['label'], locale: DataCycleCore.ui_language)
@@ -21,6 +33,13 @@ module DataCycleCore
         rescue StandardError
           (@error[:error][@template_key] ||= []) << I18n.t(:date_time, scope: [:validation, :errors], data: data, locale: DataCycleCore.ui_language)
         end
+
+        private
+
+        def min(data, value)
+          (@error[:error][@template_key] ||= []) << I18n.t(:min, scope: [:validation, :errors], data: data, min: value.to_i, length: data.length, locale: DataCycleCore.ui_language) if data.length < value.to_i
+        end
+
       end
     end
   end
