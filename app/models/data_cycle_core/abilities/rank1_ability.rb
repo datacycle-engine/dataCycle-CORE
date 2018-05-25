cbb278ce8c84a8a8559f1674decceec407c5b7fb
Patrick Rainer <rainer@pixelpoint.at>
2018-05-25 12:02:37 +0200

    added advanced_filter abilities

--- app/models/data_cycle_core/abilities/rank1_ability.rb
+++ app/models/data_cycle_core/abilities/rank1_ability.rb
@@ -5,7 +5,7 @@ module DataCycleCore
       include CanCan::Ability
 
       def initialize(user, _session = {})
-        can [:read, :settings], :backend
+        can [:read, :settings, :advanced_filter], :backend
         can :update, DataCycleCore::User, id: user.id
         can [:read, :create, :update, :destroy, :show_history], DataCycleCore::StoredFilter, user_id: user.id
         can :read, DataCycleCore::StoredFilter, system: true
