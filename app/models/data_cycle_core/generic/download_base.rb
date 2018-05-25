5b332368d6120759ef4e3e7c58871aaff8cfb4ee
Johannes Zlattinger <johannes@zlattinger.net>
2018-05-25 15:26:27 +0200

    added some manual garbage collection when downloading or importing items

--- app/models/data_cycle_core/generic/download_base.rb
+++ app/models/data_cycle_core/generic/download_base.rb
@@ -25,22 +25,32 @@ module DataCycleCore
               max_string += (options[:max_count]).to_s if options[:max_count]
               @logging.phase_started("#{type.collection_name}_#{locale}", max_string)
 
+              durations = []
+
               items.each do |item_data|
-                break if options[:max_count] && item_count >= options[:max_count]
-                item_count += 1
-                next if item_data.nil?
-                begin
-                  item_id = extract_id.call(item_data)
-                  item_name = extract_name.call(item_data)
+                durations << Benchmark.realtime do
+                  break if options[:max_count] && item_count >= options[:max_count]
+                  item_count += 1
+                  next if item_data.nil?
 
-                  item = mongo_item.find_or_initialize_by('external_id': item_id)
+                  begin
+                    item_id = extract_id.call(item_data)
+                    item_name = extract_name.call(item_data)
 
-                  item.dump ||= {}
-                  item.dump[locale] = item_data
-                  item.save!
-                  @logging.item_processed(item_name, item_id, item_count, max_string)
-                rescue StandardError => e
-                  @logging.error(item_name, item_id, item_data, e)
+                    item = mongo_item.find_or_initialize_by('external_id': item_id)
+
+                    item.dump ||= {}
+                    item.dump[locale] = item_data
+                    item.save!
+                    @logging.item_processed(item_name, item_id, item_count, max_string)
+                  rescue StandardError => e
+                    @logging.error(item_name, item_id, item_data, e)
+                  end
+
+                  next unless (item_count % 100).zero?
+
+                  GC.start
+                  @logging.info("Downloaded #{item_count} items in #{durations.sum} seconds", nil)
                 end
               end
             end
