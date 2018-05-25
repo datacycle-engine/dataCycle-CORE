8b2a6a6678220afdf4f50986b748c41d62c6c5a7
Patrick Rainer <rainer@pixelpoint.at>
2018-05-25 10:36:14 +0200

    remove table + models: tag, overlays, overlay_place_tag

--- app/models/data_cycle_core/overlay.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module DataCycleCore
-  class Overlay < ApplicationRecord
-  end
-end
--- app/models/data_cycle_core/tag.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module DataCycleCore
-  class Tag < ApplicationRecord
-  end
-end
