170ad0d3373f6a5b6b4cdeddf1e2a3968120fa18
Patrick Rainer <rainer@pixelpoint.at>
2018-05-30 12:49:39 +0200

    [rubocop] minor changes

--- app/models/data_cycle_core/generic/bergfex/endpoint.rb
+++ app/models/data_cycle_core/generic/bergfex/endpoint.rb
@@ -1,30 +1,35 @@
-class DataCycleCore::Generic::Bergfex::Endpoint
-  def initialize(host: nil, end_point: nil, partner: nil)
-    @host = host
-    @end_point = end_point
-    @partner = partner
-  end
+# frozen_string_literal: true
 
-  def lakes(lang: :de)
-    Enumerator.new do |yielder|
-      load_data.each do |lake|
-        yielder << lake
+module DataCycleCore
+  module Generic
+    module Bergfex
+      class Endpoint
+        def initialize(host: nil, end_point: nil, partner: nil)
+          @host = host
+          @end_point = end_point
+          @partner = partner
+        end
+
+        def lakes(lang: :de)
+          Enumerator.new do |yielder|
+            load_data.each do |lake|
+              yielder << lake
+            end
+          end
+        end
+
+        protected
+
+        def load_data
+          response = Faraday.new.get do |req|
+            req.url(@host + @end_point)
+            req.params['partner'] = @partner
+          end
+
+          raise DataCycleCore::Generic::RecoverableError, "error loading data from #{@host + @end_point} / partner:#{@partner}" << response.body unless response.success?
+          Nokogiri::XML(response.body).xpath('//lakes').first.to_hash['lake']
+        end
       end
-    end
-  end
-
-  protected
-
-  def load_data
-    response = Faraday.new.get do |req|
-      req.url(@host + @end_point)
-      req.params['partner'] = @partner
-    end
-
-    if response.success?
-      data = Nokogiri::XML(response.body).xpath('//lakes').first.to_hash['lake']
-    else
-      raise DataCycleCore::Generic::RecoverableError, "error loading data from #{@host + @end_point} / partner:#{@partner}" << response.body
     end
   end
 end
