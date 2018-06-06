f35877ed46e153d925bf01da6c5bc976d6ae3492
Patrick Rainer <rainer@pixelpoint.at>
2018-06-06 10:49:04 +0200

    [bugfix] creative_works idea_collection

--- app/controllers/data_cycle_core/contents_controller.rb
+++ app/controllers/data_cycle_core/contents_controller.rb
@@ -171,7 +171,7 @@ module DataCycleCore
       # Create idea_collection if it doesn't exist and active life_cycle_stage is correct
       if DataCycleCore::Feature::Container.enabled? && @object.content_type?('container') && helpers.life_cycle_items.dig(DataCycleCore.features.dig(:life_cycle, :idea_collection, :life_cycle_stage), :id) == life_cycle_params[:id] && !@object.children.where(template_name: DataCycleCore.features.dig(:life_cycle, :idea_collection, :life_cycle_stage)).exists?
         idea_collection_params = ActionController::Parameters.new({ datahash: { headline: @object.headline } }).permit!
-        idea_collection = DataCycleCore::DataHashService.create_internal_object(object_type, DataCycleCore.features.dig(:life_cycle, :idea_collection, :life_cycle_stage), idea_collection_params, current_user)
+        idea_collection = DataCycleCore::DataHashService.create_internal_object(controller_name, DataCycleCore.features.dig(:life_cycle, :idea_collection, :life_cycle_stage), idea_collection_params, current_user)
         idea_collection.is_part_of = @object.id unless @object.nil?
         idea_collection.save
       end
