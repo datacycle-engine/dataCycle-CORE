634487280f4513dbade89f4ef2c5ae214e5b2242
Manuel Mitterer <mitterer@pixelpoint.at>
2018-06-14 12:27:16 +0200

    updates for upload form

--- gulp/assets/javascripts/modules/helpers/callout_helpers.js
+++ gulp/assets/javascripts/modules/helpers/callout_helpers.js
@@ -1,7 +1,7 @@
 // Flash Callout Helpermethods
 module.exports = {
   show: function (text, type = '') {
-    let temp = $('<div data-text="' + text + '" class="flash callout ' + type + '" data-closable="" style="display: none;">' + text + '<button name="button" type="button" class="close-button" data-close="" aria-label="Dismiss alert"><span aria-hidden="true">×</span></button></div>').insertBefore('header').slideDown('fast');
+    let temp = $('<div data-text="' + text + '" class="flash flash-notification callout ' + type + '" data-closable="" style="display: none;">' + text + '<button name="button" type="button" class="close-button" data-close="" aria-label="Dismiss alert"><span aria-hidden="true">×</span></button></div>').insertBefore('header').slideDown('fast');
     setTimeout(() => {
       $(temp).slideUp('slow', function () {
         $(this).remove();
