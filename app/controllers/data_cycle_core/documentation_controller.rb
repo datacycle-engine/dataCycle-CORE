50ab863ddc4f2c54276f54a8ea81245be3ab2d69
Patrick Rainer <rainer@pixelpoint.at>
2018-07-24 10:41:33 +0200

    Fixed Brakeman warning for documentation controller

--- app/controllers/data_cycle_core/documentation_controller.rb
+++ app/controllers/data_cycle_core/documentation_controller.rb
@@ -10,7 +10,7 @@ module DataCycleCore
       markdown_path = view_paths.map(&:to_path).map { |p|
         (p.split('/')[0..-3] + ['docs']).join('/')
       }.map { |p|
-        File.join(p, params['path'] + '.md')
+        File.join(p, sanitized_path + '.md')
       }.select { |p|
         File.file?(p)
       }.first
@@ -18,6 +18,14 @@ module DataCycleCore
       markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
 
       markdown.render(File.read(markdown_path)) if markdown_path
+    end
+
+    def sanitized_path
+      sanitize(params['path']) if params.dig('path').present?
+    end
+
+    def sanitize(string)
+      ActionController::Base.helpers.sanitize(string)
     end
   end
 end
