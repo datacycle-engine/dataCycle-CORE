24a8f165c6ed88f49a5fa1051c7182b62568e7c8
Manuel Mitterer <mitterer@pixelpoint.at>
2018-07-25 15:45:00 +0200

    small js/css fixes

--- gulp/assets/javascripts/modules/helpers/quill_helpers.js
+++ gulp/assets/javascripts/modules/helpers/quill_helpers.js
@@ -2,7 +2,8 @@
 module.exports = {
   update_value: (editor) => {
     var hidden_field = $('#' + $(editor).attr('data-hidden-field-id'));
-    var text = $(editor).find('.ql-editor').html();
+    var text = ($(editor).find('.ql-editor').html() || '');
+    if (text == '<p><br></p>') text = '';
     var changed = (hidden_field.val() != text);
 
     if (changed) {
