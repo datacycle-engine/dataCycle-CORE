4b54f9a1076d2abfe7f6b9c6276006257acbed9b
Patrick Rainer <rainer@pixelpoint.at>
2018-08-01 17:37:23 +0200

    first implementation + data definition test cases

--- app/controllers/data_cycle_core/object_browser_controller.rb
+++ app/controllers/data_cycle_core/object_browser_controller.rb
@@ -11,15 +11,35 @@ module DataCycleCore
       I18n.with_locale(params[:locale] || I18n.locale) do
         @language = params.fetch(:locale, current_user.default_locale)
 
-        @definition = params.fetch(:definition, nil)
+        @definition = permitted_params.fetch(:definition, nil)
 
-        order_string = DataCycleCore::Filter::ObjectBrowserQueryBuilder.get_order_by_query_string(params[:search])
+        filter = DataCycleCore::StoredFilter.new
+        filter.language = @language
 
-        query = DataCycleCore::Filter::ObjectBrowserQueryBuilder.new(@language, @definition).in_validity_period
+        linked_table = @definition.fetch(:linked_table, nil)
+        template_name = @definition.fetch(:template_name, nil)
+        stored_filter = @definition.fetch(:stored_filter, nil)
+
+        if stored_filter.present?
+          stored_filter_params = stored_filter.to_a.map(&:to_h).map do |f|
+            f.each_with_object({}) do |(k, v), hash|
+              hash['t'] = k
+              hash['v'] = v
+            end
+          end
+          filter.parameters = stored_filter_params
+          query = filter.apply
+        else
+          query = filter.apply
+          query = query.where(content_data_type: data_cycle_object(linked_table).to_s) if data_cycle_object(linked_table)
+          query = query.where(data_type: template_name.to_s) if template_name
+        end
+
+        order_string = DataCycleCore::Filter::Search.get_order_by_query_string(params[:search])
+
+        query = query.in_validity_period
         query = query.fulltext_search(params[:search]) if params[:search].present?
         query = query.where('content_data_id NOT IN (?)', params[:excluded]) if params[:excluded].present?
-
-        # query = query.with_classification_alias_ids(get_classification_aliases_for_type(@type).map(&:id)) unless get_classification_aliases_for_type(@type).blank?
 
         query = query.classification_alias_ids([helpers.life_cycle_items&.dig(DataCycleCore.features&.dig(:life_cycle, :default_filter), :alias)&.id]) if DataCycleCore.features&.dig(:life_cycle, :default_filter).present? && params.dig(:definition, 'linked_table') == 'creative_works'
 
@@ -74,7 +94,21 @@ module DataCycleCore
       respond_to(:js)
     end
 
-    # private
+    def permitted_params
+      params.permit(*permitted_parameter_keys).reject { |_, v| v.blank? }
+    end
+
+    def permitted_parameter_keys
+      [:class, :per, :page, :id, :locale, :external, :ids, :search, { definition: {} }]
+    end
+
+    private
+
+    def data_cycle_object(object_string)
+      object_type = DataCycleCore.content_tables.find { |object| object == object_string }
+      ('DataCycleCore::' + object_type.singularize.classify).constantize
+    end
+
     #
     # def get_classification_aliases_for_type(type)
     #   if type == 'image'
