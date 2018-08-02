0a4e45df6418cad7a569687d1c445e6ff23bd193
Manuel Mitterer <mitterer@pixelpoint.at>
2018-08-02 14:37:51 +0200

    added loader for iframe overlay

--- gulp/assets/javascripts/modules/initializers/iframe_init.js
+++ gulp/assets/javascripts/modules/initializers/iframe_init.js
@@ -3,8 +3,6 @@ module.exports.initialize = function () {
 
   $(window).on('message onmessage', event => {
     if ($('#new_image_iframe:visible iframe, #new_video_iframe:visible iframe').length && event.originalEvent.data.action !== undefined && event.originalEvent.data.action == 'import') {
-      $('iframe:visible').closest('.reveal').foundation('close');
-
       var AUTH_TOKEN = $('meta[name=csrf-token]').attr('content');
       $.ajax({
         type: 'POST',
@@ -15,6 +13,8 @@ module.exports.initialize = function () {
           render_html: true
         }),
         contentType: 'application/json'
+      }).always(() => {
+        $('iframe:visible').closest('.reveal').foundation('close');
       });
     } else if ($('#content-upload-reveal').length && event.originalEvent.data.action !== undefined && event.originalEvent.data.action == 'open-upload-form') {
       $('#content-upload-reveal').foundation('open');
