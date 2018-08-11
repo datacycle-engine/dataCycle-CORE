b1cb307543edde8781f0296f1857501ace0bd402
Johannes Zlattinger <johannes@zlattinger.net>
2018-08-11 10:52:53 +0200

    fixed problem with missing paging parameters

--- app/controllers/data_cycle_core/api/v2/api_base_controller.rb
+++ app/controllers/data_cycle_core/api/v2/api_base_controller.rb
@@ -34,7 +34,7 @@ module DataCycleCore
         end
 
         def apply_paging(query)
-          page_params = permitted_params.fetch(:page, DEFAULT_PAGE_SETTINGS)
+          page_params = DEFAULT_PAGE_SETTINGS.merge(permitted_params[:page].to_h.symbolize_keys)
           query.page(page_params[:number].to_i).per(page_params[:size].to_i)
         end
 
