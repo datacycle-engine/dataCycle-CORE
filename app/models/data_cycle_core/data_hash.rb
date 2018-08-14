7a5c467b1df4635be717af1d025cd2a2e57ed583
Martin Oehzelt <oehzelt@pixelpoint.at>
2018-08-14 14:21:03 +0200

    remove transactions from data_hash, add appsignal gem

--- app/models/data_cycle_core/data_hash.rb
+++ app/models/data_cycle_core/data_hash.rb
@@ -57,85 +57,85 @@ module DataCycleCore
     def set_data_hash_attribute(key, value, current_user, save_time = Time.zone.now)
       key_hash = schema.dig('properties', key)
       return if key_hash.nil?
-      ActiveRecord::Base.transaction do
-        storage_cases_set(key, value, key_hash, save_time, current_user)
-      end
+      # ActiveRecord::Base.transaction do
+      storage_cases_set(key, value, key_hash, save_time, current_user)
+      # end
     end
 
     def to_history(save_time:, parent_id: nil, delete: false)
       origin_table = self.class.to_s.split('::')[1].tableize
       data_set_history = (self.class.to_s + '::History').safe_constantize.new
 
-      ActiveRecord::Base.transaction do
-        # cc self to history
-        data_set_history.send(origin_table.singularize.foreign_key + '=', id)
-        attributes.except('id', 'created_at', 'updated_at').each do |key, value|
-          data_set_history.send("#{key}=", value)
-        end
-        data_set_history.is_part_of = parent_id if data_set_history.respond_to?('is_part_of')
-
-        lower_bound = updated_at
-        lower_bound = save_time if lower_bound > save_time
-        data_set_history.history_valid = (lower_bound...save_time)
-        data_set_history.deleted_at = Time.zone.now.to_s(:long_usec) if delete
-        data_set_history.created_at = save_time
-        data_set_history.updated_at = save_time
-        data_set_history.save(touch: false)
-
-        # cc classification_content to history
-        classification_content.all.find_each do |item|
-          classification_history = DataCycleCore::ClassificationContent::History.new
-          classification_history.content_data_history_id = data_set_history.id
-          classification_history.content_data_history_type = data_set_history.class.to_s
-          item.attributes.except('id', 'content_data_id', 'content_data_type').each do |key, value|
-            classification_history.send("#{key}=", value)
-          end
-          classification_history.classification_id = item.classification_id
-          classification_history.save
-        end
-
-        # cc embedded data from other content tables
-        embedded_relations.each do |content_name|
-          content_relation = send(content_name[:name])
-          content_relation.each_with_index do |content_item, index|
-            new_content_history = content_item.to_history(save_time: save_time)
-            content_one_data = [new_content_history.id, new_content_history.class.to_s, '', nil]
-            content_two_data = [data_set_history.id, data_set_history.class.to_s, content_name[:name], index]
-            content_relation_history_data = ['a', 'b'].map { |selector|
-              [
-                "content_#{selector}_history_id".to_sym,
-                "content_#{selector}_history_type".to_sym,
-                "relation_#{selector}".to_sym,
-                "order_#{selector}".to_sym
-              ]
-            }.flatten
-              .zip(content_name[:table] < origin_table ? content_one_data + content_two_data : content_two_data + content_one_data).to_h
-            content_relation_history_data['history_valid'] = (content_item.updated_at...save_time)
-            DataCycleCore::ContentContent::History.create!(content_relation_history_data)
-          end
-        end
-
-        linked_relations.each do |content_name|
-          content_relation = send(content_name[:name])
-          content_relation.each_with_index do |content_item, index|
-            content_one_data = [content_item.id, content_item.class.to_s, '', nil]
-            content_two_data = [data_set_history.id, data_set_history.class.to_s, content_name[:name], index]
-            content_relation_history_data = ['a', 'b'].map { |selector|
-              [
-                "content_#{selector}_history_id".to_sym,
-                "content_#{selector}_history_type".to_sym,
-                "relation_#{selector}".to_sym,
-                "order_#{selector}".to_sym
-              ]
-            }.flatten
-              .zip(content_name[:table] < origin_table ? content_one_data + content_two_data : content_two_data + content_one_data).to_h
-            content_relation_history_data['history_valid'] = (content_item.updated_at...save_time)
-            DataCycleCore::ContentContent::History.create!(content_relation_history_data)
-          end
-        end
-
-        data_set_history.save
+      # ActiveRecord::Base.transaction do
+      # cc self to history
+      data_set_history.send(origin_table.singularize.foreign_key + '=', id)
+      attributes.except('id', 'created_at', 'updated_at').each do |key, value|
+        data_set_history.send("#{key}=", value)
       end
+      data_set_history.is_part_of = parent_id if data_set_history.respond_to?('is_part_of')
+
+      lower_bound = updated_at
+      lower_bound = save_time if lower_bound > save_time
+      data_set_history.history_valid = (lower_bound...save_time)
+      data_set_history.deleted_at = Time.zone.now.to_s(:long_usec) if delete
+      data_set_history.created_at = save_time
+      data_set_history.updated_at = save_time
+      data_set_history.save(touch: false)
+
+      # cc classification_content to history
+      classification_content.all.find_each do |item|
+        classification_history = DataCycleCore::ClassificationContent::History.new
+        classification_history.content_data_history_id = data_set_history.id
+        classification_history.content_data_history_type = data_set_history.class.to_s
+        item.attributes.except('id', 'content_data_id', 'content_data_type').each do |key, value|
+          classification_history.send("#{key}=", value)
+        end
+        classification_history.classification_id = item.classification_id
+        classification_history.save
+      end
+
+      # cc embedded data from other content tables
+      embedded_relations.each do |content_name|
+        content_relation = send(content_name[:name])
+        content_relation.each_with_index do |content_item, index|
+          new_content_history = content_item.to_history(save_time: save_time)
+          content_one_data = [new_content_history.id, new_content_history.class.to_s, '', nil]
+          content_two_data = [data_set_history.id, data_set_history.class.to_s, content_name[:name], index]
+          content_relation_history_data = ['a', 'b'].map { |selector|
+            [
+              "content_#{selector}_history_id".to_sym,
+              "content_#{selector}_history_type".to_sym,
+              "relation_#{selector}".to_sym,
+              "order_#{selector}".to_sym
+            ]
+          }.flatten
+            .zip(content_name[:table] < origin_table ? content_one_data + content_two_data : content_two_data + content_one_data).to_h
+          content_relation_history_data['history_valid'] = (content_item.updated_at...save_time)
+          DataCycleCore::ContentContent::History.create!(content_relation_history_data)
+        end
+      end
+
+      linked_relations.each do |content_name|
+        content_relation = send(content_name[:name])
+        content_relation.each_with_index do |content_item, index|
+          content_one_data = [content_item.id, content_item.class.to_s, '', nil]
+          content_two_data = [data_set_history.id, data_set_history.class.to_s, content_name[:name], index]
+          content_relation_history_data = ['a', 'b'].map { |selector|
+            [
+              "content_#{selector}_history_id".to_sym,
+              "content_#{selector}_history_type".to_sym,
+              "relation_#{selector}".to_sym,
+              "order_#{selector}".to_sym
+            ]
+          }.flatten
+            .zip(content_name[:table] < origin_table ? content_one_data + content_two_data : content_two_data + content_one_data).to_h
+          content_relation_history_data['history_valid'] = (content_item.updated_at...save_time)
+          DataCycleCore::ContentContent::History.create!(content_relation_history_data)
+        end
+      end
+
+      data_set_history.save
+      # end
       data_set_history
     end
 
