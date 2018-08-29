5fffb6d1da98d47c89b58195bf361641d1eb6e1e
Manuel Mitterer <mitterer@pixelpoint.at>
2018-08-29 10:09:33 +0200

    added filter for creator

--- app/models/data_cycle_core/filter/search.rb
+++ app/models/data_cycle_core/filter/search.rb
@@ -59,6 +59,28 @@ module DataCycleCore
         reflect(@query.where(search[:content_data_id].in(query)))
       end
 
+      def creator(ids = nil)
+        return self if ids.blank?
+
+        joined_creators = content_content.alias('joined_creators')
+        join_query = search.join(joined_creators)
+          .on(search[:content_data_id].eq(joined_creators[:content_a_id]).and(search[:content_data_type].eq(quoted(joined_creators[:content_a_type]))).and(joined_creators[:relation_a].eq('creator'))).join_sources
+
+        @query = @query.joins(join_query)
+          .where(joined_creators[:content_b_id].in(ids))
+
+        # query = Arel::SelectManager.new
+        #   .project(search[:content_data_id])
+        #   .from(search)
+        #   .join(content_content)
+        #   .on(search[:content_data_id].eq(content_content[:content_a_id]).and(search[:content_data_type].eq(quoted(content_content[:content_a_type]))).and(content_content[:relation_a].eq('creator')))
+        #   .where(content_content[:content_b_id].in(ids))
+
+        # reflect(@query.where(search[:content_data_id].in(query)))
+
+        reflect(@query)
+      end
+
       def watch_list_id(id = nil)
         manager = get_watch_list_items(id)
 
