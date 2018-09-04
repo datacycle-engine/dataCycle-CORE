914979d6c04bb43d60ddf6cbb8fec806c6d32b88
Patrick Rainer <rainer@pixelpoint.at>
2018-09-04 14:57:40 +0200

    fixed Downloader with multiple Mongoid:Documents

--- app/models/data_cycle_core/generic/collection.rb
+++ app/models/data_cycle_core/generic/collection.rb
@@ -2,7 +2,23 @@
 
 module DataCycleCore
   module Generic
-    class Collection < CollectionBase
+    class Collection
+      include Mongoid::Document
+
+      attr_accessor :data_has_changed
+
+      store_in collection: 'name'
+
+      field :external_id,  type: String
+      field :dump,         type: Hash
+      field :seen_at,      type: DateTime
+      include Mongoid::Timestamps
+
+      before_save ->(document) { document.seen_at = Time.zone.now }
+
+      def set_updated_at
+        super if data_has_changed
+      end
     end
   end
 end
