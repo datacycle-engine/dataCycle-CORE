c339dab0d8e956138592abe9c41538018cd4c802
Manuel Mitterer <mitterer@pixelpoint.at>
2018-09-05 12:05:48 +0200

    fixed merge conflict

--- db/migrate/20180808141924_add_indexes_search.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddIndexesSearch < ActiveRecord::Migration[5.1]
-  def change
-    add_index :searches, :locale
-    add_index :searches, :content_data_id
-    add_index :searches, [:locale, :content_data_id]
-  end
-end
