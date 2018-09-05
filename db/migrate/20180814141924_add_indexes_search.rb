ac02a51ae5c32ee4499a21bb3d6b9b008eafaaa9
Manuel Mitterer <mitterer@pixelpoint.at>
2018-09-05 15:46:02 +0200

    [Rubocop] removed space

--- db/migrate/20180814141924_add_indexes_search.rb
+++ db/migrate/20180814141924_add_indexes_search.rb
@@ -2,7 +2,7 @@
 
 class AddIndexesSearch < ActiveRecord::Migration[5.1]
   def change
-    add_index :searches, :locale unless index_exists?(:searches, :locale )
+    add_index :searches, :locale unless index_exists?(:searches, :locale)
     add_index :searches, :content_data_id unless index_exists?(:searches, :content_data_id)
     add_index :searches, [:locale, :content_data_id] unless index_exists?(:searches, [:locale, :content_data_id])
   end
