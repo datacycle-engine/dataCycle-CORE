3bd54b7d18e738575fe7044363d26b1bb65f1254
Manuel Mitterer <mitterer@pixelpoint.at>
2018-09-06 15:36:27 +0200

    updated migrations to check for existing indizes

--- db/migrate/20180814141924_add_indexes_search.rb
+++ db/migrate/20180814141924_add_indexes_search.rb
@@ -2,8 +2,8 @@
 
 class AddIndexesSearch < ActiveRecord::Migration[5.1]
   def change
-    add_index :searches, :locale
-    add_index :searches, :content_data_id
-    add_index :searches, [:locale, :content_data_id]
+    add_index :searches, :locale unless index_exists?(:searches, :locale )
+    add_index :searches, :content_data_id unless index_exists?(:searches, :content_data_id)
+    add_index :searches, [:locale, :content_data_id] unless index_exists?(:searches, [:locale, :content_data_id])
   end
 end
