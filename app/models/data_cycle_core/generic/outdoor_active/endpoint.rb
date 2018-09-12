630e251c57228b1bb09966a7b30c87c856fde17b
Johannes Zlattinger <johannes@zlattinger.net>
2018-09-12 17:13:33 +0200

    updated language check for external source endpoints

--- app/models/data_cycle_core/generic/outdoor_active/endpoint.rb
+++ app/models/data_cycle_core/generic/outdoor_active/endpoint.rb
@@ -79,7 +79,7 @@ module DataCycleCore
           Enumerator.new do |yielder|
             load_data(['pois'], lang)['data'].each do |poi_id_container|
               raw_data = load_data(['oois', poi_id_container['id']], lang)['poi'][0]
-              yielder << raw_data if raw_data.dig('meta', 'translation').include?(lang)
+              yielder << raw_data if raw_data.dig('meta', 'translation').include?(lang.to_s)
             end
           end
         end
@@ -88,7 +88,7 @@ module DataCycleCore
           Enumerator.new do |yielder|
             load_data(['tours'], lang)['data'].each do |tour_id_container|
               raw_data = load_data(['oois', tour_id_container['id']], lang)['tour'][0]
-              yielder << raw_data if raw_data.dig('meta', 'translation').include?(lang)
+              yielder << raw_data if raw_data.dig('meta', 'translation').include?(lang.to_s)
             end
           end
         end
