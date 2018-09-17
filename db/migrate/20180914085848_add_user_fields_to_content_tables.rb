cbb60dbee77dd95cea814f5fb628a6b5e46a44e4
Martin Oehzelt <oehzelt@pixelpoint.at>
2018-09-17 10:30:04 +0200

    WIP

---
+++ db/migrate/20180914085848_add_user_fields_to_content_tables.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class AddUserFieldsToContentTables < ActiveRecord::Migration[5.1]
+  def change
+    DataCycleCore.content_tables.each do |content_table|
+      add_column content_table.to_sym, :created_by, :uuid
+      add_column content_table.to_sym, :updated_by, :uuid
+      add_column content_table.to_sym, :deleted_by, :uuid
+      add_column content_table.to_sym, :deleted_at, :datetime
+      add_column (content_table.singularize + '_histories').to_sym, :created_by, :uuid
+      add_column (content_table.singularize + '_histories').to_sym, :updated_by, :uuid
+      add_column (content_table.singularize + '_histories').to_sym, :deleted_by, :uuid
+      add_column (content_table.singularize + '_histories').to_sym, :deleted_at, :datetime
+    end
+  end
+end
