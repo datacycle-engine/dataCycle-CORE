02ac23f6c646780b72f0a5494ea4c40aa581f4f7
Manuel Mitterer <mitterer@pixelpoint.at>
2018-10-11 08:54:31 +0200

    Revert "added integration tests + fixes"
    This reverts commit 6843aa899cd12702f5ca05740ccdd614c7213f94.

--- cypress/integration/1_basic/1_general_spec.js
+++ cypress/integration/1_basic/1_general_spec.js
@@ -1,24 +1,24 @@
 describe('General', function () {
 
-  // it('login', function () {
-  //   cy.fixture('login_users').as('usersJSON').then(() => {
-  //     cy.visit('/users/sign_in')
-  //     const user = this.usersJSON['admin']
+  it('login', function () {
+    cy.fixture('login_users').as('usersJSON').then(() => {
+      cy.visit('/users/sign_in')
+      const user = this.usersJSON['admin']
 
-  //     cy.get('input#user_email:visible').type(user.email)
-  //     cy.get('input#user_password:visible').type(user.password + '{enter}')
+      cy.get('input#user_email:visible').type(user.email)
+      cy.get('input#user_password:visible').type(user.password + '{enter}')
 
-  //     cy.location('pathname').should('eq', '/')
-  //     cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
-  //   })
-  // })
+      cy.location('pathname').should('eq', '/')
+      cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
+    })
+  })
 
-  // it('logout', function () {
-  //   cy.login('admin')
+  it('logout', function () {
+    cy.login('admin')
 
-  //   cy.get('.show-sidebar').click()
-  //   cy.get('.logout-link').click()
-  //   cy.location('pathname').should('match', /\/users\/sign_in/)
-  // })
+    cy.get('.show-sidebar').click()
+    cy.get('.logout-link').click()
+    cy.location('pathname').should('match', /\/users\/sign_in/)
+  })
 
 })
