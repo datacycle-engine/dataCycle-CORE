944634daeec826c9546e62c3285761f3b3b570fa
Martin Oehzelt <oehzelt@pixelpoint.at>
2018-10-24 10:34:16 +0200

    fix/introduce acts_as_tree for history entries

--- app/models/data_cycle_core/content/content_relations.rb
+++ app/models/data_cycle_core/content/content_relations.rb
@@ -10,6 +10,9 @@ module DataCycleCore
           table_given = options[:table_name]
           postfix = options[:postfix]
 
+          self_class = "DataCycleCore::#{table_given.classify}"
+          self_class += "::#{postfix.capitalize}" unless postfix.nil?
+
           classification_content_table = 'classification_content'
           classification_content_table += "_#{postfix}" unless postfix.nil?
           class_name = 'DataCycleCore::ClassificationContent'
@@ -53,8 +56,8 @@ module DataCycleCore
           belongs_to :updated_by_user, foreign_key: :updated_by, class_name: 'DataCycleCore::User'
           belongs_to :deleted_by_user, foreign_key: :deleted_by, class_name: 'DataCycleCore::User'
 
-          belongs_to :parent, class_name: 'DataCycleCore::Thing', foreign_key: 'is_part_of', inverse_of: :children, touch: false
-          has_many :children, class_name: 'DataCycleCore::Thing', foreign_key: 'is_part_of', inverse_of: :parent, dependent: :destroy
+          belongs_to :parent, class_name: self_class, foreign_key: 'is_part_of', inverse_of: :children, touch: false
+          has_many :children, class_name: self_class, foreign_key: 'is_part_of', inverse_of: :parent, dependent: :destroy
 
           has_many :watch_list_data_hashes, as: :hashable, dependent: :destroy
           has_many :watch_lists, through: :watch_list_data_hashes
