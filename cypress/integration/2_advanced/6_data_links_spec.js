8d6ced1a7041a3381440fe39c1fa61aac682584d
Martin Oehzelt <oehzelt@pixelpoint.at>
2018-10-25 08:54:04 +0200

    cypress string replace creative_work, thing

--- cypress/integration/2_advanced/6_data_links_spec.js
+++ cypress/integration/2_advanced/6_data_links_spec.js
@@ -13,7 +13,7 @@ describe('DataLink', function () {
     cy.createCreativeWork(cname, option).then(resp => {
       var url = resp.headers.location.replace('/edit', '')
       cy.visit(url).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
-      cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit').then((path) => {
+      cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit').then((path) => {
         id = path.substring(path.lastIndexOf('/') + 1)
       })
 
@@ -24,7 +24,7 @@ describe('DataLink', function () {
       cy.get('#data-link-overlay-new #data_link_receiver_family_name').should('be.visible').type('Test')
       cy.get('#data-link-overlay-new #data_link_permissions_write').should('be.visible').check()
       cy.get('#data-link-overlay-new [type="submit"]').should('be.visible').click()
-      cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+      cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
       cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
 
       cy.get('.detail-header-functions [data-toggle="send-link"]').click()
@@ -35,7 +35,7 @@ describe('DataLink', function () {
         cy.visit(url).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
         cy.get('.headline input[type=text]').should('be.visible').should('have.value', cname).clear().type(updated_name)
         cy.get('.submit-edit-form').click()
-        cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+        cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
         cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
       })
     })
@@ -63,14 +63,14 @@ describe('DataLink', function () {
   // })
 
   it('lock link', function () {
-    cy.visit('/creative_works/' + id).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
-    cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+    cy.visit('/things/' + id).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
+    cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
 
     cy.get('.detail-header-functions [data-toggle="send-link"]').click()
     cy.get('#send-link').should('be.visible').find('li:contains("' + email + '")').should('have.length', 1).then(function ($elem) {
       const url = '/data_links/' + $elem.prop('id').replace('data-link-', '')
       $elem.find('.invalidate-data-link').click()
-      cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+      cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
       cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
 
       cy.logout()
@@ -84,8 +84,8 @@ describe('DataLink', function () {
   })
 
   it('unlock link', function () {
-    cy.visit('/creative_works/' + id).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
-    cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+    cy.visit('/things/' + id).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
+    cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
 
     cy.get('.detail-header-functions [data-toggle="send-link"]').click()
     cy.get('#send-link').should('be.visible').find('li:contains("' + email + '")').should('have.length', 1).then(function ($elem) {
@@ -96,7 +96,7 @@ describe('DataLink', function () {
       cy.get('#' + overlay_id + ' input[value="write"]').should('be.visible').check()
       cy.get('#' + overlay_id + ' [name="data_link[valid_until]"]').should('have.length', 1).siblings('.flatpickr-input').clear().type(new Date(Date.now() + 24 * 60 * 60 * 1000).toLocaleDateString('de-DE'))
       cy.get('#' + overlay_id + ' [type="submit"]').should('be.visible').click()
-      cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+      cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
       cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
 
       cy.logout()
--- cypress/integration/2_advanced/8_embedded_object_spec.js
+++ cypress/integration/2_advanced/8_embedded_object_spec.js
@@ -19,28 +19,28 @@ describe('Embedded Object', function () {
   it('add Quote', function () {
     cy.createCreativeWork(cname, option).then(resp => {
       var url = resp.headers.location
-      id = url.substr(url.indexOf('creative_works/') + 15, 36)
+      id = url.substr(url.indexOf('things/') + 15, 36)
       cy.visit(url).get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
 
-      cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
+      cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
 
-      cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.content-object-item#creative_work_datahash_quotation_item_0').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat_headline)
+      cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.content-object-item#thing_datahash_quotation_item_0').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat_headline)
 
       cy.get('.submit-edit-form').should('be.visible').click()
-      cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+      cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
       cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
       cy.get('.detail-content-wrapper').contains(zitat_headline).should('have.length', 1)
     })
   })
 
   it('update Quote', function () {
-    cy.visit('/creative_works/' + id + '/edit').get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
-    cy.location('pathname').should('match', /\/creative_works\/.*\/edit/)
+    cy.visit('/things/' + id + '/edit').get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
+    cy.location('pathname').should('match', /\/things\/.*\/edit/)
 
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.content-object-item#creative_work_datahash_quotation_item_0').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').clear().type(updated_zitat_headline)
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.content-object-item#thing_datahash_quotation_item_0').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').clear().type(updated_zitat_headline)
 
     cy.get('.submit-edit-form').should('be.visible').click()
-    cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+    cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
     cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
     cy.get('.detail-content-wrapper').contains(updated_zitat_headline).should('have.length', 1)
   })
@@ -70,30 +70,30 @@ describe('Embedded Object', function () {
 */
 
   it('remove Quote', function () {
-    cy.visit('/creative_works/' + id + '/edit').get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
-    cy.location('pathname').should('match', /\/creative_works\/.*\/edit/)
+    cy.visit('/things/' + id + '/edit').get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
+    cy.location('pathname').should('match', /\/things\/.*\/edit/)
 
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.content-object-item').should('be.visible').find('.button.removeContentObject').should('be.visible').click()
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.content-object-item').should('be.visible').find('.button.removeContentObject').should('be.visible').click()
 
     cy.get('.submit-edit-form').should('be.visible').click()
-    cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+    cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
     cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
     cy.get('.detail-content-wrapper').contains(updated_zitat_headline).should('have.length', 0)
   })
 
   it('add multiple Quotes', function () {
-    cy.visit('/creative_works/' + id + '/edit').get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
-    cy.location('pathname').should('match', /\/creative_works\/.*\/edit/)
+    cy.visit('/things/' + id + '/edit').get('.flash.callout .close-button').should('be.visible').click().should('be.hidden')
+    cy.location('pathname').should('match', /\/things\/.*\/edit/)
 
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.content-object-item#creative_work_datahash_quotation_item_0').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat1_headline)
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.content-object-item#creative_work_datahash_quotation_item_1').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat2_headline)
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
-    cy.get('.embedded-object[data-key="creative_work[datahash][quotation]"]').should('be.visible').find('.content-object-item#creative_work_datahash_quotation_item_2').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat3_headline)
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.content-object-item#thing_datahash_quotation_item_0').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat1_headline)
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.content-object-item#thing_datahash_quotation_item_1').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat2_headline)
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.button.addContentObject').should('be.visible').click()
+    cy.get('.embedded-object[data-key="thing[datahash][quotation]"]').should('be.visible').find('.content-object-item#thing_datahash_quotation_item_2').should('be.visible').find('.quill-editor .ql-editor').should('be.visible').type(zitat3_headline)
 
     cy.get('.submit-edit-form').should('be.visible').click()
-    cy.location('pathname').should('match', /\/creative_works/).should('not.contain', '/edit')
+    cy.location('pathname').should('match', /\/things/).should('not.contain', '/edit')
     cy.get('.flash.callout').should('be.visible').should('have.class', 'success').find('.close-button').click().should('be.hidden')
 
     cy.get('.detail-content-wrapper').contains(zitat1_headline).should('have.length', 1)
