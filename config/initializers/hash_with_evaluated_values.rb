624e3f8ee36278e58ce2408cb0b1758fe7a31d63
Johannes Zlattinger <johannes@zlattinger.net>
2018-10-25 20:53:26 +0200

    introduced possiblity to add values to hashes which are evaluated at runtime (this is intended to be used for configuration files only!)

---
+++ config/initializers/hash_with_evaluated_values.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+Hash.class_eval do
+  # !!ATTENTION!!
+  # This method is intendend to be used only for configuration files provided by developers. It is not intended to be
+  # used for values provided by end users.
+  def with_evaluated_values
+    map { |key, value|
+      if value.is_a?(Hash)
+        [key, value.with_evaluated_values]
+      elsif value.is_a?(String) && /{{.*}}/.match?(value)
+        [key, eval(value[2..-3])] # rubocop:disable Security/Eval
+      else
+        [key, value]
+      end
+    }.to_h
+  end
+end
---
+++ test/models/core_extensions/hash_with_evaluated_values_test.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+require 'minitest/spec'
+require 'minitest/autorun'
+
+describe Hash do
+  describe '#with_evaluated_values' do
+    describe 'without evaluation markers' do
+      it 'should return hash unmodified' do
+        hash = {
+          one: 1,
+          two: 2,
+          three: {
+            alpha: 'α',
+            beta: 'β'
+          }
+        }
+
+        hash.with_evaluated_values.dig(:one).must_equal 1
+        hash.with_evaluated_values.dig(:two).must_equal 2
+        hash.with_evaluated_values.dig(:three, :alpha).must_equal 'α'
+        hash.with_evaluated_values.dig(:three, :beta).must_equal 'β'
+      end
+    end
+
+    describe 'with evaluation markers' do
+      it 'should return evaluated value' do
+        hash = {
+          one: 1,
+          two: '{{ 1 + 1 }}'
+        }
+
+        hash.with_evaluated_values.dig(:one).must_equal 1
+        hash.with_evaluated_values.dig(:two).must_equal 2
+      end
+
+      it 'should return evaluated value for nested hashes' do
+        hash = {
+          one: 1,
+          two: 2,
+          three: {
+            value: '{{ Math.sqrt(9) }}'
+          }
+        }
+
+        hash.with_evaluated_values.dig(:one).must_equal 1
+        hash.with_evaluated_values.dig(:two).must_equal 2
+        hash.with_evaluated_values.dig(:three, :value).must_equal 3
+      end
+
+      it 'should return date based calculations' do
+        hash = {
+          value: '{{ 3.days.ago.to_date }}'
+        }
+
+        hash.with_evaluated_values.dig(:value).must_equal Time.zone.today - 3.days
+      end
+    end
+  end
+end
