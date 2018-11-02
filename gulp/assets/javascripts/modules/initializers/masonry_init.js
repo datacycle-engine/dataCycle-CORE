48c57120028a15015d07013e09edc48f0d3c34cd
Manuel Mitterer <mitterer@pixelpoint.at>
2018-11-02 13:26:13 +0100

    removed animations for masonry tiles

--- gulp/assets/javascripts/modules/initializers/masonry_init.js
+++ gulp/assets/javascripts/modules/initializers/masonry_init.js
@@ -3,20 +3,8 @@ var masonry = require('masonry-layout');
 // Masonry Config
 module.exports.initialize = function () {
 
-  let animation_complete = false;
-
-  let show_animated = function (index, grid) {
-    setTimeout(() => {
-      if (index == 0) $('.grid .grid-loading').removeClass("show");
-      $('.grid-item').eq(index).addClass("show");
-      if ($('.grid-item').eq(index + 1).length) show_animated(index + 1, grid);
-      else animation_complete = true;
-      grid.layout();
-    }, 10);
-  };
-
-  let init = function () {
-    if ($('.grid').html() != undefined) {
+  function init(element = document) {
+    if ($(element).find('.grid').length) {
       var grid = new masonry('.grid', {
         itemSelector: '.grid-item',
         columnWidth: '.grid-sizer',
@@ -25,13 +13,14 @@ module.exports.initialize = function () {
         transitionDuration: 0
       });
 
-      show_animated(0, grid);
+      $(element).find('.grid .grid-loading').removeClass("show");
+      $(element).find('.grid-item').addClass("show");
 
-      $(document).on('lazyloaded', function () {
-        if (animation_complete) grid.layout();
+      $(window).on('load lazyloaded', function () {
+        grid.layout();
       });
     }
-  }
+  };
 
   init();
 
