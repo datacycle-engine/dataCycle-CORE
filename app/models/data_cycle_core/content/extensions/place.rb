a645423d47056cea0b052b670c02b8c02ac089af
Manuel Mitterer <mitterer@pixelpoint.at>
2018-11-05 08:09:10 +0100

    removed geo coordinates from object browser tile for places

--- app/models/data_cycle_core/content/extensions/place.rb
+++ app/models/data_cycle_core/content/extensions/place.rb
@@ -19,10 +19,6 @@ module DataCycleCore
           (try(:address)&.postal_code.present? || try(:address)&.address_locality.present? ? "#{address.postal_code} #{address.address_locality}<br>" : '') + (try(:address)&.street_address.present? ? address.street_address&.gsub(', ', '<br>') : '')
         end
 
-        def coordinates
-          "#{latitude}, #{longitude}" if latitude.present? && longitude.present?
-        end
-
         def new_content_fields
           ['name']
         end
