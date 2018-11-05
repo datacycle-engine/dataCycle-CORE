90d9e8485e3fa0ca001f6f0374a597a227bdedf2
Patrick Rainer <rainer@pixelpoint.at>
2018-11-05 11:10:25 +0100

    fixed media import test

--- test/models/generic/data_cycle_media_test.rb
+++ test/models/generic/data_cycle_media_test.rb
@@ -9,11 +9,15 @@ module DataCycleCore
         @asset_temp = DataCycleCore::Asset.count
       end
 
-      test 'see what happens' do
+      test 'import dummy assets' do
         external_source = DataCycleCore::ExternalSource.find_by(name: 'DataCycle - Media')
         external_source.import
 
-        assert_equal(3, DataCycleCore::Asset.count)
+        assert_equal(7, DataCycleCore::Asset.count)
+        assert_equal(1, DataCycleCore::Audio.count)
+        assert_equal(4, DataCycleCore::Image.count)
+        assert_equal(1, DataCycleCore::Pdf.count)
+        assert_equal(1, DataCycleCore::Video.count)
       end
     end
   end
