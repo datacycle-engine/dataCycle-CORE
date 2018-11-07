9bfd562c36b7d96491e7f254f8c33958193bea5d
Johannes Zlattinger <johannes@zlattinger.net>
2018-11-07 19:33:42 +0100

    fixed a problem regarding outdoor active import

--- app/models/data_cycle_core/generic/common/functions.rb
+++ app/models/data_cycle_core/generic/common/functions.rb
@@ -116,7 +116,9 @@ module DataCycleCore
 
         def self.add_links(data_hash, attribute, content_type, external_source_id, key_function, condition_function = nil)
           return data_hash if condition_function.present? && !condition_function.call(data_hash)
-          key_function_values = key_function.call(data_hash)
+
+          key_function_values = key_function.call(data_hash) || []
+
           data_hash.merge(
             {
               attribute =>
