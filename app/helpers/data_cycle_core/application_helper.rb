a39596ec703ced70b63c8aa0569c8ee8e31df1e1
Martin Oehzelt <oehzelt@pixelpoint.at>
2018-11-08 12:00:25 +0100

    fix application helper

--- app/helpers/data_cycle_core/application_helper.rb
+++ app/helpers/data_cycle_core/application_helper.rb
@@ -81,7 +81,7 @@ module DataCycleCore
       raise "try to render content_partial that is not a thing: #{partial} || #{parameters}" unless ['thing', 'thing_history'].include?(parameters[:content].class.class_name.underscore)
       content_parameter = parameters[:content].schema['schema_type'].underscore
       partials = [
-        "#{content_parameter}_#{parameters[:content].template_name.parameterize(separator: '_')}_#{partial}",
+        "#{parameters[:content].template_name.parameterize(separator: '_')}_#{partial}",
         "#{content_parameter}_#{partial}",
         "content_#{partial}"
       ]
