f4b70ce2b89bc1e409756a4a3abb3fc845677830
Johannes Zlattinger <johannes@zlattinger.net>
2018-11-28 08:45:35 +0100

    introduced basic schema export as xlsx-file

--- app/controllers/data_cycle_core/schema_controller.rb
+++ app/controllers/data_cycle_core/schema_controller.rb
@@ -3,6 +3,10 @@
 module DataCycleCore
   class SchemaController < ApplicationController
     def index
+      respond_to do |format|
+        format.xlsx
+        format.any
+      end
     end
   end
 end
