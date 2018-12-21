7ba40746e5da6f14aba24c905ab36c5852c8f82b
Manuel Mitterer <mitterer@pixelpoint.at>
2018-12-21 13:27:15 +0100

    applied translation_missing warning to all contents

--- app/models/data_cycle_core/warning/common.rb
+++ app/models/data_cycle_core/warning/common.rb
@@ -7,6 +7,10 @@ module DataCycleCore
         def invalid(_params, content, _context)
           !content.try(:is_valid?)
         end
+
+        def translation_missing(_params, content, _context)
+          content.translated_locales.exclude?(I18n.locale)
+        end
       end
     end
   end
