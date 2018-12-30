2ff62338f2ba211d4e7dbfe1c818f6cd1e079199
Johannes Zlattinger <johannes@zlattinger.net>
2018-12-30 09:10:39 +0100

    added separate file for mailer css styles

--- lib/data_cycle_core/css_inliner.rb
+++ lib/data_cycle_core/css_inliner.rb
@@ -5,7 +5,9 @@ module DataCycleCore
     def self.delivering_email(message)
       raise 'NotImplement' if message.multipart?
 
-      message.body = Premailer.new(message.decoded, with_html_string: true).to_inline_css
+      message.body = Premailer.new(message.decoded, with_html_string: true,
+                                                    css_to_attributes: true,
+                                                    preserve_styles: true).to_inline_css
     end
   end
 end
