209058efac2f5418ec0806e76fbcac63622524c5
Johannes Zlattinger <johannes@zlattinger.net>
2019-01-10 16:13:24 +0100

    implemented location download for google business api

--- app/models/data_cycle_core/generic/google_business/endpoint.rb
+++ app/models/data_cycle_core/generic/google_business/endpoint.rb
@@ -22,12 +22,12 @@ module DataCycleCore
           @access_token
         end
 
-        def accounts
+        def accounts(lang: nil)
           Enumerator.new do |yielder|
             next_page_token = nil
 
             loop do
-              data = load_accounts(next_page_token: next_page_token)
+              data = load_accounts(next_page_token: next_page_token, lang: lang)
 
               (data['accounts'] || []).each do |account_data|
                 yielder << account_data
@@ -40,16 +40,16 @@ module DataCycleCore
           end
         end
 
-        def locations
+        def locations(lang: :de)
           Enumerator.new do |yielder|
             accounts.each do |account|
               next_page_token = nil
 
               loop do
-                data = load_locations(account['name'], next_page_token: next_page_token)
+                data = load_locations(account['name'], next_page_token: next_page_token, lang: lang)
 
                 (data['locations'] || []).each do |location_data|
-                  yielder << location_data
+                  yielder << location_data if location_data['languageCode'] == lang.to_s
                 end
 
                 next_page_token = data['nextPageToken']
@@ -62,18 +62,19 @@ module DataCycleCore
 
         private
 
-        def load_accounts(next_page_token:)
-          load_data('https://mybusiness.googleapis.com/v4/accounts', next_page_token: next_page_token)
+        def load_accounts(next_page_token:, lang:) # rubocop:disable Lint/UnusedMethodArgument
+          load_data('https://mybusiness.googleapis.com/v4/accounts', next_page_token: next_page_token, lang: nil)
         end
 
-        def load_locations(account_path, next_page_token:)
-          load_data("https://mybusiness.googleapis.com/v4/#{account_path}/locations", next_page_token: next_page_token)
+        def load_locations(account_path, next_page_token:, lang:)
+          load_data("https://mybusiness.googleapis.com/v4/#{account_path}/locations", next_page_token: next_page_token, lang: lang)
         end
 
-        def load_data(url, next_page_token:)
+        def load_data(url, next_page_token:, lang:)
           response = Faraday.new(url: url).get do |req|
             req.headers['Authorization'] = [access_token.type, access_token.token].join(' ')
             req.params['pageToken'] = next_page_token if next_page_token.present?
+            req.params['languageCode'] = lang if lang.present?
           end
 
           JSON.parse(response.body)
