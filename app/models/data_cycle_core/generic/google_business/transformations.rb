ee7b230b696d5b0a24ac899eb4fb53b3ebb9a896
Johannes Zlattinger <johannes@zlattinger.net>
2019-01-16 08:45:02 +0100

    added categories to places imported via google business api

--- app/models/data_cycle_core/generic/google_business/transformations.rb
+++ app/models/data_cycle_core/generic/google_business/transformations.rb
@@ -8,7 +8,7 @@ module DataCycleCore
           DataCycleCore::Generic::GoogleBusiness::TransformationFunctions[*args]
         end
 
-        def self.location_to_place(_external_source_id)
+        def self.location_to_place(external_source_id)
           t(:stringify_keys)
           .>> t(:rename_keys, 'name' => 'external_key')
           .>> t(:rename_keys, 'locationName' => 'name')
@@ -22,7 +22,12 @@ module DataCycleCore
           .>> t(:add_field, 'postal_code', ->(s) { s.dig('address', 'postalCode') })
           .>> t(:add_field, 'address_country', ->(s) { s.dig('address', 'regionCode') })
           .>> t(:nest, 'address', ['street_address', 'address_locality', 'address_country', 'postal_code'])
-          .>> t(:reject_all_keys, except: ['external_key', 'name', 'location', 'address', 'contact_info'])
+          .>> t(:add_links, 'google_business_primary_category', DataCycleCore::Classification, external_source_id,
+                ->(s) { s.dig('primaryCategory', 'categoryId') || [] })
+          .>> t(:add_links, 'google_business_additional_categories', DataCycleCore::Classification, external_source_id,
+                ->(s) { (s['additionalCategories'] || []).map { |c| c['categoryId'] } })
+          .>> t(:reject_all_keys, except: ['external_key', 'name', 'location', 'address', 'contact_info',
+                                           'google_business_primary_category', 'google_business_additional_categories'])
           .>> t(:strip_all)
         end
       end
