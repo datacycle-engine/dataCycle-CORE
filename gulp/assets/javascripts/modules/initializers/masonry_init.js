72bffb975ec67f85d10107ffe49d522275c75942
Manuel Mitterer <mitterer@pixelpoint.at>
2019-04-09 15:30:48 +0200

    Updated Grid Layout without Masonry Library

--- gulp/assets/javascripts/modules/initializers/masonry_init.js
+++ gulp/assets/javascripts/modules/initializers/masonry_init.js
@@ -1,33 +1,73 @@
-var masonry = require('masonry-layout');
+// var masonry = require('masonry-layout');
 
 // Masonry Config
 module.exports.initialize = function() {
+  var el = document.createElement('div');
+  var supports_grid = typeof el.style.grid === 'string';
+  if (!supports_grid) {
+    $('body').append(
+      '<div class="html-feature-missing"><h2>Verwenden Sie bitte einen aktuellen Browser um diese Anwendung korrekt darstellen zu können!</h2></div>'
+    );
+  }
+
   function init(element = document) {
-    if ($(element).find('.grid').length) {
-      var grid = new masonry('.grid', {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-sizer',
-        gutter: '.gutter-sizer',
-        percentPosition: true,
-        transitionDuration: 0
-      });
+    if ($(element).find('.grid').length) resizeAllMasonryItems(element);
+  }
 
-      $(element)
-        .find('.grid .grid-loading')
-        .removeClass('show');
-      $(element)
-        .find('.grid-item')
-        .addClass('show');
+  function resizeMasonryItem(item) {
+    let grid = $(item)
+        .closest('.grid')
+        .get(0),
+      rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap')),
+      rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
 
-      $(window).on('load', event => {
-        grid.layout();
-      });
+    let rowSpan = Math.round(
+      (item.querySelector('.content-link').getBoundingClientRect().height + rowGap) / (rowHeight + rowGap)
+    );
 
-      $(document).on('lazyloaded', '*', event => {
-        if ($(event.target).closest('.grid').length) grid.layout();
-      });
+    item.style.gridRowEnd = 'span ' + rowSpan;
+  }
+
+  function resizeAllMasonryItems(container = document) {
+    let allItems = $(container)
+      .find('.grid-item')
+      .get();
+
+    $(container)
+      .find('.grid-loading')
+      .hide();
+
+    $(container)
+      .find('.grid-item')
+      .show();
+
+    for (let i = 0; i < allItems.length; i++) {
+      resizeMasonryItem(allItems[i]);
     }
   }
+
+  $(document).on('lazyloaded', '*', event => {
+    if ($(event.target).closest('.grid-item').length)
+      resizeMasonryItem(
+        $(event.target)
+          .closest('.grid-item')
+          .get(0)
+      );
+  });
+
+  let masonryEvents = ['load', 'resize'];
+
+  $(document)
+    .find('.grid')
+    .on('load', event => {
+      console.log('load');
+    });
+
+  masonryEvents.forEach(event => {
+    window.addEventListener(event, event => {
+      if ($(document).find('.grid').length) resizeAllMasonryItems();
+    });
+  });
 
   init();
 };
