1e346a98fc776090f8601d75946efafe391dd60d
Patrick Rainer <rainer@pixelpoint.at>
2019-04-12 16:32:40 +0200

    added hotfix and original version to media importer

--- app/models/data_cycle_core/generic/data_cycle_media/import_assets.rb
+++ app/models/data_cycle_core/generic/data_cycle_media/import_assets.rb
@@ -47,10 +47,16 @@ module DataCycleCore
                   fixed_item = DataCycleCore::Thing.find_by(
                     name: title
                   )
-                  next unless fixed_item
 
+                  # version to restore images
+                  next unless fixed_item
                   fixed_item.set_data_hash(data_hash: fixed_item.get_data_hash.merge(image_data))
                   next unless fixed_item
+
+                  # original Version
+                  # new_object = process_content(utility_object: utility_object, raw_data: image_data, options: options)
+                  # next unless new_object
+                  # File.delete(p) if credentials.dig('delete')
 
                   item_count += 1
                 end
