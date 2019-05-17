3cba726be4caf9f85ec676de41788b48b6aa9ee6
Manuel Mitterer <mitterer@pixelpoint.at>
2019-05-17 11:01:34 +0200

    updated asset_url and boolean filter

--- app/uploaders/data_cycle_core/image_uploader.rb
+++ app/uploaders/data_cycle_core/image_uploader.rb
@@ -22,6 +22,27 @@ module DataCycleCore
       end
     end
 
+    def url
+      content = model&.things&.first
+
+      return "#{asset_host}/assets/#{model.class.to_s.demodulize.underscore}/#{model.id}/#{version_name || 'original'}/#{File.basename(model.name.to_s, '.*').underscore_blanks}.#{file&.extension}" if content.nil?
+
+      copyright_holder = content.try(:copyright_holder)&.first
+      copyright_year = content.try(:copyright_year)&.to_i
+      author = content.try(:author)&.first
+
+      I18n.with_locale(content.first_available_locale) do
+        file_name = [
+          (content.title.presence || File.basename(model.name.to_s, '.*')),
+          (copyright_holder.nil? ? nil : I18n.with_locale(copyright_holder.first_available_locale) { copyright_holder.title }),
+          copyright_year,
+          (author.nil? ? nil : I18n.with_locale(author.first_available_locale) { author.title })
+        ].compact.join('_').underscore_blanks
+
+        "#{asset_host}/assets/#{model.class.to_s.demodulize.underscore}/#{model.id}/#{version_name || 'original'}/#{file_name}.#{file&.extension}"
+      end
+    end
+
     def extension_white_list
       DataCycleCore.uploader_validations.dig(self.class.name.demodulize.underscore.remove('_uploader').to_sym, :format).presence || ['jpg', 'jpeg', 'gif', 'png', 'bmp', 'tif', 'tiff']
     end
