a577427265c3727d663cd953377ca6be7f1e6608
Manuel Mitterer <mitterer@pixelpoint.at>
2019-05-21 12:43:35 +0200

    added status to thing_external_systems to show sync state

--- app/models/data_cycle_core/content/external_data.rb
+++ app/models/data_cycle_core/content/external_data.rb
@@ -3,22 +3,18 @@
 module DataCycleCore
   module Content
     module ExternalData
-      def add_external_system_data(external_system, data)
-        self.external_systems = [external_system]
-        external_data = thing_external_systems.with_external_system(external_system.id)
-        external_data.data = data
-        external_data.save
-        save(touch: false)
+      def add_external_system_data(external_system, data = nil, status = nil)
+        external_data = thing_external_systems.find_or_initialize_by(external_system_id: external_system.id)
+        external_data.update({ data: data, status: status }.compact)
       end
 
       def remove_external_system_data(external_system)
-        external_data = thing_external_systems.with_external_system(external_system.id)
-        external_data.data = nil
-        external_data.save
+        external_data = thing_external_systems.find_by(external_system_id: external_system.id)
+        external_data.update(data: nil)
       end
 
       def external_system_data(external_system)
-        thing_external_systems.with_external_system(external_system.id)&.try(&:data)
+        thing_external_systems.find_by(external_system_id: external_system.id)&.data
       end
     end
   end
