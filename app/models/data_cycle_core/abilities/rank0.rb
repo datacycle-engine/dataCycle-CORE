39e2300145106b8d7db6851132ec84c642e63447
Manuel Mitterer <mitterer@pixelpoint.at>
2019-06-13 14:07:31 +0200

    adjusted ability for guests to edit releasable contents

--- app/models/data_cycle_core/abilities/rank0.rb
+++ app/models/data_cycle_core/abilities/rank0.rb
@@ -48,23 +48,19 @@ module DataCycleCore
         end
 
         DataCycleCore::DataLink.session_edit_links(session[:can_edit_ids]).each do |link|
-          if link.is_valid? && link.item_type == 'DataCycleCore::WatchList'
-            can [:update, :import], DataCycleCore::Thing do |content|
-              release_partner_stage_id = DataCycleCore::Classification.includes(classification_aliases: :classification_tree_label).find_by(name: DataCycleCore::Feature::Releasable.get_stage('partner'), classification_aliases: { classification_tree_labels: { name: 'Release-Stati' } })&.id
+          next unless link.is_valid?
 
-              if DataCycleCore::Feature::Releasable.allowed?(content) && release_partner_stage_id.present?
-                link.item.watch_list_data_hashes.pluck(:hashable_id).include?(content.id) && content.release_status_id.presence&.ids&.include?(release_partner_stage_id)
-              else
-                link.item.watch_list_data_hashes.pluck(:hashable_id).include?(content.id)
-              end
-            end
-            can :edit, DataCycleCore::DataAttribute do |a|
-              link.item.watch_list_data_hashes.pluck(:hashable_id).include?(a.content&.id)
-            end
-          elsif link.is_valid?
-            can [:update, :import], link.item_type.constantize, id: link.item_id
-            can :edit, DataCycleCore::DataAttribute do |a|
-              link.item_id == a.content&.id
+          release_partner_stage_id = DataCycleCore::Classification.includes(classification_aliases: :classification_tree_label).find_by(name: DataCycleCore::Feature::Releasable.get_stage('partner'), classification_aliases: { classification_tree_labels: { name: 'Release-Stati' } })&.id
+
+          can [:update, :import], DataCycleCore::Thing do |content|
+            if DataCycleCore::Feature::Releasable.allowed?(content) && release_partner_stage_id.present? && link.item_type == 'DataCycleCore::WatchList'
+              link.item.watch_list_data_hashes.pluck(:hashable_id).include?(content.id) && content.release_status_id.presence&.ids&.include?(release_partner_stage_id)
+            elsif DataCycleCore::Feature::Releasable.allowed?(content) && release_partner_stage_id.present?
+              link.item_id == content.id && content.release_status_id.presence&.ids&.include?(release_partner_stage_id)
+            elsif link.item_type == 'DataCycleCore::WatchList'
+              link.item.watch_list_data_hashes.pluck(:hashable_id).include?(content.id)
+            else
+              link.item_id == content.id
             end
           end
         end
