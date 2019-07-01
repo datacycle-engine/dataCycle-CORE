cd7c951cf085853f42bb54488911eb59615223aa
Manuel Mitterer <mitterer@pixelpoint.at>
2019-07-01 11:41:15 +0200

    fixed tests

--- app/services/data_cycle_core/classification_service.rb
+++ app/services/data_cycle_core/classification_service.rb
@@ -3,10 +3,7 @@
 module DataCycleCore
   class ClassificationService
     def self.visible_classification_tree?(tree_label, scopes)
-      @visible_classification_tree ||= Hash.new do |h, key|
-        h[key] = (Array(DataCycleCore::ClassificationTreeLabel.find_by(name: tree_label)&.visibility) & Array(scopes)).size.positive?
-      end
-      @visible_classification_tree["#{tree_label}_#{Array(scopes).join}"]
+      (Array(DataCycleCore::ClassificationTreeLabel.find_by(name: tree_label)&.visibility) & Array(scopes)).size.positive?
     end
   end
 end
