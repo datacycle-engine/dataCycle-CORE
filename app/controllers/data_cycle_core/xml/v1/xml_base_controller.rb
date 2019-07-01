76495792383c1126ba49c433aa0e8c3989715969
Martin Oehzelt <oehzelt@pixelpoint.at>
2019-07-01 16:42:21 +0200

    first implementation of xml export for classification_trees, classifications

---
+++ app/controllers/data_cycle_core/xml/v1/xml_base_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Xml
+    module V1
+      class XmlBaseController < ::DataCycleCore::Api::V3::ApiBaseController
+        private
+
+        def access_denied(exception)
+          render 'error', error: exception.message, status: :access_denied
+        end
+
+        def not_found(exception)
+          render 'error', error: exception.message, status: :not_found
+        end
+
+        def set_default_response_format
+          request.format = :xml
+        end
+      end
+    end
+  end
+end
