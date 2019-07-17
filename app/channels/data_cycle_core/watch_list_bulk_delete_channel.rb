6d8cc8753004631bb090dc3fcb1a5ac023a76cd5
Manuel Mitterer <mitterer@pixelpoint.at>
2019-07-17 16:20:37 +0200

    add UI for bulk_delete in watch_lists

--- app/channels/data_cycle_core/watch_list_bulk_delete_channel.rb
+++ app/channels/data_cycle_core/watch_list_bulk_delete_channel.rb
@@ -3,8 +3,7 @@
 module DataCycleCore
   class WatchListBulkDeleteChannel < ApplicationCable::Channel
     def subscribed
-      reject && return if current_user.blank?
-      stream_from "bulk_delete_#{params[:watch_list_id]}_#{current_user.id}"
+      stream_from "bulk_delete_#{params[:watch_list_id]}"
     end
 
     def unsubscribed
