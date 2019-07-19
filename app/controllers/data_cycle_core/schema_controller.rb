09ee5fd880e440e6ddb444754ab5b8605cd0a4d2
Johannes Zlattinger <johannes@zlattinger.net>
2019-07-19 21:31:34 +0200

    [WIP] introduced basic schema documentation

--- app/controllers/data_cycle_core/schema_controller.rb
+++ app/controllers/data_cycle_core/schema_controller.rb
@@ -14,9 +14,11 @@ module DataCycleCore
 
       @schema = Thing.where(template: true, template_name: params[:id]).first&.schema if @schema.nil?
 
-      raise ActiveRecord::RecordNotFound if @schema.nil?
+      raise ActiveRecord::RecordNotFound, "Couldn't find template '#{params[:id]}'" if @schema.nil?
 
       redirect_to schema_path(id: @schema.dig('api', 'type')) if @schema.dig('api', 'type') && @schema.dig('api', 'type') != params[:id]
+
+      @template_schema = Schema::Template.new(@schema)
     end
   end
 end
--- app/models/data_cycle_core/schema.rb
+++ app/models/data_cycle_core/schema.rb
@@ -2,6 +2,56 @@
 
 module DataCycleCore
   class Schema
+    class Template
+      include Rails.application.routes.url_helpers
+
+      def initialize(template_schema)
+        @template_schema = template_schema
+      end
+
+      def property_definitions
+        @template_schema['properties']
+          .reject { |_, definition| definition['type'] == 'classification' || definition['type'] == 'key' }
+          .reject { |_, definition| definition.dig('api', 'disabled') }
+          .map { |key, definition|
+            {
+              domain: @template_schema.dig('api', 'type') || @schema['schema_type'],
+              label: key.camelize(:lower),
+              range: resolve_range(definition),
+              comment: nil
+            }
+          }.sort_by { |definition| [definition[:domain], definition[:label]] }
+      end
+
+      private
+
+      def resolve_range(definition)
+        if definition.dig('api', 'type')
+          definition.dig('api', 'type')
+        elsif definition['type'] == 'embedded'
+          DataCycleCore::Thing.find_by(template: true, template_name: definition['template_name']).schema.then { |schema|
+            [
+              schema.dig('api', 'type'),
+              schema['schema_type']
+            ]
+          }.reject(&:blank?).first.then { |type| "/schema/#{type}" }
+        else
+          case definition.dig('compute', 'type') || definition['type']
+          when 'string'
+            '//schema.org/Text'
+          when 'datetime'
+            '//schema.org/DateTime'
+          when 'number'
+            '//schema.org/Number'
+          when 'linked'
+            '//schema.org/Thing'
+          else
+            definition['type']
+          end
+        end
+      end
+    end
+
     def self.content_types
       DataCycleCore::Thing.where(template: true).map(&:schema).map { |schema| schema['content_type'] }.uniq
     end
