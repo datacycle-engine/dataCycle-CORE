482a5681db5036c33031d2e7dd7255b8f0ff8749
Martin Oehzelt <oehzelt@pixelpoint.at>
2019-07-31 15:45:50 +0200

    first version of importer

--- test/dummy/config/initializers/data_cycle_core.rb
+++ test/dummy/config/initializers/data_cycle_core.rb
@@ -25,7 +25,8 @@ DataCycleCore.setup do |config|
       Rails.root.join('..', '..', 'config', 'data_definitions', 'feature_releasable'),
       Rails.root.join('..', '..', 'config', 'data_definitions', 'feature_life_cycle'),
       Rails.root.join('..', '..', 'config', 'data_definitions', 'feature_idea_collection'),
-      Rails.root.join('..', '..', 'config', 'data_definitions', 'external_source_bergfex')
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'external_source_bergfex'),
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'external_source_karriere_at')
     ].freeze
   else
     config.default_template_paths = [
@@ -33,6 +34,7 @@ DataCycleCore.setup do |config|
       Rails.root.join('..', '..', 'config', 'data_definitions', 'data_cycle_creative_content'),
       Rails.root.join('..', '..', 'config', 'data_definitions', 'data_cycle_media'),
       Rails.root.join('..', '..', 'config', 'data_definitions', 'external_source_bergfex'),
+      Rails.root.join('..', '..', 'config', 'data_definitions', 'external_source_karriere_at'),
       Rails.root.join('..', '..', 'config', 'data_definitions', 'feature_releasable')
       # Rails.root.join('..', '..', 'config', 'data_definitions', 'feature_life_cycle'),
       # Rails.root.join('..', '..', 'config', 'data_definitions', 'feature_idea_collection')
