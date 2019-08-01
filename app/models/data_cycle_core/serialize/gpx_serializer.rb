b972a50e398e2d4e266907be44ee045dbec1dc7d
Patrick Rainer <rainer@pixelpoint.at>
2019-08-01 14:32:56 +0200

    added gpx serializer

---
+++ app/models/data_cycle_core/serialize/gpx_serializer.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Serialize
+    class GpxSerializer
+      class << self
+        def mime_type(_content)
+          'gpx/xml'
+        end
+
+        def file_extension(_mime_type)
+          '.gpx'
+        end
+
+        def serialize(content)
+          builder = Nokogiri::XML::Builder.new(encoding: 'UTF-8') do |xml|
+            xml.gpx(version: '1.1', creator: 'dataCycle', xmlns: 'http://www.topografix.com/GPX/1/1') do
+              xml.metadata do
+                xml.name content.title
+                xml.desc ActionView::Base.full_sanitizer.sanitize(content.send('description')) if content.respond_to?('description')
+                xml.time content.updated_at
+                if content.created_by_user&.name.present?
+                  xml.author do
+                    xml.name content.created_by_user&.name
+                  end
+                end
+              end
+              content.geo_properties.each do |key, value|
+                geo = content.send(key)
+                geo = RGeo::Geographic.spherical_factory(srid: 4326, has_z_coordinate: true).parse_wkt(geo) if geo.is_a?(String)
+
+                if geo.try(:geometry_type) == RGeo::Feature::Point
+                  xml.wpt(lat: geo.y, lon: geo.x) do
+                    xml.name value['label']
+                    xml.ele geo.z if geo.z
+                  end
+                elsif geo.try(:geometry_type) == RGeo::Feature::LineString
+                  xml.trk do
+                    xml.name value['label']
+                    xml.trkseg do
+                      geo.points.each do |l|
+                        xml.trkpt(lat: l.y, lon: l.x) do
+                          xml.ele l.z if l.z
+                        end
+                      end
+                    end
+                  end
+                end
+              end
+            end
+          end
+
+          builder.to_xml
+        end
+      end
+    end
+  end
+end
