0ce1378a541cf127d8072d1044f6a9d339b9a6b1
Patrick Rainer <rainer@pixelpoint.at>
2019-08-02 16:40:47 +0200

    moved download abilities from rank 99 to rank 10

--- app/models/data_cycle_core/abilities/rank99.rb
+++ app/models/data_cycle_core/abilities/rank99.rb
@@ -14,21 +14,6 @@ module DataCycleCore
         can :destroy, DataCycleCore::Thing
         can :show_related, DataCycleCore::Thing
         can [:bulk_edit, :bulk_delete], DataCycleCore::WatchList
-        can :download_zip, DataCycleCore::Thing do |content|
-          DataCycleCore::Feature::Download.allowed?(content) && DataCycleCore::Feature::Download.collection_enabled?('content')
-        end
-        can :download, DataCycleCore::WatchList do |_watch_list|
-          DataCycleCore::Feature::Download.collection_serializer_enabled?('watch_list')
-        end
-        can :download_zip, DataCycleCore::WatchList do |_watch_list|
-          DataCycleCore::Feature::Download.collection_enabled?('watch_list')
-        end
-        can :download, DataCycleCore::StoredFilter do |_stored_filter|
-          DataCycleCore::Feature::Download.collection_serializer_enabled?('stored_filter')
-        end
-        can :download_zip, DataCycleCore::StoredFilter do |_stored_filter|
-          DataCycleCore::Feature::Download.collection_enabled?('stored_filter')
-        end
       end
     end
   end
