3fcf1ce270ff8d727678b3358e1000f52f82714e
Manuel Mitterer <mitterer@pixelpoint.at>
2019-08-23 08:20:11 +0200

    rename migration to avoid name collision

--- db/migrate/20190704114636_add_omniauth_to_users_core.rb
+++ db/migrate/20190704114636_add_omniauth_to_users_core.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddOmniauthToUsers < ActiveRecord::Migration[5.1]
+class AddOmniauthToUsersCore < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :provider, :string unless column_exists? :users, :provider
     add_column :users, :uid, :string unless column_exists? :users, :uid
