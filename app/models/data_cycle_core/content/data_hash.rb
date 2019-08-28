0d456bcd52118347ea296915be8259949a55f485
Manuel Mitterer <mitterer@pixelpoint.at>
2019-08-28 10:02:05 +0200

    fix enqueing multiple cache_invalidation jobs for the same content

--- app/models/data_cycle_core/content/data_hash.rb
+++ app/models/data_cycle_core/content/data_hash.rb
@@ -111,7 +111,7 @@ module DataCycleCore
       private
 
       def invalidate_content_a_cache
-        Delayed::Job.enqueue DataCycleCore::Jobs::CacheInvalidationJob.new(self.class.name, id, :invalidate_cache)
+        Delayed::Job.enqueue DataCycleCore::Jobs::CacheInvalidationJob.new(self.class.name, id, :invalidate_cache) unless Delayed::Job.exists?(queue: 'cache_invalidation', delayed_reference_type: self.class.name, delayed_reference_id: id, locked_at: nil)
       end
 
       def invalidate_cache
