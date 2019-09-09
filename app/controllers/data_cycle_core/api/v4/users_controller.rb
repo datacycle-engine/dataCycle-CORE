42483e34e9c26a8582a93d0a00ba7149e056e477
Manuel Mitterer <mitterer@pixelpoint.at>
2019-09-09 14:49:22 +0200

    fix APIv4 params

--- app/controllers/data_cycle_core/api/v4/users_controller.rb
+++ app/controllers/data_cycle_core/api/v4/users_controller.rb
@@ -61,9 +61,9 @@ module DataCycleCore
         private
 
         def user_params
-          user_keys = DataCycleCore.features.dig(:user_api, :user_params).deep_transform_keys { |k| k.camelize(:lower) }
+          user_keys = DataCycleCore.features.dig(:user_api, :user_params)
           authorized_params = Array(user_keys.select { |_, v| v.nil? }.keys)
-          authorized_params.concat(Array(user_keys.compact.map { |k, _| { "#{k}Ids" => [] } }))
+          authorized_params.concat(Array(user_keys.compact.map { |k, _| { "#{k}_ids" => [] } }))
 
           params.permit(authorized_params).transform_keys(&:underscore)
         end
