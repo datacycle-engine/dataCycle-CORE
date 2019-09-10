b0e48bc7db24409ec52be7298b130d808d2d29fb
Manuel Mitterer <mitterer@pixelpoint.at>
2019-09-10 08:47:50 +0200

    update camelCase to snake_case in template definitions

--- app/models/data_cycle_core/filter/common/configurable.rb
+++ app/models/data_cycle_core/filter/common/configurable.rb
@@ -6,10 +6,10 @@ module DataCycleCore
       module Configurable
         def with_classification_aliases_and_treename(definition)
           return self if definition.blank?
-          raise StandardError, 'Missing data definition: treeLabel' if definition.dig('treeLabel').blank?
+          raise StandardError, 'Missing data definition: tree_label' if definition.dig('tree_label').blank?
           raise StandardError, 'Missing data definition: aliases' if definition.dig('aliases').blank?
 
-          with_classification_aliases(definition.dig('treeLabel'), definition.dig('aliases'))
+          with_classification_aliases(definition.dig('tree_label'), definition.dig('aliases'))
         end
 
         def with_external_source_names(definition)
