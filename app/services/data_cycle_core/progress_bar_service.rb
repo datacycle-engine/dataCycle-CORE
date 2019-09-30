ced72b9ddb5a9799f1772bbda055411f0223fa41
Martin Oehzelt <oehzelt@pixelpoint.at>
2019-09-30 10:09:18 +0200

    fix ProgressBarService to have proper increments for all total_count values

--- app/services/data_cycle_core/progress_bar_service.rb
+++ app/services/data_cycle_core/progress_bar_service.rb
@@ -8,13 +8,12 @@ module DataCycleCore
     end
 
     def inc
-      if @total_count > 49
-        if (@index % 500).zero?
-          fraction = (@index / (@total_count / 100.0)).round(0)
-          fraction = 100 if fraction > 100
-          print "[#{'*' * fraction}#{' ' * (100 - fraction)}] #{fraction.to_s.rjust(3)}% (#{Time.zone.now.strftime('%H:%M:%S.%3N')})\r"
-        end
-      else
+      if @index >= @total_count
+        print "[#{'*' * 100}] 100% (#{Time.zone.now.strftime('%H:%M:%S.%3N')})\n"
+        return @index += 1
+      end
+      interval ||= [@total_count / 100.0, 1.0].max.round(0)
+      if (@index % interval).zero?
         fraction = (((@index * 1.0) / @total_count) * 100.0).round(0)
         fraction = 100 if fraction > 100
         print "[#{'*' * fraction}#{' ' * (100 - fraction)}] #{fraction.to_s.rjust(3)}% (#{Time.zone.now.strftime('%H:%M:%S.%3N')})\r"
