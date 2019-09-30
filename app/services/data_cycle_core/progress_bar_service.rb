79eda524701133682361bfc20e459a8186cb14b2
Martin Oehzelt <oehzelt@pixelpoint.at>
2019-09-30 10:13:44 +0200

    tweak ProgressBarService

--- app/services/data_cycle_core/progress_bar_service.rb
+++ app/services/data_cycle_core/progress_bar_service.rb
@@ -5,6 +5,7 @@ module DataCycleCore
     def initialize(total_count = 0)
       @index = 0
       @total_count = total_count
+      @interval ||= [@total_count / 100.0, 1.0].max.round(0)
     end
 
     def inc
@@ -12,8 +13,7 @@ module DataCycleCore
         print "[#{'*' * 100}] 100% (#{Time.zone.now.strftime('%H:%M:%S.%3N')})\n"
         return @index += 1
       end
-      interval ||= [@total_count / 100.0, 1.0].max.round(0)
-      if (@index % interval).zero?
+      if (@index % @interval).zero?
         fraction = (((@index * 1.0) / @total_count) * 100.0).round(0)
         fraction = 100 if fraction > 100
         print "[#{'*' * fraction}#{' ' * (100 - fraction)}] #{fraction.to_s.rjust(3)}% (#{Time.zone.now.strftime('%H:%M:%S.%3N')})\r"
