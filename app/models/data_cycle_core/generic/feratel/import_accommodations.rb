f2b5b5a163c96e5c590d6fbe3871e49b90d18b91
Martin Oehzelt <oehzelt@pixelpoint.at>
2019-10-28 13:55:31 +0100

    update feratel importer: exclude services, offers and prices for accommodations for now

--- app/models/data_cycle_core/generic/feratel/import_accommodations.rb
+++ app/models/data_cycle_core/generic/feratel/import_accommodations.rb
@@ -34,21 +34,22 @@ module DataCycleCore
               options.dig(:import, :transformations, :image)
             )
 
-            [raw_data.dig('Services', 'Service')]&.flatten&.compact&.each do |service_data|
-              DataCycleCore::Generic::Feratel::Processing.process_room(
-                utility_object,
-                service_data,
-                options.dig(:import, :transformations, :room)
-              )
-            end
-
-            [raw_data.dig('AdditionalServices', 'AdditionalService')]&.flatten&.compact&.each do |service_data|
-              DataCycleCore::Generic::Feratel::Processing.process_additional_service(
-                utility_object,
-                service_data,
-                options.dig(:import, :transformations, :room)
-              )
-            end
+            # to include rooms, services and pricing to accommodations
+            # [raw_data.dig('Services', 'Service')]&.flatten&.compact&.each do |service_data|
+            #   DataCycleCore::Generic::Feratel::Processing.process_room(
+            #     utility_object,
+            #     service_data,
+            #     options.dig(:import, :transformations, :room)
+            #   )
+            # end
+            #
+            # [raw_data.dig('AdditionalServices', 'AdditionalService')]&.flatten&.compact&.each do |service_data|
+            #   DataCycleCore::Generic::Feratel::Processing.process_additional_service(
+            #     utility_object,
+            #     service_data,
+            #     options.dig(:import, :transformations, :room)
+            #   )
+            # end
 
             DataCycleCore::Generic::Feratel::Processing.process_accommodation(
               utility_object,
