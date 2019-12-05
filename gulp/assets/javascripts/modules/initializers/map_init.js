d8cbb033a69becbde397bfe853b92965ebaa9086
Manuel Mitterer <mitterer@pixelpoint.at>
2019-12-05 15:20:32 +0100

    add possibility for pois in TourSprung Editor

--- gulp/assets/javascripts/modules/initializers/map_init.js
+++ gulp/assets/javascripts/modules/initializers/map_init.js
@@ -3,16 +3,6 @@ var TourSprungEditor = require('./../components/tour_sprung_editor');
 
 // Map Configuration
 module.exports.initialize = function() {
-  if ($('.object-browser-overlay .item-info-scrollable').length) {
-    $('.object-browser-overlay .item-info-scrollable').on('details-changed', event => {
-      $(event.target)
-        .find('.geographic-map')
-        .each((index, item) => {
-          initMap(item);
-        });
-    });
-  }
-
   if ($('.geographic-map').length) {
     $('.geographic-map').each((index, item) => {
       initMap(item);
@@ -34,4 +24,5 @@ function initMap(item) {
   let newMap;
   if (editor == 'TourSprung') newMap = new TourSprungEditor(item);
   else newMap = new OpenLayerMap(item);
+  return newMap;
 }
