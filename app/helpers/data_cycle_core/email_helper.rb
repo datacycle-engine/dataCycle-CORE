ae67288aaf721ffc5907977bbc2dd14e8a0f13cc
Manuel Mitterer <mitterer@pixelpoint.at>
2020-01-09 14:42:17 +0100

    fixes for translated datalink_mailer

--- app/helpers/data_cycle_core/email_helper.rb
+++ app/helpers/data_cycle_core/email_helper.rb
@@ -3,7 +3,8 @@
 module DataCycleCore
   module EmailHelper
     def email_image_tag(image, **options)
-      logo_file_path = Rails.root.join('app', 'assets', 'images', image)
+      logo_file_path = Rails.root.join('app', 'assets', 'images', "#{@locale || I18n.locale.to_s}_#{image}")
+      logo_file_path = Rails.root.join('app', 'assets', 'images', image) unless File.exist?(logo_file_path)
       logo_file_path = DataCycleCore::Engine.root.join('app', 'assets', 'images', image) unless File.exist?(logo_file_path)
       attachments.inline[image] = File.read(logo_file_path)
       image_tag attachments[image].url, **options
