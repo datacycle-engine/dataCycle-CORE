b14c29b8c4123130f5d3651531a4543eff1f4f79
Johannes Zlattinger <johannes@zlattinger.net>
2020-01-16 17:36:38 +0100

    add 'Rezeptkomponente' and 'Inhaltsblock' to excluded filter classifications

--- lib/data_cycle_core/engine.rb
+++ lib/data_cycle_core/engine.rb
@@ -85,7 +85,31 @@ module DataCycleCore
     self.allowed_api_strategies = ['DataCycleCore::Api::MediaArchiveExternalSource', 'DataCycleCore::Api::GenericExternalSource', 'DataCycleCore::Api::FeratelIdentityServerExternalSource']
 
     mattr_accessor :excluded_filter_classifications
-    self.excluded_filter_classifications = ['Angebotszeitraum', 'Antwort', 'Datei', 'Frage', 'Veranstaltungstermin', 'Website', 'Zeitleiste-Eintrag', 'Zitat', 'Öffnungszeit', 'Öffnungszeit - Zeitspanne', 'Öffnungszeit - Simple', 'Overlay', 'Publikations-Plan', 'Textblock', 'EventSchedule', 'Skigebiet - Addon', 'Schneehöhe - Messpunkt', 'Event-Ticket-Angebot', 'Zimmer', 'Zutatengruppe', 'Zutat']
+    self.excluded_filter_classifications = [
+      'Angebotszeitraum',
+      'Antwort',
+      'Datei',
+      'Frage',
+      'Veranstaltungstermin',
+      'Website',
+      'Zeitleiste-Eintrag',
+      'Zitat',
+      'Öffnungszeit',
+      'Öffnungszeit - Zeitspanne',
+      'Öffnungszeit - Simple',
+      'Overlay',
+      'Publikations-Plan',
+      'Textblock',
+      'EventSchedule',
+      'Skigebiet - Addon',
+      'Schneehöhe - Messpunkt',
+      'Event-Ticket-Angebot',
+      'Zimmer',
+      'Zutatengruppe',
+      'Zutat',
+      'Rezeptkomponente',
+      'Inhaltsblock'
+    ]
 
     mattr_accessor :ui_language
     self.ui_language = :de
