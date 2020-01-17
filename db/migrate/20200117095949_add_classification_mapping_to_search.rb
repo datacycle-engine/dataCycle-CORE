d70b516be904ff940fe76f9c1ece41b2c89c18ac
Patrick Rainer <rainer@pixelpoint.at>
2020-01-17 15:01:11 +0100

    added classification_mapping for search

---
+++ db/migrate/20200117095949_add_classification_mapping_to_search.rb
@@ -0,0 +1,6 @@
+class AddClassificationMappingToSearch < ActiveRecord::Migration[5.2]
+  def change
+    add_column :searches, :classification_mapping, :jsonb
+    add_index :searches, :classification_mapping, using: :gin
+  end
+end
--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -726,7 +726,8 @@ CREATE TABLE public.searches (
     all_text text,
     boost double precision DEFAULT 1.0 NOT NULL,
     schema_type character varying DEFAULT 'Thing'::character varying NOT NULL,
-    advanced_attributes jsonb
+    advanced_attributes jsonb,
+    classification_mapping jsonb
 )
 WITH (autovacuum_vacuum_scale_factor='0.0', autovacuum_vacuum_threshold='1000', autovacuum_analyze_scale_factor='0.0', autovacuum_analyze_threshold='1000');
 
@@ -1638,6 +1639,13 @@ CREATE INDEX index_searches_on_advanced_attributes ON public.searches USING gin 
 
 
 --
+-- Name: index_searches_on_classification_mapping; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_searches_on_classification_mapping ON public.searches USING gin (classification_mapping);
+
+
+--
 -- Name: index_searches_on_content_data_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -2164,6 +2172,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20191205123950'),
 ('20191219123847'),
 ('20191219143016'),
-('20200116143539');
+('20200116143539'),
+('20200117095949');
 
 
