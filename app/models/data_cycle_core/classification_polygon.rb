59a2a31ef735a22088a2852e5aba37868deda5b1
Benjamin Preisig <preisig@pixelpoint.at>
2020-02-18 16:10:18 +0100

    Import GeoShapes to Classification Tree
    - add polygons with lower order and without parent (because of inconsistent data, mainly happening in Switzerland and Germany) to their own classification 'Nicht zugeordnet'
    - improve logging
    - tidy up

--- app/models/data_cycle_core/classification_polygon.rb
+++ app/models/data_cycle_core/classification_polygon.rb
@@ -6,27 +6,6 @@ module DataCycleCore
 
     # acts_as_paranoid
 
-    # has_many :classification_contents, dependent: :destroy
-    # has_many :things, through: :classification_contents, source: 'content_data'
-    # has_many :classification_content_histories, class_name: 'DataCycleCore::ClassificationContent::History'
-    # has_many :thing_histories, through: :classification_content_histories, source: 'content_data_history'
-
-    # has_many :classification_groups, dependent: :destroy
-    # has_many :classification_aliases, through: :classification_groups
-    # has_one :primary_classification_group, class_name: 'DataCycleCore::ClassificationGroup::PrimaryClassificationGroup'
-    # has_one :primary_classification_alias, through: :primary_classification_group, source: :classification_alias
-
-    # def ancestors
-    #   Rails.cache.fetch("#{cache_key}/ancestors", expires_in: 5.days + Random.rand(2.5.days)) do
-    #     [primary_classification_alias] + primary_classification_alias.ancestors
-    #   end
-    # end
-
-    # def descendants
-    #   Rails.cache.fetch("#{cache_key}/descendants", expires_in: 5.days + Random.rand(2.5.days)) do
-    #     primary_classification_alias.try(:descendants).try(:to_a).try(:flatten)
-    #       .try(:map, &:classifications).try(&:to_a).try(&:flatten) || []
-    #   end
-    # end
+    # TODO:  paranoid?
   end
 end
--- app/models/data_cycle_core/generic/geo_shape/import.rb
+++ app/models/data_cycle_core/generic/geo_shape/import.rb
@@ -13,14 +13,11 @@ module DataCycleCore
           import_local_shape(utility_object: utility_object, options: options, credentials: credentials)
         end
 
-        # it should open files from a directory
         def self.import_local_shape(utility_object:, options:, credentials:)
           local_dirs = Array(credentials.dig('directory'))
-          # get asset type from config (new asset type for shape/geojson?)
           geometry_type = options.dig(:import, :geometry_type).constantize
           srid = options.dig(:import, :srid).to_i
           tree_label = options.dig(:import, :tree_label)
-          # binding.pry
           external_source_id = utility_object.external_source.id
 
           local_dirs.each do |ld|
@@ -29,162 +26,70 @@ module DataCycleCore
           raise 'Unkown asset type or local dir' unless local_dirs.present? && geometry_type.present?
 
           init_logging(utility_object) do |logging|
-            # binding.pry
             phase_name = utility_object.source_type.collection_name
-            # phase_name = 'shape_import'
             logging.preparing_phase("#{utility_object.external_source.name} #{phase_name}")
             item_count = 0
-            # tags_attribute_name = options.dig(:import, :extract_tags, :attribute_name) || 'tags'
             begin
-              logging.phase_started(phase_name.to_s)
               durations = []
 
-              # loop through dirs and open files
               Dir.glob(local_dirs.map { |ld| File.join(File.expand_path(ld), '*.shp') }).each do |shapefile|
                 durations << Benchmark.realtime do
-                  # binding.pry
-                  logging.info(shapefile, '')
+                  logging.phase_started(shapefile)
                   classifications_array = []
+                  polygon_count = 0
 
-                  # it should read the content of the files ordered(ASC) by adminlevel row by row
                   RGeo::Shapefile::Reader.open(shapefile, { srid: srid }) do |file|
-                    # binding.pry
-                    item_count = 0
-
                     file.each do |record|
                       attributes = record.attributes
 
-                      # binding.pry
                       classification_polygon = geometry_type.new(admin_level: attributes['adminlevel'], geom: record.geometry)
                       next unless classification_polygon.save
-                      # binding.pry
-                      # poly_id = create_classification_polygon(record.geometry)
-                      # classifications_array.push(relation_id, name, external_source_id.from_config, poly_id)
+
                       classifications_array.push({ classification_polygon_id: classification_polygon[:id], external_key: attributes['id'], adminlevel: attributes['adminlevel'], name: attributes['locname'], parent_external_key: attributes['parent_id'], external_source_id: external_source_id, tree_name: tree_label })
 
-                      item_count += 1
+                      polygon_count += 1
                     end
                   end
+                  logging.info("Imported #{polygon_count} items", '')
 
-                  logging.info("Imported #{item_count} items", "Duration: #{durations.sum.round(6)} seconds")
-
-                  # binding.pry
-                  # sort array and loop
                   classifications_array.sort_by! { |classification| classification[:adminlevel] }
 
                   classifications_array.each do |classification|
-                    # for now we ignore polygons without parent
-                    # TODO: move polygons without parent in their own parent
-                    # binding.pry
-                    next if classification[:parent_external_key].zero? && classification[:adminlevel] > 2
+                    # polygons without parent are moved to their own classification 'uncategorized'
+                    if classification[:parent_external_key].zero? && classification[:adminlevel] > 2
+                      pc = classifications_array.detect { |f| f[:adminlevel] == 2 }
+
+                      uncategorized_classification = { external_key: pc[:external_key].to_s + '_uncategorized', adminlevel: 4, name: 'Nicht zugeordnet', parent_external_key: pc[:external_key], external_source_id: pc[:external_source_id], tree_name: pc[:tree_name] }
+
+                      import_classification(classification_data: uncategorized_classification, parent_external_key: uncategorized_classification[:parent_external_key])
+                      # binding.pry
+                      classification[:parent_external_key] = uncategorized_classification[:external_key]
+                    end
 
                     classification_alias = import_classification(classification_data: classification, parent_external_key: classification[:parent_external_key])
-                    # binding.pry
                     classification_polygon = DataCycleCore::ClassificationPolygon.find(classification[:classification_polygon_id])
-                    # binding.pry
                     classification_polygon.classification_alias_id = classification_alias.id
                     classification_polygon.save!
                   end
-
-                  logging.info('Created Classification', "Duration: #{durations.sum.round(6)} seconds")
-
-                  # create classification and add FK to class_poly
-
-                  # file = File.open(p)
-                  # title = File.basename(file, '.*')
-
-                  # ## process DataCycleImage
-                  # # write data to database e.g. DataCycleCore::ClassificationPolygon
-                  # asset_file = geometry_type.new(file: file)
-                  # next unless asset_file.save
-
-                  # image_data = {
-                  #   'name' => title,
-                  #   'asset' => asset_file.id
-                  # }
-
-                  # # get tags from filename
-                  # if options.dig(:import, :tags_from_folders) || options.dig(:import, :extract_tags, :mode) == 'folder'
-                  #   image_data[tags_attribute_name] = Pathname(File.dirname(p).gsub(Regexp.union(local_dirs.map { |ld| File.expand_path(ld) }), '')).each_filename.to_a
-                  # elsif options.dig(:import, :extract_tags, :mode) == 'filename' && options.dig(:import, :extract_tags, :delimiter).present?
-                  #   image_data[tags_attribute_name] = title.split(options.dig(:import, :extract_tags, :delimiter)).slice(0..-2)
-                  # end
-
-                  # # process tags + create classification
-                  # if image_data.dig(tags_attribute_name).present?
-                  #   process_tags(
-                  #     raw_data: image_data,
-                  #     options: { import: options.dig(:import, :extract_tags)&.deep_symbolize_keys }
-                  #   )
-                  # end
-
-                  # new_object = process_content(utility_object: utility_object, raw_data: image_data, options: options)
-                  # next unless new_object
-                  # File.delete(p) if credentials.dig('delete')
-
                   item_count += 1
                 end
                 break if options[:max_count].present? && item_count >= options[:max_count]
-
-              # rescue MiniMagick::Error => e
-              #   logging.error('MiniMagick::Error', p, nil, e)
+                logging.info('Created Classification', "Duration: #{durations.sum.round(6)} seconds")
               end
 
               GC.start
-              # logging.info("Imported #{item_count} items", "Duration: #{durations.sum.round(6)} seconds")
             ensure
               logging.phase_finished(phase_name.to_s, item_count)
             end
           end
         end
 
-        # def self.process_content(utility_object:, raw_data:, options:)
-        #   config = options.dig(:import, :transformations, :asset)
-        #   template = config&.dig(:template) || 'Bild'
-        #   tags_tree_label = options.dig(:import, :extract_tags, :tree_label) || 'Tags'
-        #   tags_attribute_name = options.dig(:import, :extract_tags, :attribute_name) || 'tags'
-        #   transformation = DataCycleCore::Generic::DataCycleMedia::Transformations.file_to_asset(tags_tree_label, tags_attribute_name)
-
-        #   DataCycleCore::Generic::Common::ImportFunctions.create_or_update_content(
-        #     utility_object: utility_object,
-        #     template: DataCycleCore::Generic::Common::ImportFunctions.load_template(template),
-        #     data: DataCycleCore::Generic::Common::ImportFunctions.merge_default_values(
-        #       config,
-        #       transformation.call(raw_data)
-        #     ).with_indifferent_access,
-        #     local: true
-        #   )
-        # end
-
         def self.init_logging(utility_object, &block)
           DataCycleCore::Generic::Common::ImportFunctions.init_logging(utility_object, &block)
         end
 
-        # def self.process_tags(raw_data:, options:)
-        #   tree_label = options.dig(:import, :tree_label) || 'Tags'
-        #   attribute_name = options.dig(:import, :attribute_name) || 'tags'
-        #   keywords = DataCycleCore::Generic::Common::ImportTags.unwind_project_data(
-        #     raw_data,
-        #     [attribute_name],
-        #     [],
-        #     []
-        #   )
-        #   keywords = DataCycleCore::Generic::Common::ImportTags.unwind_project_data(record_data,['tags'],[],[])
-        #   return if keywords&.compact.blank?
-
-        #   keywords.each do |keyword_hash|
-        #     classification_data = DataCycleCore::Generic::Common::ImportTags.extract_data(options, keyword_hash).merge(tree_name: tree_label)
-        #     import_classification(
-        #       classification_data: classification_data
-        #     )
-        #   end
-        # end
-
         def self.import_classification(classification_data:, parent_external_key:)
-          # binding.pry
           parent_classification_alias = parent_external_key ? DataCycleCore::ClassificationAlias.includes(:primary_classification, classification_tree: [:classification_tree_label]).find_by(classification_trees: { classification_tree_labels: { name: classification_data[:tree_name] } }, classifications: { external_key: parent_external_key }) : nil
-
-          # TODO: if no parent and adminlevel > 2 create or add to "Other Admin_levels"
 
           classification = DataCycleCore::Classification.includes(primary_classification_alias: [classification_tree: :classification_tree_label]).where(external_key: classification_data[:external_key], primary_classification_alias: { classification_trees: { classification_tree_labels: { name: classification_data[:tree_name] } } }).first_or_initialize do |c|
             c.name = classification_data[:name]
@@ -232,15 +137,6 @@ module DataCycleCore
           classification.save!
           classification_alias
         end
-
-        
-
-
-          # it should search for a parent classification if attribute.parent_id is present and return the classification_alias corresponding to the found external_key
-
-          # it should create a classification under the right tree (what if no parent_id, but AL>2) and return the classification_alias
-
-          # it should save the geometry of the row in the boundaries-table and add the classification_alias
       end
     end
   end
