0bf1fa3badf9742e3064ac05af0671f8d784a6f7
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-02-26 11:40:32 +0100

    filter function done, refactor data_cycle_core/filter/*

--- app/models/data_cycle_core/filter/common/date.rb
+++ app/models/data_cycle_core/filter/common/date.rb
@@ -3,11 +3,36 @@
 module DataCycleCore
   module Filter
     module Common
-      module DateRange
+      module Date
         def in_schedule(value = nil, mode = nil)
           return if value.blank?
           from_date, to_date = date_from_filter_object(value, mode)
           schedule_search(from_date&.beginning_of_day, to_date&.end_of_day, 'schedule')
+        end
+
+        def schedule_search(from, to, relation)
+          return self if relation.blank? || (from.blank? && to.blank?)
+          @joined_schedule = true
+
+          rdates = Arel::SelectManager.new.project('event_date').from(Arel::Nodes::SqlLiteral.new('unnest(schedules.rdate) AS event_date'))
+          occurrences = Arel::SelectManager.new.project('event_date').from(Arel::Nodes::SqlLiteral.new('unnest(get_occurrences(schedules.rrule::rrule, schedules.dtstart)) AS event_date'))
+          exdates = Arel::SelectManager.new.project('event_date').from(Arel::Nodes::SqlLiteral.new('unnest(schedules.exdate) AS event_date'))
+          from_node = from.blank? ? Arel::Nodes::SqlLiteral.new('NULL') : cast_tstz(from)
+          to_node = to.blank? ? Arel::Nodes::SqlLiteral.new('NULL') : cast_tstz(to)
+
+          reflect(
+            @query
+              .left_outer_joins(:scheduled_data)
+              .where(in_json(thing[:schema], 'schema_type').eq(Arel::Nodes.build_quoted('Event')))
+              .where(
+                overlap(tstzrange(from_node, to_node), tstzrange(thing[:start_date], thing[:end_date]))
+                .or(
+                  schedule[:relation].eq(Arel::Nodes.build_quoted(relation))
+                  .and(overlap(tstzrange(from_node, to_node), tstzrange(schedule[:dtstart], schedule[:dtend])))
+                  .and(in_range(tstzrange(from_node, to_node), any(Arel::Nodes::Except.new(rdates.union(occurrences), exdates))))
+                )
+              )
+          )
         end
 
         def validity_period(value = nil, mode = nil)
@@ -18,6 +43,12 @@ module DataCycleCore
           query_string = Thing.send(:sanitize_sql_for_conditions, ['things.validity_range @> ?::tstzrange', date_range])
           reflect(
             @query.where(query_string)
+          )
+        end
+
+        def in_validity_period(current_date = Time.zone.now)
+          reflect(
+            @query.where(in_range(thing[:validity_range], cast_tstz(current_date)))
           )
         end
 
@@ -62,6 +93,30 @@ module DataCycleCore
 
           reflect(
             @query.where.not(query_string)
+          )
+        end
+
+        def event_end_time(time)
+          time = DataCycleCore::MasterData::DataConverter.string_to_datetime(time)
+          reflect(
+            @query.where(thing[:start_date].lteq(Arel::Nodes.build_quoted(time.iso8601)))
+          )
+        end
+
+        def event_from_time(time)
+          time = DataCycleCore::MasterData::DataConverter.string_to_datetime(time)
+          reflect(
+            @query.where(thing[:end_date].gteq(Arel::Nodes.build_quoted(time.iso8601)))
+          )
+        end
+
+        def sort_by_proximity(date = Time.zone.now)
+          reflect(
+            @query.reorder(
+              absolute_date_diff(thing[:end_date], Arel::Nodes.build_quoted(date.iso8601)),
+              absolute_date_diff(thing[:start_date], Arel::Nodes.build_quoted(date.iso8601)),
+              thing[:start_date]
+            )
           )
         end
 
