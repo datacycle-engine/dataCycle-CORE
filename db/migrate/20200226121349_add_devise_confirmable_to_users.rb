99c194fcd470f803dd2791b37c0edb6cbb9bef3a
Patrick Rainer <rainer@pixelpoint.at>
2020-02-26 16:21:13 +0100

    update user in confirmation migration

--- db/migrate/20200226121349_add_devise_confirmable_to_users.rb
+++ db/migrate/20200226121349_add_devise_confirmable_to_users.rb
@@ -6,5 +6,7 @@ class AddDeviseConfirmableToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :confirmed_at, :datetime
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
+
+    DataCycleCore::User.update_all(confirmed_at: Time.zone.now - 1.day)
   end
 end
