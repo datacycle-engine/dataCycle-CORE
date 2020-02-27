cad50b476136da0cd2a37633da8aa0d83502c440
Manuel Mitterer <mitterer@pixelpoint.at>
2020-02-27 13:08:52 +0100

    add possibility to disable api versions

--- db/migrate/20200226121349_add_devise_confirmable_to_users.rb
+++ db/migrate/20200226121349_add_devise_confirmable_to_users.rb
@@ -7,6 +7,8 @@ class AddDeviseConfirmableToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :confirmation_sent_at, :datetime
     add_column :users, :unconfirmed_email, :string
 
+    DataCycleCore::User.connection.schema_cache.clear!
+    DataCycleCore::User.reset_column_information
     DataCycleCore::User.update_all(confirmed_at: Time.zone.now - 1.day)
   end
 end
