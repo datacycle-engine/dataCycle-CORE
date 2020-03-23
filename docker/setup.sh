8803b40d598a2368aaa5b50b4f9681ed75ae238b
Patrick Rainer <rainer@pixelpoint.at>
2020-03-23 10:49:30 +0100

    minor fixes after merge

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+# unset BUNDLE_PATH
+# unset BUNDLE_BIN
 gem install bundler
-#bundle update
+# bundle update
 bundle install
 cd test/dummy
-#RUBYOPT="--jit-verbose=1" rails s -b 0.0.0.0 -p 3000
+# RUBYOPT="--jit-verbose=1" rails s -b 0.0.0.0 -p 3000
 rails s -b 0.0.0.0 -p 3000
