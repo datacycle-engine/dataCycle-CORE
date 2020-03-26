66f8842d6c7da13a581ebaa43c7a7895a299e689
Manuel Mitterer <mitterer@pixelpoint.at>
2020-03-26 15:09:49 +0100

    add generic webhook

---
+++ app/models/data_cycle_core/export/generic/endpoint.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Export
+    module Generic
+      class Endpoint
+        def transformations
+          DataCycleCore::Export::Generic::Transformations
+        end
+
+        def initialize(**options)
+          @host = options.dig(:host)
+          @token = options.dig(:token)
+        end
+
+        def content_request(method: :post, path:, transformation:, utility_object:, data:)
+          @response = Faraday.run_request(method, File.join(@host, path), transformations.try(transformation, utility_object, data), { 'Content-Type' => 'application/json' })
+          @output_file = DataCycleCore::Generic::Logger::LogFile.new("#{utility_object.external_system.name.underscore_blanks}_webhook")
+
+          begin
+            @response = Faraday.run_request(method, File.join(@host, path), transformations.try(transformation, utility_object, data), { 'Content-Type' => 'application/json' })
+            @output_file.info("#{@response&.env&.dig(:method)&.to_s&.upcase} #{@response&.env&.dig(:url)} #{@response.body}", "#{data&.id} - #{@response&.env&.dig(:status)} #{@response&.env&.dig(:reason_phrase)}")
+            @output_file.try(:close)
+          rescue Faraday::Error => e
+            @output_file.error(e.try(:response)&.dig(:status), "#{data&.id}_#{I18n.locale}", nil, e.try(:response)&.dig(:body))
+            @output_file.try(:close)
+            raise e
+          end
+
+          @response
+        end
+      end
+    end
+  end
+end
