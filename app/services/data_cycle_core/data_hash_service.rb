af6cbec07abee668ca3e72d9384b002ba76a6e98
Manuel Mitterer <mitterer@pixelpoint.at>
2020-04-06 13:18:08 +0200

    small fix for content creation helper

--- app/services/data_cycle_core/data_hash_service.rb
+++ app/services/data_cycle_core/data_hash_service.rb
@@ -56,8 +56,7 @@ module DataCycleCore
 
       return object if object_params[:datahash].nil? && translations.nil?
 
-      datahash = DataCycleCore::DataHashService.flatten_datahash_value((object_params[:datahash] || {}).merge(translations&.delete(I18n.locale.to_s) || {}), object.schema)
-
+      datahash = DataCycleCore::DataHashService.flatten_datahash_value((object_params[:datahash] || {}).merge(translations&.delete(locale.to_s) || {}), object.schema)
       datahash['permitted_creator'] = current_user.try(:role).try(:rank) == 3 ? [DataCycleCore::Classification.find_by(name: 'Markt Office').try(:id)] : [DataCycleCore::Classification.find_by(name: 'Team CM').try(:id)]
 
       translations&.each do |l, locale_hash|
