9dc902da06d61e5cbf74230f47263b6c9c0acdfe
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-04-07 16:09:58 +0200

    enable delete for outdoor_active external_system

--- app/models/data_cycle_core/export/outdoor_active/delete.rb
+++ app/models/data_cycle_core/export/outdoor_active/delete.rb
@@ -11,8 +11,15 @@ module DataCycleCore
           Functions.delete(utility_object: utility_object, data: data)
         end
 
-        def self.filter(_data, _external_system)
-          false
+        def self.filter(data, external_system)
+          job_id = data.external_system_data(external_system)&.dig('job_id')
+          (
+            (data.template_name == 'POI' || data.template_name == 'Unterkunft') &&
+            data&.external_source&.name == 'Feratel Kärnten' &&
+            Functions.outdoor_active_system_categories(data, external_system).size.positive? &&
+            Functions.outdoor_active_system_source_keys(data, external_system).size.positive? &&
+            job_id.blank?
+          )
         end
       end
     end
