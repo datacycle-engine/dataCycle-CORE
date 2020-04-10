bced6975c2f75ed6ee58719af652649d819547c8
Patrick Rainer <rainer@pixelpoint.at>
2020-04-10 13:06:18 +0200

    moved classificaion filter method to correct module

--- app/models/data_cycle_core/filter/common/external.rb
+++ app/models/data_cycle_core/filter/common/external.rb
@@ -20,14 +20,6 @@ module DataCycleCore
           )
         end
 
-        def with_classification_aliases_and_treename(definition)
-          return self if definition.blank?
-          raise StandardError, 'Missing data definition: treeLabel' if definition.dig('treeLabel').blank?
-          raise StandardError, 'Missing data definition: aliases' if definition.dig('aliases').blank?
-
-          with_classification_aliases(definition.dig('treeLabel'), definition.dig('aliases'))
-        end
-
         def with_external_source_names(definition)
           return self if definition.blank?
           raise StandardError, 'Missing data definition: names' if definition.dig('names').blank?
