ac36a8c941ae3989fa34afe338e19b88cfcf0587
Manuel Mitterer <mitterer@pixelpoint.at>
2020-04-24 10:00:02 +0200

    small bugfix

--- app/models/data_cycle_core/export/media_archive/endpoint.rb
+++ app/models/data_cycle_core/export/media_archive/endpoint.rb
@@ -5,7 +5,7 @@ module DataCycleCore
     module MediaArchive
       class Endpoint < DataCycleCore::Export::Generic::Endpoint
         def path_transformation(data, external_system, path_type)
-          id = data&.external_source&.name&.inlucde?('Medienarchiv') && data&.external_key.present? ? data&.external_key : data&.id
+          id = data&.external_source&.name&.include?('Medienarchiv') && data&.external_key.present? ? data&.external_key : data&.id
           format(external_system.config.dig('push_config', path_type.to_s, 'path') || external_system.config.dig('push_config', 'path') || path_type.to_s, id: id)
         end
       end
