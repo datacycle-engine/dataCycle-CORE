566f0c54cfdfeed0fad155dc0e9fff6931785dd9
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-06-02 10:21:17 +0200

    minor fix for v-ticket migration/rollback

--- db/migrate/20200529140637_migrate_v_ticket_entities.rb
+++ db/migrate/20200529140637_migrate_v_ticket_entities.rb
@@ -23,16 +23,19 @@ class MigrateVTicketEntities < ActiveRecord::Migration[5.2]
     return if vticket.blank?
     vticket.imported_things.where(template_name: 'Event').each do |item|
       next unless item.external_key.match(event_prefix)
+      next if vticket.imported_things.where(external_key: item.external_key[event_prefix.size..-1].to_s).present?
       item.external_key = item.external_key[event_prefix.size..-1]
       item.save!
     end
     vticket.imported_things.where(template_name: 'Bild').each do |item|
       next unless item.external_key.match(image_prefix)
+      next if vticket.imported_things.where(external_key: item.external_key[image_prefix.size..-1].to_s).present?
       item.external_key = item.external_key[image_prefix.size..-1]
       item.save!
     end
     vticket.imported_things.where(template_name: 'Örtlichkeit').each do |item|
       next unless item.external_key.match(location_prefix)
+      next if vticket.imported_things.where(external_key: item.external_key[location_prefix.size..-1].to_s).present?
       item.external_key = item.external_key[location_prefix.size..-1]
       item.save!
     end
