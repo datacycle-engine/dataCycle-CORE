8f57c9bbf8942fd618db51ed44c382983ea621a4
Benjamin Preisig <preisig@pixelpoint.at>
2020-07-21 15:18:25 +0200

    Fix Bug for /users
    - check if method classification_aliases is available - which it is not for the users grid

--- app/models/data_cycle_core/feature/tile_border_color.rb
+++ app/models/data_cycle_core/feature/tile_border_color.rb
@@ -5,7 +5,7 @@ module DataCycleCore
     class TileBorderColor < Base
       class << self
         def class_string(content)
-          return '' unless enabled? && configuration.dig('tree_label').present?
+          return '' unless enabled? && configuration.dig('tree_label').present? && content.respond_to?(:classification_aliases)
 
           content&.classification_aliases&.to_a&.filter { |c| c.classification_tree_label&.name == 'Eyebase - Status' }&.map { |c| "#{c.classification_tree_label&.name}_#{c.internal_name}".underscore_blanks }&.join(' ')
         end
