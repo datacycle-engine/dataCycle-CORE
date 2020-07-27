d088414e321002bafd4684a843b4368b9fb0fd3e
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-07-27 14:05:45 +0200

    update data_migration for Feratel languages

--- db/migrate/20200724094112_mark_feratel_mongo_data_deleted_all_languages.rb
+++ db/migrate/20200724094112_mark_feratel_mongo_data_deleted_all_languages.rb
@@ -9,10 +9,23 @@ class MarkFeratelMongoDataDeletedAllLanguages < ActiveRecord::Migration[5.2]
         mongo_collection.where({ "dump.de.deleted_at": { '$exists' => true }, "dump.en.deleted_at": { '$exists' => false } }).find_all do |item|
           item.dump.each_key do |locale|
             next if locale == 'de'
-            item.dump[locale]['deleted_at'] =                 item.dump['de']['deleted_at']                 if item.dump['de']['deleted_at'].present?
-            item.dump[locale]['last_seen_before_delete'] =    item.dump['de']['last_seen_before_delete']    if item.dump['de']['last_seen_before_delete'].present?
-            item.dump[locale]['delete_reason'] =              item.dump['de']['delete_reason']              if item.dump['de']['delete_reason'].present?
+            next if item.dump['de']['deleted_at'].blank?
+            item.dump[locale]['deleted_at'] = item.dump['de']['deleted_at']
+            item.dump[locale]['last_seen_before_delete'] = item.dump['de']['last_seen_before_delete'] if item.dump['de']['last_seen_before_delete'].present?
+            item.dump[locale]['delete_reason'] = item.dump['de']['delete_reason'] if item.dump['de']['delete_reason'].present?
           end
+          item.data_has_changed = false
+          item.save!
+        end
+        mongo_collection.where({ "dump.de.deleted_at": { '$exists' => false }, "dump.en.deleted_at": { '$exists' => false } }).find_all do |item|
+          item.dump.each_key do |locale|
+            next if locale == 'de'
+            next if item.dump[locale]['Descriptions'].present?
+            item.dump[locale]['deleted_at'] = Time.zone.now
+            item.dump[locale]['last_seen_before_delete'] = item.seen_at
+            item.dump[locale]['delete_reason'] = 'Datamigration --> not german, has no <Descriptions> attribute --> not translated!'
+          end
+          item.data_has_changed = false
           item.save!
         end
       end
