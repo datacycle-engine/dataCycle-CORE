66c1178310de74ab18e7ed102950287c5e50c12c
Manuel MItterer <mitterer@pixelpoint.at>
2020-08-03 15:46:19 +0200

    small fix for toursprung transformation

--- app/models/data_cycle_core/export/toursprung/transformations.rb
+++ app/models/data_cycle_core/export/toursprung/transformations.rb
@@ -6,8 +6,8 @@ module DataCycleCore
       module Transformations
         def self.json_partial(utility_object, data)
           content_data = {}
-          content_data[:name] = data.translated_locales.collect { |l| [l, I18n.with_locale(l) { data.try(:name) }] }.to_h.reject { |_k, v| v.blank? }
-          content_data[:text] = data.translated_locales.collect { |l| [l, I18n.with_locale(l) { data.try(:text) }] }.to_h.reject { |_k, v| v.blank? }
+          content_data[:name] = data.translated_locales&.collect { |l| [l, I18n.with_locale(l) { data.try(:name) }] }&.to_h&.reject { |_k, v| v.blank? }
+          content_data[:text] = data.translated_locales&.collect { |l| [l, I18n.with_locale(l) { data.try(:text) }] }&.to_h&.reject { |_k, v| v.blank? }
 
           {
             resource: utility_object.external_system.credentials(:export).dig('resource'),
@@ -15,7 +15,7 @@ module DataCycleCore
             lat: data.try(:tour)&.points&.first&.y,
             lng: data.try(:tour)&.points&.first&.x,
             points: data.try(:tour)&.points&.map { |p| [p.y, p.x] }&.to_json,
-            data: content_data.to_json
+            data: content_data.compact.to_json
           }
         end
       end
