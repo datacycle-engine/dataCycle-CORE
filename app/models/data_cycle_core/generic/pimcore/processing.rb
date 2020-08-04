a18fec1c10ce77f1f46ef9da824b13005a22520d
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-08-04 15:49:12 +0200

    update pimcore importer for new variance in events, add importer for series events

--- app/models/data_cycle_core/generic/pimcore/processing.rb
+++ app/models/data_cycle_core/generic/pimcore/processing.rb
@@ -54,6 +54,16 @@ module DataCycleCore
           )
         end
 
+        def self.process_event_series(utility_object, raw_data, config)
+          DataCycleCore::Generic::Common::ImportFunctions.process_step(
+            utility_object: utility_object,
+            raw_data: raw_data,
+            transformation: DataCycleCore::Generic::Pimcore::Transformations.to_event_series(utility_object.external_source.id),
+            default: { template: 'Event' },
+            config: config
+          )
+        end
+
         def self.process_infrastructure(utility_object, raw_data, config)
           DataCycleCore::Generic::Common::ImportFunctions.process_step(
             utility_object: utility_object,
