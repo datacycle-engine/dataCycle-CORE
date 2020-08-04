3e7f1c5cfb377da5cfbc3d607c6f89e4fd853bf1
Benjamin Preisig <preisig@pixelpoint.at>
2020-08-04 12:14:05 +0200

    Add Classification for ItemAvailability
    - add new classification for ItemAvailability
    - extend currencies
    -  add 'tel:' to accepted url-templates

--- app/models/data_cycle_core/master_data/validators/string.rb
+++ app/models/data_cycle_core/master_data/validators/string.rb
@@ -55,7 +55,7 @@ module DataCycleCore
 
         def url(data)
           return if data.blank?
-          schemes = ['http', 'https', 'mailto', 'ftp', 'sftp']
+          schemes = ['http', 'https', 'mailto', 'ftp', 'sftp', 'tel']
 
           begin
             (@error[:error][@template_key] ||= []) << I18n.t(:url, scope: [:validation, :errors], data: data, locale: DataCycleCore.ui_language) unless schemes.include?(Addressable::URI.parse(data)&.scheme)
