3d78cbc94c301f158930f2e990ca0579e5f40dc4
Patrick Rainer <rainer@pixelpoint.at>
2020-08-10 13:08:40 +0200

    fixed classification alias encoding

--- app/models/data_cycle_core/classification_alias.rb
+++ app/models/data_cycle_core/classification_alias.rb
@@ -265,7 +265,7 @@ module DataCycleCore
       return unless name_i18n_changed? # && internal_name.blank?
       available_translation = I18n.available_locales.drop_while { |locale| name(locale: locale).blank? }
       return if available_translation.blank?
-      self.internal_name = name(locale: available_translation.first)
+      self.internal_name = DataCycleCore::MasterData::DataConverter.string_to_string(name(locale: available_translation.first))
     end
 
     def update_primary_classification
@@ -274,7 +274,7 @@ module DataCycleCore
       return if primary_classification.nil?
 
       primary_classification.tap do |c|
-        c.name = name
+        c.name = DataCycleCore::MasterData::DataConverter.string_to_string(name)
         c.save!
       end
     end
