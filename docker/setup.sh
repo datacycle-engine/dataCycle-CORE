65a0cf3732851f9b416263ffbcc31b0010f5c7a5
Manuel MItterer <mitterer@pixelpoint.at>
2020-08-27 08:23:44 +0200

    update setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# unset BUNDLE_PATH
-# unset BUNDLE_BIN
 echo "$(ip route|awk '/default/ { print $3 }') dockerhost" >> /etc/hosts
 gem install bundler
 #bundle update
 bundle install
 
 cd test/dummy
+rm -f tmp/pids/server.pid
 
 # RUBYOPT="--jit-verbose=1" rails s -b 0.0.0.0 -p 3000
 #rails s -b 0.0.0.0 -p 3000
