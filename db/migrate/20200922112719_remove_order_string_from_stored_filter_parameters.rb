3799d0188e8b1c3885caf68b3321fe2aeac8cb92
Patrick Rainer <rainer@pixelpoint.at>
2020-09-22 13:44:21 +0200

    fixed rails migration

--- db/migrate/20200922112719_remove_order_string_from_stored_filter_parameters.rb
+++ db/migrate/20200922112719_remove_order_string_from_stored_filter_parameters.rb
@@ -3,7 +3,7 @@
 class RemoveOrderStringFromStoredFilterParameters < ActiveRecord::Migration[5.2]
   def up
     DataCycleCore::StoredFilter.find_each do |filter|
-      order_hash = filter.parameters&.select { |f| f['t'] == 'order' && f['v'].is_a?(Hash) }&.first || []
+      order_hash = filter.parameters&.select { |f| f['t'] == 'order' }&.first || []
       next unless order_hash&.present?
       parameters = filter.parameters.without(order_hash)
       filter.parameters = parameters
