601b7d94c8a36ec729146a980929b102a0ebec9b
Manuel Mitterer <mitterer@pixelpoint.at>
2020-10-02 10:35:28 +0200

    [Fehler #22317] fix filter by language and add minitest-hooks gem for before(:all)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,6 +31,7 @@ require File.expand_path('../test/dummy/config/environment.rb', __dir__)
 # ActiveRecord::Migrator.migrations_paths << File.expand_path('../../db/migrate', __FILE__)
 # ActiveRecord::Migration.maintain_test_schema!
 require 'rails/test_help'
+require 'minitest/hooks'
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
