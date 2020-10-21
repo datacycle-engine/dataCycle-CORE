af13d4415f408547358249354fa935d7024e6bfa
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-10-21 15:14:48 +0200

    fix external_key conversion problem when new external data is generated

--- app/models/data_cycle_core/generic/common/import_functions.rb
+++ app/models/data_cycle_core/generic/common/import_functions.rb
@@ -46,7 +46,8 @@ module DataCycleCore
             global_attributes[attribute] = content.attribute_to_h(attribute).presence if content.respond_to?(attribute)
           end
 
-          global_data = global_attributes.merge(data).except('external_key')
+          global_data = global_attributes.merge(data)
+          global_data = global_data.except('external_key') unless created
 
           if config&.dig(:asset_type).present?
             if utility_object.asset_download
