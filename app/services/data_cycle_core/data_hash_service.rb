261d36447712dd5b17e6c362daa341fce6bb792c
Manuel Mitterer <mitterer@pixelpoint.at>
2020-11-04 09:48:03 +0100

    remove default_value for permitted_creator

--- app/services/data_cycle_core/data_hash_service.rb
+++ app/services/data_cycle_core/data_hash_service.rb
@@ -61,7 +61,7 @@ module DataCycleCore
       return object if object_params[:datahash].nil? && translations.nil?
 
       datahash = DataCycleCore::DataHashService.flatten_datahash_value((object_params[:datahash] || {}).merge(translations&.delete(locale.to_s) || {}), object.schema)
-      datahash['permitted_creator'] = current_user.try(:role).try(:rank) == 3 ? [DataCycleCore::Classification.find_by(name: 'Markt Office').try(:id)] : [DataCycleCore::Classification.find_by(name: 'Team CM').try(:id)] if object.property_names.include?('permitted_creator')
+      save_time = Time.zone.now
 
       I18n.with_locale(locale) do
         valid = object.set_data_hash(data_hash: datahash, current_user: current_user, prevent_history: true, source: source, new_content: true)
