3854e481535a1c85a208fad800f9f6736a8214f3
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-11-25 15:48:34 +0100

    outdoorActive export change external_system_sync helper functions

--- app/models/data_cycle_core/export/outdoor_active/webhook.rb
+++ app/models/data_cycle_core/export/outdoor_active/webhook.rb
@@ -14,7 +14,7 @@ module DataCycleCore
 
         def error(_job, _exception)
           data = DataCycleCore::Thing.find(@data.id)
-          data.add_external_system_data(@external_system, nil, 'error')
+          data.add_external_system_data(@external_system, nil, 'error', nil, false)
           raise DataCycleCore::Generic::Common::Error::GenericError, "OutdoorActive sync job is failed(error), #{@external_system_data}"
         rescue DataCycleCore::Generic::Common::Error::GenericError => e
           Appsignal.send_error(e, nil, 'background')
@@ -22,7 +22,7 @@ module DataCycleCore
 
         def failure(_job)
           data = DataCycleCore::Thing.find(@data.id)
-          data.add_external_system_data(@external_system, nil, 'failure')
+          data.add_external_system_data(@external_system, nil, 'failure', nil, false)
           raise DataCycleCore::Generic::Common::Error::GenericError, "OutdoorActive sync job is failed(failure), #{@external_system_data}"
         rescue DataCycleCore::Generic::Common::Error::GenericError => e
           Appsignal.send_error(e, nil, 'background')
@@ -35,14 +35,14 @@ module DataCycleCore
 
           case job_result.dig('job_status')
           when 'waiting'
-            data.add_external_system_data(@external_system, job_result, 'pending', 'export', external_key)
+            data.add_external_system_data(@external_system, job_result, 'pending', 'export', external_key, false)
           when 'running'
-            data.add_external_system_data(@external_system, job_result, 'pending', 'export', external_key)
+            data.add_external_system_data(@external_system, job_result, 'pending', 'export', external_key, false)
             raise DataCycleCore::Generic::Common::Error::GenericError, "OutdoorActive job is still running with id #{job_result.dig('job_id')}"
           when 'jobnotfound', 'failed'
-            data.add_external_system_data(@external_system, job_result, 'failure', 'export', external_key)
+            data.add_external_system_data(@external_system, job_result, 'failure', 'export', external_key, false)
           when 'done'
-            data.add_external_system_data(@external_system, job_result, 'success', 'export', external_key)
+            data.add_external_system_data(@external_system, job_result, 'success', 'export', external_key, false)
           else
             raise DataCycleCore::Generic::Common::Error::GenericError, "Unkown job status: #{job_result.dig('job_status')}"
           end
