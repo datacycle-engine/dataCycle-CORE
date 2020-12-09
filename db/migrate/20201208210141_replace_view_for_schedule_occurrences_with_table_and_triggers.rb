4ae3c9eb1335ba5d23b5c4fa90b1be33493bf649
Johannes Zlattinger <johannes@zlattinger.net>
2020-12-09 21:12:19 +0100

    [WIP] [#22419] first working solution with table and triggers

--- db/migrate/20201208210141_replace_view_for_schedule_occurrences_with_table_and_triggers.rb
+++ db/migrate/20201208210141_replace_view_for_schedule_occurrences_with_table_and_triggers.rb
@@ -3,17 +3,21 @@
 class ReplaceViewForScheduleOccurrencesWithTableAndTriggers < ActiveRecord::Migration[5.2]
   def up
     execute <<-SQL.squish
+      DROP VIEW IF EXISTS schedule_occurrences;
+
       CREATE TABLE schedule_occurrences (
       	id UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-      	schedule_id UUID NOT NULL REFERENCES schedules(id),
-      	thing_id UUID NOT NULL REFERENCES things(id),
+      	schedule_id UUID NOT NULL REFERENCES schedules(id) ON DELETE CASCADE,
+      	thing_id UUID NOT NULL REFERENCES things(id) ON DELETE CASCADE,
       	duration INTERVAL,
       	occurrence TSTZRANGE NOT NULL
       );
 
-      CREATE PROCEDURE generate_schedule_occurences(schedule_ids UUID[]) LANGUAGE PLPGSQL AS $$
+      CREATE OR REPLACE FUNCTION generate_schedule_occurences(schedule_ids UUID[]) RETURNS UUID[] LANGUAGE PLPGSQL AS $$
+      DECLARE
+      	schedule_occurrence_ids UUID[];
       BEGIN
-      	RAISE NOTICE '%', schedule_ids;
+      	DELETE FROM schedule_occurrences WHERE schedule_id || '{}'::UUID[] <@ schedule_ids;
 
       	WITH occurences AS (
       		SELECT
@@ -26,7 +30,7 @@ class ReplaceViewForScheduleOccurrencesWithTableAndTriggers < ActiveRecord::Migr
               	END AS duration,
       		    unnest(get_occurrences(schedules.rrule::rrule, schedules.dtstart)) AS occurence
       		FROM schedules
-      		WHERE schedules.relation::text = 'event_schedule'::text AND schedules.id || '{}'::UUID[] <@ schedule_ids
+      		WHERE schedules.relation::text = 'event_schedule'::text AND id || '{}'::UUID[] <@ schedule_ids
       		UNION
       		SELECT
       			schedules.id,
@@ -38,7 +42,7 @@ class ReplaceViewForScheduleOccurrencesWithTableAndTriggers < ActiveRecord::Migr
               	END AS duration,
       		    unnest(schedules.rdate) AS occurence
       		FROM schedules
-      		WHERE schedules.relation::text = 'event_schedule'::text AND schedules.id || '{}'::UUID[] <@ schedule_ids
+      		WHERE schedules.relation::text = 'event_schedule'::text AND id || '{}'::UUID[] <@ schedule_ids
       		UNION
       		SELECT
       			schedules.id,
@@ -50,8 +54,7 @@ class ReplaceViewForScheduleOccurrencesWithTableAndTriggers < ActiveRecord::Migr
               	END AS duration,
       		    schedules.dtstart AS occurence
       		FROM schedules
-      		WHERE schedules.relation::text = 'event_schedule'::text AND schedules.rrule IS NULL AND
-            schedules.id || '{}'::UUID[] <@ schedule_ids
+      		WHERE schedules.relation::text = 'event_schedule'::text AND schedules.rrule IS NULL AND id || '{}'::UUID[] <@ schedule_ids
       	)
       	INSERT INTO schedule_occurrences (schedule_id, thing_id, duration, occurrence)
       		SELECT
@@ -61,43 +64,31 @@ class ReplaceViewForScheduleOccurrencesWithTableAndTriggers < ActiveRecord::Migr
       		    tstzrange(occurences.occurence, occurences.occurence + occurences.duration) AS occurrence
       		FROM occurences
       		WHERE occurences.id || '{}'::UUID[] <@ schedule_ids;
+
+      	SELECT ARRAY_AGG(id) INTO schedule_occurrence_ids
+      	FROM schedule_occurrences WHERE schedule_id || '{}'::UUID[] <@ schedule_ids;
+
+      	RETURN schedule_occurrence_ids;
       END;$$;
 
-      CREATE FUNCTION generate_schedule_occurences_trigger() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
+      CREATE OR REPLACE FUNCTION generate_schedule_occurences_trigger() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
       BEGIN
-      	CALL generate_schedule_occurences(NEW.id || '{}'::UUID[]);
+      	PERFORM generate_schedule_occurences(NEW.id || '{}'::UUID[]);
 
       	RETURN NEW;
       END;$$;
 
       CREATE TRIGGER generate_schedule_occurences_trigger AFTER INSERT OR UPDATE ON schedules FOR EACH ROW EXECUTE FUNCTION generate_schedule_occurences_trigger();
 
-
-      CREATE PROCEDURE remove_schedule_occurences(schedule_ids UUID[]) LANGUAGE PLPGSQL AS $$
-      BEGIN
-      	DELETE FROM schedule_occurrences WHERE schedule_id || '{}'::UUID[] <@ schedule_ids;
-      END;$$;
-
-
-      CREATE FUNCTION remove_schedule_occurences_trigger() RETURNS TRIGGER LANGUAGE PLPGSQL AS $$
-      BEGIN
-      	CALL remove_schedule_occurences(OLD.id || '{}'::UUID[]);
-
-      	RETURN NEW;
-      END;$$;
-
-      CREATE TRIGGER remove_schedule_occurences_trigger BEFORE UPDATE OR DELETE ON schedules FOR EACH ROW EXECUTE FUNCTION remove_schedule_occurences_trigger();
+      SELECT generate_schedule_occurences(ARRAY_AGG(id)) FROM schedules WHERE schedules.relation::text = 'event_schedule'::text;
     SQL
   end
 
   def down
     execute <<-SQL.squish
-      DROP TRIGGER IF EXISTS remove_schedule_occurences_trigger ON schedules;
-      DROP FUNCTION IF EXISTS remove_schedule_occurences_trigger;
-      DROP PROCEDURE IF EXISTS remove_schedule_occurences;
       DROP TRIGGER IF EXISTS generate_schedule_occurences_trigger ON schedules;
       DROP FUNCTION IF EXISTS generate_schedule_occurences_trigger;
-      DROP PROCEDURE IF EXISTS generate_schedule_occurences;
+      DROP FUNCTION IF EXISTS generate_schedule_occurences;
       DROP TABLE IF EXISTS schedule_occurrences;
     SQL
   end
