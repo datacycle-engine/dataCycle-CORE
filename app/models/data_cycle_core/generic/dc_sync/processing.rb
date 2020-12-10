24b123e320ca43b391ee4bd3af24eedd58540880
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-12-10 16:01:29 +0100

    cleanup process_things, add fixtures

--- app/models/data_cycle_core/generic/dc_sync/processing.rb
+++ app/models/data_cycle_core/generic/dc_sync/processing.rb
@@ -6,19 +6,16 @@ module DataCycleCore
       module Processing
         def self.process_things(utility_object, raw_data, template, config)
           item_template = get_template(raw_data)
-          linked_key_translation = {}
           raw_data.dig('included')&.each do |included_item|
             attribute_name = included_item.dig('attribute_name')
             next if attribute_name.blank?
             next unless item_template.property_names.include?(attribute_name)
-            linked_key_translation[included_item.dig('attribute_name')] = {}
-            new_item = DataCycleCore::Generic::DcSync::Processing.process_things(
+            DataCycleCore::Generic::DcSync::Processing.process_things(
               utility_object,
               included_item,
               get_template(included_item).template_name,
               utility_object.external_source.import_config.dig(:things, :transformations, :thing)
             )
-            linked_key_translation[attribute_name][new_item.external_key] = new_item.id
           end
           processed_thing = nil
           raw_data.except('included', 'attribute_name', 'include_translation').each_key do |locale|
