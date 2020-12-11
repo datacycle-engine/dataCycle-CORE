1f17222b37b0a76bfd3846b4183d2a5c23404088
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-12-11 15:25:32 +0100

    add functionality to account for linked data already e.g. marked as duplicate

--- app/models/data_cycle_core/generic/dc_sync/processing.rb
+++ app/models/data_cycle_core/generic/dc_sync/processing.rb
@@ -6,23 +6,31 @@ module DataCycleCore
       module Processing
         def self.process_things(utility_object, raw_data, template, config)
           item_template = get_template(raw_data)
+          linked_key_translation = {}
           raw_data.dig('included')&.each do |included_item|
             attribute_name = included_item.dig('attribute_name')
             next if attribute_name.blank?
             next unless item_template.property_names.include?(attribute_name)
-            DataCycleCore::Generic::DcSync::Processing.process_things(
-              utility_object,
-              included_item,
-              get_template(included_item).template_name,
-              utility_object.external_source.import_config.dig(:things, :transformations, :thing)
-            )
+            linked_key_translation[included_item.dig('attribute_name')] ||= {}
+            locale = included_item.except('included', 'attribute_name').keys.first
+            found_key = DataCycleCore::Generic::Common::Functions.find_thing_ids(external_system_id: utility_object.external_source.id, external_key: included_item[locale]['id'], content_type: DataCycleCore::Thing, limit: 1)
+            linked_key_translation[attribute_name][included_item[locale]['id']] = found_key.first
+            if found_key.blank? || found_key.first == included_item[locale]['id']
+              new_item = DataCycleCore::Generic::DcSync::Processing.process_things(
+                utility_object,
+                included_item,
+                get_template(included_item).template_name,
+                utility_object.external_source.import_config.dig(:things, :transformations, :thing)
+              )
+              linked_key_translation[attribute_name][included_item[locale]['id']] = new_item.id
+            end
           end
           processed_thing = nil
           raw_data.except('included', 'attribute_name', 'include_translation').each_key do |locale|
             I18n.with_locale(locale) do
               processed_thing = DataCycleCore::Generic::Common::ImportFunctions.process_step(
                 utility_object: utility_object,
-                raw_data: raw_data[locale].except('included'),
+                raw_data: raw_data[locale].except('included').merge({ 'include_translation' => linked_key_translation }),
                 transformation: DataCycleCore::Generic::DcSync::Transformations.to_thing(utility_object.external_source.id),
                 default: { template: template },
                 config: config
--- app/models/data_cycle_core/generic/dc_sync/transformation_functions.rb
+++ app/models/data_cycle_core/generic/dc_sync/transformation_functions.rb
@@ -12,10 +12,8 @@ module DataCycleCore
 
         def self.transform_embedded(data, external_source_id)
           external_source = DataCycleCore::ExternalSystem.find(external_source_id)
-          # thing = DataCycleCore::Thing.find_by(id: s.dig('id'))
           template = DataCycleCore::Thing.find_by(template_name: data.dig('template_name'), template: true)
           template.embedded_property_names&.each do |embedded|
-            # next if data.dig(embedded).blank?
             data[embedded] = data[embedded]&.map { |item|
               handle_embedded(item, external_source)
             }&.try(:compact)
@@ -28,7 +26,6 @@ module DataCycleCore
           template = DataCycleCore::Thing.find_by(template_name: data.dig(I18n.locale.to_s, 'template_name'))
           return nil if template.blank?
           embedded = data[I18n.locale.to_s]
-          # embedded['external_system_sync'] = (embedded['external_system_syncs'] || []).push('external_key' => embedded.dig('id'), 'external_source_id' => embedded.dig('external_source_id'))
           embedded['external_key'] = embedded['id']
           embedded['external_source_id'] = external_source.id
           create_thing(embedded['id'], template, external_source) if DataCycleCore::Thing.find_by(id: embedded['id']).blank?
@@ -36,7 +33,6 @@ module DataCycleCore
 
           embedded = embedded.merge(transform_embedded(embedded, external_source.id))
           embedded
-          # make everything recursive
           # treat linked
         end
 
@@ -63,15 +59,15 @@ module DataCycleCore
           data
         end
 
-        # def self.transform_linked_keys(data)
-        #   translated_properties = data['include_translation']
-        #   translated_properties&.each_key do |property_name|
-        #     data[property_name] = Array.wrap(data[property_name])
-        #       .map { |item| translated_properties[property_name][item] }
-        #       .compact
-        #   end
-        #   data
-        # end
+        def self.transform_linked_keys(data)
+          translated_properties = data['include_translation']
+          translated_properties&.each_key do |property_name|
+            data[property_name] = Array.wrap(data[property_name])
+              .map { |item| translated_properties[property_name][item] }
+              .compact
+          end
+          data
+        end
       end
     end
   end
