2d6538c24f018ac1c4ed7f6c64a3b972fd5db3e9
Martin Oehzelt <oehzelt@pixelpoint.at>
2020-12-11 09:32:01 +0100

    towards handling embedded

--- app/models/data_cycle_core/generic/dc_sync/transformation_functions.rb
+++ app/models/data_cycle_core/generic/dc_sync/transformation_functions.rb
@@ -10,17 +10,17 @@ module DataCycleCore
         import Transproc::Recursion
         import DataCycleCore::Generic::Common::Functions
 
-        def self.transform_embedded(s, external_source_id)
+        def self.transform_embedded(data, external_source_id)
           external_source = DataCycleCore::ExternalSystem.find(external_source_id)
           # thing = DataCycleCore::Thing.find_by(id: s.dig('id'))
-          template = DataCycleCore::Thing.find_by(template_name: s.dig('template_name'), template: true)
-          template.embedded_property_names.each do |embedded|
-            next if s.dig(embedded).blank?
-            s[embedded] = s[embedded].map do |item|
+          template = DataCycleCore::Thing.find_by(template_name: data.dig('template_name'), template: true)
+          template.embedded_property_names&.each do |embedded|
+            # next if data.dig(embedded).blank?
+            data[embedded] = data[embedded]&.map { |item|
               handle_embedded(item, external_source)
-            end
+            }&.try(:compact)
           end
-          s
+          data
         end
 
         def self.handle_embedded(data, external_source)
@@ -28,11 +28,13 @@ module DataCycleCore
           template = DataCycleCore::Thing.find_by(template_name: data.dig(I18n.locale.to_s, 'template_name'))
           return nil if template.blank?
           embedded = data[I18n.locale.to_s]
-          embedded['external_system_sync'] = embedded['external_system_syncs'].push('external_key' => embedded.dig('id'), 'external_source_id' => embedded.dig('external_source_id'))
+          embedded['external_system_sync'] = (embedded['external_system_syncs'] || []).push('external_key' => embedded.dig('id'), 'external_source_id' => embedded.dig('external_source_id'))
           embedded['external_key'] = embedded['id']
           embedded['external_source_id'] = external_source.id
           create_thing(embedded['id'], template, external_source) if DataCycleCore::Thing.find_by(id: embedded['id']).blank?
           embedded.delete('external_system_syncs')
+
+          embedded = embedded.merge(transform_embedded(embedded, external_source.id))
           embedded
           # make everything recursive
           # treat linked
