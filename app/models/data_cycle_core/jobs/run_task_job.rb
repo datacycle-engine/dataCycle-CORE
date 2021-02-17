4e100715fae3c5e45a8d105ea272915ff5a05539
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-02-17 16:06:05 +0100

    add task to update search table 1 hour after deployment

---
+++ app/models/data_cycle_core/jobs/run_task_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Jobs
+    RunTaskJob = Struct.new(:task, :args) do
+      def perform
+        args ||= []
+        Rake::Task[task].execute(*args)
+      end
+
+      def enqueue(job)
+        job.priority = 0
+        job.delayed_reference_id = task
+        job.delayed_reference_type = task
+      end
+    end
+  end
+end
