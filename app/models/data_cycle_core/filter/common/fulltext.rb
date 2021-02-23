18c8333062039dfd8d289279c8ea5efc3191e75e
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-02-23 15:26:32 +0100

    optimize fulltext-query with language specific dictionaries

--- app/models/data_cycle_core/filter/common/fulltext.rb
+++ app/models/data_cycle_core/filter/common/fulltext.rb
@@ -13,7 +13,8 @@ module DataCycleCore
               .where(
                 search_exists(
                   search[:all_text].matches_all(normalized_name.split(' ').map { |item| "%#{item.strip}%" })
-                    .or(tsmatch(search[:words], tsquery(quoted(normalized_name.squish), get_dict(search[:locale]))))
+                    .or(tsmatch(search[:words], tsquery(quoted(normalized_name.squish), Arel.sql('subquery.config')))),
+                  true
                 )
               )
           )
