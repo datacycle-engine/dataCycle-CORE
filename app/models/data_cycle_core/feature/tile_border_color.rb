98907fe75a856ae42839e495e22846f089340264
Manuel Mitterer <mitterer@pixelpoint.at>
2021-03-10 09:57:23 +0100

    fix AppSignal error with user_group_tiles

--- app/models/data_cycle_core/feature/tile_border_color.rb
+++ app/models/data_cycle_core/feature/tile_border_color.rb
@@ -5,7 +5,7 @@ module DataCycleCore
     class TileBorderColor < Base
       class << self
         def class_string(content)
-          return unless enabled? && configuration[:tree_label].present? && content.respond_to?(:classification_aliases)
+          return unless enabled? && configuration[:tree_label].present? && content.is_a?(DataCycleCore::Thing) && content.respond_to?(:classification_aliases)
           return unless configuration[:template_name].blank? || content&.template_name&.in?(Array.wrap(configuration[:template_name]))
 
           content&.classification_aliases&.to_a&.filter { |c| c.classification_tree_label&.name == configuration[:tree_label] }&.map { |c| "#{c.classification_tree_label&.name}_#{c.internal_name}".underscore_blanks }&.join(' ')
