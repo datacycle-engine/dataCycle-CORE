5ebf5bbc605596f80354a152cc26adb79fd2f108
Manuel Mitterer <mitterer@pixelpoint.at>
2021-03-16 15:44:01 +0100

    [User-Story #23629] add some features

--- db/migrate/20210310141132_migrate_metadata_tours_to_column_line.rb
+++ db/migrate/20210310141132_migrate_metadata_tours_to_column_line.rb
@@ -4,7 +4,7 @@ class MigrateMetadataToursToColumnLine < ActiveRecord::Migration[5.2]
   def up
     execute <<-SQL
       UPDATE things
-      SET line = st_multi(st_geometryfromtext(metadata ->> 'tour', 4326))
+      SET line = ST_Force3DZ(st_multi(st_geometryfromtext(metadata ->> 'tour', 4326)))
       where metadata ->> 'tour' is not NULL;
     SQL
   end
