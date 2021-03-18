694962c5425b82bdaee8cabda79d3163a766aaf3
Manuel Mitterer <mitterer@pixelpoint.at>
2021-03-18 09:10:09 +0100

    add possibility to crawl subroutes for additional_value_paths

--- app/helpers/data_cycle_core/map_helper.rb
+++ app/helpers/data_cycle_core/map_helper.rb
@@ -5,28 +5,19 @@ module DataCycleCore
     def additional_map_values(contents, paths)
       return if paths.blank? || contents.blank?
 
-      contents = Array.wrap(contents)
+      Array.wrap(contents).map! { |c|
+        child_keys = (paths.keys & (c.linked_property_names | c.embedded_property_names))
 
-      contents.map! do |c|
-        values = []
+        next child_keys.map! { |ck| additional_map_values(c.try(ck)&.includes(:translations), paths[ck]) }.flatten.compact if child_keys.present?
 
-        if paths.is_a?(::Hash)
-          paths.each do |k, v|
-            values.push(additional_map_values(c.try(k), v))
-          end
-        elsif paths.is_a?(::Array)
-          property_names = paths.dup
-          geo_property_value = c.try(c.geo_properties.keys.map { |p| property_names.delete(p) }.compact.first.to_s)
-
-          values.push(
-            value_to_geojson(geo_property_value, property_names.map { |p| [p.camelize(:lower), I18n.with_locale(c.first_available_locale) { c.try(p) }] }.to_h.merge({ thingPath: thing_path(c) }))
-          )
-        end
-
-        values.flatten.reject { |v| v.blank? || v[:geometry].blank? }
-      end
-
-      contents.flatten.compact
+        value_to_geojson(
+          c.try(paths['geo'].to_s),
+          {
+            title: I18n.with_locale(c.first_available_locale) { c.try(paths['title'].to_s) },
+            thingPath: thing_path(c)
+          }
+        )
+      }.flatten.compact
     end
 
     def value_to_geojson(value, properties = {})
