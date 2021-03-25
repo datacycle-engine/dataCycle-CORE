b17f2e8697e3e16fefee0a8671177f09d4f892f8
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-03-25 09:11:40 +0100

    appease breakman

--- app/models/data_cycle_core/generic/feratel_webcam/transformations.rb
+++ app/models/data_cycle_core/generic/feratel_webcam/transformations.rb
@@ -39,9 +39,9 @@ module DataCycleCore
           t(:stringify_keys)
           .>> t(:add_field, 'external_key', ->(s) { "Feratel Webcams - Schigebiet - #{s.dig('rid').downcase}" })
           .>> t(:add_field, 'name', ->(s) { s.dig('c') || '__NO_NAME__' })
-          .>> t(:add_links, 'lifts', DataCycleCore::Thing, external_source_id, ->(s) { DataCycleCore::Thing.where("external_key ILIKE 'Feratel Webcams - Lift - #{s.dig('rid').downcase} - %' AND external_source_id = '#{external_source_id}'")&.pluck(:external_key) })
-          .>> t(:add_links, 'slopes', DataCycleCore::Thing, external_source_id, ->(s) { DataCycleCore::Thing.where("external_key ILIKE 'Feratel Webcams - Piste - #{s.dig('rid').downcase} - %' AND external_source_id = '#{external_source_id}'")&.pluck(:external_key) })
-          .>> t(:add_links, 'amenity_feature', DataCycleCore::Thing, external_source_id, ->(s) { DataCycleCore::Thing.where("external_key ILIKE 'Feratel Webcams - Zusatzangebot - #{s.dig('rid').downcase} - %' AND external_source_id = '#{external_source_id}'")&.pluck(:external_key) })
+          .>> t(:add_links, 'lifts', DataCycleCore::Thing, external_source_id, ->(s) { DataCycleCore::Thing.where('external_key ILIKE ? AND external_source_id = ?', "Feratel Webcams - Lift - #{s.dig('rid').downcase} - %", external_source_id)&.pluck(:external_key) })
+          .>> t(:add_links, 'slopes', DataCycleCore::Thing, external_source_id, ->(s) { DataCycleCore::Thing.where('external_key ILIKE ? AND external_source_id = ?', "Feratel Webcams - Piste - #{s.dig('rid').downcase} - %", external_source_id)&.pluck(:external_key) })
+          .>> t(:add_links, 'amenity_feature', DataCycleCore::Thing, external_source_id, ->(s) { DataCycleCore::Thing.where('external_key ILIKE ? AND external_source_id = ?', "Feratel Webcams - Zusatzangebot - #{s.dig('rid').downcase} - %", external_source_id)&.pluck(:external_key) })
           .>> t(:strip_all)
         end
       end
