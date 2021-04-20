3a1be863d1e7bd200f4371a8828112718851849e
Benjamin Preisig <preisig@pixelpoint.at>
2021-04-20 10:39:36 +0200

    Utilize Thesaurus in Docker Projects

--- docker/setup.sh
+++ docker/setup.sh
@@ -9,5 +9,8 @@ gem install bundler
 # bundle update
 bundle install
 
+# update project dictionaries if existing in main projects config/configurations/ts_search/
+bundle exec rake dc:update:dictionaries
+
 # RAILS_LOG_TO_STDOUT=true RUBYOPT="-W:no-deprecated -W:no-experimental" bundle exec rails s -b 0.0.0.0 -p 3000
 RAILS_LOG_TO_STDOUT=true RUBYOPT="-W:no-deprecated -W:no-experimental" bundle exec puma -C /var/www/app/vendor/gems/data-cycle-core/docker/web/puma.rb
