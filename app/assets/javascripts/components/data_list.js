3cf821b63112609eb38c3599fb4aaf64dfcb1683
Manuel Mitterer <mitterer@pixelpoint.at>
2021-04-20 16:10:57 +0200

    refactor datalist

--- app/assets/javascripts/components/data_list.js
+++ app/assets/javascripts/components/data_list.js
@@ -1,3 +1,5 @@
+import ConfirmationModal from './confirmation_modal';
+
 class DataList {
   constructor(element) {
     this.element = element;
@@ -5,13 +7,22 @@ class DataList {
     this.listId = this.element.getAttribute('list');
     this.fieldId = this.element.id;
     this.form = this.element.closest('form');
+    this.searchForm = document.getElementById('search-form');
+    this.collectionForm = document.getElementById('add-items-to-watch-list-form');
     this.requests = [];
+    this.eventListeners = {
+      onInput: this.onInput.bind(this),
+      appendStoredFilterData: this.appendStoredFilterData.bind(this),
+      showConfirmation: this.showConfirmation.bind(this)
+    };
 
     this.setup();
   }
   setup() {
     this.resetList();
-    this.element.addEventListener('input', this.onInput.bind(this));
+
+    this.element.addEventListener('input', this.eventListeners.onInput);
+    if (this.collectionForm) this.collectionForm.addEventListener('submit', this.eventListeners.appendStoredFilterData);
   }
   resetList() {
     this.list.innerHTML = '';
@@ -47,21 +58,24 @@ class DataList {
   default_callback_method(data) {
     data.forEach(element => {
       if (element && element.name && element.id)
-        this.list.innerHTML += `<option data-id="${element.id}" value="${element.name}">`;
+        this.list.insertAdjacentHTML('beforeend', `<option data-id="${element.id}" value="${element.name}">`);
     });
   }
   users_callback_method(data) {
     data.forEach(element => {
       if (element && element.name && element.id)
-        this.list.innerHTML += `<option data-familyname="${element.family_name}" data-givenname="${element.given_name}" data-id="${element.id}" value="${element.email}">`;
+        this.list.insertAdjacentHTML(
+          'beforeend',
+          `<option data-familyname="${element.family_name}" data-givenname="${element.given_name}" data-id="${element.id}" value="${element.email}">`
+        );
     });
 
     console.log('users');
 
-    if (this.selectedOptions().length) {
+    if (this.selectedOption()) {
       let user = filter_ci(list, $(input_field).val());
-      form.find('input[name="data_link[receiver][id]"]').remove();
-      form.append(
+      this.form.find('input[name="data_link[receiver][id]"]').remove();
+      this.form.append(
         '<input type="hidden" id="' +
           $(input_field).prop('id').replace('email', 'id') +
           '" name="data_link[receiver][id]" value="' +
@@ -81,7 +95,7 @@ class DataList {
         .val(user.data('familyname'))
         .prop('readonly', true);
     } else {
-      form.find('input[name="data_link[receiver][id]"]').remove();
+      this.form.find('input[name="data_link[receiver][id]"]').remove();
       $(input_field).closest('form').find('input[id$=given_name]').first().prop('readonly', false);
       $(input_field).closest('form').find('input[id$=family_name]').first().prop('readonly', false);
     }
@@ -89,32 +103,84 @@ class DataList {
   search_history_callback_method(data) {
     this.default_callback_method(data);
 
-    console.log('search_history');
+    const selectedOption = this.selectedOption();
+    const submitButton = this.form.querySelector('button[type="submit"]');
+    const storedFilterInput = this.form.querySelector('input[name="stored_filter[id]"]');
 
-    if (this.selectedOptions().length) {
-      let option_id = filter_ci(list, $(input_field).val()).data('id');
-      form.find('input[name="stored_filter[id]"]').remove();
-      form.append('<input type="hidden" id="stored_filter_id" name="stored_filter[id]" value="' + option_id + '">');
-      form.find('button[type="submit"]').text(form.find('button[type="submit"]').data('update'));
-      form.off('submit', append_stored_filter_data);
-      form.off('submit', show_confirmation).on('submit', show_confirmation);
+    if (selectedOption) {
+      if (!storedFilterInput)
+        this.form.insertAdjacentHTML(
+          'beforeend',
+          `<input type="hidden" id="stored_filter_id" name="stored_filter[id]" value="${selectedOption.dataset.id}">`
+        );
+      else storedFilterInput.value = selectedOption.dataset.id;
+
+      submitButton.innerText = submitButton.dataset.update;
+      this.form.removeEventListener('submit', this.eventListeners.appendStoredFilterData);
+      this.form.removeEventListener('submit', this.eventListeners.showConfirmation);
+      this.form.addEventListener('submit', this.eventListeners.showConfirmation);
     } else {
-      form.find('input[name="stored_filter[id]"]').remove();
-      form.find('button[type="submit"]').text(form.find('button[type="submit"]').data('save'));
-      form.off('submit', show_confirmation);
-      form.off('submit', append_stored_filter_data).on('submit', append_stored_filter_data);
+      if (storedFilterInput) storedFilterInput.remove();
+
+      submitButton.innerText = submitButton.dataset.save;
+      this.form.removeEventListener('submit', this.eventListeners.showConfirmation);
+      this.form.removeEventListener('submit', this.eventListeners.appendStoredFilterData);
+      this.form.addEventListener('submit', this.eventListeners.appendStoredFilterData);
     }
   }
-  selectedOptions() {
-    console.log(this.list.children);
-    return Array.from(this.list.children).filter(elem => {
-      console.log(elem);
+  selectedOption() {
+    return Array.from(this.list.children).find(elem => {
       return elem.value.toLowerCase() == this.element.value.toLowerCase();
     });
+  }
+  showConfirmation(event) {
+    event.preventDefault();
 
-    // $(list)
-    // .find('option')
-    // .filter((idx, elem) => $(elem).val().toLowerCase() == value.toLowerCase());
+    new ConfirmationModal({
+      text:
+        'Filterparameter aktualisieren?<br /><br />Warnung: Beeinflusst auch gespeicherte Suchen, die diese Suche verwenden.',
+      confirmationClass: 'success',
+      cancelable: true,
+      confirmationCallback: this.eventListeners.appendStoredFilterData,
+      cancelCallback: () => {
+        Rails.enableElement(event.target);
+      }
+    });
+  }
+  appendStoredFilterData(event = null) {
+    if (event) event.preventDefault();
+
+    this.searchForm.setAttribute('action', this.form.getAttribute('action'));
+    this.searchForm.setAttribute('method', this.form.getAttribute('method'));
+    this.form.querySelectorAll('input[type="hidden"]').forEach(node => {
+      this.searchForm.appendChild(node.cloneNode());
+    });
+
+    if (this.form.querySelector('#stored_filter_name'))
+      this.searchForm.insertAdjacentHTML(
+        'beforeend',
+        `<input type="hidden" name="stored_filter[name]" value="${
+          this.form.querySelector('#stored_filter_name').value
+        }">`
+      );
+
+    if (this.form.querySelector('#stored_filter_system'))
+      this.searchForm.insertAdjacentHTML(
+        'beforeend',
+        `<input type="hidden" name="stored_filter[system]" value="${
+          this.form.querySelector('#stored_filter_system').checked
+        }">`
+      );
+
+    if (this.form.querySelector('#add-items-to-watch-list-select'))
+      this.searchForm.insertAdjacentHTML(
+        'beforeend',
+        `<input type="hidden" name="watch_list_id" value="${
+          this.form.querySelector('#add-items-to-watch-list-select').value
+        }">`
+      );
+
+    this.searchForm.submit();
   }
 }
 
--- app/assets/javascripts/initializers/datalist_init.js
+++ app/assets/javascripts/initializers/datalist_init.js
@@ -1,178 +1,183 @@
-import ConfirmationModal from './../components/confirmation_modal';
+// import ConfirmationModal from './../components/confirmation_modal';
 import DataList from './../components/data_list';
 
 export default function () {
-  var field_requests = {};
+  // var field_requests = {};
 
-  let default_success = function (input_field, list, data) {
-    $(list).html('');
+  // let default_success = function (input_field, list, data) {
+  //   $(list).html('');
 
-    data.forEach(element => {
-      if (element != undefined && element.name != undefined && element.id != undefined) {
-        $(list).append('<option data-id="' + element.id + '" value="' + element.name + '">');
-      }
-    });
-  };
+  //   data.forEach(element => {
+  //     if (element != undefined && element.name != undefined && element.id != undefined) {
+  //       $(list).append('<option data-id="' + element.id + '" value="' + element.name + '">');
+  //     }
+  //   });
+  // };
 
-  let users = function (input_field, list, data) {
-    $(list).html('');
-    data.forEach(element => {
-      if (element != undefined && element.email != undefined) {
-        $(list).append(
-          '<option data-familyname="' +
-            element.family_name +
-            '" data-givenname="' +
-            element.given_name +
-            '" data-id="' +
-            element.id +
-            '" value="' +
-            element.email +
-            '">'
-        );
-      }
-    });
-    let form = $(input_field).closest('form');
+  // let users = function (input_field, list, data) {
+  //   $(list).html('');
+  //   data.forEach(element => {
+  //     if (element != undefined && element.email != undefined) {
+  //       $(list).append(
+  //         '<option data-familyname="' +
+  //           element.family_name +
+  //           '" data-givenname="' +
+  //           element.given_name +
+  //           '" data-id="' +
+  //           element.id +
+  //           '" value="' +
+  //           element.email +
+  //           '">'
+  //       );
+  //     }
+  //   });
+  //   let form = $(input_field).closest('form');
 
-    if (filter_ci(list, $(input_field).val()).length) {
-      let user = filter_ci(list, $(input_field).val());
-      form.find('input[name="data_link[receiver][id]"]').remove();
-      form.append(
-        '<input type="hidden" id="' +
-          $(input_field).prop('id').replace('email', 'id') +
-          '" name="data_link[receiver][id]" value="' +
-          user.data('id') +
-          '">'
-      );
-      $(input_field)
-        .closest('form')
-        .find('input[id$=given_name]')
-        .first()
-        .val(user.data('givenname'))
-        .prop('readonly', true);
-      $(input_field)
-        .closest('form')
-        .find('input[id$=family_name]')
-        .first()
-        .val(user.data('familyname'))
-        .prop('readonly', true);
-    } else {
-      form.find('input[name="data_link[receiver][id]"]').remove();
-      $(input_field).closest('form').find('input[id$=given_name]').first().prop('readonly', false);
-      $(input_field).closest('form').find('input[id$=family_name]').first().prop('readonly', false);
-    }
-  };
+  //   if (filter_ci(list, $(input_field).val()).length) {
+  //     let user = filter_ci(list, $(input_field).val());
+  //     form.find('input[name="data_link[receiver][id]"]').remove();
+  //     form.append(
+  //       '<input type="hidden" id="' +
+  //         $(input_field).prop('id').replace('email', 'id') +
+  //         '" name="data_link[receiver][id]" value="' +
+  //         user.data('id') +
+  //         '">'
+  //     );
+  //     $(input_field)
+  //       .closest('form')
+  //       .find('input[id$=given_name]')
+  //       .first()
+  //       .val(user.data('givenname'))
+  //       .prop('readonly', true);
+  //     $(input_field)
+  //       .closest('form')
+  //       .find('input[id$=family_name]')
+  //       .first()
+  //       .val(user.data('familyname'))
+  //       .prop('readonly', true);
+  //   } else {
+  //     form.find('input[name="data_link[receiver][id]"]').remove();
+  //     $(input_field).closest('form').find('input[id$=given_name]').first().prop('readonly', false);
+  //     $(input_field).closest('form').find('input[id$=family_name]').first().prop('readonly', false);
+  //   }
+  // };
 
-  let search_history = function (input_field, list, data) {
-    default_success(input_field, list, data);
-    let form = $(input_field).closest('form');
+  // let search_history = function (input_field, list, data) {
+  //   default_success(input_field, list, data);
+  //   let form = $(input_field).closest('form');
 
-    console.log('search_history');
+  //   console.log('search_history');
 
-    if (filter_ci(list, $(input_field).val()).length) {
-      let option_id = filter_ci(list, $(input_field).val()).data('id');
-      form.find('input[name="stored_filter[id]"]').remove();
-      form.append('<input type="hidden" id="stored_filter_id" name="stored_filter[id]" value="' + option_id + '">');
-      form.find('button[type="submit"]').text(form.find('button[type="submit"]').data('update'));
-      form.off('submit', append_stored_filter_data);
-      form.off('submit', show_confirmation).on('submit', show_confirmation);
-    } else {
-      form.find('input[name="stored_filter[id]"]').remove();
-      form.find('button[type="submit"]').text(form.find('button[type="submit"]').data('save'));
-      form.off('submit', show_confirmation);
-      form.off('submit', append_stored_filter_data).on('submit', append_stored_filter_data);
-    }
-  };
+  //   if (filter_ci(list, $(input_field).val()).length) {
+  //     let option_id = filter_ci(list, $(input_field).val()).data('id');
+  //     form.find('input[name="stored_filter[id]"]').remove();
+  //     form.append('<input type="hidden" id="stored_filter_id" name="stored_filter[id]" value="' + option_id + '">');
+  //     form.find('button[type="submit"]').text(form.find('button[type="submit"]').data('update'));
+  //     form.off('submit', append_stored_filter_data);
+  //     form.off('submit', show_confirmation).on('submit', show_confirmation);
+  //   } else {
+  //     form.find('input[name="stored_filter[id]"]').remove();
+  //     form.find('button[type="submit"]').text(form.find('button[type="submit"]').data('save'));
+  //     form.off('submit', show_confirmation);
+  //     form.off('submit', append_stored_filter_data).on('submit', append_stored_filter_data);
+  //   }
+  // };
 
-  let filter_ci = function (list, value) {
-    return $(list)
-      .find('option')
-      .filter((idx, elem) => $(elem).val().toLowerCase() == value.toLowerCase());
-  };
+  // let filter_ci = function (list, value) {
+  //   return $(list)
+  //     .find('option')
+  //     .filter((idx, elem) => $(elem).val().toLowerCase() == value.toLowerCase());
+  // };
 
-  let show_confirmation = function (event) {
-    event.preventDefault();
-    new ConfirmationModal({
-      text:
-        'Filterparameter aktualisieren?<br /><br />Warnung: Beeinflusst auch gespeicherte Suchen, die diese Suche verwenden.',
-      confirmationClass: 'success',
-      cancelable: true,
-      confirmationCallback: () => {
-        append_stored_filter_data(event);
-      },
-      cancelCallback: () => {
-        console.log('cancel datalist');
-        Rails.enableElement(event.currentTarget);
-      }
-    });
-  };
+  // let show_confirmation = function (event) {
+  //   event.preventDefault();
+  //   new ConfirmationModal({
+  //     text:
+  //       'Filterparameter aktualisieren?<br /><br />Warnung: Beeinflusst auch gespeicherte Suchen, die diese Suche verwenden.',
+  //     confirmationClass: 'success',
+  //     cancelable: true,
+  //     confirmationCallback: () => {
+  //       append_stored_filter_data(event);
+  //     },
+  //     cancelCallback: () => {
+  //       console.log('cancel datalist');
+  //       Rails.enableElement(event.currentTarget);
+  //     }
+  //   });
+  // };
 
-  let append_stored_filter_data = function (event) {
-    event.preventDefault();
+  // let append_stored_filter_data = function (event) {
+  //   event.preventDefault();
 
-    let form = $('#search-form');
-    $(form).prop('action', $(event.currentTarget).prop('action'));
-    $(form).prop('method', $(event.currentTarget).prop('method'));
-    $(form).append($(event.currentTarget).find('input[type=hidden]').clone());
-    if ($(event.currentTarget).find('#stored_filter_name').length)
-      $(form).append(
-        '<input type="hidden" name="stored_filter[name]" value="' +
-          $(event.currentTarget).find('#stored_filter_name').val() +
-          '">'
-      );
-    if ($(event.currentTarget).find('#stored_filter_system').length)
-      $(form).append(
-        '<input type="hidden" name="stored_filter[system]" value="' +
-          $(event.currentTarget).find('#stored_filter_system').is(':checked') +
-          '">'
-      );
-    if ($(event.currentTarget).find('#add-items-to-watch-list-select').length)
-      $(form).append(
-        '<input type="hidden" name="watch_list_id" value="' +
-          $(event.currentTarget).find('#add-items-to-watch-list-select').val() +
-          '">'
-      );
-    form.submit();
-  };
+  //   let form = $('#search-form');
+  //   $(form).prop('action', $(event.currentTarget).prop('action'));
+  //   $(form).prop('method', $(event.currentTarget).prop('method'));
+  //   $(form).append($(event.currentTarget).find('input[type=hidden]').clone());
+  //   if ($(event.currentTarget).find('#stored_filter_name').length)
+  //     $(form).append(
+  //       '<input type="hidden" name="stored_filter[name]" value="' +
+  //         $(event.currentTarget).find('#stored_filter_name').val() +
+  //         '">'
+  //     );
+  //   if ($(event.currentTarget).find('#stored_filter_system').length)
+  //     $(form).append(
+  //       '<input type="hidden" name="stored_filter[system]" value="' +
+  //         $(event.currentTarget).find('#stored_filter_system').is(':checked') +
+  //         '">'
+  //     );
+  //   if ($(event.currentTarget).find('#add-items-to-watch-list-select').length)
+  //     $(form).append(
+  //       '<input type="hidden" name="watch_list_id" value="' +
+  //         $(event.currentTarget).find('#add-items-to-watch-list-select').val() +
+  //         '">'
+  //     );
+  //   form.submit();
+  // };
 
   let init = (container = document) => {
-    $(container)
-      .find('.ajax-datalist')
-      .each((idx, element) => {
-        new DataList(element);
+    container.querySelectorAll('.ajax-datalist').forEach(item => {
+      new DataList(item);
+    });
 
-        $(list).html('');
-        let field_id = $(element).prop('id');
-        field_requests[field_id] = [];
+    // $(container)
+    //   .find('.ajax-datalist')
+    //   .each((idx, element) => {
+    //     new DataList(element);
 
-        $(element).on('input', event => {
-          event.preventDefault();
-          field_requests[field_id].forEach(request => {
-            request.abort();
-            field_requests[field_id] = field_requests[field_id].filter(r => r != request);
-          });
-          field_requests[field_id].push(
-            $.get(
-              DataCycle.engingePath + '/' + list_id + '/search',
-              {
-                q: $(event.currentTarget).val()
-              },
-              data => {
-                if (eval('typeof ' + list_id + ' === "function"')) {
-                  eval(list_id)(element, list, data);
-                } else default_success(element, list, data);
-              }
-            )
-          );
-        });
-      });
+    // $(list).html('');
+    // let field_id = $(element).prop('id');
+    // field_requests[field_id] = [];
+
+    // $(element).on('input', event => {
+    //   event.preventDefault();
+    //   field_requests[field_id].forEach(request => {
+    //     request.abort();
+    //     field_requests[field_id] = field_requests[field_id].filter(r => r != request);
+    //   });
+    //   field_requests[field_id].push(
+    //     $.get(
+    //       DataCycle.engingePath + '/' + list_id + '/search',
+    //       {
+    //         q: $(event.currentTarget).val()
+    //       },
+    //       data => {
+    //         if (eval('typeof ' + list_id + ' === "function"')) {
+    //           eval(list_id)(element, list, data);
+    //         } else default_success(element, list, data);
+    //       }
+    //     )
+    //   );
+    // });
+    // });
   };
 
   init();
 
-  $('#add-items-to-watch-list-form').on('submit', append_stored_filter_data);
+  // $('#add-items-to-watch-list-form').on('submit', append_stored_filter_data);
 
   $(document).on('dc:html:changed', '*', event => {
     event.stopPropagation();
+
     init(event.target);
   });
 }
