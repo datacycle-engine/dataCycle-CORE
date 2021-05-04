f41e576073c286ca5bc440d0f9fb9be03b9e08da
Patrick Rainer <rainer@pixelpoint.at>
2021-05-04 12:31:13 +0200

    finalize imgproxy setup

--- app/models/data_cycle_core/utility/virtual/asset.rb
+++ app/models/data_cycle_core/utility/virtual/asset.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
-require "openssl"
-require "base64"
+
+require 'openssl'
+require 'base64'
 
 module DataCycleCore
   module Utility
@@ -44,58 +45,61 @@ module DataCycleCore
               ].join('/')
             end
           end
+
           def imgproxy(**args)
             transformations = args.dig(:virtual_definition, 'virtual', 'transformation')
             image_processing = args.dig(:virtual_definition, 'virtual', 'processing')
 
             name = args.dig(:content).name&.parameterize(separator: '_') || args.dig(:content).id
 
-            if transformations.dig('version') == 'original'
+            if image_processing&.dig('format').present?
+              file_extension = ".#{image_processing.dig('format')}"
+            else
               content = args.dig(:content)
               if content.respond_to?(:asset) && content.send(:asset).present?
                 orig_url = content.send(:asset)&.try(:file)&.try(:url)
               else
                 orig_url = content.content_url
               end
-              [
-                Rails.application.config.asset_host,
-                'asset',
-                args.dig(:content).id,
-                imgproxy_signature(args.dig(:content).id, args.dig(:virtual_definition, 'virtual', 'processing')),
-                'orig',
-                "#{name}#{orig_url.present? ? File.extname(orig_url) : ''}"
-              ].join('/')
-            elsif transformations.dig('version') == 'dynamic'
-              [
-                Rails.application.config.asset_host,
-                'asset',
-                args.dig(:content).id,
-                imgproxy_signature(args.dig(:content).id, args.dig(:virtual_definition, 'virtual', 'processing')),
+              file_extension = orig_url.present? ? File.extname(orig_url) : ''
+            end
+
+            target_url = [
+              Rails.application.config.asset_host,
+              'asset',
+              args.dig(:content).id
+            ]
+
+            target_url << imgproxy_signature(args.dig(:content).id, args.dig(:virtual_definition, 'virtual', 'processing')) if image_processing.present? && image_processing.is_a?(::Hash)
+
+            if transformations.dig('version') == 'dynamic'
+              target_url += [
                 image_processing.dig('resize_type'),
                 image_processing.dig('width'),
                 image_processing.dig('height'),
                 image_processing.dig('enlarge'),
-                image_processing.dig('gravity'),
-                "#{name}.#{image_processing.dig('format')}"
-              ].join('/')
+                image_processing.dig('gravity')
+              ]
             else
-              [
-                Rails.application.config.asset_host,
-                'asset',
-                args.dig(:content).id,
-                imgproxy_signature(args.dig(:content).id, args.dig(:virtual_definition, 'virtual', 'processing')),
-                transformations.dig('version'),
-                "#{name}.#{image_processing.dig('format')}"
-              ].join('/')
+              target_url << transformations.dig('version')
             end
+
+            target_url << "#{name}#{file_extension}"
+
+            target_url.join('/')
           end
 
           def imgproxy_signature(content_id, processing)
-            key = [Rails.application.secrets.imgproxy_key].pack("H*")
-            salt = [Rails.application.secrets.imgproxy_salt].pack("H*")
+            raise 'Insufficient imgproxy credentials! Validate imgproxy_key and imgproxy_salt secrets!' unless Rails.application.secrets.imgproxy_key.present? && Rails.application.secrets.imgproxy_salt.present?
+
+            key = [Rails.application.secrets.imgproxy_key].pack('H*')
+            salt = [Rails.application.secrets.imgproxy_salt].pack('H*')
+
+            application_url = Rails.application.config.asset_host
+            application_url = "http://nginx:#{ENV.fetch('PUBLIC_APPLICATION_PORT', '3003')}" if Rails.env.development?
+
             url = [
-              # Rails.application.config.asset_host,
-              'http://nginx:3003',
+              application_url,
               'things',
               content_id,
               'asset',
@@ -111,13 +115,11 @@ module DataCycleCore
 
             path = "/resize:#{resize_type}:#{width}:#{height}:#{enlarge}/gravity:#{gravity}/filename:#{content_id}/plain/#{url}@#{extension}"
 
-            digest = OpenSSL::Digest.new("sha256")
-            hmac = Base64.urlsafe_encode64(OpenSSL::HMAC.digest(digest, key, "#{salt}#{path}")).tr("=", "")
+            digest = OpenSSL::Digest.new('sha256')
+            hmac = Base64.urlsafe_encode64(OpenSSL::HMAC.digest(digest, key, "#{salt}#{path}")).tr('=', '')
             hmac
           end
-
         end
-
       end
     end
   end
