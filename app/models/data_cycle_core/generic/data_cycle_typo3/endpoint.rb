bc6f123fc5256f22f7f7021524916747f1eed8f9
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-05-25 15:57:26 +0200

    add delete webpage to typo3 importer

--- app/models/data_cycle_core/generic/data_cycle_typo3/endpoint.rb
+++ app/models/data_cycle_core/generic/data_cycle_typo3/endpoint.rb
@@ -18,12 +18,21 @@ module DataCycleCore
           end
         end
 
+        def mark_deleted_webpages(*)
+          Enumerator.new do |yielder|
+            load_data('deleted').each do |item|
+              yielder << item
+            end
+          end
+        end
+
         protected
 
-        def load_data(location = nil)
+        def load_data(location = nil, from = nil)
           response = Faraday.new.get do |req|
             req.url File.join([@host, @end_point, location].compact)
             req.params['token'] = @token
+            req.params['from'] = from if from.present?
           end
           raise DataCycleCore::Generic::Common::Error::EndpointError.new("error loading data from #{File.join([@host, @end_point, location])}", response) unless response.success?
           JSON.parse(response.body)['@graph']
