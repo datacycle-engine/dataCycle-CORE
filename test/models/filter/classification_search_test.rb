71c4a536339983d2d5bc6607ad12cc490d40a995
Johannes Zlattinger <johannes@zlattinger.net>
2021-06-08 19:03:56 +0200

    minor changes

--- test/models/filter/classification_search_test.rb
+++ test/models/filter/classification_search_test.rb
@@ -10,31 +10,31 @@ module DataCycleCore
       create_content('Artikel', { name: 'HEADLINE - NO TAGS 2' })
       create_content('Artikel', { name: 'HEADLINE - NO TAGS 3' })
       create_content('Artikel', { name: 'HEADLINE - NO TAGS 4' })
-      create_content('Artikel', { name: 'HEADLINE 1', tags: get_classification_ids('Tags', ['Tag 3']) })
-      create_content('Artikel', { name: 'HEADLINE 2', tags: get_classification_ids('Tags', ['Tag 2', 'Nested Tag 1']) })
-      create_content('Artikel', { name: 'HEADLINE 3', tags: get_classification_ids('Tags', ['Tag 2', 'Tag 3']) })
-      create_content('Artikel', { name: 'HEADLINE 4', tags: get_classification_ids('Tags', ['Tag 2']) })
+      create_content('Artikel', { name: 'HEADLINE 1', tags: fetch_classification_ids('Tags', ['Tag 3']) })
+      create_content('Artikel', { name: 'HEADLINE 2', tags: fetch_classification_ids('Tags', ['Tag 2', 'Nested Tag 1']) })
+      create_content('Artikel', { name: 'HEADLINE 3', tags: fetch_classification_ids('Tags', ['Tag 2', 'Tag 3']) })
+      create_content('Artikel', { name: 'HEADLINE 4', tags: fetch_classification_ids('Tags', ['Tag 2']) })
     end
 
     test 'filter contents based on directly assigned classifications by id' do
       items = DataCycleCore::Filter::Search.new(:de)
-        .classification_alias_ids_without_subtree(find_alias_ids('Tags', 'Tag 3'))
+        .classification_alias_ids_without_subtree(fetch_classification_alias_ids('Tags', 'Tag 3'))
       assert_equal(2, items.count)
 
       items = DataCycleCore::Filter::Search.new(:de)
-        .classification_alias_ids_without_subtree(find_alias_ids('Tags', 'Tag 2'))
+        .classification_alias_ids_without_subtree(fetch_classification_alias_ids('Tags', 'Tag 2'))
       assert_equal(3, items.count)
     end
 
     test 'filter contents based on classification hierarchy by id' do
       items = DataCycleCore::Filter::Search.new(:de)
-        .classification_alias_ids_with_subtree(find_alias_ids('Tags', 'Tag 3'))
+        .classification_alias_ids_with_subtree(fetch_classification_alias_ids('Tags', 'Tag 3'))
       assert_equal(3, items.count)
 
       # same_as
       # TODO: refactor to use search ?
       items = DataCycleCore::Thing
-        .with_classification_alias_ids(find_alias_ids('Tags', 'Tag 3'))
+        .with_classification_alias_ids(fetch_classification_alias_ids('Tags', 'Tag 3'))
       assert_equal(3, items.count)
     end
 
@@ -46,7 +46,7 @@ module DataCycleCore
 
     test 'filter contents by excluding classifications by id' do
       items = DataCycleCore::Filter::Search.new(:de)
-        .not_classification_alias_ids_with_subtree(find_alias_ids('Tags', 'Tag 2'))
+        .not_classification_alias_ids_with_subtree(fetch_classification_alias_ids('Tags', 'Tag 2'))
       assert_equal(5, items.count)
     end
 
@@ -56,11 +56,12 @@ module DataCycleCore
       DataCycleCore::TestPreparations.create_content(template_name: template_name, data_hash: data)
     end
 
-    def get_classification_ids(tree_name, *alias_names)
-      DataCycleCore::ClassificationAlias.for_tree(tree_name).with_name(alias_names).map(&:classifications).flatten.map(&:id)
+    def fetch_classification_ids(tree_name, *alias_names)
+      DataCycleCore::ClassificationAlias.for_tree(tree_name).with_name(alias_names)
+        .map(&:classifications).flatten.map(&:id)
     end
 
-    def find_alias_ids(tree_name, *alias_names)
+    def fetch_classification_alias_ids(tree_name, *alias_names)
       DataCycleCore::ClassificationAlias.for_tree(tree_name).with_name(alias_names).pluck(:id)
     end
   end
