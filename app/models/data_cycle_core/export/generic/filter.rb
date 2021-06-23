3b854d405f14d98cfb42cb279ae1c2c1e34f64a0
Manuel Mitterer <mitterer@pixelpoint.at>
2021-06-23 15:51:19 +0200

    small adjustments

--- app/models/data_cycle_core/export/generic/filter.rb
+++ app/models/data_cycle_core/export/generic/filter.rb
@@ -13,55 +13,49 @@ module DataCycleCore
         ].freeze
 
         def self.filter(**args)
-          filter_endpoints(args) || AVAILABLE_WEBHOOK_FILTERS.all? { |f| send(f, args) }
+          if external_system.export_config_by_filter_key(method_name, 'endpoints').present?
+            raise 'incompatible filter config (endpoints + specific filters) for webhook detected!' if endpoint_ids.present? &&
+                                                                                                       (
+                                                                                                         external_system.export_config&.dig(:filter)&.except('endpoints').present? ||
+                                                                                                         external_system.export_config&.dig(method_name.to_sym, 'filter')&.except('endpoints').present?
+                                                                                                       )
+
+            filter_endpoints(args)
+          else
+            AVAILABLE_WEBHOOK_FILTERS.all? { |f| send(f, args) }
+          end
         end
 
         def self.filter_endpoints(data:, external_system:, method_name:)
           endpoint_ids = Array.wrap(external_system.export_config_by_filter_key(method_name, 'endpoints'))
           endpoints = DataCycleCore::StoredFilter.where(id: endpoint_ids) if endpoint_ids.present?
 
-          raise 'incompatible filter config (endpoints + specific filters) for webhook detected!' if endpoint_ids.present? &&
-                                                                                                     (
-                                                                                                       external_system.export_config&.dig(:filter)&.except('endpoints').present? ||
-                                                                                                       external_system.export_config&.dig(method_name.to_sym, 'filter')&.except('endpoints').present?
-                                                                                                     )
-
-          return false if endpoints.blank?
-
-          endpoints.any? { |e| e.apply.query.exists?(id: data.id) }
+          endpoints.present? ? endpoints.any? { |e| e.apply.query.exists?(id: data.id) } : false
         end
 
         def self.filter_presence(data:, external_system:, method_name:)
           presence_check = external_system.export_config_by_filter_key(method_name, 'presence')
           presence_check = presence_check.is_a?(Hash) ? Array.wrap(presence_check.dig(data&.template_name)) : Array.wrap(presence_check)
 
-          return true if presence_check.blank?
-
-          presence_check.all? { |p| data.try(p).present? }
+          presence_check.present? ? presence_check.all? { |p| data.try(p).present? } : true
         end
 
         def self.filter_template_names(data:, external_system:, method_name:)
           template_names = Array.wrap(external_system.export_config_by_filter_key(method_name, 'template_names'))
 
-          return true if template_names.blank?
-
-          data.template_name.in?(template_names)
+          template_names.present? ? data.template_name.in?(template_names) : true
         end
 
         def self.filter_external_system_names(data:, external_system:, method_name:)
           external_system_names = Array.wrap(external_system.export_config_by_filter_key(method_name, 'external_systems'))
 
-          return true if external_system_names.blank?
-
-          data.external_source&.identifier&.in?(external_system_names)
+          external_system_names.present? ? data.external_source&.identifier&.in?(external_system_names) : true
         end
 
         def self.filter_classifications(data:, external_system:, method_name:)
           classification_ids = Array.wrap(external_system.export_config_by_filter_key(method_name, 'classifications')).map { |f| DataCycleCore::ClassificationAlias.classification_for_tree_with_name(f['tree_label'], f['aliases']) }
 
-          return true if classification_ids.blank?
-
-          classification_ids.all? { |c| data.classifications.map(&:id).include?(c) }
+          classification_ids.present? ? classification_ids.all? { |c| data.classifications.map(&:id).include?(c) } : true
         end
 
         def self.filter_tree_labels(data:, external_system:, method_name:)
@@ -76,9 +70,7 @@ module DataCycleCore
               .uniq
           end
 
-          return true if tree_labels.blank?
-
-          tree_labels.all? { |tree_label| tree_label.in?(data_tree_labels) }
+          tree_labels.present? ? tree_labels.all? { |tree_label| tree_label.in?(data_tree_labels) } : true
         end
       end
     end
