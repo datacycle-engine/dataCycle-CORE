6984bbde7c1f6b60755670158a866569a43d5c54
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-07-15 10:18:25 +0200

    cleanup

--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -126,6 +126,8 @@ CREATE FUNCTION public.tsvectorsearchupdate() RETURNS trigger
 
 SET default_tablespace = '';
 
+SET default_with_oids = false;
+
 --
 -- Name: activities; Type: TABLE; Schema: public; Owner: -
 --
