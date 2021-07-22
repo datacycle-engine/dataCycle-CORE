abadc2ca5fb973516d84b3c676e9d33ad06b83bc
Patrick Rainer <rainer@pixelpoint.at>
2021-07-22 11:53:28 +0200

    [docker] upgrade postgres conf to version 13

--- docker/setup-postgres.sh
+++ docker/setup-postgres.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /tmp/postgres/ts_search/* /usr/share/postgresql/11/tsearch_data/
+cp /tmp/postgres/ts_search/* /usr/share/postgresql/13/tsearch_data/
 
 docker-entrypoint.sh -c config_file=/etc/postgresql.conf
