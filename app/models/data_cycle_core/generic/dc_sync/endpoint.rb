062f3423128052a7c9eaa772f121243824d502c2
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-08-06 09:59:39 +0200

    better language handling for sync

--- app/models/data_cycle_core/generic/dc_sync/endpoint.rb
+++ app/models/data_cycle_core/generic/dc_sync/endpoint.rb
@@ -55,6 +55,7 @@ module DataCycleCore
             req.headers['Content-Type'] = 'application/json'
             req.params['token'] = @token
             req.params['page'] = { number: page, size: @per }
+            req.params['language'] = @params[:locales].join(',') if @params[:locales].present?
             req.params['updated_since'] = @updated_since.to_s if @updated_since.present?
             req.params['iteration_depth'] = @iteration_depth
           end
@@ -75,6 +76,8 @@ module DataCycleCore
           response = connection.post do |req|
             req.headers['Content-Type'] = 'application/json'
             req.params['token'] = @token
+            req.params['language'] = @params[:locales].join(',') if @params[:locales].present?
+            req.params['iteration_depth'] = @iteration_depth
           end
           raise DataCycleCore::Generic::Common::Error::EndpointError.new("error loading data from #{File.join([@host, @end_point])}", response) unless response.success?
           JSON.parse(response.body)
