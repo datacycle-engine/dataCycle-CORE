096733b493d490bc71f25d41e7a26c56e76ca383
Manuel Mitterer <mitterer@pixelpoint.at>
2021-08-09 16:01:17 +0200

    implement possibility to translate Attribute labels

--- app/helpers/data_cycle_core/ui_locale_helper.rb
+++ app/helpers/data_cycle_core/ui_locale_helper.rb
@@ -5,5 +5,15 @@ module DataCycleCore
     def active_ui_locale
       current_user&.ui_locale || DataCycleCore.ui_locales.first
     end
+
+    def translated_attribute_label(template_name, key, label)
+      if template_name.present? && I18n.exists?("attribute_labels.#{template_name}.#{key}")
+        I18n.t("attribute_labels.#{template_name}.#{key}", locale: active_ui_locale)
+      elsif I18n.exists?("attribute_labels.#{key}")
+        I18n.t("attribute_labels.#{key}", locale: active_ui_locale)
+      else
+        label
+      end
+    end
   end
 end
