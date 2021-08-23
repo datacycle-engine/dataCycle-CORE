92163e11ee8f3a505b2fbfd59ff16aa649679e17
Manuel Mitterer <mitterer@pixelpoint.at>
2021-08-23 14:28:12 +0200

    small css adjustments

--- app/helpers/data_cycle_core/data_link_helper.rb
+++ app/helpers/data_cycle_core/data_link_helper.rb
@@ -25,39 +25,35 @@ module DataCycleCore
     end
 
     def finalize_agbs_label
-      label_html = []
-
-      label_html.push(
-        tag.span(
+      unless I18n.exists?('finalize_agbs_html', locale: active_ui_locale)
+        return tag.span(
           t('actions.finalize', locale: active_ui_locale),
           title: t('common.content_not_editable', locale: active_ui_locale),
           data: {
             tooltip: true
           }
         )
-      )
-
-      if I18n.exists?('finalize_agbs_html', locale: active_ui_locale)
-        label_html.push(
-          tag.span(
-            safe_join(
-              [
-                "(#{t('actions.finalize_agbs_text', locale: active_ui_locale)} ",
-                tag.i(class: 'fa fa-info-circle'),
-                ')'
-              ]
-            ),
-            title: t('finalize_agbs_html', locale: active_ui_locale),
-            data: {
-              tooltip: true,
-              allow_html: true
-            }
-          )
-        )
-
       end
 
-      safe_join(label_html, ' ')
+      t(
+        'actions.finalize_combined_html',
+        locale: active_ui_locale,
+        finalize: tag.span(
+          t('actions.finalize', locale: active_ui_locale),
+          title: t('common.content_not_editable', locale: active_ui_locale),
+          data: {
+            tooltip: true
+          }
+        ),
+        agbs: tag.span(
+          t('actions.finalize_agbs_text', locale: active_ui_locale),
+          title: t('finalize_agbs_html', locale: active_ui_locale),
+          data: {
+            tooltip: true,
+            allow_html: true
+          }
+        )
+      )
     end
   end
 end
