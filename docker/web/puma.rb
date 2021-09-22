ce1fd8e4cf4077b6a4d44d322111b9a78ef465ee
Patrick Rainer <rainer@pixelpoint.at>
2021-09-22 10:58:49 +0200

    update puma configs for production

--- docker/web/puma.rb
+++ docker/web/puma.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env puma
 # frozen_string_literal: true
 
-application_root = ENV['APPLICATION_ROOT']&.delete_suffix('/') || '/var/www/app'
+application_root = ENV['APPLICATION_ROOT']&.delete_suffix('/') || '/app'
 directory "#{application_root}/"
 rackup "#{application_root}/config.ru"
 environment ENV.fetch('RAILS_ENV', 'development')
@@ -12,11 +12,11 @@ pidfile "#{application_root}/tmp/pids/puma.pid"
 state_path "#{application_root}/tmp/pids/puma.state"
 stdout_redirect '/dev/stdout', '/dev/stderr', true
 
-threads 5, 5
+threads 1, ENV.fetch('PUMA_MAX_THREADS', 5)
 
 bind "unix://#{application_root}/tmp/sockets/puma.sock"
 
-workers 1
+workers ENV.fetch('PUMA_MAX_WORKERS', 3)
 
 preload_app!
 
@@ -25,7 +25,7 @@ before_fork do
   require 'puma_worker_killer'
 
   PumaWorkerKiller.config do |config|
-    config.ram = 2048 # mb
+    config.ram = ENV.fetch('PUMA_MAX_MEMORY', 4096) # mb
     config.frequency = 3600 # seconds
     config.percent_usage = 0.9
     config.rolling_restart_frequency = false
