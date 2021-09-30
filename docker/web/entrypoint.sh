46b8e7725615253cd2c4f8170d2983f76e4161d2
Patrick Rainer <rainer@pixelpoint.at>
2021-09-30 12:23:49 +0200

    update entrypoint.sh to override named volume

--- docker/web/entrypoint.sh
+++ docker/web/entrypoint.sh
@@ -8,10 +8,19 @@ mkdir -p /app${CORE_DUMMY_PREFIX:-}/tmp \
   && chown ruby:ruby -R /app${CORE_DUMMY_PREFIX:-}/tmp
 
 # update docker configs in named volumes
-mkdir -p /app/docker \
-  && cp -Rn ${DC_DOCKER_SETUP_PATH:-/app/docker/}* /app/dc_volumes/docker/. \
-  && rm -Rf /app/docker/* \
+DC_CORE_PATH = /app/vendor/gems/data-cycle-core
+if [ -d "$DC_CORE_PATH"  ]
+then
+  cp -Rn $DC_CORE_PATH/docker/* /app/dc_volumes/docker/.
+  echo "$DC_CORE_PATH exists."
+else
+  echo "Running core only"
+fi
+
+rm -Rf /app/docker/* \
   && cp -Rf /app/dc_volumes/docker/* /app/docker/.
+
+# update postgres ts_search config
 mkdir -p /app/docker/postgres/configurations/ts_search/ && cp -Rn /app${CORE_DUMMY_PREFIX:-}/config/configurations/ts_search/* /app/docker/postgres/configurations/ts_search/.
 chown -R ruby:ruby /app/docker
 
