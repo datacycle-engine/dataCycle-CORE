57367eaf16058690e4a8f2114c6657a7db678dca
Patrick Rainer <rainer@pixelpoint.at>
2021-10-19 08:41:08 +0200

    [docker] update compose files and bash scripts

--- docker/utility/check_bind_mounts.sh
+++ docker/utility/check_bind_mounts.sh
@@ -4,10 +4,12 @@ set -eu
 ## mongodb | app | postgres
 echo "$BASE_DATA_VOLUME_PATH"
 mkdir -p $BASE_DATA_VOLUME_PATH/{app,mongodb,postgres}
+sudo chgrp -R 1000 $BASE_DATA_VOLUME_PATH/app
 
 # nginx | app | mongodb
 echo "$BASE_LOG_VOLUME_PATH"
 mkdir -p $BASE_LOG_VOLUME_PATH/{app,mongodb,nginx}
+sudo chgrp -R 1000 $BASE_LOG_VOLUME_PATH/app
 
 ## imgproxy_data_cycle | data_cycle
 echo "$BASE_CACHE_VOLUME_PATH"
@@ -15,8 +17,10 @@ mkdir -p $BASE_CACHE_VOLUME_PATH/{data_cycle,imgproxy_data_cycle}
 
 echo "$BASE_HISTORY_VOLUME_PATH"
 mkdir -p $BASE_HISTORY_VOLUME_PATH
+sudo chgrp -R 1000 $BASE_HISTORY_VOLUME_PATH
 
 echo "$BASE_BACKUP_VOLUME_PATH"
 mkdir -p $BASE_BACKUP_VOLUME_PATH
+sudo chgrp -R 1000 $BASE_BACKUP_VOLUME_PATH
 
 echo "### bind mount directories updated ###"
