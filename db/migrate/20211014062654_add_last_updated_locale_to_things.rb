cf918eb992b4718fd7a69c922ab3eb791486ee59
Manuel Mitterer <mitterer@pixelpoint.at>
2021-10-27 12:13:57 +0200

    fix slow migration

--- db/migrate/20211014062654_add_last_updated_locale_to_things.rb
+++ db/migrate/20211014062654_add_last_updated_locale_to_things.rb
@@ -31,59 +31,63 @@ class AddLastUpdatedLocaleToThings < ActiveRecord::Migration[5.2]
       WHERE
         last_updated_locale IS NULL;
 
-      WITH subquery2 AS (
+      WITH sub_subquery2 AS (
         SELECT
-          t4.id
+          t2.thing_id,
+          t1.locale,
+          max(UPPER(t1.history_valid)) AS upper_valid
         FROM
-          thing_history_translations t4
-          INNER JOIN thing_histories t3 ON t3.id = t4.thing_history_id
-        WHERE
-          t4.id = (
-            SELECT
-              t2.id
-            FROM
-              thing_history_translations t2
-              INNER JOIN thing_histories t1 ON t1.id = t2.thing_history_id
-            WHERE
-              t3.thing_id = t1.thing_id
-              AND t4.locale = t2.locale
-            ORDER BY
-              UPPER(t2.history_valid) DESC
-            LIMIT 1))
-      UPDATE
-        thing_history_translations
-      SET
-        history_valid = tstzrange(lower(thing_history_translations.history_valid), NULL, '[)')
-      FROM
-        subquery2
+          thing_history_translations t1
+          INNER JOIN thing_histories t2 ON t2.id = t1.thing_history_id
+        GROUP BY
+          t2.thing_id,
+          t1.locale
+      ),
+      subquery2 AS (
+        SELECT
+          h1.id
+        FROM
+          thing_history_translations AS h1
+          INNER JOIN thing_histories h2 ON h2.id = h1.thing_history_id
+          JOIN sub_subquery2 AS s2 ON h2.thing_id = s2.thing_id
+            AND h1.locale = s2.locale
+            AND s2.upper_valid = UPPER(h1.history_valid))
+        UPDATE
+          thing_history_translations
+        SET
+          history_valid = tstzrange(lower(thing_history_translations.history_valid), NULL, '[)')
+        FROM
+          subquery2
       WHERE
         thing_history_translations.id = subquery2.id;
 
-      WITH subquery3 AS (
+      WITH sub_subquery3 AS (
         SELECT
-          t3.id
+          t2.thing_id,
+          t1.locale,
+          max(UPPER(t1.history_valid)) AS upper_valid
         FROM
-          thing_history_translations t4
-          INNER JOIN thing_histories t3 ON t3.id = t4.thing_history_id
-        WHERE
-          t4.id = (
-            SELECT
-              t2.id
-            FROM
-              thing_history_translations t2
-              INNER JOIN thing_histories t1 ON t1.id = t2.thing_history_id
-            WHERE
-              t3.thing_id = t1.thing_id
-              AND t4.locale = t2.locale
-            ORDER BY
-              UPPER(t2.history_valid) DESC
-            LIMIT 1))
-      UPDATE
-        content_content_histories
-      SET
-        history_valid = tstzrange(lower(content_content_histories.history_valid), NULL, '[)')
-      FROM
-        subquery3
+          thing_history_translations t1
+          INNER JOIN thing_histories t2 ON t2.id = t1.thing_history_id
+        GROUP BY
+          t2.thing_id,
+          t1.locale
+      ),
+      subquery3 AS (
+        SELECT
+          h2.id
+        FROM
+          thing_history_translations AS h1
+          INNER JOIN thing_histories h2 ON h2.id = h1.thing_history_id
+          JOIN sub_subquery3 AS s2 ON h2.thing_id = s2.thing_id
+            AND h1.locale = s2.locale
+            AND s2.upper_valid = UPPER(h1.history_valid))
+        UPDATE
+          content_content_histories
+        SET
+          history_valid = tstzrange(lower(content_content_histories.history_valid), NULL, '[)')
+        FROM
+          subquery3
       WHERE
         content_content_histories.content_a_history_id = subquery3.id;
     SQL
