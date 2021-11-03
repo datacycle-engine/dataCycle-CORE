75e925697a70e8d6894b8893c3ef3ca8bdf1bde5
Johannes Zlattinger <johannes@zlattinger.net>
2021-11-03 21:19:21 +0100

    [RUBOCOP] fix rubocop violation

--- app/models/data_cycle_core/export/common/webhook.rb
+++ app/models/data_cycle_core/export/common/webhook.rb
@@ -46,7 +46,7 @@ module DataCycleCore
         end
 
         def success(job)
-          # rubocop:disable Security/YAMLLoad
+          # rubocop:disable Security/YAMLLoad, Style/GuardClause
           if job.created_at
             Appsignal.add_distribution_value('delayed_job.waiting_time', (Time.zone.now - job.created_at) / 60,
                                              { job_class: YAML.load(job.handler).job_data['job_class'], queue: job.queue })
@@ -56,7 +56,7 @@ module DataCycleCore
             Appsignal.add_distribution_value('delayed_job.attempt_count', job.attempts,
                                              { job_class: YAML.load(job.handler).job_data['job_class'], queue: job.queue })
           end
-          # rubocop:enable Security/YAMLLoad
+          # rubocop:enable Security/YAMLLoad, Style/GuardClause
         end
       end
     end
--- config/initializers/monkey/active_job.rb
+++ config/initializers/monkey/active_job.rb
@@ -2,7 +2,7 @@
 
 module ActiveJobMetricsExtension
   def success(job)
-    # rubocop:disable Security/YAMLLoad
+    # rubocop:disable Security/YAMLLoad, Style/GuardClause
     if job.created_at
       Appsignal.add_distribution_value('delayed_job.waiting_time', (Time.zone.now - job.created_at) / 60,
                                        { job_class: YAML.load(job.handler).job_data['job_class'], queue: job.queue })
@@ -12,7 +12,7 @@ module ActiveJobMetricsExtension
       Appsignal.add_distribution_value('delayed_job.attempt_count', job.attempts,
                                        { job_class: YAML.load(job.handler).job_data['job_class'], queue: job.queue })
     end
-    # rubocop:enable Security/YAMLLoad
+    # rubocop:enable Security/YAMLLoad, Style/GuardClause
   end
 end
 
