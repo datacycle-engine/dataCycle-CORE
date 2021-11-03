336e28e526edd4375e423a6f74b0eb2bc41a0bd8
Johannes Zlattinger <johannes@zlattinger.net>
2021-11-03 19:59:28 +0100

    [RUBOCOP] fix rubocop violation

--- app/models/data_cycle_core/export/common/webhook.rb
+++ app/models/data_cycle_core/export/common/webhook.rb
@@ -47,9 +47,9 @@ module DataCycleCore
 
         def success(job)
           Appsignal.add_distribution_value('delayed_job.waiting_time', (Time.zone.now - job.created_at) / 60,
-                                           { job_class: YAML.load(job.handler).class.name, queue: job.queue })
+                                           { job_class: YAML.safe_load(job.handler).class.name, queue: job.queue })
           Appsignal.add_distribution_value('delayed_job.attempt_count', job.attempts,
-                                           { job_class: YAML.load(job.handler).class.name, queue: job.queue })
+                                           { job_class: YAML.safe_load(job.handler).class.name, queue: job.queue })
         end
       end
     end
