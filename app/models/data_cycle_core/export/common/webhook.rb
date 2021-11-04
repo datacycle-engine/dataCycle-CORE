c17d7ef6ea3a0a94b049d1853400bf9db961fe04
Johannes Zlattinger <johannes@zlattinger.net>
2021-11-04 08:51:28 +0100

    add some job metrics

--- app/models/data_cycle_core/export/common/webhook.rb
+++ app/models/data_cycle_core/export/common/webhook.rb
@@ -49,12 +49,12 @@ module DataCycleCore
           # rubocop:disable Security/YAMLLoad, Style/GuardClause
           if job.created_at
             Appsignal.add_distribution_value('delayed_job.waiting_time', (Time.zone.now - job.created_at) / 60,
-                                             { job_class: YAML.load(job.handler).job_data['job_class'], queue: job.queue })
+                                             { job_class: YAML.load(job.handler).class.name, queue: job.queue })
           end
 
           if job.attempts
             Appsignal.add_distribution_value('delayed_job.attempt_count', job.attempts,
-                                             { job_class: YAML.load(job.handler).job_data['job_class'], queue: job.queue })
+                                             { job_class: YAML.load(job.handler).class.name, queue: job.queue })
           end
           # rubocop:enable Security/YAMLLoad, Style/GuardClause
         end
