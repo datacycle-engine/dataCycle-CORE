32e3efa0648ff0ba81d63c314f69d59d273ea166
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-11-18 14:28:15 +0100

    extend meta-api to download Tours

--- app/models/data_cycle_core/generic/metaapi/endpoint.rb
+++ app/models/data_cycle_core/generic/metaapi/endpoint.rb
@@ -51,7 +51,22 @@ module DataCycleCore
           end
         end
 
+        def tours(*)
+          load_main_objects('Tour')
+        end
+
         protected
+
+        def load_main_objects(kind)
+          Enumerator.new do |yielder|
+            paging_params = load_paging_stats("Get#{kind}ListPaging")
+            (1..paging_params[:pages]).each do |page|
+              load_list("Get#{kind}List", page, kind).each do |item_id|
+                yielder << load_details("Get#{kind}Details", item_id)[kind]
+              end
+            end
+          end
+        end
 
         def load_categories(method)
           url = [@host, @end_point, method + 'Proxy.php'].join('/')
@@ -65,8 +80,50 @@ module DataCycleCore
           flatten_hash_tree(data.children.first.to_hash['Category'])
         end
 
+        def load_paging_stats(method)
+          url = [@host, @end_point, method + 'Proxy.php'].join('/')
+          response = Faraday.new.get do |req|
+            req.url url
+            req.params['ApiKey'] = @api_key
+          end
+
+          data = Nokogiri::XML(response.body)
+          raise DataCycleCore::Generic::Common::Error::EndpointError.new("error loading data from #{url}", response) if data.xpath('//errorMesssage').present?
+          clean_paging_hash(data.children.first.to_hash)
+        end
+
+        def load_list(method, page, kind)
+          url = [@host, @end_point, method + 'Proxy.php'].join('/')
+          response = Faraday.new.get do |req|
+            req.url url
+            req.params['ApiKey'] = @api_key
+            req.params['page'] = page
+          end
+
+          data = Nokogiri::XML(response.body)
+          raise DataCycleCore::Generic::Common::Error::EndpointError.new("error loading data from #{url}", response) if data.xpath('//errorMesssage').present?
+          data.children.first.to_hash[kind].map { |i| i["#{kind}ID"] }
+        end
+
+        def load_details(method, id)
+          url = [@host, @end_point, method + 'Proxy.php'].join('/')
+          response = Faraday.new.get do |req|
+            req.url url
+            req.params['ApiKey'] = @api_key
+            req.params['ObjectID'] = id
+          end
+
+          data = Nokogiri::XML(response.body)
+          raise DataCycleCore::Generic::Common::Error::EndpointError.new("error loading data from #{url}", response) if data.xpath('//errorMesssage').present?
+          data.children.first.to_hash
+        end
+
         def flatten_hash_tree(hash)
           (hash || [])&.map { |i| Array.wrap(i.except('Category')) + flatten_hash_tree(i['Category']) }&.flatten
+        end
+
+        def clean_paging_hash(hash)
+          hash.map { |k, v| { k.to_sym => v['text']&.to_i } }.inject(&:merge)
         end
       end
     end
