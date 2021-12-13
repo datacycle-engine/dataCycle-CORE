3b1e4f8b0939124561326d6e4e431d7094b63a61
Patrick Rainer <rainer@pixelpoint.at>
2021-12-13 11:53:12 +0100

    streamline serializers

---
+++ app/models/data_cycle_core/serialize/serializer/base.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Serialize
+    module Serializer
+      class Base
+        class << self
+          def translatable?
+            raise NotImplementedError, 'Implement this method in a child class'
+          end
+
+          def mime_type(_serialized_content, _content)
+            raise NotImplementedError, 'Implement this method in a child class'
+          end
+
+          def file_extension(_mime_type)
+            raise NotImplementedError, 'Implement this method in a child class'
+          end
+
+          def serialize_thing(_content, _language, _version, _transformation = nil)
+            raise NotImplementedError, 'Implement this method in a child class'
+          end
+
+          def serialize_watch_list(_watch_list, _language, _version, _transformation = nil)
+            raise NotImplementedError, 'Implement this method in a child class'
+          end
+
+          def serialize_stored_filter(_stored_filter, _language, _version, _transformation = nil)
+            raise NotImplementedError, 'Implement this method in a child class'
+          end
+        end
+      end
+    end
+  end
+end
