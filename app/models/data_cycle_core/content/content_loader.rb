a1ad5c83b743e810dc2c037bed83f79d2cc61b6b
Benjamin Preisig <preisig@pixelpoint.at>
2021-12-13 12:23:49 +0100

    Reenable unscoping with default_scope applied
    First introduced in 4198b1ef, reverted in 1e67704ae

--- app/models/data_cycle_core/content/content_loader.rb
+++ app/models/data_cycle_core/content/content_loader.rb
@@ -62,7 +62,9 @@ module DataCycleCore
         }
         content_contents_condition[content_filter] = filter.apply.select(:id).except(:order) if filter.present?
 
-        relation_contents = root_object.send(relation_name).where(content_contents: content_contents_condition)
+        relation_contents = self.class.unscoped do
+          root_object.i18n.send(relation_name).where(content_contents: content_contents_condition)
+        end
 
         relation_contents = relation_contents.joins(:translations).where(thing_translations: { locale: languages }) if same_language
         relation_contents
