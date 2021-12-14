f99a2cd6bd96ece3bbf952e043ed48e6354be80c
Oehzelt Martin <oehzelt@pixelpoint.at>
2021-12-14 11:23:13 +0100

    fix mongo remote import for remote_docker to local_docker

--- docker/utility/import_remote_mongo.sh
+++ docker/utility/import_remote_mongo.sh
@@ -24,15 +24,8 @@ REMOTE_DOCKER_HOST=$(read_var REMOTE_DOCKER_HOST $ENV_FILE)
 REMOTE_DOCKER_ENV=$(read_var REMOTE_DOCKER_ENV $ENV_FILE)
 REMOTE_COMPOSER_PROJECT_NAME=$(read_var REMOTE_COMPOSER_PROJECT_NAME $ENV_FILE)
 LOCAL_COMPOSE_PROJECT_NAME=$(read_var COMPOSE_PROJECT_NAME $ENV_FILE)
-
-# local mongodb name
 LOCAL_MONGO=$(docker exec -it "$LOCAL_COMPOSE_PROJECT_NAME"_web_1 rake data_cycle_core:mongo:name[$MONGO_UUID] | tail -n 1 | tr -d '\r')
-# LOCAL_MONGO="data_cycle_development_$MONGO_UUID"
-
-# remote mongodb name
 REMOTE_MONGO=$(DOCKER_HOST="$REMOTE_DOCKER_HOST" docker exec -it "$REMOTE_COMPOSER_PROJECT_NAME"_web_1 rake data_cycle_core:mongo:name[$MONGO_UUID] | tail -n 1 | tr -d '\r')
-# REMOTE_MONGO="data_cycle_production_$MONGO_UUID"
-
 
 print_header_message "ENV vars loaded: $ENV_FILE";
 echo "REMOTE_DOCKER_HOST          : $REMOTE_DOCKER_HOST"
