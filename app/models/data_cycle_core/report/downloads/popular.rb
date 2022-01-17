cb657b2b1a03699b0cec25aaa5f3c65e8e2ba9d7
Patrick Rainer <rainer@pixelpoint.at>
2022-01-17 13:38:18 +0100

    [#25562] introduce additionalParams for reports module

--- app/models/data_cycle_core/report/downloads/popular.rb
+++ app/models/data_cycle_core/report/downloads/popular.rb
@@ -7,12 +7,13 @@ module DataCycleCore
         def apply(params)
           limit = params&.dig(:limit) || 50
           by_month = params&.dig(:by_month) || Time.zone.now.month
+          by_year = params&.dig(:by_year) || Time.zone.now.year
           raw_query = <<-SQL.squish
             SELECT 
               "things"."id", 
               "thing_translations"."name", 
               count("things"."id") as downloads_all, 
-              sum(1) FILTER (where EXTRACT(MONTH FROM "activities"."created_at") = :by_month) AS downloads_by_month
+              sum(1) FILTER (where EXTRACT(MONTH FROM "activities"."created_at") = :by_month AND EXTRACT(YEAR FROM "activities"."created_at") = :by_year) AS downloads_by_month
             FROM things
             JOIN thing_translations ON things.id = thing_translations.thing_id
             JOIN activities ON things.id = activities.activitiable_id 
@@ -25,7 +26,22 @@ module DataCycleCore
             LIMIT :limit
           SQL
 
-          @data = ActiveRecord::Base.connection.execute(ActiveRecord::Base.send(:sanitize_sql_for_conditions, [raw_query, by_month: by_month, limit: limit]))
+          @data = ActiveRecord::Base.connection.execute(ActiveRecord::Base.send(:sanitize_sql_for_conditions, [raw_query, by_month: by_month, by_year: by_year, limit: limit]))
+        end
+
+        private
+
+        def translated_headings
+          by_month = @params&.dig(:by_month) || Time.zone.now.month
+          by_year = @params&.dig(:by_year) || Time.zone.now.year
+          by_month_string = DataCycleCore::MasterData::DataConverter.string_to_datetime([0o7, by_month, by_year].join('-'))
+          @data.fields.map do |key|
+            if key == 'downloads_by_month'
+              I18n.t "feature.report_generator.headings.#{key}", default: key, date: I18n.localize(by_month_string, format: '%B %Y'), locale: @locale
+            else
+              I18n.t "feature.report_generator.headings.#{key}", default: key, locale: @locale
+            end
+          end
         end
       end
     end
