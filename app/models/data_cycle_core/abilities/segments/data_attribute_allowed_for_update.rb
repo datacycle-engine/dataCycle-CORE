40c54613f0332ceec93b7a35ea7fcc4aeb06b9b8
Patrick Rainer <rainer@pixelpoint.at>
2022-01-31 16:48:13 +0100

    [WIP] minor fix for scopes

--- app/models/data_cycle_core/abilities/segments/data_attribute_allowed_for_update.rb
+++ app/models/data_cycle_core/abilities/segments/data_attribute_allowed_for_update.rb
@@ -12,6 +12,14 @@ module DataCycleCore
 
           false
         end
+
+        def attribute_not_disabled?(attribute)
+          attribute.definition.deep_stringify_keys.dig('ui', 'edit', 'disabled').to_s != 'true'
+        end
+
+        def attribute_not_read_only?(attribute)
+          attribute.definition.deep_stringify_keys.dig('ui', 'edit', 'readonly').to_s != 'true'
+        end
       end
     end
   end
