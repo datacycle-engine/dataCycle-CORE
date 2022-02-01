2de602604201eb5709617f4570691d2fe1f84376
Manuel Mitterer <mitterer@pixelpoint.at>
2022-02-01 12:18:34 +0100

    add fallback for file_type if asset is missing

--- app/models/data_cycle_core/utility/default_value/asset.rb
+++ app/models/data_cycle_core/utility/default_value/asset.rb
@@ -4,14 +4,16 @@ module DataCycleCore
   module Utility
     module DefaultValue
       module Asset
-        def self.file_type_classification(property_parameters:, property_definition:, **_args)
-          file_types = DataCycleCore::Asset.find_by(id: property_parameters&.first)&.content_type&.split('/')
+        def self.file_type_classification(property_parameters:, property_definition:, content:, **_args)
+          file_types = DataCycleCore::Asset.find_by(id: property_parameters&.first)&.content_type&.split('/') ||
+                       content.try(:asset)&.content_type&.split('/') ||
+                       content.try(:file_format)&.split('/')
 
           return [] if file_types.blank?
 
           tree_label = DataCycleCore::ClassificationTreeLabel.find_by(name: property_definition&.dig('tree_label'))
 
-          Array.wrap(tree_label&.create_classification_alias(*file_types)&.primary_classification&.id)
+          Array.wrap(tree_label&.create_classification_alias(*Array.wrap(file_types))&.primary_classification&.id)
         end
 
         def self.color_space_classification(property_parameters:, property_definition:, **_args)
