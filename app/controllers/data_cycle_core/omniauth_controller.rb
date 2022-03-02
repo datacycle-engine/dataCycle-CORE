013f41c534db681e470554feb7c02243cec2e072
Manuel Mitterer <mitterer@pixelpoint.at>
2022-03-02 12:39:06 +0100

    update route for pixelpoint aad

--- app/controllers/data_cycle_core/omniauth_controller.rb
+++ app/controllers/data_cycle_core/omniauth_controller.rb
@@ -2,7 +2,7 @@
 
 module DataCycleCore
   class OmniauthController < Devise::OmniauthCallbacksController
-    def pixelpoint_oauth2
+    def pixelpoint_aad_v2
       @user = DataCycleCore::User.from_omniauth(request.env['omniauth.auth'])
       sign_in_and_redirect @user
     end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -20,13 +20,13 @@ Devise.setup do |config|
   config.unlock_strategy = :none
   config.http_authenticatable = true
 
-  if ENV['PIXELPOINT_OAUTH2_CLIENT_ID'].present?
+  if ENV['PIXELPOINT_AAD_V2_CLIENT_ID'].present?
     require 'omniauth-azure-activedirectory-v2'
-    config.omniauth :pixelpoint_oauth2, {
-      name: 'pixelpoint_oauth2',
-      client_id: ENV['PIXELPOINT_OAUTH2_CLIENT_ID'],
-      client_secret: ENV['PIXELPOINT_OAUTH2_CLIENT_SECRET'],
-      tenant_id: ENV['PIXELPOINT_OAUTH2_TENANT_ID'],
+    config.omniauth :pixelpoint_aad_v2, {
+      name: 'pixelpoint_aad_v2',
+      client_id: ENV['PIXELPOINT_AAD_V2_CLIENT_ID'],
+      client_secret: ENV['PIXELPOINT_AAD_V2_CLIENT_SECRET'],
+      tenant_id: ENV['PIXELPOINT_AAD_V2_TENANT_ID'],
       strategy_class: OmniAuth::Strategies::AzureActivedirectoryV2,
       default_role: 'super_admin'
     }
