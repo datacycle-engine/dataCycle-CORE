b7ac850df27208ac77431ad5484c869a2a6afd31
Manuel Mitterer <mitterer@pixelpoint.at>
2022-03-10 16:02:30 +0100

    add elevation to mtk editor

--- app/models/data_cycle_core/feature/data_hash/geo_key_figure.rb
+++ app/models/data_cycle_core/feature/data_hash/geo_key_figure.rb
@@ -3,7 +3,7 @@
 module DataCycleCore
   module Feature
     module DataHash
-      module GipKeyFigure
+      module GeoKeyFigure
         def get_key_figure(_part_ids, _key = nil)
           # for demo purposes return random number between 100 and 1000
           rand(100...1000)
--- app/models/data_cycle_core/feature/geo_key_figure.rb
+++ app/models/data_cycle_core/feature/geo_key_figure.rb
@@ -2,18 +2,22 @@
 
 module DataCycleCore
   module Feature
-    class GipKeyFigure < Base
+    class GeoKeyFigure < Base
       class << self
         def data_hash_module
-          DataCycleCore::Feature::DataHash::GipKeyFigure
+          DataCycleCore::Feature::DataHash::GeoKeyFigure
         end
 
         def controller_module
-          DataCycleCore::Feature::ControllerFunctions::GipKeyFigure
+          DataCycleCore::Feature::ControllerFunctions::GeoKeyFigure
         end
 
         def part_id_path(content)
           configuration(content).dig('part_id_path')
+        end
+
+        def local(content)
+          configuration(content).dig('local')
         end
       end
     end
