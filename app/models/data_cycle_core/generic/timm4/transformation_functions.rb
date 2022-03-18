25764b3eacaef338da75ea00427f1c45641a749d
Oehzelt Martin <oehzelt@pixelpoint.at>
2022-03-18 14:05:44 +0100

    further extend timm4 importer

--- app/models/data_cycle_core/generic/timm4/transformation_functions.rb
+++ app/models/data_cycle_core/generic/timm4/transformation_functions.rb
@@ -26,9 +26,9 @@ module DataCycleCore
           data
         end
 
-        def self.add_contact_name(data)
-          return data if data.dig('address').blank?
-          address = data.dig('address')
+        def self.add_contact_name(data, address_attribute = nil)
+          address = address_attribute.present? ? data['address_attribute'] : data
+          return data if address.blank?
           org = nil
           org = address['organization'] if address['organization']
           contact_name = [address['formOfAddress'], address['firstname'], address['lastname']].compact.join(' ').presence
@@ -36,8 +36,22 @@ module DataCycleCore
           data
         end
 
+        def self.add_location_name(data)
+          return data if data['name'].present?
+          name = [data['formOfAddress'], data['firstname'], data['lastname']].compact.join(' ').presence
+          data['name'] = name.presence || '__no_name__'
+          data
+        end
+
+        def self.add_url(data)
+          return data if data.dig('links').blank?
+          url = data.dig('links')&.first&.dig('url')
+          data['url'] = url.starts_with?('http') ? url.strip : nil
+          data
+        end
+
         def self.add_images(data, external_source_id)
-          images = [data.dig('img'), data.dig('images')]
+          images = [data.dig('img'), data.dig('image'), data.dig('images'), data.dig('mainImage')]
             .flatten
             .compact
             .map { |i| DataCycleCore::Thing.find_by(external_source_id: external_source_id, external_key: i)&.id }
@@ -47,30 +61,136 @@ module DataCycleCore
         end
 
         def self.add_opening_hours_specification(data, external_source_id)
-          return data if data.dig('openingTimes', 'times').blank?
-          data['opening_hours_specification'] = []
-          data.dig('openingTimes', 'times').each_with_index do |time, index|
+          parse_opening_hours_specification(data, external_source_id, 'opening_hours_specification', 'openingTimes')
+        end
+
+        def self.add_dining_hours_specification(data, external_source_id)
+          parse_opening_hours_specification(data, external_source_id, 'opening_hours_specification', 'kitchenTimes')
+        end
+
+        def self.parse_opening_hours_specification(data, external_source_id, property, attribute)
+          return data if data.dig(attribute, 'times').blank?
+          data[property] = []
+          data.dig(attribute, 'times').each_with_index do |time, index|
             holidays = true if time['weekdays'].include?(8)
             opening_hours_days = time['weekdays'].select { |i| i < 8 }.map { |i| i == 7 ? 0 : i }
-            processed = DataCycleCore::Generic::Common::OpeningHours.parse_opening_times({
-              'TimeFrom' => time['startTime'],
-              'DateFrom' => Time.zone.now.beginning_of_year.to_date,
-              'TimeTo' => time['endTime'],
-              'DateTo' => (Time.zone.now.end_of_year + 5.years).to_date,
-              'Holiday' => holidays,
-              'WeekDays' => opening_hours_days
-            }, external_source_id, "#{data['id']} - #{index}")
-            data['opening_hours_specification'].push(processed)
-          end
+            if time['startDate'].present? && time['endDate'].present?
+              start_day = time['startDate'].split('-')[2]&.to_i
+              start_month = time['startDate'].split('-')[1]&.to_i
+              start_day += 1 if start_day.zero?
+              date_from = Time.new(Time.zone.now.year, start_month, start_day).in_time_zone
 
-          data['opening_hours_specification'] = data['opening_hours_specification']&.flatten
+              end_day = time['endDate'].split('-')[2]&.to_i
+              end_month = time['endDate'].split('-')[1]&.to_i
+              end_day += 1 if end_day.zero?
+              date_to = Time.new(Time.zone.now.year, end_month, end_day).in_time_zone
+
+              next_year = date_from > date_to ? 1 : 0
+              (0..5).each do |future|
+                processed = DataCycleCore::Generic::Common::OpeningHours.parse_opening_times({
+                  'TimeFrom' => time['startTime'],
+                  'DateFrom' => (date_from + future.years).to_date,
+                  'TimeTo' => time['endTime'],
+                  'DateTo' => (date_to + (future + next_year).years).to_date,
+                  'Holiday' => holidays,
+                  'WeekDays' => opening_hours_days.presence || (0..6).to_a
+                }, external_source_id, "#{data['id']} - #{index} - #{future}")
+                data[property].push(processed)
+              end
+            else
+              processed = DataCycleCore::Generic::Common::OpeningHours.parse_opening_times({
+                'TimeFrom' => time['startTime'],
+                'DateFrom' => Time.zone.now.beginning_of_year.to_date,
+                'TimeTo' => time['endTime'],
+                'DateTo' => (Time.zone.now.end_of_year + 5.years).to_date,
+                'Holiday' => holidays,
+                'WeekDays' => opening_hours_days
+              }, external_source_id, "#{data['id']} - #{index}")
+              data[property].push(processed)
+            end
+          end
+          data[property] = data[property]&.flatten
           data
         end
 
         def self.add_opening_hours_description(data)
-          return data if data.dig('openingTimes', 'additionalInformation').blank?
-          data['opening_hours_description'] = [{ 'description' => data.dig('openingTimes', 'additionalInformation') }]
+          parse_opening_hours_description(data, 'opening_hours_description', 'openingTimes')
+        end
+
+        def self.add_dining_hours_description(data)
+          parse_opening_hours_description(data, 'dining_hours_description', 'kitchenTimes')
+        end
+
+        def self.parse_opening_hours_description(data, property, attribute)
+          return data if data.dig(attribute, 'additionalInformation').blank?
+          data[property] = [{ 'description' => data.dig(attribute, 'additionalInformation') }]
           data
+        end
+
+        def self.add_line(data)
+          return data if data['sections'].blank? || data['sections'].size.zero?
+          geometry = data['sections']
+          factory = RGeo::Geographic.spherical_factory(srid: 4326, has_z_coordinate: true)
+          lines = geometry
+            .map { |section| section['line'] }
+            .map { |line|
+              line
+                .split(' ')
+                .each_slice(2)
+                .map { |long, lat| factory.point(lat.to_f, long.to_f) }
+            }
+            .map { |line| factory.line_string(line) }
+          data['line'] = factory.multi_line_string(lines)
+          data
+        end
+
+        def self.add_schedule(data)
+          return data if data['dates'].blank?
+          data['event_schedule'] = []
+          data['dates'].each do |date|
+            next if date['date'].blank?
+            next if date['date'].present? && date['dateEnd'].present? && date['date'].in_time_zone > date['dateEnd'].in_time_zone
+            dstart = date['date'].in_time_zone
+            dend = date['dateEnd']&.in_time_zone || dstart
+
+            if data['times'].blank?
+              dtstart = dstart
+              dtend = dend.end_of_day
+              data['event_schedule'] << {
+                start_time: { time: dtstart, zone: dtstart.time_zone.name },
+                end_time: { time: dtend, zone: dtend.time_zone.name },
+                duration: dtend.to_i - dtstart.to_i
+              }
+            else
+              wdays = days(dstart, dend)
+              data['times'].each do |time|
+                weekday = time['weekdays'].first
+                weekday -= 7 if weekday.present? && weekday > 6
+                next unless (weekday.present? && wdays.include?(weekday)) || weekday.blank?
+
+                dtstart = "#{dstart.to_s(:only_date)}T#{time['startTime']}".in_time_zone
+                dtend =
+                  if time['endTime'].present? && time['endTime'] != '00:00:00'
+                    "#{dend.to_s(:only_date)}T#{time['endTime']}".in_time_zone
+                  else
+                    dend.end_of_day
+                  end
+                data['event_schedule'] << {
+                  start_time: { time: dtstart, zone: dtstart.time_zone.name },
+                  end_time: { time: dtend, zone: dtend.time_zone.name },
+                  duration: dtend.to_i - dtstart.to_i
+                }
+              end
+            end
+          end
+          data
+        end
+
+        def self.days(dstart, dend)
+          return Array.wrap(dstart.wday) if dstart == dend
+          days = (dend - dstart) / 1.day.to_i
+          return (0..6).to_a if days >= 6
+          (0..days).map { |i| (dstart + i.days).wday }.sort.uniq
         end
       end
     end
