66c2036aa528d9bb3c6edab5b5bb605cff34f93b
Patrick Rainer <rainer@pixelpoint.at>
2022-04-14 16:03:31 +0200

    appease fasterer

--- app/mailers/data_cycle_core/report_mailer.rb
+++ app/mailers/data_cycle_core/report_mailer.rb
@@ -4,7 +4,8 @@ module DataCycleCore
   class ReportMailer < ApplicationMailer
     def notify(identifier:, format:, recipient:, params: {})
       report_class = DataCycleCore::Feature::ReportGenerator.by_identifier(identifier)
-      data, options = report_class.constantize.new(params: params.merge!({ key: identifier }), locale: 'de').send("to_#{format}")
+      params[:key] = identifier
+      data, options = report_class.constantize.new(params: params, locale: 'de').send("to_#{format}")
 
       attachments[options.dig(:filename)] = {
         mime_type: Mime[format.to_sym],
