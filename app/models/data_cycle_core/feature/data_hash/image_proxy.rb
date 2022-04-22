565ffd707f9c44443abd4d108d65b73c099a1a88
Patrick Rainer <rainer@pixelpoint.at>
2022-04-22 11:57:51 +0200

    [FIX] https://appsignal.com/datacycle-gmbh/sites/5c752f7b5ac13f06592b6b59/exceptions/incidents/44: ImageProxy methods must only be available for supported data_definitions

--- app/models/data_cycle_core/feature/data_hash/image_proxy.rb
+++ app/models/data_cycle_core/feature/data_hash/image_proxy.rb
@@ -4,6 +4,12 @@ module DataCycleCore
   module Feature
     module DataHash
       module ImageProxy
+        def respond_to?(method_name, include_all = false)
+          return false if method_name == :thumbnail_url && property_definitions.try(:[], method_name.to_s).blank?
+          return false if method_name == :asset_web_url && (!DataCycleCore::Feature::ImageProxy.frontend_enabled? || !DataCycleCore::Feature::ImageProxy.supported_content_type?(self))
+          super
+        end
+
         def thumbnail_url
           return DataCycleCore::Feature::ImageProxy.process_image(content: self, variant: 'thumb') if DataCycleCore::Feature::ImageProxy.frontend_enabled? && DataCycleCore::Feature::ImageProxy.supported_content_type?(self)
           super
