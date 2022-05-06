17d8f04a9c59812587c6d88f4989887525690943
Benjamin Preisig <preisig@pixelpoint.at>
2022-05-06 11:06:54 +0200

    Geocode and MapMarker from Coord-Input

--- app/assets/javascripts/components/maplibre_gl_editor.js
+++ app/assets/javascripts/components/maplibre_gl_editor.js
@@ -41,7 +41,6 @@ class MapLibreGlEditor extends MapLibreGlViewer {
 
     // this.initAdditionalControls();
     // this.initMapActions();
-    // if (this.uploadable) this.initUploadActions();
   }
   initFeatures() {
     if (!this.feature && this.value) this.feature = this.value;
@@ -52,6 +51,9 @@ class MapLibreGlEditor extends MapLibreGlViewer {
     this.initAdditionalControls();
 
     if (this.feature) this.initEditFeature();
+
+    if (this.$geoCodeButton) this.$geoCodeButton.on('click', this.geoCodeAddress.bind(this));
+
     // if (!isEmpty(this.additionalValuesOverlay))
     //   this.map.addControl(new AdditionalValuesFilterControl(this), 'bottom-left'); // TODO: locally override called methods if neccessary
     // this.extendEditorInterface();
@@ -71,10 +73,12 @@ class MapLibreGlEditor extends MapLibreGlViewer {
     // Object.assign(this.editorGui.editor.line, this.lineStyle());
     // Object.assign(this.editorGui.editor.dashedLine, this.lineStyle());
   }
-  // initEventHandlers() {
-  //   //TODO: check toursprung-editor for different handlers
-  //   this.$container.on('dc:import:data', this.importData.bind(this)).addClass('dc-import-data');
-  // }
+  initEventHandlers() {
+    //TODO: check toursprung-editor for different handlers
+    // this.$container.on('dc:import:data', this.importData.bind(this)).addClass('dc-import-data');
+    this.$latitudeField.on('change', this.updateMapMarker.bind(this));
+    this.$longitudeField.on('change', this.updateMapMarker.bind(this));
+  }
 
   initAdditionalControls() {
     this.initDrawControl();
@@ -93,7 +97,7 @@ class MapLibreGlEditor extends MapLibreGlViewer {
 
     this.map.on('draw.create', event => {
       this.feature = event.features[0];
-      // this.setCoordinates(); // TODO: for points
+      this.setCoordinates();
       this.setHiddenFieldValue(this.feature);
       // this.initGeometryEditActions(); // TODO: do we need this for point?
     });
@@ -106,7 +110,7 @@ class MapLibreGlEditor extends MapLibreGlViewer {
     // TODO:
     this.map.on('draw.update', event => {
       this.feature = event.features[0];
-      // this.setCoordinates(); // TODO: for points
+      this.setCoordinates();
       this.setHiddenFieldValue(this.feature);
       // this.initGeometryEditActions(); // TODO: do we need this for point?
     });
@@ -123,8 +127,10 @@ class MapLibreGlEditor extends MapLibreGlViewer {
   }
   initEditFeature() {
     const featureIds = this.draw.add(this.feature);
-    this.draw.changeMode('direct_select', { featureId: featureIds[0] });
+    if (this.isLineString()) this.draw.changeMode('direct_select', { featureId: featureIds[0] });
+    if (this.isPoint()) this.draw.changeMode('simple_select', { featureIds: featureIds });
   }
+  // TODO: split-view
   // async importData(event, data) {
   //   if (!this.value || (data && data.force)) {
   //     this.setUploadedFeature(data.value);
@@ -134,49 +140,10 @@ class MapLibreGlEditor extends MapLibreGlViewer {
   //     domElementHelpers.renderImportConfirmationModal(target, data.sourceId, () => this.setUploadedFeature(data.value));
   //   }
   // }
-  // initMapActions() {
-  //   this.initMapEditActions();
-  // }
   // initMapEditActions() {
-  //   if (this.feature) this.initGeometryEditActions();
 
-  //   if (!this.feature) this.initMapDrawableActions();
+  //   this.initSnapableAction(); // TODO: Manuel fragen
 
-  //   this.initSnapableAction();
-
-  //   if (this.$geoCodeButton) this.$geoCodeButton.on('click', this.geoCodeAddress.bind(this));
-
-  //   this.$latitudeField.on('change', this.updateMapMarker.bind(this));
-  //   this.$longitudeField.on('change', this.updateMapMarker.bind(this));
-  // }
-  // initMapDrawableActions() {
-  //   this.drawing = true;
-  //   this.drawableInteraction = new this.ol.interaction.Draw({
-  //     source: this.source,
-  //     type: this.type,
-  //     style: this.highlightStyleFunction.bind(this)
-  //   });
-  //   this.map.addInteraction(this.drawableInteraction);
-
-  //   this.drawableInteraction.on('drawend', event => {
-  //     this.feature = event.feature;
-  //     this.feature.setStyle();
-  //     this.disableDrawableFeature();
-  //     this.setCoordinates();
-  //     this.setHiddenFieldValue(this.getGeoJsonFromFeature());
-  //     this.initGeometryEditActions();
-  //   });
-  // }
-  // initGeometryEditActions() {
-  //   // TODO: change to ModifyableActions for Point after ol update to 6.5 using method in
-  //   // https://openlayers.org/en/latest/examples/modify-icon.html
-  //   if (this.type === 'Point') this.initTranslatableActions();
-  //   else this.initModifyableActions();
-  // }
-  // disableDrawableFeature() {
-  //   this.map.removeInteraction(this.drawableInteraction);
-  //   this.drawing = false;
-  //   this.drawableInteraction = undefined;
   // }
   // initSnapableAction() {
   //   let snap = new this.ol.interaction.Snap({
@@ -184,166 +151,62 @@ class MapLibreGlEditor extends MapLibreGlViewer {
   //   });
   //   this.map.addInteraction(snap);
   // }
-  // initTranslatableActions() {
-  //   this.translateInteraction = new this.ol.interaction.Translate({
-  //     features: new this.ol.collection([this.feature]),
-  //     hitTolerance: 1
-  //   });
+  geoCodeAddress(event) {
+    event.preventDefault();
 
-  //   this.map.addInteraction(this.translateInteraction);
+    if (this.$geoCodeButton.hasClass('disabled')) return;
 
-  //   this.translateInteraction.on('translateend', () => {
-  //     if (this.feature) {
-  //       this.setCoordinates();
-  //       this.setHiddenFieldValue(this.getGeoJsonFromFeature());
-  //       this.map.getView().animate({ duration: 300, center: this.feature.getGeometry().getCoordinates() });
-  //     }
-  //   });
-  // }
-  // initModifyableActions() {
-  //   this.modifyInteraction = new this.ol.interaction.Modify({
-  //     source: this.source,
-  //     style: this.styleFunction.bind(this)
-  //   });
+    this.$geoCodeButton.append(' <i class="fa fa-spinner fa-spin fa-fw"></i>');
+    this.$geoCodeButton.addClass('disabled');
 
-  //   this.map.addInteraction(this.modifyInteraction);
+    let addressKey = this.$geoCodeButton.data('address-key');
+    let locale = this.$geoCodeButton.data('locale');
+    let address = {
+      locale: locale
+    };
 
-  //   this.modifyInteraction.on('modifyend', () => {
-  //     if (this.feature) {
-  //       this.setHiddenFieldValue(this.getGeoJsonFromFeature());
-  //     }
-  //   });
-  // }
-  // geoCodeAddress(event) {
-  //   event.preventDefault();
+    $('.form-element.object.' + addressKey)
+      .find('.form-element')
+      .find('input')
+      .each((_index, elem) => {
+        address[elem.name.getAttributeKey()] = elem.value;
+      });
 
-  //   if (this.$geoCodeButton.hasClass('disabled')) return;
+    const promise = DataCycle.httpRequest({
+      url: '/things/geocode_address',
+      dataType: 'json',
+      data: address
+    });
 
-  //   this.$geoCodeButton.append(' <i class="fa fa-spinner fa-spin fa-fw"></i>');
-  //   this.$geoCodeButton.addClass('disabled');
+    promise
+      .then(data => {
+        if (data.error) {
+          new ConfirmationModal({
+            text: data.error
+          });
+        } else if (data && data.length == 2) {
+          this.setGeocodedValue(data);
+        }
+      })
+      .catch((_jqxhr, textStatus, error) => {
+        console.error(textStatus + ', ' + error);
+      })
+      .finally(() => {
+        this.$geoCodeButton.find('i.fa').remove();
+        this.$geoCodeButton.removeClass('disabled');
+      });
 
-  //   let addressKey = this.$geoCodeButton.data('address-key');
-  //   let locale = this.$geoCodeButton.data('locale');
-  //   let address = {
-  //     locale: locale
-  //   };
-
-  //   $('.form-element.object.' + addressKey)
-  //     .find('.form-element')
-  //     .find('input')
-  //     .each((_index, elem) => {
-  //       address[elem.name.getAttributeKey()] = elem.value;
-  //     });
-
-  //   const promise = DataCycle.httpRequest({
-  //     url: '/things/geocode_address',
-  //     dataType: 'json',
-  //     data: address
-  //   });
-
-  //   promise
-  //     .then(data => {
-  //       if (data.error) {
-  //         new ConfirmationModal({
-  //           text: data.error
-  //         });
-  //       } else if (data && data.length == 2) {
-  //         this.setGeocodedValue(data);
-  //       }
-  //     })
-  //     .catch((_jqxhr, textStatus, error) => {
-  //       console.error(textStatus + ', ' + error);
-  //     })
-  //     .finally(() => {
-  //       this.$geoCodeButton.find('i.fa').remove();
-  //       this.$geoCodeButton.removeClass('disabled');
-  //     });
-
-  //   return promise;
-  // }
-  // setGeocodedValue(data) {
-  //   if (!this.feature) {
-  //     this.feature = new this.ol.Feature();
-  //     this.source.addFeature(this.feature);
-  //     this.initTranslatableActions();
-  //   }
-
-  //   this.feature.setGeometry(new this.ol.geom.Point(data).transform('EPSG:4326', 'EPSG:3857'));
-  //   this.disableDrawableFeature();
-  //   this.setNewCoordinates();
-  // }
-  // relayUploadClick(event) {
-  //   event.preventDefault();
-
-  //   if (this.$uploadInput) {
-  //     this.$uploadInput.click();
-  //   }
-  // }
-  // async handleUploadFile(evt) {
-  //   let file = evt.target.files[0] || null;
-  //   if (!file) {
-  //     new ConfirmationModal({
-  //       text: await I18n.translate('frontend.gpx.file_missing')
-  //     });
-  //   } else {
-  //     const reader = new FileReader();
-  //     reader.onload = (_gpxFile => {
-  //       return async e => {
-  //         let xmlString = e.target.result;
-  //         let parser = new DOMParser();
-  //         let xmlDoc = parser.parseFromString(xmlString, 'text/xml');
-  //         let geoJSON = gpx(xmlDoc);
-  //         let featureGeometry = {
-  //           type: 'MultiLineString',
-  //           coordinates: []
-  //         };
-  //         if (geoJSON && geoJSON.features && geoJSON.features.length) {
-  //           geoJSON.features.forEach(feature => {
-  //             if (feature.geometry.type.includes('MultiLineString'))
-  //               featureGeometry.coordinates.push(...feature.geometry.coordinates);
-  //             else if (feature.geometry.type.includes('LineString'))
-  //               featureGeometry.coordinates.push(feature.geometry.coordinates);
-  //           });
-  //         }
-
-  //         if (!featureGeometry.coordinates || !featureGeometry.coordinates.length) {
-  //           new ConfirmationModal({
-  //             text: await I18n.translate('frontend.gpx.empty'),
-  //             confirmationText: 'Ok'
-  //           });
-  //         } else {
-  //           this.setUploadedFeature(featureGeometry);
-  //         }
-  //         this.$uploadInput.val('');
-  //       };
-  //     })(file);
-  //     reader.readAsText(file);
-  //   }
-  //   DataCycle.enableElement(this.$uploadButton);
-  // }
-  // geoJsonToWkt(geoJson) {
-  //   const wkt = wkx.Geometry.parseGeoJSON(geoJson).toEwkt();
-  //   return wkt;
-
-  //   // if (this._coordinatesEmpty(geometry && geometry.coordinates)) return;
-
-  //   // if (geometry.type.startsWith('LineString')) {
-  //   //   geometry.type = 'Multi' + geometry.type;
-  //   //   geometry.coordinates = [geometry.coordinates];
-  //   // }
-
-  //   // if (geometry.type.includes('LineString')) geometry.coordinates = this.addZCoordinate(geometry.coordinates);
-
-  //   // return this.wktFormat.writeGeometry(this.geoJsonFormat.readGeometry(geometry));
-  // }
-  // addZCoordinate(coords) {
-  //   for (let i = 0; i < coords.length; i++) {
-  //     if (Array.isArray(coords[i]) && coords[i].length == 2 && !Array.isArray(coords[i][0])) coords[i].push(0.0);
-  //     else if (Array.isArray(coords[i])) coords[i] = this.addZCoordinate(coords[i]);
-  //   }
-
-  //   return coords;
-  // }
+    return promise;
+  }
+  setGeocodedValue(data) {
+    if (!this.feature) {
+      this.updateFeature(this.getGeoJsonFromCoordinates(data, 'Point'));
+    } else {
+      this.feature.geometry.coordinates = data;
+      this.updateFeature(this.feature);
+      this.setNewCoordinates();
+    }
+  }
   setUploadedFeature(geoJson) {
     this.updateFeature(geoJson);
   }
@@ -354,36 +217,27 @@ class MapLibreGlEditor extends MapLibreGlViewer {
     this.initEditFeature();
     this.setNewCoordinates();
   }
-  // TODO:
-  // updateMapMarker(_event) {
-  //   let valid = true;
-  //   const coords = this.getGeoJsonFromInputs().coordinates;
-  //   coords.forEach(element => {
-  //     valid = valid && !isNaN(element);
-  //   });
+  updateMapMarker(_event) {
+    let valid = true;
+    const geoJson = this.getGeoJsonFromInputs();
+    const coords = geoJson.geometry.coordinates;
+    console.log(coords);
+    coords.forEach((element, index) => {
+      // TODO: catch error and show some warning "Uncaught Error: Invalid LngLat latitude value: must be between -90 and 90"
+      valid =
+        valid &&
+        !isNaN(element) &&
+        ((index == 0 && element >= -180.0 && element <= 180.0) || (index == 1 && element >= -90.0 && element <= 90.0));
+    });
 
-  //   if (valid && this.feature) {
-  //     this.feature.setGeometry(new this.ol.geom.Point(coords).transform('EPSG:4326', 'EPSG:3857'));
-  //   } else if (valid && !this.feature) {
-  //     this.feature = new this.ol.Feature({
-  //       geometry: new this.ol.geom.Point(coords).transform('EPSG:4326', 'EPSG:3857')
-  //     });
-
-  //     this.source.addFeature(this.feature);
-  //     this.disableDrawableFeature();
-  //     this.initTranslatableActions();
-  //   } else {
-  //     if (this.feature) {
-  //       this.map.removeInteraction(this.translateInteraction);
-  //       this.translateInteraction = undefined;
-  //       this.source.removeFeature(this.feature);
-  //       this.feature = undefined;
-  //     }
-  //     if (!this.drawableInteraction) this.initMapDrawableActions();
-  //   }
-
-  //   this.setNewCoordinates();
-  // }
+    if (valid) {
+      this.updateFeature(geoJson);
+    } else {
+      if (this.feature) {
+        this.draw.trash();
+      }
+    }
+  }
   // TODO:
   removeFeature(event) {
     // console.log(event);
@@ -399,30 +253,24 @@ class MapLibreGlEditor extends MapLibreGlViewer {
     this.resetHiddenFieldValue();
     this.draw.changeMode(this.getMapDrawMode());
   }
-  // TODO:
-  // initUploadActions() {
-  //   if (this.$uploadButton) this.$uploadButton.on('click', this.relayUploadClick.bind(this));
-  //   if (this.$uploadInput) this.$uploadInput.on('change', this.handleUploadFile.bind(this));
-  // }
-  // shortenCoordinates(coords) {
-  //   for (let i = 0; i < coords.length; i++) {
-  //     if (Array.isArray(coords[i])) coords[i] = this.shortenCoordinates(coords[i]);
-  //     else coords[i] = Number(coords[i].toFixed(this.precision));
-  //   }
+  shortenCoordinates(coords) {
+    for (let i = 0; i < coords.length; i++) {
+      if (Array.isArray(coords[i])) coords[i] = this.shortenCoordinates(coords[i]);
+      else coords[i] = Number(coords[i].toFixed(this.precision));
+    }
 
-  //   return coords;
-  // }
-  // getFeatureLatLon() {
-  //   let coords = this.feature.getGeometry().clone().transform('EPSG:3857', 'EPSG:4326').getCoordinates();
+    return coords;
+  }
+  getFeatureLatLon() {
+    let coords = this.feature.geometry.coordinates;
 
-  //   return this.shortenCoordinates(coords);
-  // }
+    return this.shortenCoordinates(coords);
+  }
   setNewCoordinates() {
     this.setCoordinates();
     this.setHiddenFieldValue(this.feature);
     if (this.feature) this.updateMapPosition();
   }
-  // TODO:
   setCoordinates() {
     if (!this.feature || !this.isPoint()) return;
 
@@ -430,35 +278,28 @@ class MapLibreGlEditor extends MapLibreGlViewer {
     this.$latitudeField.val(latLon[1]);
     this.$longitudeField.val(latLon[0]);
   }
-  // TODO:
   resetCoordinates() {
     if (!this.isPoint()) return;
 
     this.$latitudeField.val('');
     this.$longitudeField.val('');
   }
-  // getGeoJsonFromInputs() {
-  //   return {
-  //     type: this.type,
-  //     coordinates: [parseFloat(this.$longitudeField.val()), parseFloat(this.$latitudeField.val())]
-  //   };
-  // }
-  // getGeoJsonFromFeature() {
-  //   if (!this.feature) return;
-
-  //   return {
-  //     type: this.type,
-  //     coordinates: this.getFeatureLatLon()
-  //   };
-  // }
-  // _coordinatesEmpty(coords) {
-  //   if (isEmpty(coords)) return true;
-
-  //   if (Array.isArray(coords) && coords.some(c => Array.isArray(c)))
-  //     return coords.every(c => this._coordinatesEmpty(c));
-
-  //   return false;
-  // }
+  getGeoJsonFromInputs() {
+    return this.getGeoJsonFromCoordinates(
+      [parseFloat(this.$longitudeField.val()), parseFloat(this.$latitudeField.val())],
+      this.type
+    );
+  }
+  getGeoJsonFromCoordinates(coords, type) {
+    return {
+      type: 'Feature',
+      properties: {},
+      geometry: {
+        type: type,
+        coordinates: coords
+      }
+    };
+  }
   setHiddenFieldValue(geoJson) {
     this.value = geoJson;
 
