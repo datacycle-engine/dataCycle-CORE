b72c7d8e0d3a5eef6bbf720a6877f5ee1731b4b7
Patrick Rainer <rainer@pixelpoint.at>
2022-05-27 10:56:37 +0200

    appease rubocop

--- test/dummy/config/environments/production.rb
+++ test/dummy/config/environments/production.rb
@@ -112,7 +112,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.default_options = { from: 'noreply@localhost' }
   config.action_mailer.default_url_options = { host: ENV.fetch('APP_HOST', 'localhost:3000'), protocol: ENV.fetch('APP_PROTOCOL', 'http') }
