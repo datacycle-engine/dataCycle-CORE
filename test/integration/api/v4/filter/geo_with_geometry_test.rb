45e163d7602823b212789e3f646b1167ec6ab861
Benjamin Preisig <preisig@pixelpoint.at>
2022-05-30 17:30:52 +0200

    Fix test

--- test/integration/api/v4/filter/geo_with_geometry_test.rb
+++ test/integration/api/v4/filter/geo_with_geometry_test.rb
@@ -6,13 +6,11 @@ module DataCycleCore
   module Api
     module V4
       module Filter
-        class GeoBoxTest < DataCycleCore::V4::Base
+        class GeoWithGeometryTest < DataCycleCore::V4::Base
           before(:all) do
             DataCycleCore::Thing.where(template: false).delete_all
 
-            @test_tour = DataCycleCore::DummyDataHelper.create_data('tour')
-
-            @poi_a = DataCycleCore::V4::DummyDataHelper.create_data('poi')
+            @poi_a = DataCycleCore::V4::DummyDataHelper.create_data('minimal_poi')
             lat_long_a = {
               'latitude': 1,
               'longitude': 10
@@ -21,7 +19,7 @@ module DataCycleCore
             @poi_a.location = RGeo::Geographic.spherical_factory(srid: 4326).point(@poi_a.longitude, @poi_a.latitude)
             @poi_a.save
 
-            @poi_b = DataCycleCore::V4::DummyDataHelper.create_data('poi')
+            @poi_b = DataCycleCore::V4::DummyDataHelper.create_data('minimal_poi')
             lat_long_b = {
               'latitude': 5,
               'longitude': 5
@@ -30,11 +28,16 @@ module DataCycleCore
             @poi_b.location = RGeo::Geographic.spherical_factory(srid: 4326).point(@poi_b.longitude, @poi_b.latitude)
             @poi_b.save
 
-            @poi_c = DataCycleCore::V4::DummyDataHelper.create_data('poi')
-            lat_long_c = {}
+            @poi_c = DataCycleCore::V4::DummyDataHelper.create_data('minimal_poi')
+            lat_long_c = {
+              'latitude': 10,
+              'longitude': 1
+            }
             @poi_c.set_data_hash(partial_update: true, prevent_history: true, data_hash: lat_long_c)
+            @poi_c.location = RGeo::Geographic.spherical_factory(srid: 4326).point(@poi_c.longitude, @poi_c.latitude)
+            @poi_c.save
 
-            @poi_d = DataCycleCore::V4::DummyDataHelper.create_data('poi')
+            @poi_d = DataCycleCore::V4::DummyDataHelper.create_data('minimal_poi')
             lat_long_d = {}
             @poi_d.set_data_hash(partial_update: true, prevent_history: true, data_hash: lat_long_d)
 
@@ -68,7 +71,7 @@ module DataCycleCore
               }
             }
             post api_v4_things_path(params)
-            assert_api_count_result(2)
+            assert_api_count_result(1)
           end
         end
       end
