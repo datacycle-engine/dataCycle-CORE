72dbdb5cebcc664900f36501c567bb185e8bffc5
Benjamin Preisig <preisig@pixelpoint.at>
2022-07-11 15:46:08 +0200

    Add instrumentation for Faraday Errors

--- config/initializers/instrumentation.rb
+++ config/initializers/instrumentation.rb
@@ -12,3 +12,16 @@ ActiveSupport::Notifications.subscribe('vite_asset_path_error.datacycle') do |_n
     end
   end
 end
+
+ActiveSupport::Notifications.subscribe('faraday_error.datacycle') do |_name, _started, _finished, _unique_id, data|
+  if Rails.env.development?
+    Rails.logger.warn "Error while connecting to '#{data[:target_url]}', Exception: #{data[:exception]}"
+  else
+    Appsignal.send_error(data[:exception]) do |transaction|
+      transaction.set_namespace('faraday_error')
+      transaction.params = {
+        target_url: data[:target_url]
+      }
+    end
+  end
+end
