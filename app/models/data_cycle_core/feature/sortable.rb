b11588ae23c94df479d5a77f71cdeb8cf6efab13
Patrick Rainer <rainer@pixelpoint.at>
2022-08-11 16:00:12 +0200

    appease rubocop

--- app/models/data_cycle_core/feature/sortable.rb
+++ app/models/data_cycle_core/feature/sortable.rb
@@ -11,7 +11,6 @@ module DataCycleCore
           DataCycleCore.features.dig(name.demodulize.underscore.to_sym)&.except(:enabled)&.each do |key, value|
             sortable.concat(try(key.to_sym, value, user) || default(key.to_s, value, user) || [])
           end
-          
           sortable.select { |k, v| user.can?(:sortable, view.to_sym, k, v) }
         end
 
