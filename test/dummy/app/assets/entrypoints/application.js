1f8a1b555dba4e9affd1abde7659f1633a0687f3
Patrick Rainer <rainer@pixelpoint.at>
2022-11-22 17:27:55 +0100

    cleanup application.js in dummy

--- test/dummy/app/assets/entrypoints/application.js
+++ test/dummy/app/assets/entrypoints/application.js
@@ -1,10 +1,3 @@
 import DataCycleInit from '../../../../../app/assets/javascripts/application';
 
-// Demo for AppSignal Integration
-// import appSignalInit from '../../../../../app/assets/javascripts/initializers/app_signal_init';
-// appSignalInit('some-appsignal-fronteend-key');
-// DataCycleInit({}, () => {
-//   DataCycle.notifications.addEventListener('error', ({ detail }) => appSignal.sendError(detail));
-// });
-
 DataCycleInit();
