9a50a81e0d7da5a220c2b610d70bc56d769b7a87
Patrick Rainer <rainer@pixelpoint.at>
2022-11-23 14:12:45 +0100

    delete old abilities

--- app/models/data_cycle_core/ability.rb
+++ app/models/data_cycle_core/ability.rb
@@ -18,16 +18,7 @@ module DataCycleCore
     private
 
     def load_abilities
-      can :show, :all
-
-      [*0..10, 99].select { |r| r <= user.role&.rank.to_i }.each do |rank|
-        merge DataCycleCore::Abilities.const_get("rank_#{rank}".classify).new(user, session)
-      end
-
-      DataCycleCore.features.select { |_, v| !v.dig(:only_config) == true }.each_key do |key|
-        feature = ('DataCycleCore::Feature::' + key.to_s.classify).constantize
-        merge feature.ability_class.new(user, session) if feature.enabled? && feature.ability_class
-      end
+      DataCycleCore::Abilities::PermissionsList.add_abilities_for_user(self)
     end
   end
 end
