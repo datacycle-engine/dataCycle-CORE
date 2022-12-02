dabde5f8274553eaf498ee0b4f4ac6595d70fa22
Manuel Mitterer <mitterer@pixelpoint.at>
2022-12-02 11:49:56 +0100

    refactor activities admin page

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -21,6 +21,13 @@ crumb :admin do
   link to_html_string(t('data_cycle_core.administration', locale: active_ui_locale)), admin_path, authorized: can?(:manage, :dash_board)
 end
 
+# Activities
+crumb :activities do
+  link to_html_string(t('data_cycle_core.activities', locale: active_ui_locale)), admin_activities_path, authorized: can?(:manage, :dash_board)
+
+  parent :admin
+end
+
 # Administration
 crumb :reports do
   link to_html_string(t('data_cycle_core.reports.root', locale: active_ui_locale)), reports_path, authorized: can?(:manage, :reports)
