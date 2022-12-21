c60be1275aed04f659b7099be60f228d7bab0305
Manuel Mitterer <mitterer@pixelpoint.at>
2022-12-21 14:30:29 +0100

    small UI changes for admin dashboard

--- app/models/data_cycle_core/stats_database.rb
+++ app/models/data_cycle_core/stats_database.rb
@@ -126,11 +126,20 @@ module DataCycleCore
               end
 
           last_download = external_source.last_download.presence || 'never'
-          last_import = external_source.last_import.presence || 'never'
           last_successful_download = external_source.last_successful_download.presence || 'never'
-          last_successful_import = external_source.last_successful_import.presence || 'never'
           last_successful_download_class = last_download == last_successful_download ? 'success-color' : 'alert-color' if !external_source.deactivated && (last_download != 'never' || last_successful_download != 'never')
+          if last_successful_download_class == 'alert-color' && Delayed::Job.where("delayed_reference_type ILIKE '%download%'").where(queue: 'importers', delayed_reference_id: external_source.id, failed_at: nil, attempts: 0).where.not(locked_by: nil).exists?
+            last_successful_download = last_download
+            last_successful_download_class = 'primary-color'
+          end
+
+          last_import = external_source.last_import.presence || 'never'
+          last_successful_import = external_source.last_successful_import.presence || 'never'
           last_successful_import_class = last_import == last_successful_import ? 'success-color' : 'alert-color' if !external_source.deactivated && (last_import != 'never' || last_successful_import != 'never')
+          if last_successful_import_class == 'alert-color' && Delayed::Job.where("delayed_reference_type ILIKE '%import%'").where(queue: 'importers', delayed_reference_id: external_source.id, failed_at: nil, attempts: 0).where.not(locked_by: nil).exists?
+            last_successful_import = last_import
+            last_successful_import_class = 'primary-color'
+          end
 
           @import_modules.push(
             {
