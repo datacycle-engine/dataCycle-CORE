d450511989d92abf3b0f1c1c0d3263b07993c002
Manuel Mitterer <mitterer@pixelpoint.at>
2023-03-06 14:33:41 +0100

    fix to_history functions

--- db/migrate/20230303150323_add_function_for_to_history.rb
+++ db/migrate/20230303150323_add_function_for_to_history.rb
@@ -17,8 +17,8 @@ class AddFunctionForToHistory < ActiveRecord::Migration[6.1]
       BEGIN
       SELECT 'INSERT INTO thing_history_translations (thing_history_id, ' || string_agg(column_name, ', ') || ') SELECT ''' || new_history_id || '''::UUID, ' || string_agg('t.' || column_name, ', ') || ' FROM thing_translations t WHERE t.thing_id = ''' || content_id || '''::UUID AND (CASE WHEN ' || all_translations || '::BOOLEAN THEN t.locale IS NOT NULL ELSE t.locale = ''' || current_locale || '''::VARCHAR END);' INTO insert_query
       FROM information_schema.columns
-      WHERE table_name = 'thing_translations'
-        AND column_name NOT IN ('id', 'thing_id');
+      WHERE table_name = 'thing_history_translations'
+        AND column_name NOT IN ('id', 'thing_history_id');
 
       EXECUTE insert_query;
 
@@ -34,8 +34,8 @@ class AddFunctionForToHistory < ActiveRecord::Migration[6.1]
       BEGIN
       SELECT 'INSERT INTO classification_content_histories (content_data_history_id, ' || string_agg(column_name, ', ') || ') SELECT ''' || new_history_id || '''::UUID, ' || string_agg('t.' || column_name, ', ') || ' FROM classification_contents t WHERE t.content_data_id = ''' || content_id || '''::UUID;' INTO insert_query
       FROM information_schema.columns
-      WHERE table_name = 'classification_contents'
-        AND column_name NOT IN ('id', 'content_data_id');
+      WHERE table_name = 'classification_content_histories'
+        AND column_name NOT IN ('id', 'content_data_history_id');
 
       EXECUTE insert_query;
 
@@ -59,15 +59,15 @@ class AddFunctionForToHistory < ActiveRecord::Migration[6.1]
       BEGIN
       SELECT 'INSERT INTO content_content_histories (content_a_history_id, content_b_history_id, content_b_history_type, ' || string_agg(column_name, ', ') || ') SELECT ''' || new_history_id || '''::UUID, t.content_b_id, ''DataCycleCore::Thing'', ' || string_agg('t.' || column_name, ', ') || ' FROM content_contents t INNER JOIN things ON things.id = t.content_b_id WHERE t.content_a_id = ''' || content_id || '''::UUID AND things.content_type != ''embedded'';' INTO insert_linked_query
       FROM information_schema.columns
-      WHERE table_name = 'content_contents'
-        AND column_name NOT IN ('id', 'content_a_id', 'content_b_id');
+      WHERE table_name = 'content_content_histories'
+        AND column_name NOT IN ('id', 'content_a_history_id', 'content_b_history_id', 'content_b_history_type');
 
       EXECUTE insert_linked_query;
 
       SELECT 'INSERT INTO content_content_histories (content_a_history_id, content_b_history_id, content_b_history_type, ' || string_agg(column_name, ', ') || ') SELECT ''' || new_history_id || '''::UUID, to_thing_history (t.content_b_id, ''' || current_locale || '''::VARCHAR, ' || all_translations || '::BOOLEAN, ' || deleted || '::BOOLEAN), ''DataCycleCore::Thing::History'', ' || string_agg('t.' || column_name, ', ') || ' FROM content_contents t INNER JOIN things ON things.id = t.content_b_id WHERE t.content_a_id = ''' || content_id || '''::UUID AND things.content_type = ''embedded'';' INTO insert_embedded_query
       FROM information_schema.columns
-      WHERE table_name = 'content_contents'
-        AND column_name NOT IN ('id', 'content_a_id', 'content_b_id');
+      WHERE table_name = 'content_content_histories'
+        AND column_name NOT IN ('id', 'content_a_history_id', 'content_b_history_id', 'content_b_history_type');
 
       EXECUTE insert_embedded_query;
 
@@ -83,8 +83,8 @@ class AddFunctionForToHistory < ActiveRecord::Migration[6.1]
       BEGIN
       SELECT 'INSERT INTO schedule_histories (thing_history_id, ' || string_agg(column_name, ', ') || ') SELECT ''' || new_history_id || '''::UUID, ' || string_agg('t.' || column_name, ', ') || ' FROM schedules t WHERE t.thing_id = ''' || content_id || '''::UUID;' INTO insert_query
       FROM information_schema.columns
-      WHERE table_name = 'schedules'
-        AND column_name NOT IN ('id', 'thing_id');
+      WHERE table_name = 'schedule_histories'
+        AND column_name NOT IN ('id', 'thing_history_id');
 
       EXECUTE insert_query;
 
