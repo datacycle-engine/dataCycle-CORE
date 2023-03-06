c6b2b84e00cb9c78c05d3df9416b485c8d73a096
Manuel Mitterer <mitterer@pixelpoint.at>
2023-03-06 08:10:45 +0100

    [BRAKEMAN] fix possible SQL injection

--- app/models/data_cycle_core/content/create_history.rb
+++ app/models/data_cycle_core/content/create_history.rb
@@ -4,7 +4,17 @@ module DataCycleCore
   module Content
     module CreateHistory
       def to_history(delete: false, all_translations: false)
-        ActiveRecord::Base.connection.execute("SELECT to_thing_history ('#{id}'::UUID, '#{I18n.locale}'::VARCHAR, #{all_translations}::BOOLEAN, #{delete}::BOOLEAN);")
+        return if embedded?
+
+        ActiveRecord::Base.connection.execute(
+          ActiveRecord::Base.send(:sanitize_sql_array, [
+                                    'SELECT to_thing_history (?::UUID, ?::VARCHAR, ?::BOOLEAN, ?::BOOLEAN);',
+                                    id,
+                                    I18n.locale,
+                                    all_translations,
+                                    delete
+                                  ])
+        )
       end
     end
   end
