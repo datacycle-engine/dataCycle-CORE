634c2d97005f73a32a101e1e13d188ab37b16572
Manuel Mitterer <mitterer@pixelpoint.at>
2023-03-13 10:01:09 +0100

    fix template_validator

--- app/models/data_cycle_core/master_data/templates/template_header_contract.rb
+++ app/models/data_cycle_core/master_data/templates/template_header_contract.rb
@@ -8,8 +8,9 @@ module DataCycleCore
           required(:data).hash do
             required(:name) { str? }
             required(:type) { str? & eql?('object') }
-            required(:schema_type) { str? }
             required(:content_type) { str? & included_in?(['embedded', 'entity', 'container']) }
+            optional(:schema_type) { str? }
+            optional(:schema_ancestors) { array? }
             optional(:boost) { float? }
             optional(:features) { hash? }
             required(:properties).hash do
@@ -19,6 +20,12 @@ module DataCycleCore
               optional(:type) { str? | array? }
             end
           end
+        end
+
+        rule(:data) do
+          next unless key? && value.present? && value.is_a?(::Hash)
+
+          key.failure(:missing_schema_type_or_ancestors) unless value.key?(:schema_type) || value.key?(:schema_ancestors)
         end
       end
     end
