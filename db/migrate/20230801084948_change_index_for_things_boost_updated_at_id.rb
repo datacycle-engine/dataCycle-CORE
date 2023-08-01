6ae93bd66557dc93c3fc9aec52df7c2a1c00fe73
Manuel Mitterer <mitterer@pixelpoint.at>
2023-08-01 11:36:08 +0200

    revert index change

--- db/migrate/20230801084948_change_index_for_things_boost_updated_at_id.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class ChangeIndexForThingsBoostUpdatedAtId < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :things, [:boost, :updated_at, :id], name: 'index_things_on_boost_updated_at_id', if_exists: true
-    add_index :things, [:boost, :updated_at, :id], order: { boost: 'DESC NULLS LAST', updated_at: 'DESC NULLS LAST', id: 'DESC NULLS LAST' }, name: 'index_things_on_boost_updated_at_id', if_not_exists: true
-  end
-end
