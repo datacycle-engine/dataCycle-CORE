49b48504eb4ea13cb0e92e0cf842ebd825b4c366
Manuel Mitterer <mitterer@pixelpoint.at>
2023-10-16 12:51:08 +0200

    [#32572] allow preview configuration to change widget version

--- app/models/data_cycle_core/feature/preview.rb
+++ app/models/data_cycle_core/feature/preview.rb
@@ -4,8 +4,22 @@ module DataCycleCore
   module Feature
     class Preview < Base
       class << self
-        def available_widgets
-          DataCycleCore.features.dig(name.demodulize.underscore.to_sym, :widgets).reject { |_, v| v.blank? }
+        def available_widgets(endpoint, ui_locale = :de)
+          configuration.dig(:widgets)&.compact_blank&.transform_values do |url|
+            if url.is_a?(::Hash)
+              send("url_to_v#{url[:version] || 2}", url[:url], endpoint, ui_locale)
+            else
+              url_to_v2(url, endpoint, ui_locale)
+            end
+          end
+        end
+
+        def url_to_v2(url, endpoint, _ui_locale)
+          "#{url}?data_cycle_widget[endpoint]=#{endpoint}"
+        end
+
+        def url_to_v3(url, endpoint, ui_locale)
+          "#{url}?api-endpoint=#{endpoint}&locale=#{ui_locale}"
         end
       end
     end
