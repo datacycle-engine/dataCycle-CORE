44c31518c011d0cb1e4cedea45b694e01d5dedad
Johannes Zlattinger <zlattinger@pixelpoint.at>
2023-10-16 08:43:21 +0200

    [#32435] fix tests

--- app/models/data_cycle_core/master_data/differs/geographic.rb
+++ app/models/data_cycle_core/master_data/differs/geographic.rb
@@ -16,7 +16,9 @@ module DataCycleCore
 
         def geo_comp(a, b)
           # special case to fix a problem with linestrings from gip with changed direction (#32435)
-          return false if a.respond_to?(:coordinates) && b.respond_to?(:coordinates) && a.coordinates.flatten[0..1] != b.coordinates.flatten[0..1]
+          return false if a.respond_to?(:coordinates) && b.respond_to?(:coordinates) &&
+                          a.geometry_type != RGeo::Feature::Point && b.geometry_type != RGeo::Feature::Point &&
+                          a.coordinates.flatten[0..1] != b.coordinates.flatten[0..1]
 
           return true if a == b
           return false if a.blank? || b.blank?
--- test/dummy/db/structure.sql
+++ test/dummy/db/structure.sql
@@ -3221,6 +3221,8 @@ ALTER TABLE public.classification_contents DISABLE TRIGGER delete_ccc_relations_
 
 CREATE TRIGGER delete_ccc_relations_transitive_trigger AFTER DELETE ON public.classification_groups REFERENCING OLD TABLE AS old_classification_groups FOR EACH STATEMENT EXECUTE FUNCTION public.delete_ca_paths_transitive_trigger_1();
 
+ALTER TABLE public.classification_groups DISABLE TRIGGER delete_ccc_relations_transitive_trigger;
+
 
 --
 -- Name: classification_groups delete_collected_classification_content_relations_trigger_1; Type: TRIGGER; Schema: public; Owner: -
@@ -3287,6 +3289,8 @@ ALTER TABLE public.classification_contents DISABLE TRIGGER generate_ccc_relation
 --
 
 CREATE TRIGGER generate_ccc_relations_transitive_trigger AFTER INSERT ON public.classification_groups REFERENCING NEW TABLE AS new_classification_groups FOR EACH STATEMENT EXECUTE FUNCTION public.generate_ca_paths_transitive_trigger_4();
+
+ALTER TABLE public.classification_groups DISABLE TRIGGER generate_ccc_relations_transitive_trigger;
 
 
 --
@@ -3465,6 +3469,8 @@ ALTER TABLE public.classification_contents DISABLE TRIGGER update_ccc_relations_
 
 CREATE TRIGGER update_ccc_relations_transitive_trigger AFTER UPDATE ON public.classification_groups REFERENCING OLD TABLE AS old_classification_groups NEW TABLE AS new_classification_groups FOR EACH STATEMENT EXECUTE FUNCTION public.update_ca_paths_transitive_trigger_4();
 
+ALTER TABLE public.classification_groups DISABLE TRIGGER update_ccc_relations_transitive_trigger;
+
 
 --
 -- Name: classification_groups update_ccc_relations_trigger_4; Type: TRIGGER; Schema: public; Owner: -
@@ -3555,6 +3561,8 @@ CREATE TRIGGER update_content_content_links_trigger AFTER UPDATE OF content_a_id
 --
 
 CREATE TRIGGER update_deleted_at_ccc_relations_transitive_trigger AFTER UPDATE ON public.classification_groups REFERENCING OLD TABLE AS old_classification_groups NEW TABLE AS new_classification_groups FOR EACH STATEMENT EXECUTE FUNCTION public.delete_ca_paths_transitive_trigger_2();
+
+ALTER TABLE public.classification_groups DISABLE TRIGGER update_deleted_at_ccc_relations_transitive_trigger;
 
 
 --
