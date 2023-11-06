0b98abca7d65d6ffc4e7720d8931f577a2d1e13f
Manuel Mitterer <mitterer@pixelpoint.at>
2023-11-06 10:41:25 +0100

    [#33350] fix error in reports

--- app/models/data_cycle_core/report/downloads/content.rb
+++ app/models/data_cycle_core/report/downloads/content.rb
@@ -9,21 +9,23 @@ module DataCycleCore
           date_time_format = 'DD.MM.YYYY HH24:MI:SS'
           raise DataCycleCore::Error::RecordNotFoundError "#{thing_id} not found!" if DataCycleCore::Thing.find(thing_id).blank?
           raw_query = <<-SQL.squish
-            SELECT 
-                "things"."id", 
-                "thing_translations"."name", 
-                to_char("activities"."created_at", :date_time_format) as date_created,
-                "users"."email",
-                concat("users"."given_name", ' ', "users"."family_name") as user_display_name
+            SELECT "things"."id",
+              "thing_translations"."content"->>'name' AS "name",
+              to_char("activities"."created_at", :date_time_format) AS date_created,
+              "users"."email",
+              concat("users"."given_name", ' ', "users"."family_name") AS user_display_name
             FROM things
-            JOIN thing_translations ON things.id = thing_translations.thing_id AND thing_translations.locale = :locale
-            JOIN activities ON things.id = activities.activitiable_id 
-            OR things.id = ANY (
-              ARRAY(SELECT jsonb_array_elements_text(activities.data -> 'collection_items'))::uuid[]
-            )
-            JOIN users on activities.user_id = users.id
-            where activities.activity_type = 'download'
-            AND "things"."id" = :thing_id
+              JOIN thing_translations ON things.id = thing_translations.thing_id
+              AND thing_translations.locale = :locale
+              JOIN activities ON things.id = activities.activitiable_id
+              OR things.id = ANY (
+                ARRAY(
+                  SELECT jsonb_array_elements_text(activities.data->'collection_items')
+                )::uuid []
+              )
+              JOIN users ON activities.user_id = users.id
+            WHERE activities.activity_type = 'download'
+              AND "things"."id" = :thing_id
             ORDER BY date_created DESC
           SQL
 
