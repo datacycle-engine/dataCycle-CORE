e029b48d89c4f7ce7d55f38518372f8934a24bcf
Manuel Mitterer <mitterer@pixelpoint.at>
2023-11-07 15:29:16 +0100

    [#33318] add button to rebuild classification mappings to admin dash_board

--- app/controllers/data_cycle_core/dash_board_controller.rb
+++ app/controllers/data_cycle_core/dash_board_controller.rb
@@ -72,55 +72,10 @@ module DataCycleCore
       redirect_to admin_path
     end
 
-    def import_templates
-      @errors = nil
-      @duplicates = nil
+    def rebuild_classification_mappings
+      DataCycleCore::RunTaskJob.perform_now('db:configure:rebuild_transitive_tables')
 
-      template_importer = DataCycleCore::MasterData::Templates::TemplateImporter.new
-      template_importer.import
-
-      if template_importer.errors.blank? && template_importer.mixin_errors.blank? && template_importer.duplicates.blank?
-        flash[:notice] = I18n.t :imported, scope: [:controllers, :job], data: 'data types', locale: helpers.active_ui_locale
-      else
-        error_level = template_importer.errors.blank? && template_importer.mixin_errors.blank? ? :notice : :error
-        @errors = [*template_importer.errors, *template_importer.mixin_errors]
-        @duplicates = template_importer.duplicates
-        puts 'duplicates:'
-        ap @duplicates
-        puts 'errors:'
-        ap @errors
-
-        flash[error_level] = "#{@errors.size} errors/#{@duplicates.size} warnings were encountered"
-      end
-
-      redirect_to admin_path
-    end
-
-    def import_classifications
-      MasterData::ImportClassifications.import_all
-
-      flash[:notice] = I18n.t :imported, scope: [:controllers, :job], data: 'basic classification trees', locale: helpers.active_ui_locale
-
-      redirect_to admin_path
-    end
-
-    def import_external_systems
-      @errors = nil
-      errors = MasterData::ImportExternalSystems.import_all
-
-      if errors.blank?
-        flash[:notice] = I18n.t :imported, scope: [:controllers, :job], data: 'import external systems', locale: helpers.active_ui_locale
-      else
-        @errors = errors
-        puts 'errors:'
-        ap errors
-        flash[:error] = I18n.t('dash_board.errors_were_encountered', locale: helpers.active_ui_locale)
-      end
-
-      redirect_to admin_path
-    end
-
-    def classifications
+      redirect_to(admin_path, notice: I18n.t('dash_board.maintenance.classification_mappings_rebuilt', locale: helpers.active_ui_locale))
     end
 
     def activities
