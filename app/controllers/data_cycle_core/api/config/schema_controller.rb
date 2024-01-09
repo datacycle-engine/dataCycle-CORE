d1d6c783cf971faa261398f27518d5461433e94a
Samuel Hafner <hafner@pixelpoint.at>
2024-01-09 16:45:27 +0100

    [#34044] fetch all embedded template definitions

--- app/controllers/data_cycle_core/api/config/schema_controller.rb
+++ app/controllers/data_cycle_core/api/config/schema_controller.rb
@@ -8,27 +8,19 @@ module DataCycleCore
 
         def index
           contents = DataCycleCore::ThingTemplate.all.to_a
-          render json: schema_api_format(contents) { contents.map(&:schema_sorted) }.to_json
-          # else
-          #   render json: { error: 'No ids given!' }, layout: false, status: :bad_request
-          # end
+          render json: schema_api_format(contents) { contents.map(&:schema_as_json) }.to_json
         end
 
         def show
           name = params[:template_name]
           template = DataCycleCore::ThingTemplate.find_by(template_name: name)
           if template.present?
-            embedded = template.template_thing.embedded_property_names
-            content = Array.wrap(template.schema_sorted)
-            embedded.each do |property_name|
-              content[0]['properties'][property_name]['schema_template_url'] = show_api_config_schema_index_url(template_name: content[0]['properties'][property_name]['template_name'])
-            end
+            content = Array.wrap(template.schema_as_json)
             render json: schema_api_format(content) { content }.to_json
           else
             error = "Couldn't find ThingTemplate '#{name}'"
-            render json: { error: error }, layout: false, status: :not_found
+            render json: { error: }, layout: false, status: :not_found
           end
-
         end
 
         def permitted_parameter_keys
