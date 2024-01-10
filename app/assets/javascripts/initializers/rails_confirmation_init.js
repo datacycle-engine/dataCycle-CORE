6086d9172e4654c2888298c32dffdc27ffab1df2
Manuel Mitterer <mitterer@pixelpoint.at>
2024-01-10 11:16:53 +0100

    [#34196] fix confirmation dialog

--- app/assets/javascripts/initializers/rails_confirmation_init.js
+++ app/assets/javascripts/initializers/rails_confirmation_init.js
@@ -1,19 +1,19 @@
-import ConfirmationModal from './../components/confirmation_modal';
+import ConfirmationModal from "./../components/confirmation_modal";
 
 export default function () {
-  Rails.confirm = function (message, element) {
-    if (element.dataset.confirmed) return true;
+	Rails.confirm = function (message, element) {
+		if (element.dataset.confirmed) return true;
 
-    new ConfirmationModal({
-      text: message,
-      confirmationClass: 'alert',
-      cancelable: true,
-      confirmationCallback: () => {
-        element.dataset.confirmed = true;
-        element.click();
-      }
-    });
+		new ConfirmationModal({
+			text: message,
+			confirmationClass: "alert",
+			cancelable: true,
+			confirmationCallback: () => {
+				element.dataset.confirmed = true;
+				element.click();
+			},
+		});
 
-    return false;
-  };
+		return false;
+	};
 }
