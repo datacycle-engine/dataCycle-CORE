8f5cb6f9fa2f37c0b435bd765711942aae17b006
Manuel Mitterer <mitterer@pixelpoint.at>
2024-01-12 13:16:53 +0100

    small refactor for classification_mapping_job

--- app/jobs/data_cycle_core/classification_mapping_job.rb
+++ app/jobs/data_cycle_core/classification_mapping_job.rb
@@ -18,31 +18,31 @@ module DataCycleCore
       'data_cycle_core_classification_alias_update_mappings'
     end
 
-    def perform(id, classification_ids)
+    def perform(id, to_insert = [], to_delete = [])
       ca = DataCycleCore::ClassificationAlias.find_by(id:)
 
       return if ca.nil?
 
-      to_insert = classification_ids - ca.classification_ids
-      to_delete = ca.classification_ids - classification_ids
+      insert_ids = Array.wrap(to_insert) - ca.classification_ids
+      delete_ids = Array.wrap(to_delete).intersection(ca.classification_ids)
 
-      if to_insert.present?
-        ca.classification_groups.insert_all(to_insert.map { |cid| { classification_id: cid } }, unique_by: :classification_groups_ca_id_c_id_uq_idx, returning: false)
+      if insert_ids.present?
+        ca.classification_groups.insert_all(insert_ids.map { |cid| { classification_id: cid } }, unique_by: :classification_groups_ca_id_c_id_uq_idx, returning: false)
 
-        DataCycleCore::Classification.where(id: to_insert).find_each do |c|
+        DataCycleCore::Classification.where(id: insert_ids).find_each do |c|
           ca.send(:classifications_added, c)
         end
       end
 
-      if to_delete.present?
-        ca.classification_groups.where(classification_id: to_delete).delete_all
+      if delete_ids.present?
+        ca.classification_groups.where(classification_id: delete_ids).delete_all
 
-        DataCycleCore::Classification.where(id: to_delete).find_each do |c|
+        DataCycleCore::Classification.where(id: delete_ids).find_each do |c|
           ca.send(:classifications_removed, c)
         end
       end
 
-      if to_insert.present? || to_delete.present? ? ca.update(updated_at: Time.zone.now) : true
+      if insert_ids.present? || delete_ids.present? ? ca.update(updated_at: Time.zone.now) : true
         ActionCable.server.broadcast('classification_update', { type: 'unlock', id: })
       else
         ActionCable.server.broadcast('classification_update', { type: 'error', id: })
