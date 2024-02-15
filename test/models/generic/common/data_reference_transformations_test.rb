c28da4e91247b0259fa0544335c4943685518f18
Manuel Mitterer <mitterer@pixelpoint.at>
2024-02-15 09:01:46 +0100

    add possibility to use non-strings as external_keys in data_reference transformation

--- test/models/generic/common/data_reference_transformations_test.rb
+++ test/models/generic/common/data_reference_transformations_test.rb
@@ -686,5 +686,47 @@ describe DataCycleCore::Generic::Common::DataReferenceTransformations do
         mapping_table[['CLASSIFICATION TREE TWO', 'I']]
       )
     end
+
+    it 'should resolve external content references with non-string keys' do
+      raw_data = {
+        'contents_1' => [
+          {
+            'contents_2' => [
+              {
+                'contents_3' => [
+                  { 'id' => 2 },
+                  { 'id' => 4 }
+                ]
+              }
+            ]
+          }
+        ],
+        'additional_content' => { 'id' => 'SOME ADDITIONAL EXTERNAL ID' }
+      }
+
+      transformed_data = subject.add_external_content_references(raw_data, 'content', 'EXTERNAL SOURCE ID',
+                                                                 ['contents_1', 'contents_2', 'contents_3', 'id'])
+      transformed_data = subject.add_external_content_references(transformed_data, 'additional_content', 'EXTERNAL SOURCE ID',
+                                                                 ['additional_content', 'id'])
+
+      load_things_stub = lambda do |_external_source_id, _external_keys|
+        {
+          '2' => '00000000-0000-0000-0000-000000000001',
+          '4' => '00000000-0000-0000-0000-000000000002',
+          'SOME ADDITIONAL EXTERNAL ID' => '00000000-0000-0000-0000-000000000003'
+        }
+      end
+
+      subject.stub :load_things, load_things_stub do
+        transformed_data = subject.resolve_references(transformed_data)
+
+        assert_equal(2, transformed_data['content'].size)
+        assert_includes(transformed_data['content'], '00000000-0000-0000-0000-000000000001')
+        assert_includes(transformed_data['content'], '00000000-0000-0000-0000-000000000002')
+
+        assert_equal(1, transformed_data['additional_content'].size)
+        assert_includes(transformed_data['additional_content'], '00000000-0000-0000-0000-000000000003')
+      end
+    end
   end
 end
