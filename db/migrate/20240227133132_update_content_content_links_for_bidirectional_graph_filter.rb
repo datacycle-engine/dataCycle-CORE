8e58ccb60dafe51ed818e37f933a47f247f4ce23
mstubinger <stubinger@pixelpoint.at>
2024-03-12 08:36:25 +0100

    (WIP) [#28520] update migration file for bi-directional graph filter -> content_content_links

--- db/migrate/20240227133132_update_content_content_links_for_bidirectional_graph_filter.rb
+++ db/migrate/20240227133132_update_content_content_links_for_bidirectional_graph_filter.rb
@@ -40,8 +40,12 @@ SELECT generate_content_content_links( array(select id from content_contents) );
 
   def down
     execute <<-SQL.squish
+
+    TRUNCATE content_content_links;
+
     DROP FUNCTION generate_content_content_links(uuid[]);
-      CREATE OR REPLACE FUNCTION generate_content_content_links(content_content_ids uuid[]) RETURNS void LANGUAGE plpgsql AS $$ BEGIN
+
+    CREATE OR REPLACE FUNCTION generate_content_content_links(content_content_ids uuid[]) RETURNS void LANGUAGE plpgsql AS $$ BEGIN
       INSERT INTO content_content_links (
           content_a_id,
           content_b_id,
@@ -70,7 +74,8 @@ SELECT generate_content_content_links( array(select id from content_contents) );
 
       $$;
 
-SELECT generate_content_content_links( array(select id from content_contents) );
+
+    SELECT generate_content_content_links( array(select id from content_contents) );
 
     SQL
   end
