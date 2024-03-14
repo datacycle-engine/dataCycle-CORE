130cd1a9fe2cc2249295b55cfc0b0d4970e53f0a
mstubinger <stubinger@pixelpoint.at>
2024-03-14 07:52:42 +0100

    (WIP) [#28520] core-funct. of bi-dir. graph filter, applied to related_to_query

--- db/migrate/20240227133132_update_content_content_links_for_bidirectional_graph_filter.rb
+++ db/migrate/20240227133132_update_content_content_links_for_bidirectional_graph_filter.rb
@@ -1,39 +1,40 @@
+# frozen_string_literal: true
+
 class UpdateContentContentLinksForBidirectionalGraphFilter < ActiveRecord::Migration[6.1]
-
   def up
-    execute <<-SQL.squish
-      DROP FUNCTION IF EXISTS generate_content_content_links;
+    execute <<~SQL.squish
+            DROP FUNCTION IF EXISTS generate_content_content_links;
 
-      CREATE OR REPLACE FUNCTION generate_content_content_links(content_content_ids uuid[]) RETURNS VOID LANGUAGE plpgsql AS $$ BEGIN
-      INSERT INTO content_content_links (
-          content_a_id,
-          content_b_id,
-          content_content_id,
-          relation
-        )
-      SELECT content_contents.content_a_id AS "content_a_id",
-        content_contents.content_b_id AS "content_b_id",
-        content_contents.id AS "content_content_id",
-        content_contents.relation_a AS "relation"
-      FROM content_contents
-      WHERE content_contents.id = ANY(content_content_ids)
-      UNION
-      SELECT content_contents.content_b_id AS "content_a_id",
-        content_contents.content_a_id AS "content_b_id",
-        content_contents.id AS "content_content_id",
-        content_contents.relation_b AS "relation"
-      FROM content_contents
-      WHERE content_contents.id = ANY(content_content_ids)
-        ON CONFLICT (content_content_id, content_a_id, content_b_id, relation) DO
-        NOTHING;
+            CREATE OR REPLACE FUNCTION generate_content_content_links(content_content_ids uuid[]) RETURNS VOID LANGUAGE plpgsql AS $$ BEGIN
+            INSERT INTO content_content_links (
+                content_a_id,
+                content_b_id,
+                content_content_id,
+                relation
+              )
+            SELECT content_contents.content_a_id AS "content_a_id",
+              content_contents.content_b_id AS "content_b_id",
+              content_contents.id AS "content_content_id",
+              content_contents.relation_a AS "relation"
+            FROM content_contents
+            WHERE content_contents.id = ANY(content_content_ids)
+            UNION
+            SELECT content_contents.content_b_id AS "content_a_id",
+              content_contents.content_a_id AS "content_b_id",
+              content_contents.id AS "content_content_id",
+              content_contents.relation_b AS "relation"
+            FROM content_contents
+            WHERE content_contents.id = ANY(content_content_ids)
+              ON CONFLICT (content_content_id, content_a_id, content_b_id, relation) DO
+              NOTHING;
 
-      RETURN;
+            RETURN;
 
-      END;
+            END;
 
-      $$;
+            $$;
 
-SELECT generate_content_content_links( array(select id from content_contents) );
+      SELECT generate_content_content_links( array(select id from content_contents) );
 
     SQL
   end
@@ -79,5 +80,4 @@ SELECT generate_content_content_links( array(select id from content_contents) );
 
     SQL
   end
-
 end
