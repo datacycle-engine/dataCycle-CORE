3d72dc6be4c388f3e63bf17c7178362e126ed7d2
Manuel Mitterer <mitterer@pixelpoint.at>
2024-04-10 10:17:25 +0200

    fix namespace for test

--- test/models/master_data/validators/collection_test.rb
+++ test/models/master_data/validators/collection_test.rb
@@ -3,54 +3,58 @@
 require 'test_helper'
 
 module DataCycleCore
-  class CollectionTest < ActiveSupport::TestCase
-    def subject
-      DataCycleCore::MasterData::Validators::Collection
-    end
+  module MasterData
+    module Validators
+      class CollectionTest < ActiveSupport::TestCase
+        def subject
+          DataCycleCore::MasterData::Validators::Collection
+        end
 
-    def setup
-      @user = DataCycleCore::User.first
-      @stored_filter = DataCycleCore::StoredFilter.create(name: 'test suche 1', user: @user, language: ['de'])
-      @watch_list = DataCycleCore::WatchList.create(full_path: 'test Inhaltssammlung 1', user: @user)
-    end
+        def setup
+          @user = DataCycleCore::User.first
+          @stored_filter = DataCycleCore::StoredFilter.create(name: 'test suche 1', user: @user, language: ['de'])
+          @watch_list = DataCycleCore::WatchList.create(full_path: 'test Inhaltssammlung 1', user: @user)
+        end
 
-    def validation_hash
-      {
-        'label' => 'Ersteller',
-        'type' => 'collection'
-      }
-    end
+        def validation_hash
+          {
+            'label' => 'Ersteller',
+            'type' => 'collection'
+          }
+        end
 
-    test 'successfully validates collection without validation' do
-      validator = subject.new([@stored_filter.id], validation_hash)
-      assert validator.error[:error].blank?
-      assert validator.error[:warning].blank?
-    end
+        test 'successfully validates collection without validation' do
+          validator = subject.new([@stored_filter.id], validation_hash)
+          assert validator.error[:error].blank?
+          assert validator.error[:warning].blank?
+        end
 
-    test 'successfully validates collection without api flag with soft_api validation' do
-      validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api' => true } }))
-      assert validator.error[:error].blank?
-      assert validator.error[:warning].present?
-    end
+        test 'successfully validates collection without api flag with soft_api validation' do
+          validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api' => true } }))
+          assert validator.error[:error].blank?
+          assert validator.error[:warning].present?
+        end
 
-    test 'successfully validates collection with api flag with soft_api validation' do
-      @stored_filter.update(api: true)
-      validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api' => true } }))
-      assert validator.error[:error].blank?
-      assert validator.error[:warning].blank?
-    end
+        test 'successfully validates collection with api flag with soft_api validation' do
+          @stored_filter.update(api: true)
+          validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api' => true } }))
+          assert validator.error[:error].blank?
+          assert validator.error[:warning].blank?
+        end
 
-    test 'successfully validates collection without api_users with soft_api_users validation' do
-      validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api_users' => true } }))
-      assert validator.error[:error].blank?
-      assert validator.error[:warning].blank?
-    end
+        test 'successfully validates collection without api_users with soft_api_users validation' do
+          validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api_users' => true } }))
+          assert validator.error[:error].blank?
+          assert validator.error[:warning].blank?
+        end
 
-    test 'successfully validates collection with api_users with soft_api_users validation' do
-      @stored_filter.update(api: true, api_users: [@user.id])
-      validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api_users' => true } }))
-      assert validator.error[:error].blank?
-      assert validator.error[:warning].present?
+        test 'successfully validates collection with api_users with soft_api_users validation' do
+          @stored_filter.update(api: true, api_users: [@user.id])
+          validator = subject.new([@stored_filter.id], validation_hash.merge({ 'validations' => { 'soft_api_users' => true } }))
+          assert validator.error[:error].blank?
+          assert validator.error[:warning].present?
+        end
+      end
     end
   end
 end
