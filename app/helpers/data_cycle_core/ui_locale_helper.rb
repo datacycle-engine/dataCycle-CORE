a97949b0a3d0de50c2e7bb3749ec5c72143bf638
Manuel Mitterer <mitterer@pixelpoint.at>
2024-04-10 10:49:56 +0200

    fix label for embedded properties

--- app/helpers/data_cycle_core/ui_locale_helper.rb
+++ app/helpers/data_cycle_core/ui_locale_helper.rb
@@ -48,11 +48,13 @@ module DataCycleCore
     end
 
     def attribute_viewer_label_tag(key:, definition:, content:, options: nil, accordion_controls: false, i18n_count: 1, **args, &block)
-      label_html = ActionView::OutputBuffer.new(tag.span(translated_attribute_label(key, definition, content, options, i18n_count), class: 'attribute-label-text', title: translated_attribute_label(key, definition, content, options, i18n_count)))
+      parent = contextual_content({ content: }.merge(args.slice(:parent)))
 
-      label_html.prepend(tag.i(class: 'fa fa-language translatable-attribute-icon')) if attribute_translatable?(key, definition, content)
+      label_html = ActionView::OutputBuffer.new(tag.span(translated_attribute_label(key, definition, parent, options, i18n_count), class: 'attribute-label-text', title: translated_attribute_label(key, definition, parent, options, i18n_count)))
+
+      label_html.prepend(tag.i(class: 'fa fa-language translatable-attribute-icon')) if attribute_translatable?(key, definition, parent)
       label_html.prepend(tag.i(class: "dc-type-icon property-icon key-#{key.attribute_name_from_key} type-#{definition&.dig('type')} #{"type-#{definition&.dig('type')}-#{definition.dig('ui', 'show', 'type')}" if definition&.dig('ui', 'show', 'type').present?}"))
-      label_html << render('data_cycle_core/contents/content_score', key:, content: contextual_content({ content: }.merge(args.slice(:parent))), definition:) if definition.key?('content_score')
+      label_html << render('data_cycle_core/contents/content_score', key:, content: parent, definition:) if definition.key?('content_score')
       label_html << render('data_cycle_core/contents/viewers/shared/accordion_toggle_buttons', button_type: 'children') if accordion_controls
       label_html << tag.span(tag.i(class: 'fa fa-clipboard'), class: 'copy-to-clipboard', data: { value: options[:copy_to_clipboard], dc_tooltip: t('actions.copy_to_clipboard', locale: active_ui_locale) }) if options&.dig(:copy_to_clipboard).present?
       label_html << capture(&block) if block
