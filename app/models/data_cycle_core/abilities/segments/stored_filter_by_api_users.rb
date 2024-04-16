c4968ba3990a0af1a6374242a4c281b5ea64f0a1
Manuel Mitterer <mitterer@pixelpoint.at>
2024-04-16 15:58:59 +0200

    reintroduce legacy segment for compatibility

---
+++ app/models/data_cycle_core/abilities/segments/stored_filter_by_api_users.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Abilities
+    module Segments
+      class StoredFilterByApiUsers < StoredFilterByApiAndSharedUsers
+      end
+    end
+  end
+end
