0b456ab837d2c14ae05461c1bad9a44f8c9a6028
mstubinger <stubinger@pixelpoint.at>
2024-04-26 14:47:37 +0200

    [#28520] Graph filter: use label names for relation selection dropdown instead of translations

--- app/models/data_cycle_core/thing_template.rb
+++ app/models/data_cycle_core/thing_template.rb
@@ -30,6 +30,10 @@ module DataCycleCore
     end
     alias properties property_names
 
+    def property_labels
+      property_names.index_with { |property_name| property_definitions.values_at(property_name).map { |pn| pn&.dig('label') }.compact }
+    end
+
     def template_thing
       DataCycleCore::Thing.new(thing_template: self)
     end
@@ -74,6 +78,27 @@ module DataCycleCore
 
     def schema_ancestors
       Array.wrap(schema&.[]('schema_ancestors')).deep_dup.then { |a| a.present? && !a.all?(::Array) ? [a] : a }
+    end
+
+    def self.all_property_labels(joined_by = nil)
+      # Fetch all ThingTemplates and convert them into a hash with template_name as key and property_labels as value
+      all_labels = DataCycleCore::ThingTemplate.all.to_h { |tt| [tt.template_name, tt.property_labels] }
+
+      # Use each_with_object to merge property labels into a single hash
+      props = all_labels.each_with_object({}) do |(_, v), merged_props|
+        # Merge the property labels for each key into the merged_props hash
+        v.each do |key, labels|
+          merged_props[key] ||= []
+          # Convert the labels array into a set to remove duplicates, then merge it with the existing labels
+          merged_props[key] |= labels
+        end
+      end
+
+      # If joined_by is nil, return the processed hash
+      return props if joined_by.nil?
+
+      # If joined_by is provided, join the labels for each key with the specified delimiter
+      props.transform_values { |labels| labels.join(joined_by) }
     end
 
     def self.template_things
