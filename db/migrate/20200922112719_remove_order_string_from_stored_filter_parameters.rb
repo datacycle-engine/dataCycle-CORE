26ecd91f703d22230a259d0529904748372fbba5
Patrick Rainer <rainer@pixelpoint.at>
2024-05-13 17:28:39 +0200

    disable obsolete migrations

--- db/migrate/20200922112719_remove_order_string_from_stored_filter_parameters.rb
+++ db/migrate/20200922112719_remove_order_string_from_stored_filter_parameters.rb
@@ -2,15 +2,15 @@
 
 class RemoveOrderStringFromStoredFilterParameters < ActiveRecord::Migration[5.2]
   def up
-    return unless ActiveRecord::Base.connection.table_exists? 'stored_filters'
+    # return unless ActiveRecord::Base.connection.table_exists? 'stored_filters'
 
-    DataCycleCore::StoredFilter.find_each do |filter|
-      order_hash = filter.parameters&.select { |f| f['t'] == 'order' }&.first || []
-      next if order_hash.blank?
-      parameters = filter.parameters.without(order_hash)
-      filter.parameters = parameters
-      filter.save(touch: false)
-    end
+    # DataCycleCore::StoredFilter.find_each do |filter|
+    #   order_hash = filter.parameters&.select { |f| f['t'] == 'order' }&.first || []
+    #   next if order_hash.blank?
+    #   parameters = filter.parameters.without(order_hash)
+    #   filter.parameters = parameters
+    #   filter.save(touch: false)
+    # end
   end
 
   def down
