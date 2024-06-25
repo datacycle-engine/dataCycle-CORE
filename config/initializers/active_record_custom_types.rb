6970857ca067faf9452c8f9ca7e4b2f2b4e35fae
Manuel Mitterer <mitterer@pixelpoint.at>
2024-06-25 13:59:49 +0200

    add more custom_types for multiranges

--- config/initializers/active_record_custom_types.rb
+++ config/initializers/active_record_custom_types.rb
@@ -4,8 +4,17 @@ raise 'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter#initialize_type_map i
 
 module PostgresCustomTypesExtension
   def initialize_type_map(m)
-    m.alias_type 'regconfig', 'varchar'
-    m.alias_type 'tstzmultirange', 'tstzrange'
+    # regconfig to tsvector
+    m.register_type('regconfig', ActiveRecord::Type::String.new)
+
+    # multirange types
+    m.register_type('int4multirange', ActiveRecord::Type::String.new)
+    m.register_type('int8multirange', ActiveRecord::Type::String.new)
+    m.register_type('nummultirange', ActiveRecord::Type::String.new)
+    m.register_type('datemultirange', ActiveRecord::Type::String.new)
+    m.register_type('tsmultirange', ActiveRecord::Type::String.new)
+    m.register_type('tstzmultirange', ActiveRecord::Type::String.new)
+
     super(m)
   end
 end
