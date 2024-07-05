118b711905f7f09faf086de1081d404b31e2c0f4
balljul <juliusball197@gmail.com>
2024-07-05 11:52:56 +0200

    ADDED new module

--- app/models/data_cycle_core/generic/common/download_concept_schemes_from_data.rb
+++ app/models/data_cycle_core/generic/common/download_concept_schemes_from_data.rb
@@ -3,45 +3,35 @@
 module DataCycleCore
   module Generic
     module Common
-      module DownloadConceptsFromData
+      module DownloadConceptSchemesFromData
         def self.download_content(utility_object:, options:)
-          DataCycleCore::Generic::Common::DownloadFunctions.download_concepts_from_data(
+          DataCycleCore::Generic::Common::DownloadFunctions.download_concept_schemes_from_data(
             download_object: utility_object,
-            iterator: method(:load_concepts_from_mongo).to_proc,
+            iterator: method(:load_concept_schemes_from_mongo).to_proc,
             data_id: method(:data_id).to_proc,
             data_name: method(:data_name).to_proc,
             options:
           )
         end
 
-        def self.load_concepts_from_mongo(options:, lang:)
+        def self.load_concept_schemes_from_mongo(options:, lang:)
           raise ArgumentError, 'missing read_type for loading location ranges' if options.dig(:download, :read_type).nil?
           read_type = Mongoid::PersistenceContext.new(DataCycleCore::Generic::Collection, collection: options[:download][:read_type])
-
-          concept_name = options.dig(:download, :concept_name_path)
-          concept_id = options.dig(:download, :concept_id_path) || concept_name
-
-          concept_path = options.dig(:download, :concept_path)
-          concept_id_path = [concept_path, concept_id].compact_blank.join('.')
-          concept_name_path = [concept_path, concept_name].compact_blank.join('.')
 
           DataCycleCore::Generic::Collection2.with(read_type) do |mongo|
             mongo.collection.aggregate([
               {
-                '$match' => { "dump.#{lang}" => { '$exists' => true } }
-              },
-              {
-                '$unwind' => "$dump.#{lang}.#{concept_path}"
+                '$match' => { "dump.#{lang}.tree_label" => { '$exists' => true } }
               }, {
                 '$project' => {
-                  'id' => "$dump.#{lang}.#{concept_id_path}",
-                  'name' => "$dump.#{lang}.#{concept_name_path}"
+                  'id.text' => "$dump.#{lang}.tree_label",
+                  'name.text' => "$dump.#{lang}.tree_label"
                 }
               }, {
                 '$group' => {
                   '_id' => '$id',
                   'id' => { '$first' => '$id' },
-                  'name' => { '$first' => '$name' }
+                  'name' => { '$first' => '$name' },
                 }
               }
             ]).to_a
