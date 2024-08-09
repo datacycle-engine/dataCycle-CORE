8b8d5b715d2e3a960c28a2b5efb0103304a0eed1
Manuel Mitterer <mitterer@pixelpoint.at>
2024-08-09 11:53:13 +0200

    [#38034] fix error in dowload_bulk_touch, refactor import_jobs

--- app/jobs/data_cycle_core/import_partial_job.rb
+++ app/jobs/data_cycle_core/import_partial_job.rb
@@ -1,41 +1,17 @@
 # frozen_string_literal: true
 
 module DataCycleCore
-  class ImportPartialJob < ApplicationJob
-    # there is a bug in ActiveJob in combination with DelayedJob that prevents
-    # @provider_job_id to be available in the perform actions and callbacks!!
-    # it is available in the enque-callbacks
-
-    queue_as :importers
-
-    def delayed_reference_id
-      arguments[0].to_s
-    end
-
+  class ImportPartialJob < ImportJob
     def delayed_reference_type
       "import_#{arguments[1]}"
     end
 
     def perform(uuid, import_name, mode = nil)
-      external_source = ExternalSystem.find(uuid)
-      options = {}
-      options[:mode] = mode if mode.present?
-      pid = Process.fork do
-        ExternalSystem.find(uuid).import_single(import_name, options)
-      rescue StandardError => e
-        ActiveSupport::Notifications.instrument "#{self.class.name.demodulize.underscore}_failed.datacycle", {
-          exception: e,
-          external_system: external_source
-        }
-        external_source.config['last_import_failed'] = true
-        external_source.config['last_import_exception'] = "#{e} (#{Time.zone.now})"
-        external_source.save!
+      super(uuid) do |external_system|
+        options = {}
+        options[:mode] = mode if mode.present?
+        external_system.import_single(import_name, options)
       end
-      Process.waitpid(pid)
-
-      external_source.reload
-      ActiveRecord::Base.establish_connection
-      raise external_source.config.dig('last_import_exception') if external_source.config.dig('last_import_failed')
     end
   end
 end
