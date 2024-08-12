53375bd672535d42cd9201fafdf0d4f39fed646a
Manuel Mitterer <mitterer@pixelpoint.at>
2024-08-12 09:40:51 +0200

    possibly fix vite error

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
-import RubyPlugin from 'vite-plugin-ruby';
-import { resolve } from 'path';
-import gzipPlugin from 'rollup-plugin-gzip';
+import RubyPlugin from "vite-plugin-ruby";
+import { resolve } from "path";
+import gzipPlugin from "rollup-plugin-gzip";
 
 export default ({ mode }) => {
-  return {
-    resolve: {
-      alias: {
-        '@core_assets': resolve(__dirname, 'app/assets')
-      }
-    },
-    build: {
-      chunkSizeWarningLimit: 5000,
-      brotliSize: false
-    },
-    plugins: [RubyPlugin(), ...(mode == 'development' ? [] : [gzipPlugin()])]
-  };
+	return {
+		resolve: {
+			alias: {
+				"@core_assets": resolve(__dirname, "app/assets"),
+			},
+		},
+		build: {
+			chunkSizeWarningLimit: 5000,
+			brotliSize: false,
+		},
+		plugins: [RubyPlugin(), ...(mode === "development" ? [] : [gzipPlugin()])],
+	};
 };
