99d35311d3ceebae25c431e8d47c5b15f5d2447a
Samuel Hafner <hafner@pixelpoint.at>
2024-09-20 09:02:41 +0200

    corretly set deleted_at filter in download data from data

--- app/models/data_cycle_core/generic/common/download_functions.rb
+++ app/models/data_cycle_core/generic/common/download_functions.rb
@@ -870,7 +870,7 @@ module DataCycleCore
                   source_filter = I18n.with_locale(locale) { source_filter.with_evaluated_values }
                   last_download = download_object.external_source.last_successful_download
                   source_filter[:updated_at] = { '$gte': last_download } if last_download.present? && ['full', 'reset'].exclude?(options[:mode])
-                  source_filter[:deleted_at] = { '$exists': false }
+                  source_filter.deep_merge!({ "dump.#{locale}.deleted_at" => { '$exists': false } })
                 end
 
                 begin
