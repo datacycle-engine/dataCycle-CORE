21d4083c8deea3f5252b79c89393999dd31dfa5b
Samuel Hafner <hafner@pixelpoint.at>
2024-09-24 08:31:57 +0200

    revert: DataCycleCore::Generic:.Collection inherits from Colelction

--- app/models/data_cycle_core/generic/collection2.rb
+++ app/models/data_cycle_core/generic/collection2.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
+# For unknown reasons, the Collection2 class CANNOT inherit from DataCycleCore::Generic::Collection. If you do this, it wont work anymore.
 module DataCycleCore
   module Generic
-    class Collection2 < Collection
+    class Collection2
+      include Mongoid::Document
+
+      attr_accessor :data_has_changed, :external_system_has_changed, :keep_seen_at
+
+      store_in collection: 'name'
+
+      field :external_id,      type: String
+      field :dump,             type: Hash
+      field :external_system,  type: Hash
+      field :seen_at,          type: DateTime
+      include Mongoid::Timestamps
+
+      before_save ->(document) { document.seen_at = Time.zone.now unless keep_seen_at }
+
+      def set_updated_at
+        super if data_has_changed
+      end
     end
   end
 end
