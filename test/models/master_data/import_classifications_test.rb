0edda61d1f0fe0f311b6484114981fa4aded938a
Johannes Zlattinger <zlattinger@pixelpoint.at>
2024-10-11 20:33:40 +0200

    [RUBOCOP] fix violations

--- test/models/master_data/import_classifications_test.rb
+++ test/models/master_data/import_classifications_test.rb
@@ -9,14 +9,14 @@ describe DataCycleCore::MasterData::ImportClassifications do
   include DataCycleCore::MinitestSpecHelper
 
   before do
-    imported_classifications = DataCycleCore::MasterData::ImportClassifications
+    DataCycleCore::MasterData::ImportClassifications
       .import_all(classification_paths: ['test/fixtures/classifications/examples/'])
   end
 
   def load_single_category(*names)
     categories = DataCycleCore::ClassificationAlias.from_tree(names.first)
     assert_not_nil(categories)
-    assert(categories.size > 0)
+    assert(categories.size.positive?)
 
     categories.find { |category| category.full_path == names.join(' > ') }
   end
@@ -27,30 +27,29 @@ describe DataCycleCore::MasterData::ImportClassifications do
     assert_nil(category.description)
     assert_nil(category.uri)
     assert(category.assignable)
-    assert(!category.internal)
+    assert_not(category.internal)
 
     category = load_single_category('Basic Categories', 'Basic Subcategory 1.2')
     assert_not_nil(category)
     assert_nil(category.description)
     assert_nil(category.uri)
     assert(category.assignable)
-    assert(!category.internal)
+    assert_not(category.internal)
 
     category = load_single_category('Basic Categories', 'Basic Subcategory 1.2', 'Basic Sub-subcategory 1.2.1')
     assert_not_nil(category)
     assert_nil(category.description)
     assert_nil(category.uri)
     assert(category.assignable)
-    assert(!category.internal)
+    assert_not(category.internal)
 
     category = load_single_category('Basic Categories', 'Basic Subcategory 1.2', 'Basic Sub-subcategory 1.2.2')
     assert_not_nil(category)
     assert_nil(category.description)
     assert_nil(category.uri)
     assert(category.assignable)
-    assert(!category.internal)
+    assert_not(category.internal)
   end
-
 
   it 'validates descriptive categories' do
     category = load_single_category('Descriptive Categories', 'Descriptive Subcategory 1.1')
@@ -58,28 +57,28 @@ describe DataCycleCore::MasterData::ImportClassifications do
     assert_equal 'Description for subcategory 1.1', category.description
     assert_nil category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Descriptive Categories', 'Descriptive Subcategory 1.2')
     assert_not_nil category
     assert_equal 'Description for subcategory 1.2', category.description
     assert_nil category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Descriptive Categories', 'Descriptive Subcategory 1.2', 'Descriptive Sub-subcategory 1.2.1')
     assert_not_nil category
     assert_equal 'Description for sub-subcategory 1.2.1', category.description
     assert_nil category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Descriptive Categories', 'Descriptive Subcategory 1.2', 'Descriptive Sub-subcategory 1.2.2')
     assert_not_nil category
     assert_equal 'Description for sub-subcategory 1.2.2', category.description
     assert_nil category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
   end
 
   it 'validates internal categories' do
@@ -118,30 +117,29 @@ describe DataCycleCore::MasterData::ImportClassifications do
     assert_nil category.description
     assert_equal 'http://example.com/subcategory1.1', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Categories with URI', 'URI Subcategory 1.2')
     assert_not_nil category
     assert_nil category.description
     assert_equal 'http://example.com/subcategory1.2', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Categories with URI', 'URI Subcategory 1.2', 'URI Sub-subcategory 1.2.1')
     assert_not_nil category
     assert_nil category.description
     assert_equal 'http://example.com/sub-subcategory1.2.1', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Categories with URI', 'URI Subcategory 1.2', 'URI Sub-subcategory 1.2.2')
     assert_not_nil category
     assert_nil category.description
     assert_equal 'http://example.com/sub-subcategory1.2.2', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
   end
-
 
   it 'validates mixed categories' do
     category = load_single_category('Mixed Categories', 'Internal Mixed Subcategory 1.1')
@@ -156,7 +154,7 @@ describe DataCycleCore::MasterData::ImportClassifications do
     assert_equal 'Description for subcategory 1.2', category.description
     assert_equal 'http://example.com/subcategory1.2', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Mixed Categories', 'Mixed Subcategory 1.2', 'Internal Mixed Sub-subcategory 1.2.1')
     assert_not_nil category
@@ -170,7 +168,7 @@ describe DataCycleCore::MasterData::ImportClassifications do
     assert_equal 'Description for sub-subcategory 1.2.2', category.description
     assert_equal 'http://example.com/sub-subcategory1.2.2', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Mixed Categories', 'Internal Mixed Subcategory 1.3')
     assert_not_nil category
@@ -184,7 +182,7 @@ describe DataCycleCore::MasterData::ImportClassifications do
     assert_equal 'Another sub-description', category.description
     assert_equal 'http://example.com/sub-subcategory1.3.1', category.uri
     assert category.assignable
-    assert !category.internal
+    assert_not category.internal
 
     category = load_single_category('Mixed Categories', 'Internal Mixed Subcategory 1.3', 'Internal Mixed Sub-subcategory 1.3.2')
     assert_not_nil category
