f42642ad49a834003f1fd65e0a6dd39763dcc421
Samuel Hafner <hafner@pixelpoint.at>
2024-10-14 13:25:08 +0200

    [#38905]  Refactor faraday_default_connection.rb to add retry functionality and follow redirects

--- config/initializers/faraday_default_connection.rb
+++ config/initializers/faraday_default_connection.rb
@@ -3,8 +3,11 @@
 Rails.application.reloader.to_prepare do
   Faraday.default_connection = Faraday.new do |faraday|
     faraday.use DataCycleCore::FaradayRaiseExcept404
+    faraday.use FaradayMiddleware::FollowRedirects
     faraday.options[:open_timeout] = 30
     faraday.options[:timeout] = 30
+    faraday.request :retry, max: 3, interval: 30, backoff_factor: 2, retry_statuses: [503, 504]
+
     faraday.adapter Faraday.default_adapter
   end
 end
