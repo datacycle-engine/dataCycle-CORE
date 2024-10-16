936f1d24475448f4d2c9cff0a50035c70bb7d770
Samuel Hafner <hafner@pixelpoint.at>
2024-10-16 09:48:30 +0200

    set dataCycle as User-Agent default header on faraday default connection

--- config/initializers/faraday_default_connection.rb
+++ config/initializers/faraday_default_connection.rb
@@ -7,7 +7,7 @@ Rails.application.reloader.to_prepare do
     faraday.options[:open_timeout] = 30
     faraday.options[:timeout] = 30
     faraday.request :retry, max: 3, interval: 30, backoff_factor: 2, retry_statuses: [503, 504]
-
+    faraday.headers['User-Agent'] = 'dataCycle'
     faraday.adapter Faraday.default_adapter
   end
 end
