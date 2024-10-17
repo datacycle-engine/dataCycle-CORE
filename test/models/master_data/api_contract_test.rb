caffc1eb055848e24a1e7d9f611344b16c152f3a
Manuel Mitterer <mitterer@pixelpoint.at>
2024-10-17 16:00:04 +0200

    fix api_contract_test

--- test/models/master_data/api_contract_test.rb
+++ test/models/master_data/api_contract_test.rb
@@ -10,26 +10,31 @@ module DataCycleCore
 
     test 'filter[classifications][in][withSubtree][] returns error for invalid params' do
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: [0] } } } })
+
       assert_equal 1, validation.errors.count
       assert_equal [:filter, :classifications, :in, :withSubtree, 0], validation.errors.to_a.first.path
-      assert_equal 'must be a string', validation.errors.to_a.first.to_s
+      assert_equal 'must be a valid UUIDv4 or a list of UUIDv4s separated by commas', validation.errors.to_a.first.to_s
 
       validation = @validator.call({ filter: { classifications: { in: { withoutSubtree: ['0'] } } } })
+
       assert_equal 1, validation.errors.count
       assert_equal [:filter, :classifications, :in, :withoutSubtree, 0], validation.errors.to_a.first.path
       assert_equal 'is in invalid format', validation.errors.to_a.first.to_s
 
       validation = @validator.call({ filter: { classifications: { in: { withoutSubtree: [' '] } } } })
+
       assert_equal 1, validation.errors.count
       assert_equal [:filter, :classifications, :in, :withoutSubtree, 0], validation.errors.to_a.first.path
       assert_equal 'is in invalid format', validation.errors.to_a.first.to_s
 
       validation = @validator.call({ filter: { classifications: { notIn: { withSubtree: ['b6d30f72-8a3d-47ee-b21b-860ed6fae232', '0'] } } } })
+
       assert_equal 1, validation.errors.count
       assert_equal [:filter, :classifications, :notIn, :withSubtree, 1], validation.errors.to_a.first.path
       assert_equal 'is in invalid format', validation.errors.to_a.first.to_s
 
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: ['234234 ,   b6d30f72-8a3d-47ee-b21b-860ed6fae232', '0'] } } } })
+
       assert_equal 2, validation.errors.count
       assert_equal [:filter, :classifications, :in, :withSubtree, 0], validation.errors.to_a.first.path
       assert_equal 'is in invalid format', validation.errors.to_a.first.to_s
@@ -39,21 +44,27 @@ module DataCycleCore
 
     test 'filter[classifications][in][withSubtree][] works with valid params' do
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: ['b6d30f72-8a3d-47ee-b21b-860ed6fae232'] } } } })
+
       assert_empty validation.errors
 
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: ['b6d30f72-8a3d-47ee-b21b-860ed6fae232', 'b6d30f72-8a3d-47ee-b21b-860ed6fae232', 'b6d30f72-8a3d-47ee-b21b-860ed6fae232'] } } } })
+
       assert_empty validation.errors
 
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: ['b6d30f72-8a3d-47ee-b21b-860ed6fae232,b6d30f72-8a3d-47ee-b21b-860ed6fae232,b6d30f72-8a3d-47ee-b21b-860ed6fae232', 'b6d30f72-8a3d-47ee-b21b-860ed6fae232,b6d30f72-8a3d-47ee-b21b-860ed6fae232,b6d30f72-8a3d-47ee-b21b-860ed6fae232', 'b6d30f72-8a3d-47ee-b21b-860ed6fae232,b6d30f72-8a3d-47ee-b21b-860ed6fae232'] } } } })
+
       assert_empty validation.errors
 
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: ['  b6d30f72-8a3d-47ee-b21b-860ed6fae232   ', '   b6d30f72-8a3d-47ee-b21b-860ed6fae232    ', 'b6d30f72-8a3d-47ee-b21b-860ed6fae232'] } } } })
+
       assert_empty validation.errors
 
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: ['  b6d30f72-8a3d-47ee-b21b-860ed6fae232  , b6d30f72-8a3d-47ee-b21b-860ed6fae232 ,    b6d30f72-8a3d-47ee-b21b-860ed6fae232,b6d30f72-8a3d-47ee-b21b-860ed6fae232', '   b6d30f72-8a3d-47ee-b21b-860ed6fae232 ,b6d30f72-8a3d-47ee-b21b-860ed6fae232   ', 'b6d30f72-8a3d-47ee-b21b-860ed6fae232'] } } } })
+
       assert_empty validation.errors
 
       validation = @validator.call({ filter: { classifications: { in: { withSubtree: [''] } } } })
+
       assert_empty validation.errors
     end
   end
