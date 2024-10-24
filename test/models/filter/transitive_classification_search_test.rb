021490ce3e7a0c3ec8794822f6d136112a363eef
Manuel Mitterer <mitterer@pixelpoint.at>
2024-10-24 15:36:23 +0200

    [#37981] fix generate_ccc_from_classification_ids, add test

--- test/models/filter/transitive_classification_search_test.rb
+++ test/models/filter/transitive_classification_search_test.rb
@@ -18,18 +18,21 @@ module DataCycleCore
       @tree3 = DataCycleCore::ClassificationTreeLabel.create!(name: 'Tree 3')
       @dummy_tree = DataCycleCore::ClassificationTreeLabel.create!(name: 'Tree Dummy')
       @tree2.create_classification_alias('parent 1', 'mapped 1')
+      @tree2.create_classification_alias('parent 1', 'mapped 1.1')
       @tree3.create_classification_alias('parent 2', 'mapped 2')
       @dummy_tree.create_classification_alias('parent dummy', 'mapped dummy')
 
       tag1 = DataCycleCore::ClassificationAlias.for_tree(@tags.name).find_by!(internal_name: 'Tag 1')
+      tag2 = DataCycleCore::ClassificationAlias.for_tree(@tags.name).find_by!(internal_name: 'Tag 2')
       mapped1 = DataCycleCore::ClassificationAlias.for_tree(@tree2.name).find_by!(internal_name: 'mapped 1')
+      @mapped12 = DataCycleCore::ClassificationAlias.for_tree(@tree2.name).find_by!(internal_name: 'mapped 1.1')
       mapped2 = DataCycleCore::ClassificationAlias.for_tree(@tree3.name).find_by!(internal_name: 'mapped 2')
       parent1 = DataCycleCore::ClassificationAlias.for_tree(@tree2.name).find_by!(internal_name: 'parent 1')
       dummy = DataCycleCore::ClassificationAlias.for_tree(@dummy_tree.name).find_by!(internal_name: 'mapped dummy')
 
-      mapped1.update!(classification_ids: [mapped1.primary_classification.id, tag1.primary_classification.id])
-      mapped2.update!(classification_ids: [mapped2.primary_classification.id, parent1.primary_classification.id])
       mapped1.update!(classification_ids: [mapped1.primary_classification.id, tag1.primary_classification.id, dummy.primary_classification.id])
+      @mapped12.update!(classification_ids: [@mapped12.primary_classification.id, tag2.primary_classification.id])
+      mapped2.update!(classification_ids: [mapped2.primary_classification.id, parent1.primary_classification.id])
     end
 
     after(:all) do
@@ -117,6 +120,16 @@ module DataCycleCore
     test 'filter contents based on mapped (2 hops) parent classifications by id' do
       items = DataCycleCore::Filter::Search.new(:de).classification_alias_ids_with_subtree(
         DataCycleCore::ClassificationAlias.for_tree(@tree3.name).with_internal_name('parent 2').pluck(:id)
+      )
+
+      assert_equal(1, items.query.size)
+    end
+
+    test 'filter contents based on mapped (1 hop) parent classifications by id after removing 1 mapping' do
+      @mapped12.update!(classification_ids: [@mapped12.primary_classification.id])
+
+      items = DataCycleCore::Filter::Search.new(:de).classification_alias_ids_with_subtree(
+        DataCycleCore::ClassificationAlias.for_tree(@tree2.name).with_internal_name('parent 1').pluck(:id)
       )
 
       assert_equal(1, items.query.size)
