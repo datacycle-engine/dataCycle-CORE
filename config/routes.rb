72bba27ec13a6575de5aca04326a11c8db242f78
Manuel Mitterer <mitterer@pixelpoint.at>
2024-10-31 10:12:32 +0100

    [RUBOCOP] Rails/MatchRoute

--- config/routes.rb
+++ config/routes.rb
@@ -406,14 +406,14 @@ DataCycleCore::Engine.routes.draw do
                   match '/concepts(/:external_key)', via: [:get, :post], to: 'classification_trees#by_external_key', as: 'classification_trees_by_external_key'
                   match '/things/select(/:external_keys)', to: 'contents#select_by_external_keys', as: 'things_select_by_external_key', via: [:get, :post]
                   match '/:external_key', via: [:get, :post], to: 'external_systems#show', as: 'external_sources'
-                  match '', via: :post, to: 'external_systems#create'
+                  post '', to: 'external_systems#create'
                   match '(/:external_key)', via: [:put, :patch], to: 'external_systems#update', as: 'external_sources_update'
-                  match '(/:external_key)', via: [:delete], to: 'external_systems#destroy', as: 'external_sources_delete'
+                  delete '(/:external_key)', to: 'external_systems#destroy', as: 'external_sources_delete'
                   match '/search/availability', via: [:get, :post], to: 'external_systems#search_availability', as: 'external_source_search_availability'
                   match '/search/additional_service', via: [:get, :post], to: 'external_systems#search_additional_service', as: 'external_source_search_additional_service'
                 end
 
-                match 'external_systems/:external_system_id/things/:id', to: 'external_systems_export#show', via: :get
+                get 'external_systems/:external_system_id/things/:id', to: 'external_systems_export#show'
               end
             end
           end
