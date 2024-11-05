f04d7fdf1e05b3eb701c8500c5f001b8fc73c0da
Manuel Mitterer <mitterer@pixelpoint.at>
2024-11-05 15:42:12 +0100

    [RUBOCOP] fix some issues

--- app/models/data_cycle_core/export/generic/transformations.rb
+++ app/models/data_cycle_core/export/generic/transformations.rb
@@ -5,7 +5,7 @@ module DataCycleCore
     module Generic
       module Transformations
         def self.json_partial(utility_object, data)
-          api_version = utility_object.external_system.credentials(:export).dig('api_version') || DataCycleCore.main_config.dig('api', 'default')
+          api_version = utility_object.external_system.credentials(:export)['api_version'] || DataCycleCore.main_config.dig('api', 'default')
 
           "DataCycleCore::Api::#{api_version.classify}::ContentsController".safe_constantize&.render(
             assigns: try("common_data_#{api_version}", utility_object, data),
@@ -24,7 +24,7 @@ module DataCycleCore
             include_parameters: utility_object.external_system.config.dig('export_config', name.demodulize.underscore, 'include_parameters') || utility_object.external_system.config.dig('export_config', 'include_parameters') || [],
             fields_parameters: utility_object.external_system.config.dig('export_config', name.demodulize.underscore, 'fields_parameters') || utility_object.external_system.config.dig('export_config', 'fields_parameters') || [],
             field_filter: (utility_object.external_system.config.dig('export_config', name.demodulize.underscore, 'fields_parameters') || utility_object.external_system.config.dig('export_config', 'fields_parameters')).present?,
-            token: (utility_object.external_system.credentials(:export).dig('token_type') || 'body') == 'body' ? utility_object.external_system.credentials(:export).dig('token') : nil,
+            token: (utility_object.external_system.credentials(:export)['token_type'] || 'body') == 'body' ? utility_object.external_system.credentials(:export)['token'] : nil,
             permitted_params: {}
           }
         end
