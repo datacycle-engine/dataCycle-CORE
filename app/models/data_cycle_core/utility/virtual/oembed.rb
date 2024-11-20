fe5f4bfea7959669c2d7dfae94ae9d9002aa7d60
Markus Stubinger <stubinger@pixelpoint.at>
2024-11-20 07:26:20 +0100

    [#37947] fix oembed virtual

--- app/models/data_cycle_core/utility/virtual/oembed.rb
+++ app/models/data_cycle_core/utility/virtual/oembed.rb
@@ -7,11 +7,16 @@ module DataCycleCore
         class << self
           def dc_url(thing)
             template = DataCycleCore::ThingTemplate.find_by(template_name: thing[:content].template_name)
-            return nil if template.blank? || template['schema'].dig('features', 'oembed', 'allowed') != true
+            return nil if thing.blank? || template.blank? || DataCycleCore.oembed_providers['oembed_providers'].flat_map { |provider| provider['output']&.flat_map { |output| output['template_names'] } }.compact.exclude?(template.template_name)
             thing_id = thing[:content][:id]
             host = Rails.application.config.action_mailer.default_url_options[:host]
             protocol = Rails.application.config.action_mailer.default_url_options[:protocol]
             "#{protocol}://#{host}/oembed?thing_id=#{thing_id}"
+          end
+
+          def fetch(thing)
+            return nil if thing.blank? || thing[:content]&.id.blank?
+            DataCycleCore::MasterData::Validators::Oembed.valid_oembed_from_thing_id(thing[:content].id)&.dig(:oembed)
           end
         end
       end
