936ab920ecc1744ed975a08bf63c69dfb216e43f
Manuel Mitterer <mitterer@pixelpoint.at>
2024-12-04 13:38:19 +0100

    add delete_concepts import module, fix source_filter for import_tags

---
+++ app/models/data_cycle_core/generic/common/delete_concepts.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+module DataCycleCore
+  module Generic
+    module Common
+      module DeleteConcepts
+        def self.import_data(utility_object:, options:)
+          DataCycleCore::Generic::Common::ImportFunctions.delete_concepts(
+            utility_object: utility_object.tap { |obj| obj.mode = :full },
+            iterator: method(:load_concepts).to_proc,
+            data_processor: method(:process_concepts).to_proc,
+            options:
+          )
+        end
+
+        def self.load_concepts(mongo_item, locale, source_filter)
+          source_filter ||= { "dump.#{locale}.deleted_at": { '$exists': true } }
+
+          mongo_item.where(source_filter)
+        end
+
+        def self.process_concepts(utility_object:, raw_data:, locale:, options:)
+          return if raw_data.blank?
+
+          external_key_path = options.dig(:import, :external_key_path).split('.')
+          external_keys = raw_data.filter_map { |data| data.dump[locale]&.dig(*external_key_path) }
+          external_keys.map! { |key| [options.dig(:import, :external_key_prefix), key].join } if options.dig(:import, :external_key_prefix)
+
+          to_destroy = DataCycleCore::ClassificationTree.includes(:sub_classification_alias).where(
+            sub_classification_alias: { external_source_id: utility_object.external_source.id, external_key: external_keys }
+          )
+
+          to_destroy.destroy_all
+        end
+      end
+    end
+  end
+end
--- app/models/data_cycle_core/generic/common/import_tags.rb
+++ app/models/data_cycle_core/generic/common/import_tags.rb
@@ -14,16 +14,16 @@ module DataCycleCore
               utility_object,
               options.dig(:import, :tree_label),
               method(:load_root_classifications).to_proc,
-              ->(_, _, _) { [] },
+              ->(_, _, _, _ = nil) { [] },
               ->(_, _, _) {},
               method(:extract_data).to_proc,
               options
             )
           end
 
-          def load_root_classifications(mongo_item, locale, options)
+          def load_root_classifications(mongo_item, locale, options, source_filter = nil)
             options = options.with_evaluated_values
-            source_filter = options.dig(:import, :source_filter) || {}
+            source_filter ||= options.dig(:import, :source_filter) || {}
 
             attribute_name = ['dump', locale, options.dig(:import, :tag_path) || options.dig(:import, :tag_id_path)].join('.')
             path_array = ['dump', locale.to_s, parse_common_tag_path(options)].flatten.join('.').split('.')
