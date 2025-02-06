dc94e89b38aa4900c44365569ded49069c938757
Manuel Mitterer <mitterer@pixelpoint.at>
2025-02-06 09:23:23 +0100

    run merge job in background

--- db/data_migrate/20250206070710_merge_duplicate_system_classifications.rb
+++ db/data_migrate/20250206070710_merge_duplicate_system_classifications.rb
@@ -5,7 +5,7 @@ class MergeDuplicateSystemClassifications < ActiveRecord::Migration[7.1]
   # disable_ddl_transaction!
 
   def up
-    DataCycleCore::RunTaskJob.perform_now('dc:concepts:merge_system_duplicates')
+    DataCycleCore::RunTaskJob.perform_later('dc:concepts:merge_system_duplicates')
   end
 
   def down
