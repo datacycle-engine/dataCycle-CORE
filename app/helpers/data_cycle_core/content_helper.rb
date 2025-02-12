11d62113f2cd9e58a5678a117b2ef4cc23a5ffbe
Manuel Mitterer <mitterer@pixelpoint.at>
2025-02-12 14:40:08 +0100

    add aspect_ratio for image tiles, adjust watch_list buttons

--- app/helpers/data_cycle_core/content_helper.rb
+++ app/helpers/data_cycle_core/content_helper.rb
@@ -8,5 +8,14 @@ module DataCycleCore
         (id.sub(/(.*)-(\w+)$/, '\2').hex ^ Digest::MD5.hexdigest(key)[0..11].hex).to_s(16).rjust(12, '0')
       ].join('-')
     end
+
+    def aspect_ratio(content)
+      width = content.try(:width)&.to_f
+      height = content.try(:height)&.to_f
+
+      return 16.to_r / 9 unless width&.positive? && height&.positive?
+
+      (width / height).to_r
+    end
   end
 end
