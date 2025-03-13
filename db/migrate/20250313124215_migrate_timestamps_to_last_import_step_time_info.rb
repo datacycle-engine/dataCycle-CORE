973bab9bc85fbb52a2874b211e748f70674eb653
Deniese Lafleur <lafleur@pixelpoint.at>
2025-03-13 15:40:11 +0100

    outsourced codeparts to method, added prefixes for keys

--- db/migrate/20250313124215_migrate_timestamps_to_last_import_step_time_info.rb
+++ db/migrate/20250313124215_migrate_timestamps_to_last_import_step_time_info.rb
@@ -1,33 +1,44 @@
 class MigrateTimestampsToLastImportStepTimeInfo < ActiveRecord::Migration[7.1]
   def up
     external_systems = DataCycleCore::ExternalSystem.all
-    external_systems.each do |cur_ext_sys|
-      cur_ext_sys.import_config.each_key do |key|
-        step_json = {
-          last_try: cur_ext_sys.last_import,
-          last_try_time: cur_ext_sys.last_import_time,
-          last_successful_try: cur_ext_sys.last_successful_import,
-          last_successful_try_time: cur_ext_sys.last_successful_import_time
-        }
-        cur_ext_sys.merge_last_import_step_time_info(key, step_json)
-      end
-
-      cur_ext_sys.download_config.each_key do |key|
-        step_json = {
-          last_try: cur_ext_sys.last_download,
-          last_try_time: cur_ext_sys.last_download_time,
-          last_successful_try: cur_ext_sys.last_successful_download,
-          last_successful_try_time: cur_ext_sys.last_successful_download_time
-        }
-
-        cur_ext_sys.merge_last_import_step_time_info(key, step_json)
-      end
-
-      cur_ext_sys.save
+    external_systems.each do |external_system|
+      migrate_config_steps(external_system, :import)
+      migrate_config_steps(external_system, :download)
+      external_system.save
     end
   end
 
   def down
     DataCycleCore::ExternalSystem.update_all(last_import_step_time_info: {})
+  end
+
+  def migrate_config_steps(external_system, action)
+    case action
+    when :import
+      config = external_system.import_config
+      key_prefix = 'i_'
+      last_try = external_system.last_import
+      last_try_time = external_system.last_import_time
+      last_successful_try = external_system.last_successful_import
+      last_successful_try_time = external_system.last_successful_import_time
+    when :download
+      config = external_system.download_config
+      key_prefix = 'd_'
+      last_try = external_system.last_download
+      last_try_time = external_system.last_download_time
+      last_successful_try = external_system.last_successful_download
+      last_successful_try_time = external_system.last_successful_download_time
+    end
+
+    return if config.blank?
+    config.each_key do |key|
+      step_json = {
+        last_try: last_try,
+        last_try_time: last_try_time,
+        last_successful_try: last_successful_try,
+        last_successful_try_time: last_successful_try_time
+      }
+      external_system.merge_last_import_step_time_info(key_prefix + key, step_json)
+    end
   end
 end
