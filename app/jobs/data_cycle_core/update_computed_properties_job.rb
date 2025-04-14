9232fca2f0a4afc31cf03c5c32abfec0b9beaa31
Manuel Mitterer <mitterer@pixelpoint.at>
2025-04-14 16:27:31 +0200

    fix keys for UpdateComputedPropertiesJob

--- app/jobs/data_cycle_core/update_computed_properties_job.rb
+++ app/jobs/data_cycle_core/update_computed_properties_job.rb
@@ -32,9 +32,10 @@ module DataCycleCore
         content.available_locales.each do |locale|
           translated_computed_keys = content.computed_property_names.intersection(content.translatable_property_names)
 
+          data_hash = {}
+          keys = locale == content.first_available_locale ? content.computed_property_names : translated_computed_keys
+
           I18n.with_locale(locale) do
-            data_hash = {}
-            keys = locale == content.first_available_locale ? content.computed_property_names : translated_computed_keys
             content.add_computed_values(data_hash:, keys:, force: true)
             content.webhook_priority = WEBHOOK_PRIORITY
             content.set_data_hash(data_hash:, update_computed: false)
