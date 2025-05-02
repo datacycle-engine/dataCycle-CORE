8518cf8a3d470023d91c2b95fd100f3d0d4286cc
Manuel Mitterer <mitterer@pixelpoint.at>
2025-05-02 10:28:40 +0200

    refactor by_name_value_source

--- app/models/data_cycle_core/utility/default_value/embedded.rb
+++ app/models/data_cycle_core/utility/default_value/embedded.rb
@@ -18,19 +18,22 @@ module DataCycleCore
             end
             template_name = Array.wrap(property_definition['template_name'])
             first_template = template_name.first
-            templates = DataCycleCore::ThingTemplate.where(
-              template_name: template_name
-            ).index_by(&:template_name)
+            templates = DataCycleCore::ThingTemplate
+              .where(template_name: template_name)
+              .index_by(&:template_name)
 
             values.map do |value|
               value['template_name'] ||= first_template
 
               template = templates[value['template_name']].template_thing
-              value.each do |k,v|
-                if template.classification_property_names.include?(k)
-                  tree_label = template.properties_for(k)&.dig('tree_label')
-                  value[k] = DataCycleCore::Concept.for_tree(tree_label).where(internal_name: v).pluck(:classification_id)
-                end
+              value.each do |k, v|
+                next unless template.classification_property_names.include?(k)
+
+                tree_label = template.properties_for(k)&.dig('tree_label')
+                value[k] = DataCycleCore::Concept
+                  .for_tree(tree_label)
+                  .where(internal_name: v)
+                  .pluck(:classification_id)
               end
               value
             end
