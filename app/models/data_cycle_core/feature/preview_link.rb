8d31203c4417e1ec8ff7fd495202ad6e67e2c806
Deniese Lafleur <lafleur@pixelpoint.at>
2025-06-24 09:06:31 +0200

    [#43029] refactored to use feature

--- app/models/data_cycle_core/feature/preview_link.rb
+++ app/models/data_cycle_core/feature/preview_link.rb
@@ -3,6 +3,18 @@
 module DataCycleCore
   module Feature
     class PreviewLink < Base
+      def self.allowed?(content = nil)
+        return false if content.blank?
+        enabled? && configuration(content)['allowed'] && configuration(content)['module'].present? && configuration(content)['method'].present?
+      end
+
+      def self.build(content, locale)
+        preview_link_builder = DataCycleCore.features.dig(:preview_link, :module)&.safe_constantize
+        builder_methode = DataCycleCore.features.dig(:preview_link, :method)
+        return nil unless preview_link_builder.present?
+
+        preview_link_builder.send(builder_methode, content, locale)
+      end
     end
   end
 end
