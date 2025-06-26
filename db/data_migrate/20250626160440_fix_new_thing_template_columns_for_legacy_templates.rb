d71458a15b5cdb0440928f7ff0cca327b48ed324
Manuel Mitterer <mitterer@pixelpoint.at>
2025-06-26 16:42:37 +0200

    [#43963] fix problem with legacy templates

---
+++ db/data_migrate/20250626160440_fix_new_thing_template_columns_for_legacy_templates.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class FixNewThingTemplateColumnsForLegacyTemplates < ActiveRecord::Migration[7.1]
+  # uncomment the following line to disable transactions
+  # disable_ddl_transaction!
+
+  def up
+    templates = DataCycleCore::ThingTemplate.where(content_type: nil).map do |template|
+      api_schema_types = Array.wrap(template.schema&.dig('schema_ancestors') || template.schema&.dig('schema_type'))
+        .map { |a| Array.wrap(a) }
+        .reduce(&:zip)
+        .flatten
+
+      api_schema_types << "dcls:#{template.template_name}"
+      api_schema_types.concat(Array.wrap(template.schema.dig('api', 'type'))) if template.schema.dig('api', 'type').present?
+      api_schema_types.compact!
+      api_schema_types.uniq!
+
+      {
+        template_name: template.template_name,
+        content_type: template.schema&.dig('content_type') || 'entity',
+        api_schema_types: api_schema_types,
+        boost: template.schema&.dig('boost')&.to_i || 1
+      }
+    end
+
+    DataCycleCore::ThingTemplate.upsert_all(templates, unique_by: :template_name)
+  end
+
+  def down
+  end
+end
